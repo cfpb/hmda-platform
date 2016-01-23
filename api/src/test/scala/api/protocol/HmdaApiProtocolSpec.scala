7a9271138ae05c9f614d069b38ab873a1a612c44
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-01-22 19:00:30 -0500

    Add Status JSON property test

---
+++ api/src/test/scala/api/protocol/HmdaApiProtocolSpec.scala
@@ -0,0 +1,19 @@
+package hmda.api.protocol
+
+import scala.concurrent.ExecutionContext
+import org.scalatest._
+import prop._
+import hmda.api.model.{ Status, ModelGenerators }
+import spray.json._
+
+class HmdaApiProtocolSpec extends PropSpec with PropertyChecks with MustMatchers with ModelGenerators with HmdaApiProtocol {
+
+  property("status must convert to and from json") {
+    forAll(statusGen) { (s) =>
+      //println(s)
+      s.toJson.convertTo[Status] mustBe (s)
+    }
+
+  }
+
+}
