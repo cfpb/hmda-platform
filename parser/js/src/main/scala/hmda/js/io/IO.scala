db3c38e8fc9f2c6d89069421355a3abb517c7a2a
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-02-03 15:12:49 -0500

    Add comments to JS IO

--- parser/js/src/main/scala/hmda/js/io/IO.scala
+++ parser/js/src/main/scala/hmda/js/io/IO.scala
@@ -8,10 +8,14 @@ trait IO {
   val fs = global.require("fs")
   val options = js.Dynamic.literal(encoding = "UTF-8")
 
+  // Wrapper around nodejs fs.readFileSync
+  // https://nodejs.org/api/fs.html#fs_fs_readfilesync_file_options
   def readFileSync(path: String): String = {
     fs.readFileSync(path, options).asInstanceOf[String]
   }
 
+  // Wrapper around nodejs fs.readFile
+  // https://nodejs.org/api/fs.html#fs_fs_readfile_file_options_callback
   def readFile(path: String): Future[String] = {
     val promise: Promise[String] = Promise()
     fs.readFile(path, options, { (err: js.Dynamic, data: String) =>
