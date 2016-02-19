3102f06d24492e979160f914e2a281345110ad45
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-02-19 18:18:18 -0500

    Refactor to better reflect cross platform implementations

--- validation/jvm/src/main/scala/hmda/validation/dsl/PlatformDsl.scala
+++ validation/jvm/src/main/scala/hmda/validation/dsl/PlatformDsl.scala
@@ -6,16 +6,20 @@ trait PlatformDsl {
   def validTimestampFormat[T]: Predicate[T] = new Predicate[T] {
     override def validate: (T) => Boolean = _.asInstanceOf[AnyRef] match {
       case s: String =>
-        try {
-          val format = new SimpleDateFormat("yyyyMMddHHmm")
-          format.setLenient(false)
-          format.parse(s)
-          true
-        } catch {
-          case e: Exception => false
-        }
+        checkDateFormat(s)
       case _ => false
     }
     override def failure: String = s"invalid timestamp format"
+  }
+
+  def checkDateFormat[T](s: String): Boolean = {
+    try {
+      val format = new SimpleDateFormat("yyyyMMddHHmm")
+      format.setLenient(false)
+      format.parse(s)
+      true
+    } catch {
+      case e: Exception => false
+    }
   }
 }
