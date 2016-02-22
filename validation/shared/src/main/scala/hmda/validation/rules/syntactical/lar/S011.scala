86247b453c731bdd5011e01774fa38cddf001c66
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-02-22 09:36:24 -0500

    Add S0111

---
+++ validation/shared/src/main/scala/hmda/validation/rules/syntactical/lar/S011.scala
@@ -0,0 +1,12 @@
+package hmda.validation.rules.syntactical.lar
+
+import hmda.model.fi.lar.LoanApplicationRegister
+import hmda.validation.dsl.{Failure, Success, CommonDsl, Result}
+
+
+class S011 extends CommonDsl {
+  def apply(lars: Iterable[LoanApplicationRegister]): Result = {
+    lars.size is greaterThan(0)
+  }
+
+}
---
+++ validation/shared/src/test/scala/hmda/validation/rules/syntactical/lar/S011Spec.scala
@@ -0,0 +1,11 @@
+package hmda.validation.rules.syntactical.lar
+
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.{MustMatchers, PropSpec}
+
+
+class S011Spec extends PropSpec with PropertyChecks with MustMatchers {
+
+  //TODO: implement once https://github.com/cfpb/hmda-platform/issues/71 is merged
+  property("LAR list must not be empty") (pending)
+}
