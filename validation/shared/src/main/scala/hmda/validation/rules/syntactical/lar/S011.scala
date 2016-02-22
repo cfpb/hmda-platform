8cc284551f8edf5912b93bd30fbcb2a92bd84fd0
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-02-22 10:12:25 -0500

    Refactor from class to object

--- validation/shared/src/main/scala/hmda/validation/rules/syntactical/lar/S011.scala
+++ validation/shared/src/main/scala/hmda/validation/rules/syntactical/lar/S011.scala
@@ -1,9 +1,9 @@
 package hmda.validation.rules.syntactical.lar
 
 import hmda.model.fi.lar.LoanApplicationRegister
-import hmda.validation.dsl.{ Failure, Success, CommonDsl, Result }
+import hmda.validation.dsl.{ CommonDsl, Result }
 
-class S011 extends CommonDsl {
+object S011 extends CommonDsl {
   def apply(lars: Iterable[LoanApplicationRegister]): Result = {
     lars.size is greaterThan(0)
   }
