d4f2347800e8cd86fb2e3ea55353e0e007614c9e
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-02-23 14:57:31 -0500

    Remove js.Dynamic.literal

--- parser/js/src/main/scala/hmda/js/io/FileIO.scala
+++ parser/js/src/main/scala/hmda/js/io/FileIO.scala
@@ -6,19 +6,18 @@ import scala.scalajs.js.Dynamic._
 
 trait FileIO {
   val fs = global.require("fs")
-  val options = js.Dynamic.literal(encoding = "UTF-8")
 
   // Wrapper around nodejs fs.readFileSync
   // https://nodejs.org/api/fs.html#fs_fs_readfilesync_file_options
   def readFileSync(path: String): String = {
-    fs.readFileSync(path, options).asInstanceOf[String]
+    fs.readFileSync(path, "UTF-8").asInstanceOf[String]
   }
 
   // Wrapper around nodejs fs.readFile
   // https://nodejs.org/api/fs.html#fs_fs_readfile_file_options_callback
   def readFile(path: String): Future[String] = {
     val promise: Promise[String] = Promise()
-    fs.readFile(path, options, { (err: js.Dynamic, data: String) =>
+    fs.readFile(path, "UTF-8", { (err: js.Dynamic, data: String) =>
       if (err != null)
         promise.failure(new RuntimeException("could not read file"))
       else
