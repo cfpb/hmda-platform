a779facfa68bdb87b3543751829728ea25905b84
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2016-02-29 12:24:46 -0500

    Implement toCSV method on LoanApplicationRegister
    https://github.com/cfpb/hmda-platform/issues/69

--- model/shared/src/main/scala/hmda/model/fi/lar/LoanApplicationRegister.scala
+++ model/shared/src/main/scala/hmda/model/fi/lar/LoanApplicationRegister.scala
@@ -20,8 +20,16 @@ case class LoanApplicationRegister(
     lienStatus: Int
 ) {
   def toCSV: String = {
-    //TODO: implement CSV output
-    ""
+    s"$id|$respondentId|$agencyCode|${loan.id}|${loan.applicationDate}" +
+      s"|${loan.loanType}|${loan.propertyType}|${loan.purpose}|${loan.occupancy}" +
+      s"|${loan.amount}|$preapprovals|$actionTaken|$actionTakenDate" +
+      s"|${geography.msa}|${geography.state}|${geography.county}|${geography.tract}" +
+      s"|${applicant.ethnicity}|${applicant.coEthnicity}|${applicant.race1}" +
+      s"|${applicant.race2}|${applicant.race3}|${applicant.race4}|${applicant.race5}" +
+      s"|${applicant.coRace1}|${applicant.coRace2}|${applicant.coRace3}" +
+      s"|${applicant.coRace4}|${applicant.coRace5}|${applicant.sex}|${applicant.coSex}" +
+      s"|${applicant.income}|$purchaserType|${denial.reason1}|${denial.reason2}" +
+      s"|${denial.reason3}|$rateSpread|$hoepaStatus|$lienStatus"
   }
 
   def toDAT: String = {
