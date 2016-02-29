76365be99e8ddc8f85892a73fd9d14d451fca71d
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2016-02-29 15:59:44 -0500

    Symmetrical test of LarCsvParser and lar.toCSV

--- parser/shared/src/test/scala/hmda/parser/fi/lar/LarCsvParserSpec.scala
+++ parser/shared/src/test/scala/hmda/parser/fi/lar/LarCsvParserSpec.scala
@@ -1,8 +1,14 @@
 package hmda.parser.fi.lar
 
+import hmda.model.fi.lar.LoanApplicationRegister
 import org.scalatest.{ PropSpec, MustMatchers }
 import org.scalatest.prop.PropertyChecks
 
-class LarCsvParserSpec extends PropSpec with PropertyChecks with MustMatchers {
-  property("Loan Application Register must be parsed from CSV")(pending)
+class LarCsvParserSpec extends PropSpec with PropertyChecks with MustMatchers with LarGenerators {
+
+  property("Loan Application Register must be parsed from CSV") {
+    forAll(larGen) { (lar: LoanApplicationRegister) =>
+      LarCsvParser(lar.toCSV) mustBe lar
+    }
+  }
 }
