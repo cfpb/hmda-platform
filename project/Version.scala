a14cf6a3c20babfcbf456d103fb3551692d2d3ec
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-03-01 14:46:46 -0500

    Upgrade to Akka 2.4.2

--- project/Version.scala
+++ project/Version.scala
@@ -3,8 +3,7 @@ object Version {
   val logback    = "1.1.3"
   val scalaTest  = "3.0.0-M15"
   val scalaCheck = "1.12.5" 
-  val akkaHttp   = "2.0.2"
-  val akka       = "2.4.1"
+  val akka       = "2.4.2"
   val scalaJSDom = "0.8.2"
   val scalaTags  = "0.5.4"
 }
