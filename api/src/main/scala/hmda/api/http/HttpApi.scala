06aa28e98acac52bfbfa12c5b40b528fdbaa9483
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-03-01 14:56:16 -0500

    Remove unnecessary Service trait

--- api/src/main/scala/hmda/api/http/HttpApi.scala
+++ api/src/main/scala/hmda/api/http/HttpApi.scala
@@ -2,9 +2,11 @@ package hmda.api.http
 
 import java.net.InetAddress
 import java.time.Instant
+import akka.actor.ActorSystem
 import akka.event.LoggingAdapter
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
 import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
 import hmda.api.model.Status
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import hmda.api.protocol.HmdaApiProtocol
@@ -12,6 +14,8 @@ import spray.json._
 
 trait HttpApi extends HmdaApiProtocol {
 
+  implicit val system: ActorSystem
+  implicit val materializer: ActorMaterializer
   val log: LoggingAdapter
 
   val routes = {
