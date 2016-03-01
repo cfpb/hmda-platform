ec2debc0c1f7b31569fdaf2c17c225894016e0fb
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-03-01 15:06:31 -0500

    Add upload endpoint

--- api/src/main/scala/hmda/api/http/HttpApi.scala
+++ api/src/main/scala/hmda/api/http/HttpApi.scala
@@ -2,21 +2,30 @@ package hmda.api.http
 
 import java.net.InetAddress
 import java.time.Instant
+import akka.Done
 import akka.actor.ActorSystem
 import akka.event.LoggingAdapter
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.Multipart
+import akka.http.scaladsl.model.Multipart.BodyPart
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{ Sink, Framing }
+import akka.util.ByteString
 import hmda.api.model.Status
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import hmda.api.protocol.HmdaApiProtocol
 import spray.json._
+
+import scala.concurrent.Future
 
 trait HttpApi extends HmdaApiProtocol {
 
   implicit val system: ActorSystem
   implicit val materializer: ActorMaterializer
   val log: LoggingAdapter
+
+  val splitLines = Framing.delimiter(ByteString("\n"), 2048, allowTruncation = true)
 
   val routes = {
     pathSingleSlash {
@@ -28,7 +37,28 @@ trait HttpApi extends HmdaApiProtocol {
           log.debug(status.toJson.toString)
           ToResponseMarshallable(status)
         }
-      }
+      } ~
+        path("upload") {
+          post {
+            entity(as[Multipart.FormData]) { formData =>
+              val done: Future[Done] = formData.parts.mapAsync(1) {
+                case b: BodyPart if b.filename.exists(_.endsWith(".dat")) =>
+                  b.entity.dataBytes
+                    .via(splitLines)
+                    .map(_.utf8String)
+                    .runForeach(line => println(line))
+                //printActor ! line
+                case _ => Future.successful(Done)
+              }.runWith(Sink.ignore)
+
+              onSuccess(done) { _ =>
+                complete {
+                  "ok!"
+                }
+              }
+            }
+          }
+        }
     }
   }
 }
