6ff99c2f0d42134bc884f89daa5e6f3c9320e015
L Dean <laura.dean@cfpb.gov>
2016-03-01 20:49:44 -0500

    added more possible values (particularly empty/NA strings) and cleaned up a bit

--- parser/shared/src/test/scala/hmda/parser/fi/FIGenerators.scala
+++ parser/shared/src/test/scala/hmda/parser/fi/FIGenerators.scala
@@ -1,5 +1,8 @@
 package hmda.parser.fi
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 import org.scalacheck.Gen
 
 trait FIGenerators {
@@ -12,9 +15,22 @@ trait FIGenerators {
     Gen.alphaStr
   }
 
-  // utility function(s)
+  // utility functions
 
   def stringOfN(n: Int, genOne: Gen[Char]): Gen[String] = {
     Gen.listOfN(n, genOne).map(_.mkString)
+  }
+
+  // this name may be too similar to Gen.option. (to be fair, it's not unrelated. just... domain-specific.)
+  def optional[T](g: Gen[T], emptyVal: String = ""): Gen[String] = {
+    Gen.oneOf(g.map(_.toString), Gen.const(emptyVal))
+  }
+
+  def dateGen: Gen[String] = {
+    for {
+      year <- Gen.choose(2017, 2020) // or could parameterize if needed
+      month <- Gen.choose(1, 12)
+      day <- Gen.choose(1, 28) // NOTE: this misses some dates! if that's a problem, we can do something more complex.
+    } yield LocalDate.of(year, month, day).format(DateTimeFormatter.ofPattern("yyyyMMdd"))
   }
 }
