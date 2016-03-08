cda58b31004c8f0920881e62d8377f230adce8b0
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-03-08 10:18:08 -0500

    Add JS Validation Engine test

--- validation/js/src/test/scala/hmda/validation/engine/ts/TsValidationSpec.scala
+++ validation/js/src/test/scala/hmda/validation/engine/ts/TsValidationSpec.scala
@@ -8,10 +8,9 @@ import org.scalatest.{ MustMatchers, PropSpec }
 import scala.concurrent.ExecutionContext
 
 //TODO: fails if JSExecutionContext.Implicits.queue is used (runNow is deprecated, but works)
-//TODO: fails in Travis CI with org.scalajs.jsenv.ComJSEnv$ComClosedException: JSCom has been closed
-//class TsValidationEngineSpec extends PropSpec with PropertyChecks with MustMatchers with TsGenerators with CommonTsValidationEngine with ScalaFutures with CommonTsValidationSpec {
-//
-//  override implicit val ec: ExecutionContext = scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
+class TsValidationEngineSpec extends PropSpec with PropertyChecks with MustMatchers with TsGenerators with TsValidationEngine with ScalaFutures with CommonTsValidationSpec {
 
-//}
+  override implicit val ec: ExecutionContext = scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
+
+}
 
