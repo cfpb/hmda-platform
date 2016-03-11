6158c3a33edbd3f1bbec61f633227c0c87374007
L Dean <lgdean@users.noreply.github.com>
2016-03-11 11:30:54 -0500

    add TS DAT parser test, with fake data in real/correct format

--- parser/shared/src/test/scala/hmda/parser/fi/ts/TsDatParserSpec.scala
+++ parser/shared/src/test/scala/hmda/parser/fi/ts/TsDatParserSpec.scala
@@ -1,10 +1,48 @@
 package hmda.parser.fi.ts
 
+import hmda.parser.util.FITestData
 import org.scalatest._
-import org.scalatest.prop.PropertyChecks
 
-class TsDatParserSpec extends PropSpec with PropertyChecks with MustMatchers with TsGenerators {
+class TsDatParserSpec extends FlatSpec with MustMatchers {
 
-  property("Transmittal Sheet must be parsed from CSV")(pending)
+  import FITestData._
 
+  val tsData = TsDatParser(tsDAT)
+
+  "TS Parser" should "parse transmittal sheet respondent info" in {
+    val respondent = tsData.respondent
+    respondent.id mustBe "0011013201"
+    respondent.name mustBe "SMALL BANK USA, NA"
+    respondent.address mustBe "1850 TYSONS BLVD., SUITE 50"
+    respondent.city mustBe "MCLEAN"
+    respondent.state mustBe "VA"
+    respondent.zipCode mustBe "22102"
+  }
+
+  it should "parse other info about the respondent" in {
+    tsData.taxId mustBe "20-1177984"
+  }
+
+  it should "parse transmittal info" in {
+    tsData.agencyCode mustBe 9
+    tsData.timestamp mustBe 201501171330L
+    tsData.activityYear mustBe 2013
+    tsData.totalLines mustBe 551
+  }
+
+  it should "parse parent info" in {
+    val parent = tsData.parent
+    parent.name mustBe "BIGS USA, INC."
+    parent.address mustBe "412 THIRD AVENUE"
+    parent.city mustBe "NEW YORK"
+    parent.zipCode mustBe "10012"
+  }
+
+  it should "parse contact info" in {
+    val contact = tsData.contact
+    contact.name mustBe "Bob Smith"
+    contact.phone mustBe "555-555-5555"
+    contact.fax mustBe "999-999-9999"
+    contact.email mustBe "bob.smith@bank.com"
+  }
 }
