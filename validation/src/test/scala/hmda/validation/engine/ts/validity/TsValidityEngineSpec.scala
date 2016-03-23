fbbc5ad16c9ef37fd8b331fba1fcf12169d90aae
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-03-23 16:05:27 -0400

    Add TsValidityEngine test

---
+++ validation/src/test/scala/hmda/validation/engine/ts/validity/TsValidityEngineSpec.scala
@@ -0,0 +1,50 @@
+package hmda.validation.engine.ts.validity
+
+import hmda.model.fi.ts.{ Contact, Respondent }
+import hmda.parser.fi.ts.TsGenerators
+import hmda.validation.rules.ts.validity.ValidityUtils
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{ MustMatchers, PropSpec }
+import org.scalatest.prop.PropertyChecks
+
+import scala.concurrent.ExecutionContext
+
+class TsValidityEngineSpec extends PropSpec with PropertyChecks with MustMatchers with TsGenerators with TsValidityEngine with ScalaFutures with ValidityUtils {
+  override implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+
+  property("Transmittal Sheet must be valid") {
+    forAll(tsGen) { ts =>
+      whenever(respondentNotEmpty(ts.respondent)) {
+        validate(ts).isSuccess mustBe true
+      }
+    }
+  }
+
+  property("Transmittal Sheet fails V105 (Respondent Mailing Address)") {
+    forAll(tsGen) { ts =>
+      whenever(ts.id == 1) {
+        val badTs = ts.copy(contact = Contact("", "", "", "sqwdqw"))
+        validate(badTs).isFailure mustBe true
+      }
+    }
+  }
+
+  property("Transmittal Sheet fails V140 (Respondent State Code)") {
+    forAll(tsGen) { ts =>
+      whenever(ts.id == 1) {
+        val badTs = ts.copy(respondent = Respondent("", "", "", "", "XXX", ""))
+        validate(badTs).isFailure mustBe true
+      }
+    }
+  }
+
+  property("Transmittal Sheet fails V155 (Respondent Email Address)") {
+    forAll(tsGen) { ts =>
+      whenever(ts.id == 1) {
+        val badTs = ts.copy(respondent = Respondent("", "", "", "", "CA", "awdqwd"))
+        validate(badTs).isFailure mustBe true
+      }
+    }
+  }
+
+}
