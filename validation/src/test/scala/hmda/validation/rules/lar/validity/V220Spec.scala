374e4b6efdd69277d88ee150eb63714ab6ca38da
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2016-03-29 18:00:05 -0400

    Test the failure case for V220
    LoanType must be 1,2,3, or 4

--- validation/src/test/scala/hmda/validation/rules/lar/validity/V220Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/lar/validity/V220Spec.scala
@@ -1,7 +1,9 @@
 package hmda.validation.rules.lar.validity
 
+import hmda.model.fi.lar.{ Loan, LoanApplicationRegister }
 import hmda.parser.fi.lar.LarGenerators
-import hmda.validation.dsl.Success
+import hmda.validation.dsl.{ Failure, Success }
+import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{ MustMatchers, PropSpec }
 
@@ -13,4 +15,18 @@ class V220Spec extends PropSpec with PropertyChecks with MustMatchers with LarGe
       }
     }
   }
+
+  val badLoanTypeGen: Gen[Int] = {
+    val belowRange = Gen.choose(Integer.MIN_VALUE, 0)
+    val aboveRange = Gen.choose(5, Integer.MAX_VALUE)
+    Gen.oneOf(belowRange, aboveRange)
+  }
+
+  property("Loan Type other than 1,2,3,4 is invalid") {
+    forAll(larGen, badLoanTypeGen) { (lar: LoanApplicationRegister, x: Int) =>
+      val invalidLoan: Loan = lar.loan.copy(loanType = x)
+      V220(invalidLoan) mustBe Failure("is not contained in valid values domain")
+    }
+  }
+
 }
