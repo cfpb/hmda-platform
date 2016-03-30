90c0cf04d7803bcbb67899b418f88d37b49720a3
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-03-30 15:54:40 -0400

    Update upload test

--- frontend/src/test/scala/hmda/api/http/HttpApiSpec.scala
+++ frontend/src/test/scala/hmda/api/http/HttpApiSpec.scala
@@ -1,10 +1,11 @@
 package hmda.api.http
 
-import akka.event.{ NoLogging, LoggingAdapter }
+import akka.event.{ LoggingAdapter, NoLogging }
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import hmda.api.model.Status
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.util.ByteString
 import org.scalatest.{ MustMatchers, WordSpec }
 
 import scala.concurrent.ExecutionContext
@@ -22,7 +23,7 @@ class HttpApiSpec extends WordSpec with MustMatchers with ScalatestRouteTest wit
       }
     }
 
-    "return OK when uploading a HMDA file" in {
+    "return proper response when uploading a HMDA file" in {
       val csv = "1|0123456789|9|201301171330|2013|99-9999999|900|MIKES SMALL BANK   XXXXXXXXXXX|1234 Main St       XXXXXXXXXXXXXXXXXXXXX|Sacramento         XXXXXX|CA|99999-9999|MIKES SMALL INC    XXXXXXXXXXX|1234 Kearney St    XXXXXXXXXXXXXXXXXXXXX|San Francisco      XXXXXX|CA|99999-1234|Mrs. Krabappel     XXXXXXXXXXX|916-999-9999|999-753-9999|krabappel@gmail.comXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n" +
         "2|0123456789|9|ABCDEFGHIJKLMNOPQRSTUVWXY|20130117|4|3|2|1|10000|1|5|20130119|06920|06|034|0100.01|4|5|7|4|3|2|1|8|7|6|5|4|1|2|9000|0|9|8|7|01.05|2|4\n" +
         "2|0123456789|9|ABCDEFGHIJKLMNOPQRSTUVWXY|20130117|4|3|2|1|10000|1|5|20130119|06920|06|034|0100.01|4|5|7|4|3|2|1|8|7|6|5|4|1|2|9000|0|9|8|7|01.05|2|4\n" +
@@ -32,6 +33,7 @@ class HttpApiSpec extends WordSpec with MustMatchers with ScalatestRouteTest wit
 
       Post("/upload/1", file) ~> routes ~> check {
         status mustEqual StatusCodes.OK
+        responseAs[String] mustEqual "uploaded"
       }
     }
 
