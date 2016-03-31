c64c5612eaa778b0c135aabddebc020414ae0564
L Dean <lgdean@users.noreply.github.com>
2016-03-31 02:19:36 -0400

    start adding a concept of when/then to the DSL

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V375.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V375.scala
@@ -1,17 +1,22 @@
 package hmda.validation.rules.lar.validity
 
 import hmda.model.fi.lar.LoanApplicationRegister
-import hmda.validation.dsl.{ CommonDsl, Result, Success }
+import hmda.validation.dsl.{ Failure, CommonDsl, Result, Success }
 
 object V375 extends CommonDsl {
 
   val okLoanTypes = List(2, 3, 4)
 
   def apply(lar: LoanApplicationRegister): Result = {
-    if (lar.purchaserType == 2) {
-      lar.loan.loanType is containedIn(okLoanTypes)
-    } else {
-      Success()
+    when(lar.purchaserType is equalTo(2), lar.loan.loanType is containedIn(okLoanTypes))
+  }
+
+  // if we like this then let's move it up to CommonDsl. one question: are commas better, or a curried something? yum.
+  // another question/idea: would we want to introduce a "then" concept to make it read better?
+  def when(condition: Result, thenTest: Result): Result = {
+    condition match {
+      case Success() => thenTest
+      case Failure(_) => Success()
     }
   }
 }
