fb2144d2f80e9759d8be614d5bc09316559b2bf5
L Dean <lgdean@users.noreply.github.com>
2016-04-04 14:58:02 -0400

    refactor: use when, now that it's been merged

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V347.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V347.scala
@@ -1,18 +1,15 @@
 package hmda.validation.rules.lar.validity
 
 import hmda.model.fi.lar.LoanApplicationRegister
-import hmda.validation.dsl.{ Result, Success }
+import hmda.validation.dsl.Result
 import hmda.validation.rules.EditCheck
 
 object V347 extends EditCheck[LoanApplicationRegister] {
   override def name: String = "V347"
 
   def apply(lar: LoanApplicationRegister): Result = {
-    // TODO replace with when, after relevant merge happens
-    if ((1 to 9).contains(lar.purchaserType)) {
+    when(lar.purchaserType is containedIn(1 to 9)) {
       lar.actionTakenType is containedIn(List(1, 6))
-    } else {
-      Success()
     }
   }
 }
