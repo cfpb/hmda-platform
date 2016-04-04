6eefd04ecb444aed20af37726761c5088b0e8cfe
L Dean <lgdean@users.noreply.github.com>
2016-04-04 16:38:10 -0400

    refactor: pull doCheck up into ValidationApi

--- validation/src/main/scala/hmda/validation/engine/lar/validity/LarValidityEngine.scala
+++ validation/src/main/scala/hmda/validation/engine/lar/validity/LarValidityEngine.scala
@@ -3,14 +3,9 @@ package hmda.validation.engine.lar.validity
 import hmda.model.fi.lar.LoanApplicationRegister
 import hmda.validation.api.ValidationApi
 import hmda.validation.engine.lar.LarCommonEngine
-import hmda.validation.rules.EditCheck
 import hmda.validation.rules.lar.validity._
 
 trait LarValidityEngine extends LarCommonEngine with ValidationApi {
-
-  private def doCheck(check: EditCheck[LoanApplicationRegister], lar: LoanApplicationRegister): LarValidation = {
-    convertResult(lar, check(lar), check.name)
-  }
 
   def validate(lar: LoanApplicationRegister): LarValidation = {
     val checks = List(
--- validation/src/main/scala/hmda/validation/engine/ts/validity/TsValidityEngine.scala
+++ validation/src/main/scala/hmda/validation/engine/ts/validity/TsValidityEngine.scala
@@ -2,18 +2,10 @@ package hmda.validation.engine.ts.validity
 
 import hmda.model.fi.ts.TransmittalSheet
 import hmda.validation.api.ValidationApi
-import hmda.validation.engine.ValidationError
 import hmda.validation.engine.ts.TsCommonEngine
-import hmda.validation.rules.EditCheck
 import hmda.validation.rules.ts.validity.{ V105, V140, V155 }
 
-import scalaz._
-
 trait TsValidityEngine extends TsCommonEngine with ValidationApi {
-
-  private def doCheck[T](check: EditCheck[T], input: T): ValidationNel[ValidationError, T] = {
-    convertResult(input, check(input), check.name)
-  }
 
   def validate(ts: TransmittalSheet): TsValidation = {
     val checks: List[TsValidation] = List(
