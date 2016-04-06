298f84f0c0c87d7d6688b86c25205e60da53574c
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-04-06 14:13:18 -0400

    Add LarValidation Actor and test

---
+++ api/src/main/scala/hmda/api/processing/lar/LarValidation.scala
@@ -0,0 +1,56 @@
+package hmda.api.processing.lar
+
+import akka.actor.{ Actor, ActorLogging, Props }
+import hmda.model.fi.lar.LoanApplicationRegister
+import hmda.validation.engine.ValidationError
+import hmda.validation.engine.lar.LarEngine
+
+object LarValidation {
+  def props: Props = Props(new LarValidation)
+
+  case class CheckLar(lar: LoanApplicationRegister)
+  case class CheckSyntacticalLar(lar: LoanApplicationRegister)
+  case class CheckValidityLar(lar: LoanApplicationRegister)
+
+  trait LarValidationError
+  case object LarSyntacticalError extends LarValidationError
+  case object LarValidityError extends LarValidationError
+
+  trait ValidationType
+  case object LarSyntactical extends ValidationType
+  case object LarValidity extends ValidationType
+  case object LarFull extends ValidationType
+
+}
+
+class LarValidation extends Actor with ActorLogging with LarEngine {
+  import LarValidation._
+
+  override def receive: Receive = {
+    case CheckSyntacticalLar(lar) =>
+      sender() ! validationErrors(lar, LarSyntactical)
+    case CheckValidityLar(lar) =>
+      sender() ! validationErrors(lar, LarValidity)
+    case CheckLar(lar) =>
+      sender() ! validationErrors(lar, LarFull)
+
+    case _ =>
+      log.error(s"Unsupported message sent to ${self.path}")
+  }
+
+  private def validationErrors(lar: LoanApplicationRegister, validationType: ValidationType): List[ValidationError] = {
+    val validation = validationType match {
+      case LarFull => validateLar(lar).disjunction
+      case LarSyntactical => checkSyntactical(lar).disjunction
+      case LarValidity => checkValidity(lar).disjunction
+    }
+    if (validation.isRight) {
+      Nil
+    } else {
+      val lErrors = validation.toEither.left.get
+      val errors = lErrors.head :: lErrors.tail.toList
+      errors
+    }
+  }
+
+}
---
+++ api/src/test/scala/hmda/api/processing/lar/LarValidationSpec.scala
@@ -0,0 +1,40 @@
+package hmda.api.processing.lar
+
+import java.io.File
+
+import akka.actor.ActorSystem
+import akka.testkit.{ ImplicitSender, TestKit }
+import hmda.api.processing.lar.LarValidation.CheckLar
+import hmda.parser.fi.lar.LarCsvParser
+import org.scalatest.{ BeforeAndAfterAll, MustMatchers, WordSpecLike }
+
+import scala.io.Source
+
+class LarValidationSpec(_system: ActorSystem)
+    extends TestKit(_system)
+    with WordSpecLike
+    with ImplicitSender
+    with MustMatchers
+    with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("hmda-lar-validation-test"))
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  val larValidation = system.actorOf(LarValidation.props, "larValidation")
+
+  val lines = Source.fromFile(new File("parser/src/test/resources/txt/THE_LYONS_NATIONAL_BANK.txt")).getLines()
+  val lars = lines.drop(1).map(l => LarCsvParser(l))
+
+  "A valid HMDA File" must {
+    "validate all lars in sample files" in {
+      lars.foreach { lar =>
+        larValidation ! CheckLar(lar)
+        expectMsg(Nil)
+      }
+    }
+  }
+
+}
