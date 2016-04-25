b63feed1ec62ee7afbb7f834d271613f76ff9659
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-04-25 16:02:47 -0400

    Update test

--- validation/src/test/scala/hmda/validation/rules/lar/validity/V295Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/lar/validity/V295Spec.scala
@@ -1,6 +1,6 @@
 package hmda.validation.rules.lar.validity
 
-import hmda.validation.dsl.Success
+import hmda.validation.dsl.{ Failure, Success }
 import hmda.validation.rules.lar.LarEditCheckSpec
 
 class V295Spec extends LarEditCheckSpec {
@@ -14,4 +14,24 @@ class V295Spec extends LarEditCheckSpec {
       }
     }
   }
+
+  property("Succeeds when MSA/MD is NA and county is also NA") {
+    forAll(larGen) { lar =>
+      val validGeography = lar.geography.copy(msa = "NA", county = "NA")
+      val validLar = lar.copy(geography = validGeography)
+      V295(validLar) mustBe a[Success]
+    }
+  }
+
+  //TODO: this fails, but should it? if there is a valid combination of state/county, do we care if msa == NA?
+  //property("Fails when MSA/MD is NA but county has a value") {
+  //  forAll(larGen) { lar =>
+  //    whenever(lar.geography.county != "NA") {
+  //      val invalidGeography = lar.geography.copy(msa = "NA", county = "001", state = "06")
+  //      val invalidLar = lar.copy(geography = invalidGeography)
+  //      println(invalidLar)
+  //      V295(invalidLar) mustBe a[Failure]
+  //    }
+  //  }
+  //}
 }
