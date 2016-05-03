1abcc2355a7d6609afb49477e2928c15c2198299
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-05-03 11:16:25 -0400

    Update V285 to typeclass pattern

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V285.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V285.scala
@@ -7,6 +7,9 @@ import hmda.validation.rules.EditCheck
 
 object V285 extends EditCheck[LoanApplicationRegister] {
 
+  import hmda.validation.dsl.PredicateDefaults._
+  import hmda.validation.dsl.PredicateSyntax._
+
   val cbsaTracts = CBSATractLookup.values
 
   val stateCodes = cbsaTracts.map(c => c.state).distinct
