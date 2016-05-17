50d0213363ef7ab080f17675133b3d1045f484e0
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-05-17 11:27:13 -0400

    bring inlien with test failure refactor

--- validation/src/test/scala/hmda/validation/rules/lar/validity/V525Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/lar/validity/V525Spec.scala
@@ -19,7 +19,7 @@ class V525Spec extends LarEditCheckSpec with BadValueUtils {
   property("HOEPA status other than 1, or 2 is invalid") {
     forAll(larGen, badHoepaStatusGen) { (lar: LoanApplicationRegister, x: Int) =>
       val invalidLar: LoanApplicationRegister = lar.copy(hoepaStatus = x)
-      V525(invalidLar) mustBe Failure("is not contained in valid values domain")
+      V525(invalidLar) mustBe a[Failure]
     }
   }
 
