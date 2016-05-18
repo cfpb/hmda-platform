a47e83310b2cab23be5bbafef6c4f281e8741ae7
Nick Grippin <ngrippin@discovertechnologies.com>
2016-05-18 16:36:38 -0400

    all the testing

--- validation/src/test/scala/hmda/validation/rules/lar/validity/V338Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/lar/validity/V338Spec.scala
@@ -6,14 +6,6 @@ import hmda.validation.rules.lar.LarEditCheckSpec
 import org.scalacheck.Gen
 
 class V338Spec extends LarEditCheckSpec {
-  property("All applicants not meeting the criteria must pass") {
-    forAll(larGen) { lar =>
-      whenever(larNotApplicable(lar)) {
-        lar.mustPass
-      }
-    }
-  }
-
   val okAction = List(1, 2, 3, 4, 5, 7, 8)
   val actionTakenGen: Gen[Int] = Gen.oneOf(okAction)
 
@@ -29,23 +21,88 @@ class V338Spec extends LarEditCheckSpec {
   val incomeGen: Gen[Int] = Gen.choose(Int.MinValue, Int.MaxValue)
 
   property("Applicant meeting the criteria with an incorrect income must fail") {
-    forAll(larGen, actionTakenGen, incomeGen) { (lar: LoanApplicationRegister, x: Int, income: Int) =>
+    forAll(larGen, actionTakenGen, incomeGen) { (lar: LoanApplicationRegister, x: Int, y: Int) =>
       val applicant = lar.applicant.copy(ethnicity = 4, race1 = 7, sex = 4, coEthnicity = 5,
-        coRace1 = 8, coSex = 5, income = income.toString)
+        coRace1 = 8, coSex = 5, income = y.toString)
       val invalidLar = lar.copy(applicant = applicant, actionTakenType = x)
       invalidLar.mustFail
     }
   }
 
-  override def check: EditCheck[LoanApplicationRegister] = V338
+  val invalidEthnicityGen: Gen[Int] = Gen.choose(1, 3)
 
-  private def larNotApplicable(lar: LoanApplicationRegister): Boolean = {
-    (lar.applicant.ethnicity != 4) ||
-      (lar.applicant.race1 != 7) ||
-      (lar.applicant.sex != 4) ||
-      (lar.applicant.coEthnicity != 5) ||
-      (lar.applicant.coRace1 != 8) ||
-      (lar.applicant.coSex != 5) ||
-      !(okAction contains lar.actionTakenType)
+  property("Applicant not meeting the ethnicity criteria must pass") {
+    forAll(larGen, actionTakenGen, invalidEthnicityGen) { (lar: LoanApplicationRegister, y: Int, x: Int) =>
+      val applicant = lar.applicant.copy(ethnicity = x, race1 = 7, sex = 4, coEthnicity = 5,
+        coRace1 = 8, coSex = 5, income = "whatever")
+      val newLar = lar.copy(applicant = applicant, actionTakenType = y)
+      newLar.mustPass
+    }
   }
+
+  val invalidRaceGen: Gen[Int] = Gen.choose(1, 6)
+
+  property("Applicant not meeting the race criteria must pass") {
+    forAll(larGen, actionTakenGen, invalidRaceGen) { (lar: LoanApplicationRegister, y: Int, x: Int) =>
+      val applicant = lar.applicant.copy(ethnicity = 4, race1 = x, sex = 4, coEthnicity = 5,
+        coRace1 = 8, coSex = 5, income = "whatever")
+      val newLar = lar.copy(applicant = applicant, actionTakenType = y)
+      newLar.mustPass
+    }
+  }
+
+  val invalidSexGen: Gen[Int] = Gen.choose(1, 3)
+
+  property("Applicant not meeting the sex criteria must pass") {
+    forAll(larGen, actionTakenGen, invalidSexGen) { (lar: LoanApplicationRegister, y: Int, x: Int) =>
+      val applicant = lar.applicant.copy(ethnicity = 4, race1 = 7, sex = x, coEthnicity = 5,
+        coRace1 = 8, coSex = 5, income = "whatever")
+      val newLar = lar.copy(applicant = applicant, actionTakenType = y)
+      newLar.mustPass
+    }
+  }
+
+  val invalidCoEthnicityGen: Gen[Int] = Gen.choose(1, 4)
+
+  property("Applicant not meeting the co-ethnicity criteria must pass") {
+    forAll(larGen, actionTakenGen, invalidCoEthnicityGen) { (lar: LoanApplicationRegister, y: Int, x: Int) =>
+      val applicant = lar.applicant.copy(ethnicity = 4, race1 = 7, sex = 4, coEthnicity = x,
+        coRace1 = 8, coSex = 5, income = "whatever")
+      val newLar = lar.copy(applicant = applicant, actionTakenType = y)
+      newLar.mustPass
+    }
+  }
+
+  val invalidCoRaceGen: Gen[Int] = Gen.choose(1, 7)
+
+  property("Applicant not meeting the co-race criteria must pass") {
+    forAll(larGen, actionTakenGen, invalidCoRaceGen) { (lar: LoanApplicationRegister, y: Int, x: Int) =>
+      val applicant = lar.applicant.copy(ethnicity = 4, race1 = 7, sex = 4, coEthnicity = 5,
+        coRace1 = x, coSex = 5, income = "whatever")
+      val newLar = lar.copy(applicant = applicant, actionTakenType = y)
+      newLar.mustPass
+    }
+  }
+
+  val invalidCoSexGen: Gen[Int] = Gen.choose(1, 4)
+
+  property("Applicant not meeting the co-sex criteria must pass") {
+    forAll(larGen, actionTakenGen, invalidCoSexGen) { (lar: LoanApplicationRegister, y: Int, x: Int) =>
+      val applicant = lar.applicant.copy(ethnicity = 4, race1 = 7, sex = 4, coEthnicity = 5,
+        coRace1 = 8, coSex = x, income = "whatever")
+      val newLar = lar.copy(applicant = applicant, actionTakenType = y)
+      newLar.mustPass
+    }
+  }
+
+  property("LAR not meeting the actionTakenType criteria must pass") {
+    forAll(larGen) { (lar: LoanApplicationRegister) =>
+      val applicant = lar.applicant.copy(ethnicity = 4, race1 = 7, sex = 4, coEthnicity = 5,
+        coRace1 = 8, coSex = 5, income = "whatever")
+      val newLar = lar.copy(applicant = applicant, actionTakenType = 6)
+      newLar.mustPass
+    }
+  }
+
+  override def check: EditCheck[LoanApplicationRegister] = V338
 }
