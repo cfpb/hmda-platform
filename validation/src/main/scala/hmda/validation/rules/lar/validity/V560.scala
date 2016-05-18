2194ffad02d15e85b30b1a4fe40c95e06bfc1cb9
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-05-18 14:31:39 -0400

    added override

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V560.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V560.scala
@@ -9,11 +9,11 @@ object V560 extends EditCheck[LoanApplicationRegister] {
   val actionTakenTypes = List(1, 2, 3, 4, 5, 7, 8)
   val lienStatusType = List(1, 2, 3)
 
-  def apply(lar: LoanApplicationRegister): Result = {
+  override def apply(lar: LoanApplicationRegister): Result = {
     when(lar.actionTakenType is containedIn(actionTakenTypes)) {
       lar.lienStatus is containedIn(lienStatusType)
     }
   }
 
-  def name = "V560"
+  override def name = "V560"
 }
