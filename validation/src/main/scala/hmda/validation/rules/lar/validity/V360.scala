a341eb3582e75145216a78905335933b280c99a3
Nick Grippin <ngrippin@discovertechnologies.com>
2016-05-19 16:37:01 -0400

    switched to list approach

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V360.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V360.scala
@@ -8,13 +8,9 @@ import hmda.validation.dsl.PredicateSyntax._
 
 object V360 extends EditCheck[LoanApplicationRegister] {
   override def apply(lar: LoanApplicationRegister): Result = {
-    notEqualIgnoreSpace(lar.denial.reason1, lar.denial.reason2) and
-      notEqualIgnoreSpace(lar.denial.reason2, lar.denial.reason3) and
-      notEqualIgnoreSpace(lar.denial.reason3, lar.denial.reason1)
-  }
+    val denialList = List(lar.denial.reason1, lar.denial.reason2, lar.denial.reason3).filterNot(_.isEmpty)
 
-  private def notEqualIgnoreSpace(reason1: String, reason2: String): Result = {
-    (reason1 not equalTo(reason2)) or (reason1 not numeric)
+    denialList.distinct.size is equalTo(denialList.size)
   }
 
   override def name: String = "V360"
