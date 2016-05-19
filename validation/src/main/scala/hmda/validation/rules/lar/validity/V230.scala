7908407bba7f59a8a92efaa0c56e51aec20966d5
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-05-19 11:51:12 -0400

    updated for pr #310

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V230.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V230.scala
@@ -3,13 +3,15 @@ package hmda.validation.rules.lar.validity
 import hmda.model.fi.lar.{ LoanApplicationRegister }
 import hmda.validation.dsl.Result
 import hmda.validation.rules.EditCheck
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
 
 object V230 extends EditCheck[LoanApplicationRegister] {
 
-  val occupancyStatusTypes = List(1, 2, 3)
+  val occupancyStatuses = List(1, 2, 3)
 
   override def apply(lar: LoanApplicationRegister): Result = {
-    lar.loan.occupancy is containedIn(occupancyStatusTypes)
+    lar.loan.occupancy is containedIn(occupancyStatuses)
   }
 
   override def name = "V230"
