83f0118b8ce633ed40deb0eca9b04fad72ec1a90
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2016-05-19 14:12:21 -0400

    Update two Edit Specs to use RaceEditSpec utilities

--- validation/src/test/scala/hmda/validation/rules/lar/validity/V475Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/lar/validity/V475Spec.scala
@@ -6,7 +6,7 @@ import hmda.validation.rules.EditCheck
 import hmda.validation.rules.lar.LarEditCheckSpec
 import org.scalacheck.Gen
 
-class V475Spec extends LarEditCheckSpec {
+class V475Spec extends RaceEditCheckSpec {
   import Gen.posNum
 
   property("Succeeds when applicant race1 is not 6 or 7") {
@@ -22,43 +22,28 @@ class V475Spec extends LarEditCheckSpec {
   val race1Gen: Gen[Int] = Gen.choose(6, 7)
 
   property("Succeeds when applicant race1 is 6-7 and race2-race5 are blank") {
-    forAll(larGen, race1Gen) { (lar, race1) =>
-      val applicant = lar.applicant.copy(race1 = race1, race2 = "", race3 = "", race4 = "", race5 = "")
-      val newLar = lar.copy(applicant = applicant)
-      V475(newLar) mustBe a[Success]
-    }
+    succeedsWithRace(List("6", "", "", "", ""))
+    succeedsWithRace(List("7", "", "", "", ""))
   }
 
   property("Fails when applicant race1 is 6-7 and race2 is not blank (race3,4,5 blank)") {
-    forAll(larGen, race1Gen, posNum[Int]) { (lar, race1, race2) =>
-      val applicant = lar.applicant.copy(race1 = race1, race2 = race2.toString, race3 = "", race4 = "", race5 = "")
-      val newLar = lar.copy(applicant = applicant)
-      V475(newLar) mustBe a[Failure]
-    }
+    failsWithRace(List("6", "1", "", "", ""))
+    failsWithRace(List("7", "-4", "", "", ""))
   }
 
   property("Fails when applicant race1 is 6-7 and race3 is not blank (race2,4,5 blank)") {
-    forAll(larGen, race1Gen, posNum[Int]) { (lar, race1, race3) =>
-      val applicant = lar.applicant.copy(race1 = race1, race3 = race3.toString, race2 = "", race4 = "", race5 = "")
-      val newLar = lar.copy(applicant = applicant)
-      V475(newLar) mustBe a[Failure]
-    }
+    failsWithRace(List("6", "", "0", "", ""))
+    failsWithRace(List("7", "", "5", "", ""))
   }
 
   property("Fails when applicant race1 is 6-7 and race4 is not blank (race2,3,5 blank)") {
-    forAll(larGen, race1Gen, posNum[Int]) { (lar, race1, race4) =>
-      val applicant = lar.applicant.copy(race1 = race1, race4 = race4.toString, race2 = "", race3 = "", race5 = "")
-      val newLar = lar.copy(applicant = applicant)
-      V475(newLar) mustBe a[Failure]
-    }
+    failsWithRace(List("6", "", "", "2", ""))
+    failsWithRace(List("7", "", "", "11", ""))
   }
 
   property("Fails when applicant race1 is 6-7 and race5 is not blank (race2,3,4 blank)") {
-    forAll(larGen, race1Gen, posNum[Int]) { (lar, race1, race5) =>
-      val applicant = lar.applicant.copy(race1 = race1, race5 = race5.toString, race2 = "", race3 = "", race4 = "")
-      val newLar = lar.copy(applicant = applicant)
-      V475(newLar) mustBe a[Failure]
-    }
+    failsWithRace(List("6", "", "", "", "4"))
+    failsWithRace(List("7", "", "", "", "9"))
   }
 
   override def check: EditCheck[LoanApplicationRegister] = V475
