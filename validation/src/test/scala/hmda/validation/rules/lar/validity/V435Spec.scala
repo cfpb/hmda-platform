424ee54a9b7011d77ef4dcc970a06bb6961045ad
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-05-20 16:18:10 -0400

    fixed edit documentation

--- validation/src/test/scala/hmda/validation/rules/lar/validity/V435Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/lar/validity/V435Spec.scala
@@ -16,7 +16,7 @@ class V435Spec extends LarEditCheckSpec with BadValueUtils {
 
   val validActionType: Gen[Int] = intOutsideRange(7, 8)
 
-  property("Valid if loan purpose not 1 or 2") {
+  property("Valid if action taken is not 7 or 8") {
     forAll(larGen, validActionType) { (lar, x) =>
       val validLar = lar.copy(actionTakenType = x)
       validLar.mustPass
@@ -26,7 +26,7 @@ class V435Spec extends LarEditCheckSpec with BadValueUtils {
   val invalidActionType: Gen[Int] = Gen.oneOf(7, 8)
   val invalidPreApproval = intOtherThan(1)
 
-  property("Preaproval other than 1 is invalid") {
+  property("Preapproval other than 1 is invalid when action taken is 7 or 8") {
     forAll(larGen, invalidActionType, invalidPreApproval) { (lar: LoanApplicationRegister, ac: Int, pr: Int) =>
       val invalidLar: LoanApplicationRegister = lar.copy(
         actionTakenType = ac,
