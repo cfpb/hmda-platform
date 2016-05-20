16a9ef238d3741f406c95723800604df6f903116
Nick Grippin <ngrippin@discovertechnologies.com>
2016-05-20 11:13:23 -0400

    refactored for readability

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V385.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V385.scala
@@ -12,13 +12,17 @@ object V385 extends EditCheck[LoanApplicationRegister] {
   val validDenialReasons = List("") ::: requiredDenialReasons
   val okActionTaken = List(3, 7)
 
-  def apply(lar: LoanApplicationRegister): Result = {
+  override def apply(lar: LoanApplicationRegister): Result = {
     val denialReasons = List(lar.denial.reason1, lar.denial.reason2, lar.denial.reason3)
 
-    denialReasons.forall(validDenialReasons.contains(_)) is equalTo(true) and
+    val denialsReasonsAreValid = denialReasons.forall(validDenialReasons.contains(_)) is equalTo(true)
+
+    val atLeastOneDenialIfRequired =
       when((lar.agencyCode is equalTo(1)) and (lar.actionTakenType is containedIn(okActionTaken))) {
         denialReasons.intersect(requiredDenialReasons).length is greaterThan(0)
       }
+
+    denialsReasonsAreValid and atLeastOneDenialIfRequired
   }
 
   override def name: String = "V385"
