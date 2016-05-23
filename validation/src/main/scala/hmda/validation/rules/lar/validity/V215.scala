9aeb7e9e522d861859bc913f4965a1d3a7376ae6
Nick Grippin <ngrippin@discovertechnologies.com>
2016-05-23 11:08:02 -0400

    added 215

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V215.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V215.scala
@@ -1,16 +1,16 @@
 package hmda.validation.rules.lar.validity
 
 import hmda.model.fi.lar.LoanApplicationRegister
-import hmda.validation.dsl.{ Result, Success }
+import hmda.validation.dsl.Result
 import hmda.validation.rules.EditCheck
 import hmda.validation.dsl.PredicateCommon._
 import hmda.validation.dsl.PredicateSyntax._
 
-import scala.util.Try
-
 object V215 extends EditCheck[LoanApplicationRegister] {
   override def apply(lar: LoanApplicationRegister): Result = {
-    Success()
+    when(lar.actionTakenType is equalTo(6)) {
+      lar.loan.applicationDate is equalTo("NA")
+    }
   }
 
   override def name = "V215"
---
+++ validation/src/test/scala/hmda/validation/rules/lar/validity/V215Spec.scala
@@ -0,0 +1,38 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.fi.lar.LoanApplicationRegister
+import hmda.validation.dsl.{ Failure, Success }
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+import org.scalacheck.Gen
+
+class V215Spec extends LarEditCheckSpec {
+  property("If action taken type must = 6, then date application received = NA") {
+    forAll(larGen) { lar =>
+      whenever(lar.loan.applicationDate == "NA") {
+        val newLar = lar.copy(actionTakenType = 6)
+        newLar.mustPass
+      }
+    }
+  }
+
+  property("Passes if actionTakenType is not 6") {
+    forAll(larGen) { lar =>
+      whenever(lar.actionTakenType != 6) {
+        lar.mustPass
+      }
+    }
+  }
+
+  val invalidDate: Gen[String] = Gen.alphaStr.filter(_ != "NA")
+
+  property("Fails if actionTakenType is 6 and date application received is not NA") {
+    forAll(larGen, invalidDate) { (lar, altDate) =>
+      val naLoan = lar.loan.copy(applicationDate = altDate)
+      val newLar = lar.copy(loan = naLoan, actionTakenType = 6)
+      newLar.mustFail
+    }
+  }
+
+  override def check: EditCheck[LoanApplicationRegister] = V215
+}
