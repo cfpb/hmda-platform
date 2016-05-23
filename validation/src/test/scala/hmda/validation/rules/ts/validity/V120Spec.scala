5a0b642f1e57ac3826e8a502bd293e6acc0e0719
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-05-23 15:12:35 -0400

    take out unneccessary whenever and add badPhoneGen to utils

--- validation/src/test/scala/hmda/validation/rules/ts/validity/V120Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/ts/validity/V120Spec.scala
@@ -3,16 +3,14 @@ package hmda.validation.rules.lar.validity
 import hmda.model.fi.ts.TransmittalSheet
 import hmda.validation.rules.EditCheck
 import hmda.validation.rules.ts.TsEditCheckSpec
-import hmda.validation.rules.ts.validity.V120
+import hmda.validation.rules.ts.validity.{ V120, ValidityUtils }
 import org.scalacheck.Gen
 
-class V120Spec extends TsEditCheckSpec {
+class V120Spec extends TsEditCheckSpec with ValidityUtils {
 
   property("passes with properly formed number") {
     forAll(tsGen) { ts =>
-      whenever(ts.contact.phone.length == 12) {
-        ts.mustPass
-      }
+      ts.mustPass
     }
   }
 
@@ -25,14 +23,5 @@ class V120Spec extends TsEditCheckSpec {
   }
 
   override def check: EditCheck[TransmittalSheet] = V120
-
-  implicit def badPhoneNumberGen: Gen[String] = {
-    for {
-      p1 <- Gen.numStr
-      p2 <- Gen.numStr
-      p3 <- Gen.numStr
-      sep <- Gen.oneOf(List(".", "/", ""))
-    } yield List(p1.take(3).toString, sep, p2.take(3).toString, sep, p3.take(4).toString).mkString
-  }
 
 }
--- validation/src/test/scala/hmda/validation/rules/ts/validity/V135Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/ts/validity/V135Spec.scala
@@ -3,10 +3,10 @@ package hmda.validation.rules.lar.validity
 import hmda.model.fi.ts.TransmittalSheet
 import hmda.validation.rules.EditCheck
 import hmda.validation.rules.ts.TsEditCheckSpec
-import hmda.validation.rules.ts.validity.V135
+import hmda.validation.rules.ts.validity.{ V135, ValidityUtils }
 import org.scalacheck.Gen
 
-class V135Spec extends TsEditCheckSpec {
+class V135Spec extends TsEditCheckSpec with ValidityUtils {
 
   property("passes with properly formed number") {
     forAll(tsGen) { ts =>
@@ -23,14 +23,5 @@ class V135Spec extends TsEditCheckSpec {
   }
 
   override def check: EditCheck[TransmittalSheet] = V135
-
-  implicit def badPhoneNumberGen: Gen[String] = {
-    for {
-      p1 <- Gen.numStr
-      p2 <- Gen.numStr
-      p3 <- Gen.numStr
-      sep <- Gen.oneOf(List(".", "/", ""))
-    } yield List(p1.take(3).toString, sep, p2.take(3).toString, sep, p3.take(4).toString).mkString
-  }
 
 }
