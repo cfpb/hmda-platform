14852672a152f4fa61abe742f3890c7468b2d74a
Nick Grippin <ngrippin@discovertechnologies.com>
2016-05-23 13:53:07 -0400

    checkpoint

---
+++ validation/src/main/scala/hmda/validation/rules/ts/validity/V111.scala
@@ -0,0 +1,30 @@
+package hmda.validation.rules.ts.validity
+
+import hmda.model.fi.ts.TransmittalSheet
+import hmda.validation.dsl.Result
+import hmda.validation.rules.EditCheck
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+
+/*
+ Respondent name, address, city, state, and zip code must not = blank
+ */
+object V105 extends EditCheck[TransmittalSheet] {
+
+  override def apply(ts: TransmittalSheet): Result = {
+    val respondent = ts.respondent
+    val respName = respondent.name
+    val respAddress = respondent.address
+    val respCity = respondent.city
+    val respState = respondent.state
+    val respZipCode = respondent.zipCode
+
+    (respName not empty) and
+      (respAddress not empty) and
+      (respCity not empty) and
+      (respState not empty) and
+      (respZipCode not empty)
+  }
+
+  override def name: String = "V105"
+}
