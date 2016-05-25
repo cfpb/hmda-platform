9df61f2797292207f5110a8d11e783b40a966f5d
Nick Grippin <ngrippin@discovertechnologies.com>
2016-05-25 11:32:29 -0400

    switched to Try

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V265.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V265.scala
@@ -3,21 +3,21 @@ package hmda.validation.rules.lar.validity
 import java.text.SimpleDateFormat
 
 import hmda.model.fi.lar.LoanApplicationRegister
-import hmda.validation.dsl.{ Failure, Result, Success }
+import hmda.validation.dsl.Result
 import hmda.validation.rules.EditCheck
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+
+import scala.util.Try
 
 object V265 extends EditCheck[LoanApplicationRegister] {
   override def apply(lar: LoanApplicationRegister): Result = {
     val date = lar.actionTakenDate.toString
 
-    try {
-      val format = new SimpleDateFormat("yyyyMMdd")
-      format.setLenient(false)
-      format.parse(date)
-      Success()
-    } catch {
-      case e: Exception => Failure("Date not properly formatted")
-    }
+    val format = new SimpleDateFormat("yyyyMMdd")
+    format.setLenient(false)
+
+    Try(format.parse(date)).isSuccess is equalTo(true)
   }
 
   override def name = "V265"
