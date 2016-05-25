61a05cbdfd34a80448eaba82975e9c265a8ffbf4
Nick Grippin <ngrippin@discovertechnologies.com>
2016-05-25 09:12:55 -0400

    added types for clear code

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V275.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V275.scala
@@ -10,8 +10,8 @@ import scala.util.Try
 
 object V275 extends EditCheck[LoanApplicationRegister] {
   override def apply(lar: LoanApplicationRegister): Result = {
-    val actionTakenDate = lar.actionTakenDate
-    val dateReceived = lar.loan.applicationDate
+    val actionTakenDate: Int = lar.actionTakenDate
+    val dateReceived: String = lar.loan.applicationDate
 
     when(dateReceived not equalTo("NA")) {
       Try((actionTakenDate - dateReceived.toInt) is greaterThanOrEqual(0))
