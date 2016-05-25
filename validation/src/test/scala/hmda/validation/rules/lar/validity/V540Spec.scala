7d98985c0bed5a41ae8a689643ae4988d0aa5ce5
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2016-05-25 10:01:11 -0400

    Use "action" instead of "actionTaken" where it's unambiguous

--- validation/src/test/scala/hmda/validation/rules/lar/validity/V540Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/lar/validity/V540Spec.scala
@@ -7,14 +7,14 @@ import org.scalacheck.Gen
 
 class V540Spec extends LarEditCheckSpec with BadValueUtils {
 
-  val relevantActionTakenList = List(2, 3, 4, 5, 7, 8)
+  val relevantActions = List(2, 3, 4, 5, 7, 8)
 
-  val irrelevantActionTakenGen: Gen[Int] =
+  val irrelevantActionGen: Gen[Int] =
     Gen.choose(Int.MinValue, Int.MaxValue)
-      .filter(!relevantActionTakenList.contains(_))
+      .filter(!relevantActions.contains(_))
 
-  property("Valid when Action taken not 2,3,4,5,7 or 8") {
-    forAll(larGen, irrelevantActionTakenGen) { (lar, x) =>
+  property("Valid when Action Taken not 2,3,4,5,7 or 8") {
+    forAll(larGen, irrelevantActionGen) { (lar, x) =>
       val validLar = lar.copy(actionTakenType = x)
       validLar.mustPass
     }
@@ -27,11 +27,11 @@ class V540Spec extends LarEditCheckSpec with BadValueUtils {
     }
   }
 
-  val actionTakenGen: Gen[Int] = Gen.oneOf(relevantActionTakenList)
+  val relevantActionGen: Gen[Int] = Gen.oneOf(relevantActions)
   val badHoepaStatusGen: Gen[Int] = Gen.choose(Int.MinValue, Int.MaxValue).filter(_ != 2)
 
   property("HOEPA status other than 2 is invalid when action taken = 2,3,4,5,7,8") {
-    forAll(larGen, badHoepaStatusGen, actionTakenGen) { (lar: LoanApplicationRegister, hs: Int, at: Int) =>
+    forAll(larGen, badHoepaStatusGen, relevantActionGen) { (lar: LoanApplicationRegister, hs: Int, at: Int) =>
       val invalidLar: LoanApplicationRegister = lar.copy(
         actionTakenType = at,
         hoepaStatus = hs
