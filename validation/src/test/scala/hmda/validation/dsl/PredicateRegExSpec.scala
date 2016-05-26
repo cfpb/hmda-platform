28b977f88e20dd3840c5041fb1eaa087bbdc08e1
Nick Grippin <ngrippin@discovertechnologies.com>
2016-05-26 16:49:33 -0400

    all generator cases

--- validation/src/test/scala/hmda/validation/dsl/PredicateRegExSpec.scala
+++ validation/src/test/scala/hmda/validation/dsl/PredicateRegExSpec.scala
@@ -8,6 +8,7 @@ import org.scalatest.prop.PropertyChecks
 import hmda.validation.dsl.PredicateRegEx._
 import hmda.validation.dsl.PredicateCommon._
 import hmda.validation.dsl.PredicateSyntax._
+import org.scalacheck.Gen
 
 class PredicateRegExSpec
     extends PropSpec
@@ -17,9 +18,42 @@ class PredicateRegExSpec
     with LarSyntacticalEngine
     with TsGenerators {
 
+  // Email
+
   property("A valid email must pass the regex") {
     forAll(emailGen) { email =>
       email is validEmail
     }
   }
+
+  property("An alphanumeric string will fail the regex") {
+    forAll(Gen.alphaStr) { email =>
+      email not validEmail
+    }
+  }
+
+  // Phone
+
+  property("A valid phone number must pass the regex") {
+    forAll(phoneGen) { phone =>
+      phone is validPhoneNumber
+    }
+  }
+
+  // Zip code
+
+  property("A valid zip code must pass the regex") {
+    forAll(zipGen) { zip =>
+      zip is validZipCode
+    }
+  }
+
+  // Tax ID
+
+  property("A valid tax ID must pass the regex") {
+    forAll(taxIdGen) { taxId =>
+      taxId is validTaxId
+    }
+  }
+
 }
