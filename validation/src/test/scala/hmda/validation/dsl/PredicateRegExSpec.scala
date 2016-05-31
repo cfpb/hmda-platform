2fd1d4e94435650679e3edc4cb861af9020035f9
Nick Grippin <ngrippin@discovertechnologies.com>
2016-05-31 09:29:05 -0400

    fix tests

--- validation/src/test/scala/hmda/validation/dsl/PredicateRegExSpec.scala
+++ validation/src/test/scala/hmda/validation/dsl/PredicateRegExSpec.scala
@@ -1,7 +1,7 @@
 package hmda.validation.dsl
 
 import hmda.parser.fi.ts.TsGenerators
-import org.scalatest.PropSpec
+import org.scalatest.{ MustMatchers, PropSpec }
 import org.scalatest.prop.PropertyChecks
 import hmda.validation.dsl.PredicateRegEx._
 import hmda.validation.dsl.PredicateSyntax._
@@ -10,19 +10,20 @@ import org.scalacheck.Gen
 class PredicateRegExSpec
     extends PropSpec
     with PropertyChecks
-    with TsGenerators {
+    with TsGenerators
+    with MustMatchers {
 
   // Email **************
 
   property("A valid email must pass the email regex") {
     forAll(emailGen) { email =>
-      email is validEmail
+      validEmail.validate(email) mustBe true
     }
   }
 
   property("An alphanumeric string will fail the email regex") {
     forAll(Gen.alphaStr) { email =>
-      email not validEmail
+      validEmail.validate(email) mustBe false
     }
   }
 
@@ -33,76 +34,76 @@ class PredicateRegExSpec
   property("An improperly formatted email must fail the email regex") {
     val testCases = List("test@.", "test@test.", "test@.com", "@test.com", "@.", " @ . ", "!test@test.com",
       "test@test.com!", "123@456.789")
-    testCases.foreach(_ not validEmail)
+    testCases.foreach(validEmail.validate(_) mustBe false)
   }
 
   // Phone **************
 
   property("A valid phone number must pass the phone regex") {
     forAll(phoneGen) { phone =>
-      phone is validPhoneNumber
+      validPhoneNumber.validate(phone) mustBe true
     }
   }
 
   property("A numeric string will fail the phone regex") {
     forAll(Gen.numStr) { phone =>
-      phone not validPhoneNumber
+      validPhoneNumber.validate(phone) mustBe false
     }
   }
 
   property("An empty string will fail the phone regex") {
-    "" not validPhoneNumber
+    validPhoneNumber.validate("") mustBe false
   }
 
   property("An improperly formatted phone number must fail the phone number regex") {
     val testCases = List("123-456-789", "--", " - - ", "0-0-0", "abc-def-ghij", "(123) 456-7890", "123 456 7890",
       "1-800-123-4567")
-    testCases.foreach(_ not validPhoneNumber)
+    testCases.foreach(validPhoneNumber.validate(_) mustBe false)
   }
 
   // Zip code **************
 
   property("A valid zip code must pass the zip code regex") {
     forAll(zipGen) { zip =>
-      zip is validZipCode
+      validZipCode.validate(zip) mustBe true
     }
   }
 
   property("A numeric string of length != 5 will fail the zip code regex") {
     forAll(Gen.numStr.filter(_.length != 5)) { zip =>
-      zip not validZipCode
+      validZipCode.validate(zip) mustBe false
     }
   }
 
   property("An empty string will fail the zip code regex") {
-    "" not validZipCode
+    validZipCode.validate("") mustBe false
   }
 
   property("An improperly formatted zip code must fail the zip code regex") {
     val testCases = List("1234", " ", "     ", "0-0", "1234-5678", "123456-789", "abcde")
-    testCases.foreach(_ not validZipCode)
+    testCases.foreach(validZipCode.validate(_) mustBe false)
   }
 
   // Tax ID **************
 
   property("A valid tax ID must pass the tax ID regex") {
     forAll(taxIdGen) { taxId =>
-      taxId is validTaxId
+      validTaxId.validate(taxId) mustBe true
     }
   }
 
   property("A numeric string must fail the tax ID regex") {
     forAll(Gen.numStr) { taxId =>
-      taxId not validTaxId
+      validTaxId.validate(taxId) mustBe false
     }
   }
 
   property("An empty string must fail the tax ID regex") {
-    "" not validTaxId
+    validTaxId.validate("") mustBe false
   }
 
   property("An improperly formatted tax ID must fail the tax ID regex") {
     val testCases = List(" - ", "-", "123-456789", "ab-defjhij")
-    testCases.foreach(_ not validTaxId)
+    testCases.foreach(validTaxId.validate(_) mustBe false)
   }
 }
