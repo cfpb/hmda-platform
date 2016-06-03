67bab6dc229a2d643520f086f82c599203c55b8d
Betsy Lorton <elorton@gmail.com>
2016-06-03 14:41:27 -0400

    Allow updating income limit with environment variable
    The values in Quality edits commonly change from year to year. By adding
    Q014's income limit to the application's configuration, we allow
    updating the value in the future without updating the code.
    
    Also, refactored application.conf for readability.

--- validation/src/main/scala/hmda/validation/rules/lar/quality/Q014.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/quality/Q014.scala
@@ -1,5 +1,6 @@
 package hmda.validation.rules.lar.quality
 
+import com.typesafe.config.ConfigFactory
 import hmda.model.fi.lar.LoanApplicationRegister
 import hmda.validation.dsl.Result
 import hmda.validation.rules.EditCheck
@@ -7,12 +8,15 @@ import hmda.validation.dsl.PredicateCommon._
 import hmda.validation.dsl.PredicateSyntax._
 
 object Q014 extends EditCheck[LoanApplicationRegister] {
+  val config = ConfigFactory.load()
+  val max_income = config.getInt("hmda.validation.quality.Q014.applicant.max_income")
+
   override def name: String = "Q014"
 
   override def apply(lar: LoanApplicationRegister): Result = {
     val income = lar.applicant.income
     when(income is numeric) {
-      income.toInt is lessThan(3000)
+      income.toInt is lessThan(max_income)
     }
   }
 }
--- validation/src/test/scala/hmda/validation/rules/lar/quality/Q014Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/lar/quality/Q014Spec.scala
@@ -1,13 +1,17 @@
 package hmda.validation.rules.lar.quality
 
+import com.typesafe.config.ConfigFactory
 import hmda.model.fi.lar.LoanApplicationRegister
 import hmda.validation.rules.EditCheck
 import hmda.validation.rules.lar.{ BadValueUtils, LarEditCheckSpec }
 import org.scalacheck.Gen
 
 class Q014Spec extends LarEditCheckSpec with BadValueUtils {
+  val config = ConfigFactory.load()
+  val max_income = config.getInt("hmda.validation.quality.Q014.applicant.max_income")
+
   property("passes when income is less than stated limit") {
-    forAll(larGen, Gen.choose(1, 3000)) { (lar, i) =>
+    forAll(larGen, Gen.choose(1, max_income)) { (lar, i) =>
       val validApplicant = lar.applicant.copy(income = i.toString)
       val validLar = lar.copy(applicant = validApplicant)
       validLar.mustPass
@@ -15,7 +19,7 @@ class Q014Spec extends LarEditCheckSpec with BadValueUtils {
   }
 
   property("fails when income is too high (over configured limit)") {
-    forAll(larGen, Gen.choose(3001, Int.MaxValue)) { (lar, i) =>
+    forAll(larGen, Gen.choose(max_income + 1, Int.MaxValue)) { (lar, i) =>
       val validApplicant = lar.applicant.copy(income = i.toString)
       val validLar = lar.copy(applicant = validApplicant)
       validLar.mustFail
