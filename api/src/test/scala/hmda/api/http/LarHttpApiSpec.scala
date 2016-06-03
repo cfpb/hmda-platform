2b61fa3b5380d2bea30698a0ba54158af14e15cd
Nick Grippin <ngrippin@discovertechnologies.com>
2016-06-03 11:17:49 -0400

    fix spacing

--- api/src/test/scala/hmda/api/http/LarHttpApiSpec.scala
+++ api/src/test/scala/hmda/api/http/LarHttpApiSpec.scala
@@ -51,8 +51,7 @@ class LarHttpApiSpec extends WordSpec with MustMatchers with ScalatestRouteTest 
       }
     }
 
-    "filter syntactical, validity, or quality only for invalid LAR with " +
-      "all 3 kinds of errors" in {
+    "filter syntactical, validity, or quality only for invalid LAR with all 3 kinds of errors" in {
         val badLoanType = lar.loan.copy(loanType = 0, amount = 9000, propertyType = 2)
         val badLar = lar.copy(agencyCode = 0, loan = badLoanType, purchaserType = 1)
         Post("/lar/validate", badLar) ~> larRoutes ~> check {
