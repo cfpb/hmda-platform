e13c67c3e835f8c103d0fab45dd98a93563a1cd3
Nick Grippin <ngrippin@discovertechnologies.com>
2016-06-06 11:01:12 -0400

    start

---
+++ validation/src/main/scala/hmda/validation/rules/lar/quality/Q038.scala
@@ -0,0 +1,24 @@
+package hmda.validation.rules.lar.quality
+
+import hmda.model.fi.lar.LoanApplicationRegister
+import hmda.validation.dsl.Result
+import hmda.validation.rules.EditCheck
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+
+import com.typesafe.config.ConfigFactory
+
+object Q038 extends EditCheck[LoanApplicationRegister] {
+  override def apply(lar: LoanApplicationRegister): Result = {
+
+    val config = ConfigFactory.load()
+    val loanAmount = config.getInt("hmda.validation.quality.Q005.loan.amount")
+
+    when((lar.purchaserType is containedIn(List(1, 2, 3, 4))) and
+      (lar.loan.propertyType is containedIn(List(1, 2)))) {
+      lar.loan.amount is lessThanOrEqual(loanAmount)
+    }
+  }
+
+  override def name = "Q038"
+}
