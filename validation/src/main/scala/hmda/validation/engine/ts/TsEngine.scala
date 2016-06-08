6e41d8c18e3eebc9070dee7336da9e203fcce3e5
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-06-08 11:48:33 -0400

    updared TsEngine with qualityEngine

--- validation/src/main/scala/hmda/validation/engine/ts/TsEngine.scala
+++ validation/src/main/scala/hmda/validation/engine/ts/TsEngine.scala
@@ -1,13 +1,15 @@
 package hmda.validation.engine.ts
 
 import hmda.model.fi.ts.TransmittalSheet
+import hmda.validation.engine.ts.quality.TsQualityEngine
 import hmda.validation.engine.ts.syntactical.TsSyntacticalEngine
 import hmda.validation.engine.ts.validity.TsValidityEngine
+
 import scalaz._
 import Scalaz._
 import scala.concurrent.Future
 
-trait TsEngine extends TsSyntacticalEngine with TsValidityEngine {
+trait TsEngine extends TsSyntacticalEngine with TsValidityEngine with TsQualityEngine {
 
   def validateTs(ts: TransmittalSheet): Future[TsValidation] = {
     val fSyntactical = checkSyntactical(ts)
