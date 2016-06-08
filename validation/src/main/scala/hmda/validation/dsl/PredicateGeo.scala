7a8d2dd93362824fc0a22c8be179f06f10abe657
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-06-08 17:04:54 -0400

    added predicate geo

---
+++ validation/src/main/scala/hmda/validation/dsl/PredicateGeo.scala
@@ -0,0 +1,80 @@
+package hmda.validation.dsl
+
+import hmda.model.census.CBSATractLookup
+import hmda.model.fi.lar.Geography
+
+object PredicateGeo {
+  val cbsaTracts = CBSATractLookup.values
+
+  val validMsaCombinationSet = cbsaTracts.map { cbsa =>
+    (cbsa.geoIdMsa, cbsa.state, cbsa.county, cbsa.tractDecimal)
+  }.toSet
+
+  val validMdCombinationSet = cbsaTracts.map { cbsa =>
+    (cbsa.metDivFp, cbsa.state, cbsa.county, cbsa.tractDecimal)
+  }.toSet
+
+  val validMsaCombinationSetNoTract = cbsaTracts.map { cbsa =>
+    (cbsa.geoIdMsa, cbsa.state, cbsa.county)
+  }.toSet
+
+  val validMdCombinationSetNoTract = cbsaTracts.map { cbsa =>
+    (cbsa.metDivFp, cbsa.state, cbsa.county)
+  }.toSet
+
+  val validStateCountyTractCombinationSet = cbsaTracts.map { cbsa =>
+    (cbsa.state, cbsa.county, cbsa.tractDecimal)
+  }.toSet
+
+  val validStateCountyCombinationSet = cbsaTracts.map { cbsa =>
+    (cbsa.state, cbsa.county)
+  }.toSet
+
+  val smallCounties = cbsaTracts
+    .filter { cbsa => cbsa.smallCounty == 1 }
+    .map { cbsa => (cbsa.state, cbsa.county) }
+    .toSet
+
+  implicit def smallCounty: Predicate[Geography] = new Predicate[Geography] {
+    override def validate: (Geography) => Boolean = _.asInstanceOf[AnyRef] match {
+      case geo: Geography => smallCounties.contains((geo.state, geo.county))
+      case _ => false
+    }
+    override def failure: String = "county is not small"
+  }
+
+  implicit def validStateCountyCombination: Predicate[Geography] = new Predicate[Geography] {
+    override def validate: (Geography) => Boolean = _.asInstanceOf[AnyRef] match {
+      case geo: Geography => validStateCountyCombinationSet.contains((geo.state, geo.county))
+      case _ => false
+    }
+    override def failure: String = "state and county combination is not valid"
+  }
+
+  implicit def validStateCountyTractCombination: Predicate[Geography] = new Predicate[Geography] {
+    override def validate: (Geography) => Boolean = _.asInstanceOf[AnyRef] match {
+      case geo: Geography => validStateCountyTractCombinationSet.contains((geo.state, geo.county, geo.tract))
+      case _ => false
+    }
+    override def failure: String = "state, county, and census tract combination is not valid"
+  }
+
+  implicit def validCompleteCombination: Predicate[Geography] = new Predicate[Geography] {
+    override def validate: (Geography) => Boolean = _.asInstanceOf[AnyRef] match {
+      case geo: Geography => validMsaCombinationSet.contains((geo.msa, geo.state, geo.county, geo.tract)) ||
+        validMdCombinationSet.contains((geo.msa, geo.state, geo.county, geo.tract))
+      case _ => false
+    }
+    override def failure: String = "state, county, msa, and census tract combination is not valid"
+  }
+
+  implicit def validStateCountyMsaCombination: Predicate[Geography] = new Predicate[Geography] {
+    override def validate: (Geography) => Boolean = _.asInstanceOf[AnyRef] match {
+      case geo: Geography => validMsaCombinationSetNoTract.contains((geo.msa, geo.state, geo.county)) ||
+        validMdCombinationSetNoTract.contains((geo.msa, geo.state, geo.county))
+      case _ => false
+    }
+    override def failure: String = "state, county, msa, and census tract combination is not valid"
+  }
+
+}
--- validation/src/main/scala/hmda/validation/rules/lar/validity/V290.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V290.scala
@@ -1,36 +1,19 @@
 package hmda.validation.rules.lar.validity
 
-import hmda.model.census.CBSATractLookup
 import hmda.model.fi.lar.LoanApplicationRegister
 import hmda.validation.dsl.Result
 import hmda.validation.rules.EditCheck
 import hmda.validation.dsl.PredicateCommon._
 import hmda.validation.dsl.PredicateSyntax._
+import hmda.validation.dsl.PredicateGeo._
 
 object V290 extends EditCheck[LoanApplicationRegister] {
 
-  val cbsaTracts = CBSATractLookup.values
-
-  val validCombinations = cbsaTracts.map { cbsa =>
-    (cbsa.state, cbsa.county, cbsa.geoIdMsa)
-  }.toSet
-
-  val validMdCombinations = cbsaTracts.map { cbsa =>
-    (cbsa.state, cbsa.county, cbsa.metDivFp)
-  }.toSet
-
   override def name: String = "V290"
 
-  override def apply(input: LoanApplicationRegister): Result = {
-    val msa = input.geography.msa
-    val state = input.geography.state
-    val county = input.geography.county
-
-    val combination = (state, county, msa)
-
-    when(msa not equalTo("NA")) {
-      (combination is containedIn(validCombinations)) or
-        (combination is containedIn(validMdCombinations))
+  override def apply(lar: LoanApplicationRegister): Result = {
+    when(lar.geography.msa not equalTo("NA")) {
+      lar.geography is validStateCountyMsaCombination
     }
   }
 
