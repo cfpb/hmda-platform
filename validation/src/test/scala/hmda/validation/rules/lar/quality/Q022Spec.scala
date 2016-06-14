f9d70cedca6433aa3303d18d4053fb8f6eaba8aa
Nick Grippin <ngrippin@discovertechnologies.com>
2016-06-14 16:25:53 -0400

    more tests

--- validation/src/test/scala/hmda/validation/rules/lar/quality/Q022Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/lar/quality/Q022Spec.scala
@@ -8,13 +8,17 @@ import org.scalacheck.Gen
 import org.scalatest.{ MustMatchers, PropSpec }
 import org.scalatest.prop.PropertyChecks
 
+import scala.concurrent.{ ExecutionContext, Future }
+
 class Q022Spec extends PropSpec with PropertyChecks with MustMatchers with LarGenerators with BadValueUtils {
-  // Cases meeting preconditions
+  implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+
   property("Passes if activity year is within two years after application date") {
     forAll(larGen, Gen.choose(0, 2)) { (lar, x) =>
       whenever(lar.loan.applicationDate != "NA") {
         val applicationYear = lar.loan.applicationDate.substring(0, 4).toInt
         Q022(lar, applicationYear + x) mustBe a[Success]
+        Q022(lar, Future(applicationYear + x)).map(x => x mustBe Success())
       }
     }
   }
@@ -24,15 +28,18 @@ class Q022Spec extends PropSpec with PropertyChecks with MustMatchers with LarGe
       whenever(lar.loan.applicationDate != "NA") {
         val applicationYear = lar.loan.applicationDate.substring(0, 4).toInt
         Q022(lar, applicationYear + x) mustBe a[Failure]
+        Q022(lar, Future(applicationYear + x)).map(x => x mustBe a[Failure])
       }
     }
   }
 
   property("Passes if activity year is equal to NA") {
     forAll(larGen) { lar =>
-      whenever(lar.loan.applicationDate == "NA") {
-        Q022(lar, 2099) mustBe a[Success]
-      }
+      val newLoan = lar.loan.copy(applicationDate = "NA")
+      val newLar = lar.copy(loan = newLoan)
+
+      Q022(newLar, 2099) mustBe a[Success]
+      Q022(newLar, Future(2099)).map(x => x mustBe Success())
     }
   }
 }
