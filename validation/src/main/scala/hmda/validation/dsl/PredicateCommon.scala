ec16ab558462ae702872e8dc81d1bf78691b8a78
L Dean <lgdean@users.noreply.github.com>
2016-07-11 10:35:00 -0700

    remove implicit from predicates, except equalTo
    this change allows equalTo to be used implicitly, as in V338 and V520

--- validation/src/main/scala/hmda/validation/dsl/PredicateCommon.scala
+++ validation/src/main/scala/hmda/validation/dsl/PredicateCommon.scala
@@ -6,33 +6,33 @@ import scala.util.Try
 object PredicateCommon {
   implicit def equalTo[T](that: T): Predicate[T] = (_: T) == that
 
-  implicit def greaterThan[T](that: T)(implicit ord: Ordering[T]): Predicate[T] = ord.gt(_: T, that)
+  def greaterThan[T](that: T)(implicit ord: Ordering[T]): Predicate[T] = ord.gt(_: T, that)
 
-  implicit def greaterThanOrEqual[T](that: T)(implicit ord: Ordering[T]): Predicate[T] = ord.gteq(_: T, that)
+  def greaterThanOrEqual[T](that: T)(implicit ord: Ordering[T]): Predicate[T] = ord.gteq(_: T, that)
 
-  implicit def lessThan[T](that: T)(implicit ord: Ordering[T]): Predicate[T] = ord.lt(_: T, that)
+  def lessThan[T](that: T)(implicit ord: Ordering[T]): Predicate[T] = ord.lt(_: T, that)
 
-  implicit def lessThanOrEqual[T](that: T)(implicit ord: Ordering[T]): Predicate[T] = ord.lteq(_: T, that)
+  def lessThanOrEqual[T](that: T)(implicit ord: Ordering[T]): Predicate[T] = ord.lteq(_: T, that)
 
-  implicit def between[T](lower: T, upper: T)(implicit ord: Ordering[T]): Predicate[T] = { x: T =>
+  def between[T](lower: T, upper: T)(implicit ord: Ordering[T]): Predicate[T] = { x: T =>
     ord.lteq(lower, x) && ord.lteq(x, upper)
   }
 
-  implicit def numericallyBetween(lower: String, upper: String): Predicate[String] = { x: String =>
+  def numericallyBetween(lower: String, upper: String): Predicate[String] = { x: String =>
     Try(between(BigDecimal(lower), BigDecimal(upper)).validate(BigDecimal(x))).getOrElse(false)
   }
 
-  implicit def numericallyLessThan(upper: String): Predicate[String] = { x: String =>
+  def numericallyLessThan(upper: String): Predicate[String] = { x: String =>
     Try(lessThan(BigDecimal(upper)).validate(BigDecimal(x))).getOrElse(false)
   }
 
-  implicit def numericallyLessThanOrEqual(upper: String): Predicate[String] = { x: String =>
+  def numericallyLessThanOrEqual(upper: String): Predicate[String] = { x: String =>
     Try(lessThanOrEqual(BigDecimal(upper)).validate(BigDecimal(x))).getOrElse(false)
   }
 
-  implicit def oneOf[T](domain: T*): Predicate[T] = containedIn(domain)
+  def oneOf[T](domain: T*): Predicate[T] = containedIn(domain)
 
-  implicit def containedIn[T](domain: Seq[T]): Predicate[T] = domain.contains(_: T)
+  def containedIn[T](domain: Seq[T]): Predicate[T] = domain.contains(_: T)
 
   def containedIn[T](domain: Set[T]): Predicate[T] = domain.contains(_: T)
 
@@ -42,7 +42,7 @@ object PredicateCommon {
     case _ => throw new NotImplementedError("'numeric' doesn't handle non-number/string values yet")
   }
 
-  implicit def empty[T]: Predicate[T] = (_: T) match {
+  def empty[T]: Predicate[T] = (_: T) match {
     case s: String => s.isEmpty
     case _ => throw new NotImplementedError("'empty' doesn't handle non-string values yet")
   }
