9a53e1a765c71f8f5ba06cbb38e6715f5c87b11d
Nick Grippin <ngrippin@discovertechnologies.com>
2016-07-12 09:20:19 -0400

    added errors to response and tests

--- api/src/main/scala/hmda/api/http/LarHttpApi.scala
+++ api/src/main/scala/hmda/api/http/LarHttpApi.scala
@@ -3,20 +3,24 @@ package hmda.api.http
 import akka.actor.ActorSystem
 import akka.event.LoggingAdapter
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
 import hmda.api.protocol.validation.ValidationResultProtocol
 import akka.pattern.ask
 import hmda.parser.fi.lar.LarCsvParser
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.{ HttpResponse, StatusCodes }
+import akka.http.scaladsl.model.{ ContentTypes, _ }
 import akka.util.Timeout
 import hmda.api.processing.lar.SingleLarValidation.{ CheckAll, CheckQuality, CheckSyntactical, CheckValidity }
 import hmda.api.protocol.fi.lar.LarProtocol
 import hmda.model.fi.lar.LoanApplicationRegister
 import hmda.validation.engine.ValidationError
+
 import scala.concurrent.ExecutionContext
 import scala.util.{ Failure, Success }
+
+import spray.json._
 
 trait LarHttpApi extends LarProtocol with ValidationResultProtocol {
 
@@ -33,7 +37,7 @@ trait LarHttpApi extends LarProtocol with ValidationResultProtocol {
           entity(as[String]) { s =>
             LarCsvParser(s) match {
               case Right(lar) => complete(ToResponseMarshallable(lar))
-              case Left(error) => complete(HttpResponse(StatusCodes.UnprocessableEntity))
+              case Left(errors) => complete(errorsAsResponse(errors))
             }
           }
         }
@@ -64,6 +68,11 @@ trait LarHttpApi extends LarProtocol with ValidationResultProtocol {
         }
       }
     }
+
+  def errorsAsResponse(list: List[String]): HttpResponse = {
+    val errorEntity = HttpEntity(ContentTypes.`application/json`, list.toJson.toString)
+    HttpResponse(StatusCodes.UnprocessableEntity, entity = errorEntity)
+  }
 
   val larRoutes = parseLarRoute ~ validateLarRoute
 
