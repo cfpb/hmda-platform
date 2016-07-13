fc8baa0bafa4830dfc5bce0d5fffb26c228b1f62
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-07-13 13:16:43 -0400

    added more tests to document fail cases

--- parser/src/test/scala/hmda/parser/fi/lar/LarCsvParserSpec.scala
+++ parser/src/test/scala/hmda/parser/fi/lar/LarCsvParserSpec.scala
@@ -11,4 +11,28 @@ class LarCsvParserSpec extends PropSpec with PropertyChecks with MustMatchers wi
       LarCsvParser(lar.toCSV).isRight mustBe true
     }
   }
+
+  property("Must return length error on too long csv") {
+    forAll(larGen) { (lar: LoanApplicationRegister) =>
+      LarCsvParser(lar.toCSV + "|0|1").left.get mustBe List("Incorrect number of fields: 41")
+    }
+  }
+
+  val unparsableLarCsvOneField = "a|0123456789|9|ABCDEFGHIJKLMNOPQRSTUVWXY|NA|4|2|2|1|100|3|6|20130119|14454|25|025|0001.00|4|3|5|4|3|2|1|6|||||1|2|NA|0||||NA|2|4"
+
+  property("Must return name of non-integer field") {
+    LarCsvParser(unparsableLarCsvOneField).left.get mustBe List("Record Identifier is not an Integer")
+  }
+
+  val unparsableLarCsvTwoFields = "a|0123456789|b|ABCDEFGHIJKLMNOPQRSTUVWXY|NA|4|2|2|1|100|3|6|20130119|14454|25|025|0001.00|4|3|5|4|3|2|1|6|||||1|2|NA|0||||NA|2|4"
+
+  property("Must return names of non-integer fields") {
+    LarCsvParser(unparsableLarCsvTwoFields).left.get mustBe List("Record Identifier is not an Integer", "Agency Code is not an Integer")
+  }
+
+  val unparsableLarCsvTwoFieldsTooLong = "a|0123456789|b|ABCDEFGHIJKLMNOPQRSTUVWXY|NA|4|2|2|1|100|3|6|20130119|14454|25|025|0001.00|4|3|5|4|3|2|1|6|||||1|2|NA|0||||NA|2"
+
+  property("Must return only length error on too short csv") {
+    LarCsvParser(unparsableLarCsvTwoFieldsTooLong).left.get mustBe List("Incorrect number of fields: 38")
+  }
 }
