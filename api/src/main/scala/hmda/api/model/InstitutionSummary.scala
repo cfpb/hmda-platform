4b833b50e1aa79078bca9cbf8b75f4265424099f
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-07-29 10:22:43 -0400

    remove unused method

--- api/src/main/scala/hmda/api/model/InstitutionSummary.scala
+++ api/src/main/scala/hmda/api/model/InstitutionSummary.scala
@@ -2,14 +2,4 @@ package hmda.api.model
 
 import hmda.model.fi.Filing
 
-case class InstitutionSummary(id: String, name: String, filings: Seq[Filing]) {
-
-  def noInstitution: Boolean = {
-    (id == "") && (name == "")
-  }
-
-  def noFiling: Boolean = {
-    filings.isEmpty
-  }
-
-}
+case class InstitutionSummary(id: String, name: String, filings: Seq[Filing])
