c5074d0d0e7e8c968806ea788c53cc1b53c89df5
Hans Keeler <hans.keeler@cfpb.gov>
2016-08-01 16:26:35 -0400

    Comment on Q022 2-year timeframe logic

--- validation/src/main/scala/hmda/validation/rules/lar/quality/Q022.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/quality/Q022.scala
@@ -9,10 +9,13 @@ import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.Try
 
 object Q022 {
+
   def apply(lar: LoanApplicationRegister, year: Int): Result = {
     // This edit should not fail if applicationDate is equal to "NA" or is otherwise non-convertible
     val applicationYear = parseYear(lar).getOrElse(year)
 
+    // The two year time frame is to allow for construction loans, where properties are typically built within two years.
+    // SEE: https://github.com/cfpb/hmda-platform/issues/469
     (year - applicationYear) is between(0, 2)
   }
 
