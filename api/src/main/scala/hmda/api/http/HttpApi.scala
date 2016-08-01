149db98bc850d46ccea0c9cda23b92247449e8c6
Nick Grippin <ngrippin@discovertechnologies.com>
2016-08-01 08:47:08 -0400

    removed more old code

--- api/src/main/scala/hmda/api/http/HttpApi.scala
+++ api/src/main/scala/hmda/api/http/HttpApi.scala
@@ -22,13 +22,11 @@ trait HttpApi extends HmdaApiProtocol with HmdaCustomDirectives {
     pathSingleSlash {
       get {
         time {
-          val requestTime = System.currentTimeMillis()
           complete {
             val now = Instant.now.toString
             val host = InetAddress.getLocalHost.getHostName
             val status = Status("OK", "hmda-api", now, host)
             log.debug(status.toJson.toString)
-            log.debug("Elapsed time: " + (System.currentTimeMillis() - requestTime) + "ms")
             ToResponseMarshallable(status)
           }
         }
