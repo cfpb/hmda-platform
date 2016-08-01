bf1a5f0145edd669a425ad7ded4e66ec476fc6ba
L Dean <lgdean@users.noreply.github.com>
2016-08-01 09:45:30 -0700

    move the institution check into its own method

--- validation/src/main/scala/hmda/validation/rules/ts/syntactical/S025.scala
+++ validation/src/main/scala/hmda/validation/rules/ts/syntactical/S025.scala
@@ -7,17 +7,23 @@ import hmda.validation.context.ValidationContext
 import hmda.validation.dsl.PredicateCommon._
 import hmda.validation.dsl.PredicateSyntax.PredicateOps
 import hmda.validation.dsl.{ Failure, Result, Success }
+import hmda.validation.rules.EditCheck
 
 object S025 {
 
-  def name = "S025"
-
-  def apply(ts: TransmittalSheet, ctx: ValidationContext): Result = {
-    compare(ts.respondent.id, ts.agencyCode, ctx)
+  def inContext(ctx: ValidationContext) = {
+    ctx.institution match {
+      case Some(inst) => new S025(inst)
+      case None => new EmptyEditCheck
+    }
   }
 
-  def apply(lar: LoanApplicationRegister, ctx: ValidationContext): Result = {
-    compare(lar.respondentId, lar.agencyCode, ctx)
+  def name = "S025" // this would go away too
+
+  // TODO the necessary things to make an EditCheck[T <: HasControlId] or whatever.
+  // after that, then the next two functions could go away entirely.
+  def apply(ts: TransmittalSheet, ctx: ValidationContext): Result = {
+    compare(ts.respondent.id, ts.agencyCode, ctx)
   }
 
   private def compare(filingRespId: String, filingAgencyCode: Int, ctx: ValidationContext): Result = {
@@ -36,4 +42,31 @@ object S025 {
     }
   }
 
+}
+
+class S025(institution: Institution) extends EditCheck[LoanApplicationRegister] {
+  def name = "S025"
+
+  def apply(ts: TransmittalSheet): Result = {
+    compare(ts.respondent.id, ts.agencyCode)
+  }
+
+  def apply(lar: LoanApplicationRegister): Result = {
+    compare(lar.respondentId, lar.agencyCode)
+  }
+
+  private def compare(filingRespId: String, filingAgencyCode: Int): Result = {
+    institution.respondentId match {
+      case Left(invalid) => new Failure()
+      case Right(validRespId) => {
+        (filingRespId is equalTo(validRespId.id)) and (filingAgencyCode is equalTo(institution.agency.value))
+      }
+    }
+  }
+}
+
+class EmptyEditCheck extends EditCheck[LoanApplicationRegister] {
+  def name = "empty"
+  def apply(lar: LoanApplicationRegister): Result = Success()
+  def apply(transmittalSheet: TransmittalSheet): Result = Success()
 }
--- validation/src/test/scala/hmda/validation/rules/ts/syntactical/S025Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/ts/syntactical/S025Spec.scala
@@ -76,28 +76,28 @@ class S025Spec extends WordSpec with MustMatchers {
     "succeed when institution is not present in ValidationContext" in {
       val ctx = ValidationContext(None)
 
-      S025(lar, ctx) mustBe Success()
+      S025.inContext(ctx).apply(lar) mustBe Success()
     }
 
     "fail when the Institution's respondent ID cannot be derived" in {
       val institution = Institution(1, "Test Bank", Set(ExternalId("111111", FdicCertNo), ExternalId("9876543-21", FederalTaxId)), CFPB, Bank, Active)
       val ctx = ValidationContext(Some(institution))
 
-      S025(lar, ctx) mustBe Failure()
+      S025.inContext(ctx).apply(lar) mustBe Failure()
     }
 
     "succeed when LAR's agency code and respondent ID match the Instititution's" in {
       val institution = Institution(1, "Test Bank", Set(ExternalId("999999", RssdId), ExternalId("9876543-21", FederalTaxId)), CFPB, Bank, Active)
       val ctx = ValidationContext(Some(institution))
 
-      S025(lar, ctx) mustBe Success()
+      S025.inContext(ctx).apply(lar) mustBe Success()
     }
 
     "fail when LAR's agency code and respondent ID do NOT match the Instititution's" in {
       val institution = Institution(1, "Test Bank", Set(ExternalId("111111", RssdId), ExternalId("9876543-21", FederalTaxId)), CFPB, Bank, Active)
       val ctx = ValidationContext(Some(institution))
 
-      S025(lar, ctx) mustBe Failure()
+      S025.inContext(ctx).apply(lar) mustBe Failure()
     }
   }
 
