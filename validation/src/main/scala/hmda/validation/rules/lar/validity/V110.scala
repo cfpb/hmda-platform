1c8fb9b7013af8bb90eca55092a33157df5edc94
L Dean <lgdean@users.noreply.github.com>
2016-08-08 18:25:34 -0700

    extract common Predicate for V105, V110, Q033

--- validation/src/main/scala/hmda/validation/rules/lar/validity/V110.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/validity/V110.scala
@@ -4,6 +4,7 @@ import hmda.model.fi.ts.TransmittalSheet
 import hmda.model.institution.Institution
 import hmda.model.institution.InstitutionType._
 import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateHmda._
 import hmda.validation.dsl.PredicateSyntax._
 import hmda.validation.dsl.Result
 import hmda.validation.rules.EditCheck
@@ -12,14 +13,8 @@ class V110(institution: Institution) extends EditCheck[TransmittalSheet] {
   override def name: String = "V110"
 
   override def apply(ts: TransmittalSheet): Result = {
-    val parent = ts.parent
-
     when(institution.institutionType is oneOf(DependentMortgageCompany, Affiliate)) {
-      (parent.name not empty) and
-        (parent.address not empty) and
-        (parent.city not empty) and
-        (parent.state not empty) and
-        (parent.zipCode not empty)
+      ts.parent is completeNameAndAddress
     }
   }
 }
--- validation/src/main/scala/hmda/validation/rules/ts/validity/V105.scala
+++ validation/src/main/scala/hmda/validation/rules/ts/validity/V105.scala
@@ -3,7 +3,7 @@ package hmda.validation.rules.ts.validity
 import hmda.model.fi.ts.TransmittalSheet
 import hmda.validation.dsl.Result
 import hmda.validation.rules.EditCheck
-import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateHmda._
 import hmda.validation.dsl.PredicateSyntax._
 
 /*
@@ -12,18 +12,7 @@ import hmda.validation.dsl.PredicateSyntax._
 object V105 extends EditCheck[TransmittalSheet] {
 
   override def apply(ts: TransmittalSheet): Result = {
-    val respondent = ts.respondent
-    val respName = respondent.name
-    val respAddress = respondent.address
-    val respCity = respondent.city
-    val respState = respondent.state
-    val respZipCode = respondent.zipCode
-
-    (respName not empty) and
-      (respAddress not empty) and
-      (respCity not empty) and
-      (respState not empty) and
-      (respZipCode not empty)
+    ts.respondent is completeNameAndAddress
   }
 
   override def name: String = "V105"
