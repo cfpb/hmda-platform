af01bf063c3ed8aa8974aac82df340c94efd7058
L Dean <lgdean@users.noreply.github.com>
2016-08-11 09:46:44 -0700

    add V110 to TsValidityEngine

--- validation/src/main/scala/hmda/validation/engine/ts/TsEngine.scala
+++ validation/src/main/scala/hmda/validation/engine/ts/TsEngine.scala
@@ -20,7 +20,7 @@ trait TsEngine extends TsSyntacticalEngine with TsValidityEngine with TsQualityE
       fs <- fSyntactical
     } yield {
       (
-        checkValidity(ts)
+        checkValidity(ts, ctx)
         |@| fs
         |@| checkQuality(ts)
       )((_, _, _) => ts)
