49ad486fbf0691bfffe8edca6957cbc4d3c7d5a5
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-08-15 10:50:21 -0400

    remove persistRawData and ununsed imports

--- persistence/src/test/scala/hmda/persistence/processing/HmdaFileParserSpec.scala
+++ persistence/src/test/scala/hmda/persistence/processing/HmdaFileParserSpec.scala
@@ -1,17 +1,14 @@
 package hmda.persistence.processing
 
-import java.io.File
 import java.time.Instant
 
 import akka.testkit.TestProbe
 import com.typesafe.config.ConfigFactory
 import hmda.actor.test.ActorSpec
-import hmda.model.fi.lar.LoanApplicationRegister
 import hmda.parser.fi.lar.LarCsvParser
 import hmda.persistence.CommonMessages.GetState
 import hmda.persistence.processing.HmdaRawFile._
 import hmda.persistence.processing.HmdaFileParser._
-import org.iq80.leveldb.util.FileUtils
 
 class HmdaFileParserSpec extends ActorSpec {
   import hmda.model.util.FITestData._
@@ -19,8 +16,6 @@ class HmdaFileParserSpec extends ActorSpec {
   val config = ConfigFactory.load()
 
   val submissionId = "12345-2017-1"
-
-  val hmdaFileUpload = createHmdaRawFile(system, submissionId)
 
   val hmdaFileParser = createHmdaFileParser(system, submissionId)
 
@@ -31,7 +26,6 @@ class HmdaFileParserSpec extends ActorSpec {
   val badLines = fiCSVParseError.split("\n")
 
   override def beforeAll() {
-    persistRawData()
     super.beforeAll()
   }
 
@@ -55,12 +49,6 @@ class HmdaFileParserSpec extends ActorSpec {
     lars.map {
       case Right(l) => probe.send(hmdaFileParser, LarParsed(l))
       case Left(errors) => probe.send(hmdaFileParser, LarParsedErrors(errors))
-    }
-  }
-
-  private def persistRawData(): Unit = {
-    for (line <- lines) {
-      probe.send(hmdaFileUpload, AddLine(timestamp, line.toString))
     }
   }
 
