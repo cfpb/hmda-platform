8d1516c5c23b88fca941e7eb367e9405c2c7ecf9
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-08-17 14:12:45 -0400

    Change ValidationError to ADT

--- validation/src/main/scala/hmda/validation/engine/ValidationError.scala
+++ validation/src/main/scala/hmda/validation/engine/ValidationError.scala
@@ -1,9 +1,14 @@
 package hmda.validation.engine
 
-case class ValidationError(id: String, msg: String)
-case class ValidationErrors(errors: Seq[ValidationError])
-object ValidationErrors {
-  def empty(): ValidationErrors = {
-    ValidationErrors(Nil)
-  }
+sealed trait ValidationError {
+  def id: String
+  def msg: String
 }
+case class SyntacticalError(id: String, msg: String) extends ValidationError
+case class ValidityError(id: String, msg: String) extends ValidationError
+case class QualityError(id: String, msg: String) extends ValidationError
+case class MacroError(id: String, msg: String) extends ValidationError
+
+case class ValidationErrors(errors: Seq[ValidationError])
+
+
