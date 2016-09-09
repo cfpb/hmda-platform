9ca5e04faa259ba40b0f9dbeca4983e761cdee99
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-09-09 13:09:14 -0400

    Add HmdaActor trait

---
+++ persistence/src/main/scala/hmda/persistence/processing/HmdaActor.scala
@@ -0,0 +1,15 @@
+package hmda.persistence.processing
+
+import akka.actor.{ Actor, ActorLogging }
+
+trait HmdaActor extends Actor with ActorLogging {
+
+  override def preStart(): Unit = {
+    log.debug(s"Actor started at ${self.path}")
+  }
+
+  override def postStop(): Unit = {
+    log.debug(s"Actor stopped at ${self.path}")
+  }
+
+}
