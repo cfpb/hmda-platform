12ff218b099a18aac0a0d69db0c1e9412e7929da
L Dean <lgdean@users.noreply.github.com>
2016-09-12 15:50:26 -0700

    simplify logic with match/case
    also change the whitespace to make the "when" consequences line up.
    the indentation was changing anyway, so might as well do it all at once.

--- validation/src/main/scala/hmda/validation/rules/lar/quality/Q030.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/quality/Q030.scala
@@ -6,7 +6,7 @@ import hmda.validation.context.ValidationContext
 import hmda.validation.dsl.PredicateCommon._
 import hmda.validation.dsl.PredicateGeo._
 import hmda.validation.dsl.PredicateSyntax._
-import hmda.validation.dsl.Result
+import hmda.validation.dsl.{ Failure, Result, Success }
 import hmda.validation.rules.{ EditCheck, IfInstitutionPresentIn }
 
 class Q030 private (institution: Institution) extends EditCheck[LoanApplicationRegister] {
@@ -15,37 +15,28 @@ class Q030 private (institution: Institution) extends EditCheck[LoanApplicationR
   override def apply(lar: LoanApplicationRegister): Result = {
     when(lar.actionTakenType is containedIn(1 to 6)) {
       val geo: Geography = lar.geography
-      when(geo is Geography("NA", "NA", "NA", "NA")) {
-        institution.cra is false
-      } and
-        when(geo.state is "NA") {
-          geo.county is "NA"
-        } and
-        when(geo.county is "NA") {
-          geo.state is "NA"
-        } and
-        when(institution.cra is true) {
-          (geo.state not "NA") and (geo.county not "NA")
-        } and
-        when(institution.cra is false) {
-          when(geo is smallCounty) {
-            geo.tract is "NA"
-          }
-        } and
-        when(geo.tract is "NA") {
-          (geo is smallCounty) or (geo.county is "NA")
-        } and
-        when(geo.msa not "NA") {
-          (geo is validStateCountyMsaCombination) and
-            when(geo.tract not "NA") {
-              geo is validCompleteCombination
+      (geo, institution.cra) match {
+        case (Geography("NA", "NA", "NA", "NA"), false) => Success()
+        case (Geography(_, "NA", _, _), _) => Failure()
+        case (Geography(_, _, "NA", _), _) => Failure()
+        case _ =>
+          when(institution.cra is false) {
+            when(geo is smallCounty) {
+              geo.tract is "NA"
             }
-        } and
-        when(geo.msa is "NA") {
-          when(geo.tract not "NA") {
-            geo is validStateCountyTractCombination
+          } and when(geo.tract is "NA") {
+            geo is smallCounty // and therefore also a valid state/county combination; no need to check again.
+          } and when(geo.msa not "NA") {
+            (geo is validStateCountyMsaCombination) and
+              when(geo.tract not "NA") {
+                geo is validCompleteCombination
+              }
+          } and when(geo.msa is "NA") {
+            when(geo.tract not "NA") {
+              geo is validStateCountyTractCombination
+            }
           }
-        }
+      }
     }
   }
 }
