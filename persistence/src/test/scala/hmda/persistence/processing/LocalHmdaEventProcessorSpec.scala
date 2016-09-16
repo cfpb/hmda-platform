efece3485a7e6193ae0ea7d4c6d7de3fca443a5e
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-09-16 09:14:52 -0400

    Add submission status test

--- persistence/src/test/scala/hmda/persistence/processing/LocalHmdaEventProcessorSpec.scala
+++ persistence/src/test/scala/hmda/persistence/processing/LocalHmdaEventProcessorSpec.scala
@@ -1,15 +1,25 @@
 package hmda.api.processing
 
-import akka.actor.ActorSystem
+import akka.actor.{ ActorRef, ActorSystem }
 import akka.testkit.{ EventFilter, TestProbe }
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent._
+import scala.concurrent.duration._
 import com.typesafe.config.ConfigFactory
 import hmda.actor.test.ActorSpec
 import hmda.persistence.processing.LocalHmdaEventProcessor._
-import hmda.model.fi.SubmissionId
-import hmda.persistence.CommonMessages.Event
+import hmda.model.fi._
+import hmda.persistence.CommonMessages.{ Event, GetState }
 import hmda.persistence.processing.HmdaFileParser.{ ParsingCompleted, ParsingStarted }
 import hmda.persistence.processing.HmdaFileValidator.{ ValidationCompleted, ValidationCompletedWithErrors, ValidationStarted }
 import hmda.persistence.processing.HmdaRawFile.{ UploadCompleted, UploadStarted }
+import hmda.persistence.HmdaSupervisor._
+import hmda.persistence.institutions.SubmissionPersistence
+import hmda.persistence.processing.LocalHmdaEventProcessor
+import hmda.persistence.institutions.SubmissionPersistence._
+import org.scalatest.Assertion
 
 class LocalHmdaEventProcessorSpec extends ActorSpec {
 
@@ -30,16 +40,35 @@ class LocalHmdaEventProcessorSpec extends ActorSpec {
       )
     )
 
+  val duration = 5.seconds
+  implicit val timeout = Timeout(duration)
+  implicit val ec = system.dispatcher
+
   val probe = TestProbe()
 
-  val eventProcessor = createLocalHmdaEventProcessor(system)
+  val supervisor = createSupervisor(system)
+  val fEventProcessor = (supervisor ? FindActorByName(LocalHmdaEventProcessor.name)).mapTo[ActorRef]
+  val eventProcessor = Await.result(fEventProcessor, duration)
+
+  val submissionId = SubmissionId("0", "2017", 1)
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    val fSubmissions = (supervisor ? FindSubmissions(SubmissionPersistence.name, submissionId.institutionId, submissionId.period)).mapTo[ActorRef]
+    for {
+      s <- fSubmissions
+    } yield {
+      s ! CreateSubmission
+    }
+  }
 
   "Event processor" must {
-    val submissionId = SubmissionId("0", "2017", 1)
 
     "process upload start message from event stream" in {
       val msg = s"Upload started for submission $submissionId"
-      checkEventStreamMessage(msg, UploadStarted(submissionId))
+      val status = UploadStarted(submissionId)
+      checkEventStreamMessage(msg, status)
+      checkSubmissionStatus(Uploading)
     }
 
     "process upload completed message from event stream" in {
@@ -71,7 +100,21 @@ class LocalHmdaEventProcessorSpec extends ActorSpec {
     "process validation completed from event stream" in {
       val msg = s"validation completed for submission $submissionId"
       checkEventStreamMessage(msg, ValidationCompleted(submissionId))
+      checkSubmissionStatus(Validated)
     }
+  }
+
+  def checkSubmissionStatus(status: SubmissionStatus): Assertion = {
+    //TODO: find a way to avoid having to sleep here
+    Thread.sleep(500)
+    val fSubmissions = (supervisor ? FindSubmissions(SubmissionPersistence.name, submissionId.institutionId, submissionId.period)).mapTo[ActorRef]
+    val fSubmissionStatus = for {
+      a <- fSubmissions
+      b <- (a ? GetState).mapTo[Seq[Submission]]
+    } yield b.head.submissionStatus
+
+    val submissionStatus = Await.result(fSubmissionStatus, duration)
+    submissionStatus mustBe status
   }
 
   private def checkEventStreamMessage(msg: String, event: Event): Unit = {
@@ -79,5 +122,10 @@ class LocalHmdaEventProcessorSpec extends ActorSpec {
     EventFilter.debug(msg, source = actorSource) intercept {
       system.eventStream.publish(event)
     }
+  }
+
+  override def afterAll(): Unit = {
+    Thread.sleep(2000)
+    system.terminate()
   }
 }
