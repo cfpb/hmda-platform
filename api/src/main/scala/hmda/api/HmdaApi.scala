aebf6685fd9e0098944d3f506031d61e6d5195c1
Nick Grippin <ngrippin@discovertechnologies.com>
2016-09-22 08:47:05 -0400

    remove weird import thing

--- api/src/main/scala/hmda/api/HmdaApi.scala
+++ api/src/main/scala/hmda/api/HmdaApi.scala
@@ -8,7 +8,7 @@ import akka.pattern.ask
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
-import hmda.api.http.{ InstitutionsHttpApi, _ }
+import hmda.api.http._
 import hmda.persistence.HmdaSupervisor
 import hmda.persistence.HmdaSupervisor.FindActorByName
 import hmda.persistence.demo.DemoData
