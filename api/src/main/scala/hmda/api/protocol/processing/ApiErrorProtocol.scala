0cb929cad6107398764ba90ad902611e4ebf7910
Betsy Lorton <elorton@gmail.com>
2016-10-06 11:24:13 -0400

    Silence compiler warning on JSON serializer
    The warning stated: "match may not be exhaustive.
    It would fail on the following inputs: JsArray(_), JsFalse,
    JsNull, JsNumber(_), JsObject(_), JsTrue"

--- api/src/main/scala/hmda/api/protocol/processing/ApiErrorProtocol.scala
+++ api/src/main/scala/hmda/api/protocol/processing/ApiErrorProtocol.scala
@@ -2,7 +2,7 @@ package hmda.api.protocol.processing
 
 import akka.http.scaladsl.model.Uri.Path
 import hmda.api.model.ErrorResponse
-import spray.json.{ DefaultJsonProtocol, JsString, JsValue, RootJsonFormat }
+import spray.json.{ DefaultJsonProtocol, DeserializationException, JsString, JsValue, RootJsonFormat }
 
 trait ApiErrorProtocol extends DefaultJsonProtocol {
   implicit object PathJsonFormat extends RootJsonFormat[Path] {
@@ -10,6 +10,7 @@ trait ApiErrorProtocol extends DefaultJsonProtocol {
     override def read(path: JsValue): Path =
       path match {
         case JsString(s) => Path(s)
+        case _ => throw new DeserializationException("Unable to deserialize")
       }
   }
 
