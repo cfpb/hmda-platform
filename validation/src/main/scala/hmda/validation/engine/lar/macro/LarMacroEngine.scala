ecd68f6212272c14e0eaa42908b4f8f7e4f0c6df
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-10-15 08:54:37 -0400

    Add Q008

--- validation/src/main/scala/hmda/validation/engine/lar/macro/LarMacroEngine.scala
+++ validation/src/main/scala/hmda/validation/engine/lar/macro/LarMacroEngine.scala
@@ -6,7 +6,8 @@ import hmda.validation.engine.{ Macro, ValidationErrorType }
 import hmda.validation.engine.lar.LarCommonEngine
 import hmda.validation.rules.AggregateEditCheck
 import hmda.validation.rules.lar.`macro`.MacroEditTypes.LoanApplicationRegisterSource
-import hmda.validation.rules.lar.`macro`.Q007
+import hmda.validation.rules.lar.`macro`.{ Q007, Q008 }
+
 import scala.concurrent.{ ExecutionContext, Future }
 
 trait LarMacroEngine extends LarCommonEngine with ValidationApi {
@@ -14,7 +15,8 @@ trait LarMacroEngine extends LarCommonEngine with ValidationApi {
   def checkMacro(larSource: LoanApplicationRegisterSource)(implicit ec: ExecutionContext): Future[LarSourceValidation] = {
     Future.sequence(
       List(
-        Q007
+        Q007,
+        Q008
       ).map(checkAggregate(_, larSource, "", Macro))
     )
       .map(checks => validateAll(checks, larSource))
