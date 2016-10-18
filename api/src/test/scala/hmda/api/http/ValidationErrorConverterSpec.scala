398870ab3d611816d456fe3aac69e37c5245684b
Nick Grippin <ngrippin@discovertechnologies.com>
2016-10-18 12:13:06 -0400

    updated tests

--- api/src/test/scala/hmda/api/http/ValidationErrorConverterSpec.scala
+++ api/src/test/scala/hmda/api/http/ValidationErrorConverterSpec.scala
@@ -2,11 +2,14 @@ package hmda.api.http
 
 import hmda.api.model._
 import hmda.model.fi.lar.LoanApplicationRegister
+import hmda.model.fi.ts.TransmittalSheet
 import hmda.model.util.FITestData._
 import hmda.parser.fi.lar.LarCsvParser
+import hmda.parser.fi.ts.TsCsvParser
 import hmda.validation.context.ValidationContext
-import hmda.validation.engine.{ Macro, Quality, Syntactical, Validity }
+import hmda.validation.engine._
 import hmda.validation.engine.lar.LarEngine
+import hmda.validation.engine.ts.TsEngine
 import org.scalatest.{ MustMatchers, WordSpec }
 
 class ValidationErrorConverterSpec extends WordSpec with MustMatchers with ValidationErrorConverter with LarEngine {
@@ -15,18 +18,21 @@ class ValidationErrorConverterSpec extends WordSpec with MustMatchers with Valid
     "be converted to edit check summary" in {
       val badLars: Seq[LoanApplicationRegister] = fiCSVEditErrors.split("\n").tail.map(line => LarCsvParser(line).right.get)
       val ctx = ValidationContext(None, Some(2017))
-      val errors = badLars.flatMap(lar => validationErrors(lar, ctx, validateLar).errors)
+      val larErrors = badLars.flatMap(lar => validationErrors(lar, ctx, validateLar).errors)
+
+      val tsErrors = Seq(ValidationError("8299422144", "S020", Syntactical))
+
       val syntacticalEditResults =
-        validationErrorsToEditResults(Nil, errors, Syntactical)
+        validationErrorsToEditResults(tsErrors, larErrors, Syntactical)
       val validityEditResults =
-        validationErrorsToEditResults(Nil, errors, Validity)
+        validationErrorsToEditResults(tsErrors, larErrors, Validity)
       val qualityEditResults =
-        validationErrorsToEditResults(Nil, errors, Quality)
+        validationErrorsToEditResults(tsErrors, larErrors, Quality)
       val macroEditResults =
-        validationErrorsToEditResults(Nil, errors, Macro)
+        validationErrorsToEditResults(tsErrors, larErrors, Macro)
       val summaryEditResults = SummaryEditResults(syntacticalEditResults, validityEditResults, qualityEditResults, macroEditResults)
 
-      val s020 = EditResult("S020", ts = false, Seq(LarEditResult(LarId("8299422144")), LarEditResult(LarId("2185751599"))))
+      val s020 = EditResult("S020", ts = true, Seq(LarEditResult(LarId("8299422144")), LarEditResult(LarId("2185751599"))))
       val s010 = EditResult("S010", ts = false, Seq(LarEditResult(LarId("2185751599"))))
       summaryEditResults.syntactical.edits.head mustBe s020
       summaryEditResults.syntactical.edits.tail.contains(s010) mustBe true
--- api/src/test/scala/hmda/api/http/institutions/SubmissionPathsSpec.scala
+++ api/src/test/scala/hmda/api/http/institutions/SubmissionPathsSpec.scala
@@ -78,7 +78,7 @@ class SubmissionPathsSpec extends InstitutionHttpApiSpec {
     val expectedSummary = SummaryEditResults(
       EditResults(
         List(
-          EditResult("S020", ts = false, List(LarEditResult(LarId("loan1")))),
+          EditResult("S020", ts = true, List(LarEditResult(LarId("loan1")))),
           EditResult("S010", ts = false, List(LarEditResult(LarId("loan1"))))
         )
       ),
@@ -126,12 +126,15 @@ class SubmissionPathsSpec extends InstitutionHttpApiSpec {
     val v1 = ValidationError("loan1", "V280", Validity)
     val v2 = ValidationError("loan2", "V285", Validity)
     val v3 = ValidationError("loan3", "V285", Validity)
-    val validationErrors = LarValidationErrors(Seq(s1, s2, v1, v2, v3))
+    val larValidationErrors = LarValidationErrors(Seq(s1, s2, v1, v2, v3))
+
+    val tsValidationErrors = TsValidationErrors(Seq(s2))
 
     val fValidate: Future[Unit] = for {
       h <- fHmdaValidator
     } yield {
-      h ! validationErrors
+      h ! larValidationErrors
+      h ! tsValidationErrors
     }
 
   }
