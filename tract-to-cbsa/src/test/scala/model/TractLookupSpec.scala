8e6222c5617954453168144336aff9e57637b4df
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-10-31 16:27:47 -0400

    add test files

---
+++ tract-to-cbsa/src/test/scala/model/TractLookupSpec.scala
@@ -0,0 +1,23 @@
+package model
+
+import org.scalatest.{ MustMatchers, WordSpec }
+
+class TractLookupSpec extends WordSpec with MustMatchers {
+
+  "State Abrv Lookup" must {
+
+    "have properly lengthed values" in {
+      val lookup = TractLookup.values
+      lookup.forall(tract => tract.state.length == 2) mustBe true
+      lookup.forall(tract => tract.county.length == 3) mustBe true
+      lookup.forall(tract => tract.key.length == 5) mustBe true
+      lookup.forall(tract => tract.tract.length == 6) mustBe true
+      lookup.forall(tract => tract.tractDec.length == 7) mustBe true
+    }
+
+    "have the correct number of tracts" in {
+      val lookup = TractLookup.values
+      lookup.size mustBe 74002
+    }
+  }
+}
