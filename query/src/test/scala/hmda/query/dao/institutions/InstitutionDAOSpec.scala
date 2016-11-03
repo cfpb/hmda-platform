cda65d5905158131c3f8c58d40aeb9cc23aaaa23
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-11-03 16:30:36 -0400

    Implement cake pattern for institutions dao

--- query/src/test/scala/hmda/query/dao/institutions/InstitutionDAOSpec.scala
+++ query/src/test/scala/hmda/query/dao/institutions/InstitutionDAOSpec.scala
@@ -1,20 +1,20 @@
-package hmda.query.sql.institutions
+package hmda.query.dao.institutions
 
+import hmda.query.sql.institutions.Institutions
 import org.scalatest.{ AsyncWordSpec, BeforeAndAfterAll, MustMatchers }
+import slick.driver.H2Driver
 import slick.driver.H2Driver.api._
 import slick.jdbc.meta.MTable
 import hmda.query.model.institutions.InstitutionQueryGenerators._
-import scala.concurrent.ExecutionContext.Implicits.global
 
-class InstitutionsSpec extends AsyncWordSpec with MustMatchers with BeforeAndAfterAll {
+class InstitutionDAOSpec extends AsyncWordSpec with MustMatchers with BeforeAndAfterAll with InstitutionDAO with H2Driver {
 
   var db: Database = _
-
   val institutions = TableQuery[Institutions]
 
   override def beforeAll(): Unit = {
     super.beforeAll()
-    db = Database.forConfig("h2mem1")
+    db = Database.forConfig("h2mem")
   }
 
   override def afterAll(): Unit = {
@@ -25,29 +25,23 @@ class InstitutionsSpec extends AsyncWordSpec with MustMatchers with BeforeAndAft
   "Institutions" must {
     "create schema" in {
       val fTables = for {
-        s <- createSchema()
+        s <- db.run(createSchema())
         tables <- db.run(MTable.getTables)
       } yield tables
 
       fTables.map { tables =>
         tables.size mustBe 1
-        tables.count(_.name.name.equalsIgnoreCase("institutions")) mustBe 1
       }
-
     }
 
     "insert new institution" in {
       val i = createInstitution()
-      val fInsert = db.run(institutions += i)
+      val fInsert = db.run(save(i))
 
       fInsert.map { x =>
         x mustBe 1
       }
     }
-  }
-
-  private def createSchema() = {
-    db.run(institutions.schema.create)
   }
 
   private def createInstitution() = {
