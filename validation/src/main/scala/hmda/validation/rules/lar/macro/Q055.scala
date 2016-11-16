0b226be6f084578b16d98a2fa198c84845163a58
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2016-11-16 11:22:37 -0500

    add Q055 and Q061

---
+++ validation/src/main/scala/hmda/validation/rules/lar/macro/Q055.scala
@@ -0,0 +1,38 @@
+package hmda.validation.rules.lar.`macro`
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+import hmda.model.fi.lar.LoanApplicationRegister
+import hmda.validation.dsl.Result
+import hmda.validation.rules.AggregateEditCheck
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+import hmda.validation.rules.lar.`macro`.MacroEditTypes._
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+object Q055 extends AggregateEditCheck[LoanApplicationRegisterSource, LoanApplicationRegister] {
+
+  val config = ConfigFactory.load()
+  val multiplier = config.getDouble("hmda.validation.macro.Q055.numOfLarsMultiplier")
+
+  override def name = "Q055"
+
+  override def apply(lars: LoanApplicationRegisterSource)(implicit system: ActorSystem, materializer: ActorMaterializer, ec: ExecutionContext): Future[Result] = {
+
+    val hoepaLoans =
+      count(lars.filter(lar => lar.hoepaStatus == 1 && lar.actionTakenType == 1 && lar.rateSpread != "NA")
+        .filter(lar => lar.rateSpread.toInt >= 5))
+
+    val total = count(lars)
+
+    for {
+      h <- hoepaLoans
+      t <- total
+    } yield {
+      h.toDouble is lessThanOrEqual(t * multiplier)
+    }
+
+  }
+}
