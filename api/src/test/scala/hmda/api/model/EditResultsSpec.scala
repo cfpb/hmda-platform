22d3342bc6d3c15e6ba3567930f0e7c7fccbc311
Nick Grippin <ngrippin@discovertechnologies.com>
2016-12-21 14:36:59 -0500

    updated

--- api/src/test/scala/hmda/api/model/EditResultsSpec.scala
+++ api/src/test/scala/hmda/api/model/EditResultsSpec.scala
@@ -4,9 +4,9 @@ import org.scalatest.{ MustMatchers, PropSpec }
 import org.scalatest.prop.PropertyChecks
 
 class EditResultsSpec extends PropSpec with PropertyChecks with MustMatchers {
-  val e1 = EditResult("V001", ts = true, Seq(LarEditResult(LarId("abc")), LarEditResult(LarId("def"))))
-  val e2 = EditResult("S001", ts = false, Seq(LarEditResult(LarId("ghi")), LarEditResult(LarId("jkl"))))
-  val m1 = MacroResult("Q001", Seq.empty)
+  val e1 = EditResult("V001", "", ts = true, Seq(LarEditResult(LarId("abc")), LarEditResult(LarId("def"))))
+  val e2 = EditResult("S001", "", ts = false, Seq(LarEditResult(LarId("ghi")), LarEditResult(LarId("jkl"))))
+  val m1 = MacroResult("Q001", Set.empty)
 
   val sum = SummaryEditResults(EditResults(Seq(e2)), EditResults(Seq(e1)), EditResults.empty, MacroResults(Seq(m1)))
 
