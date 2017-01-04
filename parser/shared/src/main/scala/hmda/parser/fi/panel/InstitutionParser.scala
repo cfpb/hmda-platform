c5188f71780144e07119047fb25215d790903c70
Nick Grippin <ngrippin@discovertechnologies.com>
2017-01-04 12:12:28 -0500

    test parser engine created

---
+++ parser/shared/src/main/scala/hmda/parser/fi/panel/InstitutionParser.scala
@@ -0,0 +1,107 @@
+package hmda.parser.fi.panel
+
+import hmda.model.institution.Agency._
+import hmda.model.institution.DepositoryType.{ Depository, NonDepository }
+import hmda.model.institution.ExternalIdType._
+import hmda.model.institution.InstitutionType._
+import hmda.model.institution.{ Agency, ExternalId, Institution, InstitutionType }
+
+object InstitutionParser {
+  def apply(s: String): Institution = {
+    val values = (s + " ").split('|').map(_.trim)
+    val agency = convertStringToAgency(values(2))
+    val institutionType = convertIntToInstitutionType(values(3).toInt)
+    Institution(
+      "0", //How do we determine internal id?
+      agency,
+      values(0).toInt,
+      convertStringToExternalId(values(1), institutionType, agency),
+      institutionType,
+      stringToBoolean(values(4)),
+      getExternalIdSet(values),
+      values(10),
+      values(11),
+      values(12),
+      values(13),
+      values(14),
+      values(15),
+      values(16),
+      stringToBoolean(values(17)),
+      values(18),
+      values(19).toInt,
+      values(20),
+      values(21),
+      values(22),
+      values(23).toInt,
+      values(24).toInt,
+      values(25).toInt,
+      values(26),
+      values(27),
+      values(28),
+      values(29)
+    )
+  }
+
+  private def stringToBoolean(s: String): Boolean = {
+    s match {
+      case "1" => true
+      case "0" => false
+    }
+  }
+
+  private def convertStringToAgency(s: String): Agency = {
+    s match {
+      case "9" => CFPB
+      case "3" => FDIC
+      case "2" => FRS
+      case "7" => HUD
+      case "5" => NCUA
+      case "1" => OCC
+    }
+  }
+
+  private def getExternalIdSet(values: Array[String]): Set[ExternalId] = {
+    Set(
+      ExternalId(values(5), FederalTaxId),
+      ExternalId(values(6), RssdId),
+      ExternalId(values(7), FdicCertNo),
+      ExternalId(values(8), NcuaCharterId),
+      ExternalId(values(9), OccCharterId)
+    )
+  }
+
+  private def convertStringToExternalId(s: String, i: InstitutionType, a: Agency): ExternalId = {
+    i.depositoryType match {
+      case Depository =>
+        (a: @unchecked) match {
+          case OCC => ExternalId(s, OccCharterId)
+          case NCUA => ExternalId(s, NcuaCharterId)
+          case FDIC => ExternalId(s, FdicCertNo)
+          case FRS => ExternalId(s, RssdId)
+          case CFPB => ExternalId(s, RssdId)
+        }
+      case NonDepository =>
+        a match {
+          case FRS => ExternalId(s, RssdId)
+          case HUD => ExternalId(s, FederalTaxId) //Special cases here, but may not be necessary to fix
+          case _ => ExternalId(s, FederalTaxId)
+        }
+    }
+  }
+
+  private def convertIntToInstitutionType(i: Int): InstitutionType = {
+    if (Set(1, 2, 3, 7).contains(i)) {
+      Bank
+    } else if (Set(4, 5).contains(i)) {
+      SavingsAndLoan
+    } else if (i == 6) {
+      CreditUnion
+    } else if (i == 8) {
+      Affiliate
+    } else if (i == 14) {
+      IndependentMortgageCompany
+    } else {
+      MBS
+    }
+  }
+}
--- parser/shared/src/main/scala/hmda/parser/fi/panel/PanelCsvParser.scala
+++ parser/shared/src/main/scala/hmda/parser/fi/panel/PanelCsvParser.scala
@@ -1,106 +1,33 @@
 package hmda.parser.fi.panel
 
-import hmda.model.institution.Agency._
-import hmda.model.institution.DepositoryType.{Depository, NonDepository}
-import hmda.model.institution.ExternalIdType._
-import hmda.model.institution.InstitutionType._
-import hmda.model.institution.{Agency, ExternalId, Institution, InstitutionType}
+import javax.swing.JFileChooser
+import javax.swing.filechooser.FileNameExtensionFilter
 
-/**
-  * Created by grippinn on 1/3/17.
-  */
-object PanelCsvParser {
-  def apply(s: String): Institution = {
-    val values = (s + " ").split('|').map(_.trim)
-    val agency = convertStringToAgency(values(1))
-    val institutionType = convertIntToInstitutionType(values(4).toInt)
-    Institution(
-      values(0),
-      agency,
-      values(2).toInt,
-      convertStringToExternalId(values(3), institutionType, agency),
-      institutionType,
-      values(5).toBoolean,
-      getExternalIdSet(values, institutionType, agency),
-      values(11),
-      values(12),
-      values(13),
-      values(14),
-      values(15),
-      values(16),
-      values(17),
-      values(18).toBoolean,
-      values(19),
-      values(20).toInt,
-      values(21),
-      values(22),
-      values(23),
-      values(24).toInt,
-      values(25).toInt,
-      values(26).toInt,
-      values(27),
-      values(28),
-      values(29),
-      values(30)
-    )
+import scala.io.Source
+import scala.util.Try
+
+object PanelCsvParser extends App {
+  val file = pickFile()
+  val lines = file.getLines().toList.tail
+
+  for (line <- lines) {
+    val i = InstitutionParser(line)
+    println(i)
   }
 
-  private def convertStringToAgency(s: String): Agency = {
-    s match {
-      case "9" => CFPB
-      case "3" => FDIC
-      case "2" => FRS
-      case "7" => HUD
-      case "5" => NCUA
-      case "1" => OCC
-    }
-  }
+  def pickFile(): Source = {
+    val chooser = new JFileChooser()
+    chooser.setCurrentDirectory(new java.io.File("."))
+    chooser.setDialogTitle("Input File")
+    chooser.setFileSelectionMode(JFileChooser.FILES_ONLY)
+    chooser.setFileFilter(new FileNameExtensionFilter("csv", "csv"))
 
-  private def getExternalIdSet(values: Array[String], i: InstitutionType, a: Agency): Set[ExternalId] = {
-    Set(convertStringToExternalId(values(6), i, a),
-      convertStringToExternalId(values(7), i, a),
-      convertStringToExternalId(values(8), i, a),
-      convertStringToExternalId(values(9), i, a),
-      convertStringToExternalId(values(10), i, a))
-  }
-
-  private def convertStringToExternalId(s: String, i: InstitutionType, a: Agency): ExternalId = {
-    i.depositoryType match {
-      case Depository =>
-        a match {
-          case OCC => ExternalId(s, OccCharterId)
-          case NCUA => ExternalId(s, NcuaCharterId)
-          case FDIC => ExternalId(s, FdicCertNo)
-          case FRS => ExternalId(s, RssdId)
-          case CFPB => ExternalId(s, RssdId)
-        }
-      case NonDepository =>
-        a match {
-          case FRS => ExternalId(s, RssdId)
-          case HUD => ExternalId(s, FederalTaxId) //Special case, may not be necessary to fix
-          case _ => ExternalId(s, FederalTaxId)
-        }
-    }
-  }
-
-  private def convertIntToInstitutionType(i: Int): InstitutionType = {
-    if(Set(1, 2, 3, 7).contains(i)) {
-      Bank
-    }
-    else if(Set(4, 5).contains(i)) {
-      SavingsAndLoan
-    }
-    else if(i == 6) {
-      CreditUnion
-    }
-    else if (i == 8) {
-      Affiliate
-    }
-    else if(i == 14) {
-      IndependentMortgageCompany
-    }
-    else {
-      MBS
+    if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION &&
+      Try(Source.fromFile(chooser.getSelectedFile)).isSuccess) {
+      Source.fromFile(chooser.getSelectedFile)
+    } else {
+      println("\nWARNING: Unable to read file input.")
+      Source.fromFile(".")
     }
   }
 }
