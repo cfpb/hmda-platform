15e3b72c1ef02d1e26cd036e3e423e084338e460
Nick Grippin <ngrippin@discovertechnologies.com>
2017-01-04 15:27:05 -0500

    institutions model updated

--- model/shared/src/test/scala/hmda/model/institution/InstitutionGenerators.scala
+++ model/shared/src/test/scala/hmda/model/institution/InstitutionGenerators.scala
@@ -7,13 +7,38 @@ object InstitutionGenerators {
   implicit def institutionGen: Gen[Institution] = {
     for {
       id <- Gen.alphaStr
-      name <- Gen.alphaStr
-      externalIds <- Gen.listOf(externalIdGen)
       agency <- agencyGen
-      active <- Gen.oneOf(true, false)
-      cra <- Gen.oneOf(true, false)
+      activityYear <- Gen.choose(2000, 2020)
+      respondentId <- externalIdGen
       institutionType <- institutionTypeGen
-    } yield Institution(id, name, externalIds.toSet, agency, institutionType, active, cra)
+      cra <- Gen.oneOf(true, false)
+      externalIds <- Gen.listOf(externalIdGen)
+      emailDomain2015 <- Gen.alphaStr
+      emailDomain2014 <- Gen.alphaStr
+      emailDomain2013 <- Gen.alphaStr
+      respondentName <- Gen.alphaStr
+      respondentState <- Gen.alphaStr
+      respondentCity <- Gen.alphaStr
+      respondentFipsStateNumber <- Gen.alphaStr
+      hmdaFilerFlag <- Gen.oneOf(true, false)
+      parentRespondentId <- Gen.alphaStr
+      parentIdRssd <- Gen.numStr
+      parentName <- Gen.alphaStr
+      parentCity <- Gen.alphaStr
+      parentState <- Gen.alphaStr
+      assets <- Gen.numStr
+      otherLenderCode <- Gen.numStr
+      topHolderIdRssd <- Gen.numStr
+      topHolderName <- Gen.alphaStr
+      topHolderCity <- Gen.alphaStr
+      topHolderState <- Gen.alphaStr
+      topHolderCountry <- Gen.alphaStr
+    } yield Institution(id, agency, activityYear, respondentId, institutionType, cra = cra,
+      externalIds.toSet, emailDomain2015, emailDomain2014, emailDomain2013, respondentName,
+      respondentState, respondentCity, respondentFipsStateNumber, hmdaFilerFlag = hmdaFilerFlag,
+      parentRespondentId, parentIdRssd.toInt, parentName, parentCity, parentState, assets.toInt,
+      otherLenderCode.toInt, topHolderIdRssd.toInt, topHolderName, topHolderCity, topHolderState,
+      topHolderCountry)
   }
 
   implicit def agencyGen: Gen[Agency] = {
