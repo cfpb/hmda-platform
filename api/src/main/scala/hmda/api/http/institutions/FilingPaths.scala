d42872a9eb585fedeb33924bdcc5925e7ef2b046
Betsy Lorton <elorton@gmail.com>
2017-01-05 12:13:36 -0500

    Extract ExecutionContext from institutionsRoutes
    This reduces duplication and nesting level in individual paths.

--- api/src/main/scala/hmda/api/http/institutions/FilingPaths.scala
+++ api/src/main/scala/hmda/api/http/institutions/FilingPaths.scala
@@ -29,36 +29,33 @@ trait FilingPaths extends InstitutionProtocol with ApiErrorProtocol with HmdaCus
   implicit val timeout: Timeout
 
   // institutions/<institutionId>/filings/<period>
-  def filingByPeriodPath(institutionId: String) =
+  def filingByPeriodPath(institutionId: String)(implicit ec: ExecutionContext) =
     path("filings" / Segment) { period =>
-      extractExecutionContext { executor =>
-        timedGet { uri =>
-          implicit val ec: ExecutionContext = executor
-          val supervisor = system.actorSelection("/user/supervisor")
-          val fFilings = (supervisor ? FindFilings(FilingPersistence.name, institutionId)).mapTo[ActorRef]
-          val fSubmissions = (supervisor ? FindSubmissions(SubmissionPersistence.name, institutionId, period)).mapTo[ActorRef]
+      timedGet { uri =>
+        val supervisor = system.actorSelection("/user/supervisor")
+        val fFilings = (supervisor ? FindFilings(FilingPersistence.name, institutionId)).mapTo[ActorRef]
+        val fSubmissions = (supervisor ? FindSubmissions(SubmissionPersistence.name, institutionId, period)).mapTo[ActorRef]
 
-          val fDetails = for {
-            f <- fFilings
-            s <- fSubmissions
-            d <- filingDetailsByPeriod(period, f, s)
-          } yield d
+        val fDetails = for {
+          f <- fFilings
+          s <- fSubmissions
+          d <- filingDetailsByPeriod(period, f, s)
+        } yield d
 
-          onComplete(fDetails) {
-            case Success(filingDetails) =>
-              val filing = filingDetails.filing
-              if (filing.institutionId == institutionId && filing.period == period)
-                complete(ToResponseMarshallable(filingDetails))
-              else if (filing.institutionId == institutionId && filing.period != period) {
-                val errorResponse = ErrorResponse(404, s"$period filing not found for institution $institutionId", uri.path)
-                complete(ToResponseMarshallable(StatusCodes.NotFound -> errorResponse))
-              } else {
-                val errorResponse = ErrorResponse(404, s"Institution $institutionId not found", uri.path)
-                complete(ToResponseMarshallable(StatusCodes.NotFound -> errorResponse))
-              }
-            case Failure(error) =>
-              completeWithInternalError(uri, error)
-          }
+        onComplete(fDetails) {
+          case Success(filingDetails) =>
+            val filing = filingDetails.filing
+            if (filing.institutionId == institutionId && filing.period == period)
+              complete(ToResponseMarshallable(filingDetails))
+            else if (filing.institutionId == institutionId && filing.period != period) {
+              val errorResponse = ErrorResponse(404, s"$period filing not found for institution $institutionId", uri.path)
+              complete(ToResponseMarshallable(StatusCodes.NotFound -> errorResponse))
+            } else {
+              val errorResponse = ErrorResponse(404, s"Institution $institutionId not found", uri.path)
+              complete(ToResponseMarshallable(StatusCodes.NotFound -> errorResponse))
+            }
+          case Failure(error) =>
+            completeWithInternalError(uri, error)
         }
       }
     }
