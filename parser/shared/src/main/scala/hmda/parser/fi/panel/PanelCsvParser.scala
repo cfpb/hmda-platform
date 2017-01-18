5c87de62842f7f7dd548894c6756aafcbfe69d10
Nick Grippin <ngrippin@discovertechnologies.com>
2017-01-18 11:31:20 -0500

    removed file chooser

--- parser/shared/src/main/scala/hmda/parser/fi/panel/PanelCsvParser.scala
+++ parser/shared/src/main/scala/hmda/parser/fi/panel/PanelCsvParser.scala
@@ -1,33 +1,21 @@
 package hmda.parser.fi.panel
 
-import javax.swing.JFileChooser
-import javax.swing.filechooser.FileNameExtensionFilter
-
 import scala.io.Source
 import scala.util.Try
 
 object PanelCsvParser extends App {
-  val file = pickFile()
+  var file: Source = _
+  if (Try(Source.fromFile(args(0))).isSuccess) {
+    file = Source.fromFile(args(0))
+  } else {
+    println("\nWARNING: Unable to read file input.")
+    System.exit(1)
+  }
+
   val lines = file.getLines().toList.tail
 
   for (line <- lines) {
     val i = InstitutionParser(line)
     println(i)
-  }
-
-  def pickFile(): Source = {
-    val chooser = new JFileChooser()
-    chooser.setCurrentDirectory(new java.io.File("."))
-    chooser.setDialogTitle("Input File")
-    chooser.setFileSelectionMode(JFileChooser.FILES_ONLY)
-    chooser.setFileFilter(new FileNameExtensionFilter("csv", "csv"))
-
-    if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION &&
-      Try(Source.fromFile(chooser.getSelectedFile)).isSuccess) {
-      Source.fromFile(chooser.getSelectedFile)
-    } else {
-      println("\nWARNING: Unable to read file input.")
-      Source.fromFile(".")
-    }
   }
 }
