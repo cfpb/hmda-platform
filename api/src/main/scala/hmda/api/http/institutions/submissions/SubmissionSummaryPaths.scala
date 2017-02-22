8ca66027e8009f8b15348e754ad9fe55baaef968
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-02-22 12:02:48 -0500

    Add real data in response from TS

--- api/src/main/scala/hmda/api/http/institutions/submissions/SubmissionSummaryPaths.scala
+++ api/src/main/scala/hmda/api/http/institutions/submissions/SubmissionSummaryPaths.scala
@@ -1,16 +1,27 @@
 package hmda.api.http.institutions.submissions
 
-import akka.actor.ActorSystem
+import akka.actor.{ ActorRef, ActorSystem }
+import akka.pattern.ask
 import akka.event.LoggingAdapter
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.{ HttpResponse, StatusCodes }
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import hmda.api.http.{ HmdaCustomDirectives, ValidationErrorConverter }
-import hmda.api.model.institutions.submissions.{ FileSummary, RespondentSummary, SubmissionSummary, ContactSummary }
+import hmda.api.model.ErrorResponse
+import hmda.api.model.institutions.submissions.{ ContactSummary, FileSummary, RespondentSummary, SubmissionSummary }
 import hmda.api.protocol.processing.{ ApiErrorProtocol, EditResultsProtocol, InstitutionProtocol, SubmissionProtocol }
+import hmda.model.fi.SubmissionId
+import hmda.model.fi.ts.{ Contact, Parent, Respondent, TransmittalSheet }
+import hmda.persistence.HmdaSupervisor.FindProcessingActor
+import hmda.persistence.messages.CommonMessages.GetState
+import hmda.persistence.processing.HmdaFileValidator
+import hmda.persistence.processing.HmdaFileValidator._
+
 import scala.concurrent.ExecutionContext
+import scala.util.{ Failure, Success }
 
 trait SubmissionSummaryPaths
     extends InstitutionProtocol
@@ -29,17 +40,36 @@ trait SubmissionSummaryPaths
   // institutions/<institutionId>/filings/<period>/submissions/<submissionId>/summary
   // NOTE:  This is currently a mocked, static endpoint
   def submissionSummaryPath(institutionId: String)(implicit ec: ExecutionContext) =
-    path("filings" / Segment / "submissions" / IntNumber / "summary") { (period, submissionId) =>
+    path("filings" / Segment / "submissions" / IntNumber / "summary") { (period, seqNr) =>
       timedGet { uri =>
+        val submissionId = SubmissionId(institutionId, period, seqNr)
+
         val supervisor = system.actorSelection("/user/supervisor")
+        val validatorF = (supervisor ? FindProcessingActor(HmdaFileValidator.name, submissionId)).mapTo[ActorRef]
 
-        val contactSummary = ContactSummary("Your Name", "123-456-7890", "your.name@bank.com")
-        val respondentSummary = RespondentSummary("Bank", "1234567890", "0987654321", "CFPB", contactSummary)
-        val fileSummary = FileSummary("lar.dat", "2016", 25)
+        val tsF = for {
+          validator <- validatorF
+          s <- (validator ? GetState).mapTo[HmdaFileValidationState]
+          ts = s.ts
+        } yield ts
 
-        val submissionSummary = SubmissionSummary(respondentSummary, fileSummary)
+        onComplete(tsF) {
+          case Success(x) => x match {
+            case Some(t) =>
+              val contactSummary = ContactSummary(t.contact.name, t.contact.phone, t.contact.email)
+              val respondentSummary = RespondentSummary(t.respondent.name, t.respondent.id, t.taxId, t.agencyCode.toString, contactSummary)
 
-        complete(ToResponseMarshallable(submissionSummary))
+              val fileSummary = FileSummary("lar.dat", period, 25)
+              val submissionSummary = SubmissionSummary(respondentSummary, fileSummary)
+              complete(ToResponseMarshallable(submissionSummary))
+            case None =>
+              val errorResponse = ErrorResponse(404, s"submission $submissionId not found", uri.path)
+              complete(ToResponseMarshallable(StatusCodes.NotFound -> errorResponse))
+          }
+
+          case Failure(error) =>
+            completeWithInternalError(uri, error)
+        }
       }
     }
 }
--- api/src/test/scala/hmda/api/http/institutions/submissions/SubmissionSummaryPathsSpec.scala
+++ api/src/test/scala/hmda/api/http/institutions/submissions/SubmissionSummaryPathsSpec.scala
@@ -1,17 +1,38 @@
 package hmda.api.http.institutions.submissions
 
-import akka.http.javadsl.model.StatusCodes
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.actor.ActorRef
+import akka.pattern.ask
+import akka.http.scaladsl.model.StatusCodes
 import hmda.api.http.InstitutionHttpApiSpec
-import hmda.api.model.Receipt
-import spray.json.{ JsBoolean, JsObject }
+import hmda.model.fi.SubmissionId
+import hmda.persistence.HmdaSupervisor.FindProcessingActor
+import hmda.persistence.processing.HmdaFileValidator
+import org.scalatest.BeforeAndAfterAll
+import scala.concurrent.Await
+import hmda.parser.fi.ts.TsCsvParser
 
-class SubmissionSummaryPathsSpec extends InstitutionHttpApiSpec {
-  val supervisor = system.actorSelection("/user/supervisor")
+class SubmissionSummaryPathsSpec extends InstitutionHttpApiSpec with BeforeAndAfterAll {
+  import hmda.model.util.FITestData._
+
+  val institutionId = "0"
+  val period = "2017"
+  val seqNr = 1
+  val submissionId = SubmissionId(institutionId, period, seqNr)
+
+  val lines = fiCSV.split("\n")
+  val ts = TsCsvParser(lines(0)).right.get
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    val supervisor = system.actorSelection("/user/supervisor")
+    val validatorF = (supervisor ? FindProcessingActor(HmdaFileValidator.name, submissionId)).mapTo[ActorRef]
+    val validator = Await.result(validatorF, duration)
+    validator ! ts
+  }
 
   "Submission Summary Paths" must {
     "return a 200" in {
-      getWithCfpbHeaders("/institutions/0/filings/2017/submissions/1/summary") ~> institutionsRoutes ~> check {
+      getWithCfpbHeaders(s"/institutions/$institutionId/filings/$period/submissions/$seqNr/summary") ~> institutionsRoutes ~> check {
         status mustBe StatusCodes.OK
       }
     }
