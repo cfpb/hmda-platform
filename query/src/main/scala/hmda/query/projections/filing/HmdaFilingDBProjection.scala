f592190968cb63c8885035b92bc587b8ca81d5c1
Nick Grippin <ngrippin@discovertechnologies.com>
2017-03-08 11:40:24 -0500

    fix command

--- query/src/main/scala/hmda/query/projections/filing/HmdaFilingDBProjection.scala
+++ query/src/main/scala/hmda/query/projections/filing/HmdaFilingDBProjection.scala
@@ -2,14 +2,12 @@ package hmda.query.projections.filing
 
 import akka.actor.{ ActorRef, ActorSystem, Props }
 import akka.pattern.pipe
-import hmda.model.fi.SubmissionId
 import hmda.persistence.messages.CommonMessages.{ Command, Event }
 import hmda.persistence.messages.events.processing.CommonHmdaValidatorEvents.{ HmdaValidatorEvent, LarValidated }
 import hmda.persistence.model.HmdaActor
 import hmda.query.DbConfiguration
 import hmda.query.model.filing.LoanApplicationRegisterQuery
 import hmda.query.repository.filing.FilingComponent
-import org.h2.command.Command
 
 import scala.concurrent.ExecutionContext
 
@@ -19,7 +17,7 @@ object HmdaFilingDBProjection extends FilingComponent with DbConfiguration {
   val larTotalMsaRepository = new LarTotalMsaRepository(config)
   val modifiedLarRepository = new ModifiedLarRepository(config)
 
-  case class CreateSchema()
+  case object CreateSchema extends Command
   case class DeleteLars(respondentId: String)
   case class LarInserted(n: Int)
   case class FilingSchemaCreated() extends Event
@@ -40,7 +38,7 @@ class HmdaFilingDBProjection(filingPeriod: String) extends HmdaActor {
   import hmda.query.repository.filing.LarConverter._
 
   override def receive: Receive = {
-    case CreateSchema() =>
+    case CreateSchema =>
       val schemaCreated = for {
         s <- larRepository.createSchema()
       } yield s
