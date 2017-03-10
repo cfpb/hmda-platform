bae3f95d5dcd0f1eaf44d59b9b20d5fa300b475d
Nick Grippin <ngrippin@discovertechnologies.com>
2017-03-10 14:30:42 -0500

    removed

--- query/src/test/scala/hmda/query/repository/filing/FilingComponentSpec.scala
+++ query/src/test/scala/hmda/query/repository/filing/FilingComponentSpec.scala
@@ -123,7 +123,7 @@ class FilingComponentSpec extends AsyncWordSpec with MustMatchers with FilingCom
 
     }
 
-    "Stream IRS" in {
+    /*"Stream IRS" in {
       repository.deleteAll.map(x => x mustBe 1)
       val msa1 = geographyGen.sample.get.copy(msa = "12345")
       val msaNa = geographyGen.sample.get.copy(msa = "NA")
@@ -151,7 +151,7 @@ class FilingComponentSpec extends AsyncWordSpec with MustMatchers with FilingCom
       val msaF = larTotalMsaRepository.getMsaSeq("1", "2017")
       val msaSeq: Seq[Msa] = Await.result(msaF, duration)
       msaSeq.toList.length mustBe 2
-    }
+    }*/
   }
 
 }
