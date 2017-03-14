f9c97e668f8844ec7cac18322be5265b12ec2c50
Nick Grippin <ngrippin@discovertechnologies.com>
2017-03-14 17:42:58 -0400

    potentially uncomment irs tests

--- api/src/test/scala/hmda/api/http/institutions/submissions/SubmissionIrsPathsSpec.scala
+++ api/src/test/scala/hmda/api/http/institutions/submissions/SubmissionIrsPathsSpec.scala
@@ -49,8 +49,8 @@ class SubmissionIrsPathsSpec
       val msa1 = geographyGen.sample.get.copy(msa = "12345")
       val msaNa = geographyGen.sample.get.copy(msa = "NA")
       val loan = loanGen.sample.get.copy(amount = 12)
-      val lar1 = toLoanApplicationRegisterQuery(larGen.sample.get.copy(respondentId = "0", geography = msa1, loan = loan))
-      val lar2 = toLoanApplicationRegisterQuery(larGen.sample.get.copy(respondentId = "0", geography = msaNa, loan = loan))
+      val lar1 = toLoanApplicationRegisterQuery(larGen.sample.get.copy(geography = msa1, loan = loan)).copy(institutionId = "0")
+      val lar2 = toLoanApplicationRegisterQuery(larGen.sample.get.copy(geography = msaNa, loan = loan)).copy(institutionId = "0")
       val query1 = lar1.copy(period = "2017")
       val query2 = lar2.copy(period = "2017")
 
@@ -59,10 +59,10 @@ class SubmissionIrsPathsSpec
 
       getWithCfpbHeaders("/institutions/0/filings/2017/submissions/1/irs") ~> institutionsRoutes ~> check {
         status mustBe StatusCodes.OK
-        //val irs = responseAs[Irs]
-        //irs.totals.amount mustBe 24
-        //irs.totals.lars mustBe 2
-        //irs.msas.length mustBe 2
+        val irs = responseAs[Irs]
+        irs.totals.amount mustBe 24
+        irs.totals.lars mustBe 2
+        irs.msas.length mustBe 2
       }
     }
   }
