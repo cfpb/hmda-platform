57790ca738850e2ce7b9a39470be035b2c33bb18
Betsy Lorton <elorton@gmail.com>
2017-04-10 18:14:34 -0400

    Reimplement Q022 using ValidationContext
    Previously, Q022 wasn't implemented with our accepted method of
    providing the activity year to edits.

--- validation/src/test/scala/hmda/validation/rules/lar/quality/Q022Spec.scala
+++ validation/src/test/scala/hmda/validation/rules/lar/quality/Q022Spec.scala
@@ -1,6 +1,7 @@
 package hmda.validation.rules.lar.quality
 
 import hmda.model.fi.lar.LarGenerators
+import hmda.validation.context.ValidationContext
 import hmda.validation.dsl.Success
 import hmda.validation.dsl.Failure
 import hmda.validation.rules.lar.BadValueUtils
@@ -8,18 +9,17 @@ import org.scalacheck.Gen
 import org.scalatest.{ MustMatchers, PropSpec }
 import org.scalatest.prop.PropertyChecks
 
-import scala.concurrent.{ ExecutionContext, Future }
-
 class Q022Spec extends PropSpec with PropertyChecks with MustMatchers with LarGenerators with BadValueUtils {
-  implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
 
   property("Passes if activity year is within two years after application date") {
     forAll(larGen, Gen.choose(0, 2), dateGen) { (lar, x, date) =>
       val newLoan = lar.loan.copy(applicationDate = date.toString)
       val newLar = lar.copy(loan = newLoan)
       val applicationYear = newLar.loan.applicationDate.substring(0, 4).toInt
-      Q022(newLar, applicationYear + x) mustBe Success()
-      Q022(newLar, Future(applicationYear + x)).map(x => x mustBe Success())
+
+      val ctx = ValidationContext(None, Some(applicationYear + x))
+
+      Q022.inContext(ctx)(newLar) mustBe Success()
     }
   }
 
@@ -28,8 +28,10 @@ class Q022Spec extends PropSpec with PropertyChecks with MustMatchers with LarGe
       val newLoan = lar.loan.copy(applicationDate = date.toString)
       val newLar = lar.copy(loan = newLoan)
       val applicationYear = newLar.loan.applicationDate.substring(0, 4).toInt
-      Q022(newLar, applicationYear + x) mustBe a[Failure]
-      Q022(newLar, Future(applicationYear + x)).map(x => x mustBe a[Failure])
+
+      val ctx = ValidationContext(None, Some(applicationYear + x))
+
+      Q022.inContext(ctx)(newLar) mustBe Failure()
     }
   }
 
@@ -38,8 +40,15 @@ class Q022Spec extends PropSpec with PropertyChecks with MustMatchers with LarGe
       val newLoan = lar.loan.copy(applicationDate = "NA")
       val newLar = lar.copy(loan = newLoan)
 
-      Q022(newLar, 2099) mustBe a[Success]
-      Q022(newLar, Future(2099)).map(x => x mustBe Success())
+      val ctx = ValidationContext(None, Some(2099))
+      Q022.inContext(ctx)(newLar) mustBe a[Success]
+    }
+  }
+
+  property("Passes if activity year is not provided in ValidationContext") {
+    forAll(larGen) { lar =>
+      val ctx = ValidationContext(None, None)
+      Q022.inContext(ctx)(lar) mustBe a[Success]
     }
   }
 }
