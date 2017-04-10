a67ffbf6b69c0249a358336abd8bccb1dd9d2cee
Nick Grippin <ngrippin@discovertechnologies.com>
2017-04-10 11:09:54 -0400

    cleanup

--- query/src/main/scala/hmda/query/model/filing/Msa.scala
+++ query/src/main/scala/hmda/query/model/filing/Msa.scala
@@ -19,40 +19,21 @@ case class Msa(
 )
 
 case class MsaWithName(
-    id: String = "",
-    name: String = "",
-    totalLars: Int = 0,
-    totalAmount: Int = 0,
-    conv: Int = 0,
-    FHA: Int = 0,
-    VA: Int = 0,
-    FSA: Int = 0,
-    oneToFourFamily: Int = 0,
-    MFD: Int = 0,
-    multiFamily: Int = 0,
-    homePurchase: Int = 0,
-    homeImprovement: Int = 0,
-    refinance: Int = 0
-) {
-  def fromMsa(name: Option[String], msa: Msa): MsaWithName = {
-    new MsaWithName(
-      msa.id,
-      name.getOrElse(""),
-      msa.totalLars,
-      msa.totalAmount,
-      msa.conv,
-      msa.FHA,
-      msa.VA,
-      msa.FSA,
-      msa.oneToFourFamily,
-      msa.MFD,
-      msa.multiFamily,
-      msa.homePurchase,
-      msa.homeImprovement,
-      msa.refinance
-    )
-  }
-}
+    id: String,
+    name: String,
+    totalLars: Int,
+    totalAmount: Int,
+    conv: Int,
+    FHA: Int,
+    VA: Int,
+    FSA: Int,
+    oneToFourFamily: Int,
+    MFD: Int,
+    multiFamily: Int,
+    homePurchase: Int,
+    homeImprovement: Int,
+    refinance: Int
+)
 
 case class MsaSummary(
     lars: Int,
@@ -95,10 +76,29 @@ case class Irs(msas: List[MsaWithName], totals: MsaSummary)
 case object Irs {
   def createIrs(msas: List[Msa]): Irs = {
     val codeMap = CbsaLookup.codeMap
-    val msaSeqWithName = msas.map(m => MsaWithName().fromMsa(codeMap.get(m.id), m))
+    val msaSeqWithName = msas.map(m => addMsaName(m, codeMap.get(m.id)))
 
     val summary = msas.foldLeft(MsaSummary.empty) { (summary, msa) => summary + msa }
 
     Irs(msaSeqWithName, summary)
+  }
+
+  private def addMsaName(msa: Msa, name: Option[String]): MsaWithName = {
+    MsaWithName(
+      msa.id,
+      name.getOrElse(""),
+      msa.totalLars,
+      msa.totalAmount,
+      msa.conv,
+      msa.FHA,
+      msa.VA,
+      msa.FSA,
+      msa.oneToFourFamily,
+      msa.MFD,
+      msa.multiFamily,
+      msa.homePurchase,
+      msa.homeImprovement,
+      msa.refinance
+    )
   }
 }
