4826e7eabe50d3bd25ae2f465dd295599b6ad1d9
Betsy Lorton <elorton@gmail.com>
2017-05-08 17:46:38 -0400

    New StatsLookup trait holds repeated code for ValidationStats

--- validation/src/main/scala/hmda/validation/rules/ts/quality/Q012.scala
+++ validation/src/main/scala/hmda/validation/rules/ts/quality/Q012.scala
@@ -1,9 +1,6 @@
 package hmda.validation.rules.ts.quality
 
 import akka.pattern.ask
-import hmda.validation._
-import akka.util.Timeout
-import com.typesafe.config.ConfigFactory
 import hmda.model.fi.ts.TransmittalSheet
 import hmda.model.institution.Institution
 import hmda.validation.{ AS, EC, MAT }
@@ -15,7 +12,6 @@ import hmda.validation.dsl.PredicateCommon._
 import hmda.validation.dsl.PredicateSyntax._
 
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
 object Q012 {
   def inContext(ctx: ValidationContext): AggregateEditCheck[TransmittalSheet, TransmittalSheet] = {
@@ -23,16 +19,11 @@ object Q012 {
   }
 }
 
-class Q012 private (institution: Institution, year: Int) extends AggregateEditCheck[TransmittalSheet, TransmittalSheet] {
+class Q012 private (institution: Institution, year: Int) extends AggregateEditCheck[TransmittalSheet, TransmittalSheet] with StatsLookup {
   override def name: String = "Q012"
 
   override def apply[as: AS, mat: MAT, ec: EC](input: TransmittalSheet): Future[Result] = {
     val system = implicitly[AS[_]]
-    val configuration = ConfigFactory.load()
-    val duration = configuration.getInt("hmda.actor.timeout")
-    implicit val timeout = Timeout(duration.seconds)
-
-    val validationStats = system.actorSelection("/user/validation-stats")
     val fLastYearTaxId = (validationStats ? FindTaxId(institution.id, (year - 1).toString)).mapTo[String]
 
     for {
