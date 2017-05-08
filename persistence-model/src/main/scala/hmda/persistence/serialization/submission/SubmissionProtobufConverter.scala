f582d3041c1914b8287c546c2dfea7523b1c04e9
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-05-08 11:50:38 -0400

    Add submission protobuf serializer

--- persistence-model/src/main/scala/hmda/persistence/serialization/submission/SubmissionProtobufConverter.scala
+++ persistence-model/src/main/scala/hmda/persistence/serialization/submission/SubmissionProtobufConverter.scala
@@ -1,6 +1,6 @@
 package hmda.persistence.serialization.submission
 
-import hmda.model.fi.{ Submission, SubmissionId, SubmissionStatus }
+import hmda.model.fi.{ Submission, SubmissionId, SubmissionStatus, Created, Uploading, Uploaded, Parsing, Parsed, ParsedWithErrors, Validating, ValidatedWithErrors, Validated, Signed, Failed }
 import hmda.persistence.messages.events.institutions.SubmissionEvents.{ SubmissionCreated, SubmissionStatusUpdated }
 import hmda.persistence.model.serialization.SubmissionEvents._
 
@@ -14,9 +14,25 @@ object SubmissionProtobufConverter {
 
   def submissionStatusUpdatedFromProtobuf(msg: SubmissionStatusUpdatedMessage): SubmissionStatusUpdated = ???
 
-  def submissionToProtobuf(obj: Submission): SubmissionMessage = ???
+  def submissionToProtobuf(obj: Submission): SubmissionMessage = {
+    SubmissionMessage(
+      id = Some(submissionIdToProtobuf(obj.id)),
+      status = Some(submissionStatusToProtobuf(obj.status)),
+      start = obj.start,
+      end = obj.end,
+      receipt = obj.receipt
+    )
+  }
 
-  def submissionFromProtobuf(msg: SubmissionMessage): Submission = ???
+  def submissionFromProtobuf(msg: SubmissionMessage): Submission = {
+    Submission(
+      id = submissionIdFromProtobuf(msg.id.getOrElse(SubmissionIdMessage())),
+      status = submissionStatusFromProtobuf(msg.status.getOrElse(SubmissionStatusMessage())),
+      start = msg.start,
+      end = msg.end,
+      receipt = msg.receipt
+    )
+  }
 
   def submissionIdToProtobuf(obj: SubmissionId): SubmissionIdMessage = {
     SubmissionIdMessage(
@@ -34,8 +50,28 @@ object SubmissionProtobufConverter {
     )
   }
 
-  def submissionStatusToProtobuf(obj: SubmissionStatus): SubmissionStatusMessage = ???
+  def submissionStatusToProtobuf(obj: SubmissionStatus): SubmissionStatusMessage = {
+    SubmissionStatusMessage(
+      code = obj.code,
+      message = obj.message,
+      description = obj.description
+    )
+  }
 
-  def submissionStatusFromProtobuf(msg: SubmissionStatusMessage): SubmissionStatus = ???
+  def submissionStatusFromProtobuf(msg: SubmissionStatusMessage): SubmissionStatus = {
+    msg.code match {
+      case 1 => Created
+      case 2 => Uploading
+      case 3 => Uploaded
+      case 4 => Parsing
+      case 5 => ParsedWithErrors
+      case 6 => Parsed
+      case 7 => Validating
+      case 8 => ValidatedWithErrors
+      case 9 => Validated
+      case 10 => Signed
+      case -1 => Failed(msg.message)
+    }
+  }
 
 }
--- persistence-model/src/test/scala/hmda/persistence/serialization/submission/SubmissionProtobufConverterSpec.scala
+++ persistence-model/src/test/scala/hmda/persistence/serialization/submission/SubmissionProtobufConverterSpec.scala
@@ -3,7 +3,7 @@ package hmda.persistence.serialization.submission
 import org.scalatest.{ MustMatchers, PropSpec }
 import org.scalatest.prop.PropertyChecks
 import hmda.model.institution.SubmissionGenerators._
-import hmda.persistence.model.serialization.SubmissionEvents.SubmissionIdMessage
+import hmda.persistence.model.serialization.SubmissionEvents.{ SubmissionIdMessage, SubmissionMessage, SubmissionStatusMessage }
 import hmda.persistence.serialization.submission.SubmissionProtobufConverter._
 
 class SubmissionProtobufConverterSpec extends PropSpec with PropertyChecks with MustMatchers {
@@ -12,6 +12,20 @@ class SubmissionProtobufConverterSpec extends PropSpec with PropertyChecks with 
     forAll(submissionIdGen) { submissionId =>
       val protobuf = submissionIdToProtobuf(submissionId).toByteArray
       submissionIdFromProtobuf(SubmissionIdMessage.parseFrom(protobuf)) mustBe submissionId
+    }
+  }
+
+  property("Submission Status must serialize to protobuf and back") {
+    forAll(submissionStatusGen) { submissionStatus =>
+      val protobuf = submissionStatusToProtobuf(submissionStatus).toByteArray
+      submissionStatusFromProtobuf(SubmissionStatusMessage.parseFrom(protobuf)) mustBe submissionStatus
+    }
+  }
+
+  property("Submission must serialize to protobuf and back") {
+    forAll(submissionGen) { submission =>
+      val protobuf = submissionToProtobuf(submission).toByteArray
+      submissionFromProtobuf(SubmissionMessage.parseFrom(protobuf)) mustBe submission
     }
   }
 
