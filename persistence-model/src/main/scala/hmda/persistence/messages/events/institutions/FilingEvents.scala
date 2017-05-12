568e8672114602fcdebc4c9c1f229fa8b6b314ba
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-05-12 13:33:58 -0400

    Update documentation, fix typo

--- persistence-model/src/main/scala/hmda/persistence/messages/events/institutions/FilingEvents.scala
+++ persistence-model/src/main/scala/hmda/persistence/messages/events/institutions/FilingEvents.scala
@@ -4,7 +4,7 @@ import hmda.model.fi.Filing
 import hmda.persistence.messages.CommonMessages.Event
 
 object FilingEvents {
-  sealed trait SubmissionEvent extends Event
-  case class FilingCreated(filing: Filing) extends SubmissionEvent
-  case class FilingStatusUpdated(filing: Filing) extends SubmissionEvent
+  sealed trait FilingEvent extends Event
+  case class FilingCreated(filing: Filing) extends FilingEvent
+  case class FilingStatusUpdated(filing: Filing) extends FilingEvent
 }
