cb1b83b21a7e9cf7c8f821ca105e218924c71d9d
Betsy Lorton <elorton@gmail.com>
2017-05-26 10:51:50 -0400

    correction: use specific event type for HmdaFileValidator Events

--- persistence-model/src/main/scala/hmda/persistence/messages/events/processing/HmdaFileValidatorEvents.scala
+++ persistence-model/src/main/scala/hmda/persistence/messages/events/processing/HmdaFileValidatorEvents.scala
@@ -6,12 +6,12 @@ import hmda.persistence.messages.CommonMessages.Event
 object HmdaFileValidatorEvents {
   trait HmdaFileValidatorEvent extends Event
 
-  case class TsSyntacticalError(error: ValidationError) extends Event
-  case class TsValidityError(error: ValidationError) extends Event
-  case class TsQualityError(error: ValidationError) extends Event
-  case class LarSyntacticalError(error: ValidationError) extends Event
-  case class LarValidityError(error: ValidationError) extends Event
-  case class LarQualityError(error: ValidationError) extends Event
-  case class LarMacroError(error: ValidationError) extends Event
-  case class EditsVerified(editType: ValidationErrorType, verified: Boolean) extends Event
+  case class TsSyntacticalError(error: ValidationError) extends HmdaFileValidatorEvent
+  case class TsValidityError(error: ValidationError) extends HmdaFileValidatorEvent
+  case class TsQualityError(error: ValidationError) extends HmdaFileValidatorEvent
+  case class LarSyntacticalError(error: ValidationError) extends HmdaFileValidatorEvent
+  case class LarValidityError(error: ValidationError) extends HmdaFileValidatorEvent
+  case class LarQualityError(error: ValidationError) extends HmdaFileValidatorEvent
+  case class LarMacroError(error: ValidationError) extends HmdaFileValidatorEvent
+  case class EditsVerified(editType: ValidationErrorType, verified: Boolean) extends HmdaFileValidatorEvent
 }
