4e19331190ea08b02ddaefa71a33351ecf1b1bc4
Betsy Lorton <elorton@gmail.com>
2017-05-26 12:09:25 -0400

    All HmdaFileValidatorEvents convert to protobuf

--- persistence-model/src/main/scala/hmda/persistence/serialization/validation/HmdaFileValidatorProtobufConverter.scala
+++ persistence-model/src/main/scala/hmda/persistence/serialization/validation/HmdaFileValidatorProtobufConverter.scala
@@ -1,7 +1,8 @@
 package hmda.persistence.serialization.validation
 
 import hmda.model.validation._
-import hmda.persistence.model.serialization.HmdaFileValidatorEvents.{ ValidationErrorMessage, ValidationErrorTypeMessage }
+import hmda.persistence.messages.events.processing.HmdaFileValidatorEvents._
+import hmda.persistence.model.serialization.HmdaFileValidatorEvents._
 
 object HmdaFileValidatorProtobufConverter {
 
@@ -42,5 +43,81 @@ object HmdaFileValidatorProtobufConverter {
       case Quality => QualityValidationError(errorId, ruleName, ts)
       case Macro => MacroValidationError(ruleName)
     }
+  }
+
+  def editsVerifiedToProtobuf(event: EditsVerified): EditsVerifiedMessage = {
+    EditsVerifiedMessage(
+      editType = validationErrorTypeToProtobuf(event.editType),
+      verified = event.verified
+    )
+  }
+  def editsVerifiedFromProtobuf(msg: EditsVerifiedMessage): EditsVerified = {
+    EditsVerified(
+      editType = validationErrorTypeFromProtobuf(msg.editType),
+      verified = msg.verified
+    )
+  }
+
+  def tsSyntacticalErrorToProtobuf(err: TsSyntacticalError): TsSyntacticalErrorMessage = {
+    val errProto = validationErrorToProtobuf(err.error)
+    TsSyntacticalErrorMessage(error = Some(errProto))
+  }
+  def tsSyntacticalErrorFromProtobuf(msg: TsSyntacticalErrorMessage): TsSyntacticalError = {
+    val err = validationErrorFromProtobuf(msg.error.getOrElse(ValidationErrorMessage()))
+    TsSyntacticalError(error = err)
+  }
+
+  def tsValidityErrorToProtobuf(err: TsValidityError): TsValidityErrorMessage = {
+    val errProto = validationErrorToProtobuf(err.error)
+    TsValidityErrorMessage(error = Some(errProto))
+  }
+  def tsValidityErrorFromProtobuf(msg: TsValidityErrorMessage): TsValidityError = {
+    val err = validationErrorFromProtobuf(msg.error.getOrElse(ValidationErrorMessage()))
+    TsValidityError(error = err)
+  }
+
+  def tsQualityErrorToProtobuf(err: TsQualityError): TsQualityErrorMessage = {
+    val errProto = validationErrorToProtobuf(err.error)
+    TsQualityErrorMessage(error = Some(errProto))
+  }
+  def tsQualityErrorFromProtobuf(msg: TsQualityErrorMessage): TsQualityError = {
+    val err = validationErrorFromProtobuf(msg.error.getOrElse(ValidationErrorMessage()))
+    TsQualityError(error = err)
+  }
+
+  def larSyntacticalErrorToProtobuf(err: LarSyntacticalError): LarSyntacticalErrorMessage = {
+    val errProto = validationErrorToProtobuf(err.error)
+    LarSyntacticalErrorMessage(error = Some(errProto))
+  }
+  def larSyntacticalErrorFromProtobuf(msg: LarSyntacticalErrorMessage): LarSyntacticalError = {
+    val err = validationErrorFromProtobuf(msg.error.getOrElse(ValidationErrorMessage()))
+    LarSyntacticalError(error = err)
+  }
+
+  def larValidityErrorToProtobuf(err: LarValidityError): LarValidityErrorMessage = {
+    val errProto = validationErrorToProtobuf(err.error)
+    LarValidityErrorMessage(error = Some(errProto))
+  }
+  def larValidityErrorFromProtobuf(msg: LarValidityErrorMessage): LarValidityError = {
+    val err = validationErrorFromProtobuf(msg.error.getOrElse(ValidationErrorMessage()))
+    LarValidityError(error = err)
+  }
+
+  def larQualityErrorToProtobuf(err: LarQualityError): LarQualityErrorMessage = {
+    val errProto = validationErrorToProtobuf(err.error)
+    LarQualityErrorMessage(error = Some(errProto))
+  }
+  def larQualityErrorFromProtobuf(msg: LarQualityErrorMessage): LarQualityError = {
+    val err = validationErrorFromProtobuf(msg.error.getOrElse(ValidationErrorMessage()))
+    LarQualityError(error = err)
+  }
+
+  def larMacroErrorToProtobuf(err: LarMacroError): LarMacroErrorMessage = {
+    val errProto = validationErrorToProtobuf(err.error)
+    LarMacroErrorMessage(error = Some(errProto))
+  }
+  def larMacroErrorFromProtobuf(msg: LarMacroErrorMessage): LarMacroError = {
+    val err = validationErrorFromProtobuf(msg.error.getOrElse(ValidationErrorMessage()))
+    LarMacroError(error = err)
   }
 }
