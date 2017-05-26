04ea22152ab93e9895c6ca59bad59f4b407c2e7b
Nick Grippin <ngrippin@discovertechnologies.com>
2017-05-26 12:08:24 -0400

    converter and serializer

--- persistence-model/src/main/scala/hmda/persistence/messages/events/validation/SubmissionLarStatsEvents.scala
+++ persistence-model/src/main/scala/hmda/persistence/messages/events/validation/SubmissionLarStatsEvents.scala
@@ -3,7 +3,8 @@ package hmda.persistence.messages.events.validation
 import hmda.persistence.messages.CommonMessages.Event
 
 object SubmissionLarStatsEvents {
-  case class SubmittedLarsUpdated(totalSubmitted: Int) extends Event
+  trait SubmissionLarStatsEvent extends Event
+  case class SubmittedLarsUpdated(totalSubmitted: Int) extends SubmissionLarStatsEvent
   case class MacroStatsUpdated(
     totalValidated: Int,
     q070Total: Int,
@@ -14,5 +15,5 @@ object SubmissionLarStatsEvents {
     q072Sold: Int,
     q075Ratio: Double,
     q076Ratio: Double
-  ) extends Event
+  ) extends SubmissionLarStatsEvent
 }
