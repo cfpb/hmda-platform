39c975b5f681b334b5ff3f8db1c1f980499bd7ae
Nick Grippin <ngrippin@discovertechnologies.com>
2017-05-30 12:01:29 -0400

    update test generators

--- persistence-model/src/test/scala/hmda/persistence/serialization/validation/SubmissionLarStatsProtobufSerializerSpec.scala
+++ persistence-model/src/test/scala/hmda/persistence/serialization/validation/SubmissionLarStatsProtobufSerializerSpec.scala
@@ -1,7 +1,6 @@
 package hmda.persistence.serialization.validation
 
 import hmda.persistence.messages.events.validation.SubmissionLarStatsEvents.{ MacroStatsUpdated, SubmittedLarsUpdated }
-import hmda.persistence.model.serialization.SubmissionLarStatsEvents.{ MacroStatsUpdatedMessage, SubmittedLarsUpdatedMessage }
 import org.scalacheck.Gen
 import org.scalatest.{ MustMatchers, PropSpec }
 import org.scalatest.prop.PropertyChecks
@@ -18,18 +17,18 @@ class SubmissionLarStatsProtobufSerializerSpec extends PropSpec with PropertyChe
   }
 
   property("MacroStatsUpdated messages must be serialized to binary and back") {
-    val difGen = Gen.choose(-50000, 50000)
-    forAll(Gen.choose(0, 50000)) { num =>
+    val intGen = Gen.choose(0, 1000)
+    forAll(Gen.choose(0d, 1d)) { d =>
       val msg = MacroStatsUpdated(
-        totalValidated = num + difGen.sample.get,
-        q070Total = num + difGen.sample.get,
-        q070Sold = num + difGen.sample.get,
-        q071Total = num + difGen.sample.get,
-        q071Sold = num + difGen.sample.get,
-        q072Total = num + difGen.sample.get,
-        q072Sold = num + difGen.sample.get,
-        q075Ratio = num + difGen.sample.get,
-        q076Ratio = num + difGen.sample.get
+        totalValidated = intGen.sample.get,
+        q070Total = intGen.sample.get,
+        q070Sold = intGen.sample.get,
+        q071Total = intGen.sample.get,
+        q071Sold = intGen.sample.get,
+        q072Total = intGen.sample.get,
+        q072Sold = intGen.sample.get,
+        q075Ratio = d,
+        q076Ratio = d
       )
       val bytes = serializer.toBinary(msg)
       serializer.fromBinary(bytes, serializer.MacroStatsUpdatedManifest) mustBe msg
