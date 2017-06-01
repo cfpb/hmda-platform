5ed30f5ee7658e21d00c318d97eb1fce8272d510
Nick Grippin <ngrippin@discovertechnologies.com>
2017-06-01 09:27:01 -0400

    filing view serializer

---
+++ query/src/test/scala/hmda/query/serialization/HmdaFilingViewProtobufConverterSpec.scala
@@ -0,0 +1,18 @@
+package hmda.query.serialization
+
+import hmda.persistence.model.serialization.HmdaFilingViewState.FilingViewStateMessage
+import hmda.query.view.filing.HmdaFilingView.FilingViewState
+import hmda.query.serialization.HmdaFilingViewProtobufConverter._
+import org.scalacheck.Gen
+import org.scalatest.{ MustMatchers, PropSpec }
+import org.scalatest.prop.PropertyChecks
+
+class HmdaFilingViewProtobufConverterSpec extends PropSpec with PropertyChecks with MustMatchers {
+  property("InstitutionViewState must serialize to protobuf and back") {
+    forAll(Gen.choose(0l, 100000l), Gen.choose(0l, 100000l)) { (size, seq) =>
+      val state = FilingViewState(size, seq)
+      val protobuf = filingViewStateToProtobuf(state).toByteArray
+      filingViewStateFromProtobuf(FilingViewStateMessage.parseFrom(protobuf)) mustBe state
+    }
+  }
+}
