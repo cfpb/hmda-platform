08dad6076018af836d1adc634733de05470ebc63
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-06-01 16:05:17 -0400

    Add read operation

--- query/src/main/scala/hmda/query/repository/CassandraRepository.scala
+++ query/src/main/scala/hmda/query/repository/CassandraRepository.scala
@@ -6,7 +6,6 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
 import com.datastax.driver.core.{ Cluster, ResultSet, Row }
 import hmda.query.CassandraConfig._
-
 import scala.concurrent.duration._
 import scala.concurrent.Future
 
@@ -14,6 +13,7 @@ trait CassandraRepository[A] {
 
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
   implicit val timeout = 10.seconds
 
   val keyspace = "hmda_query"
