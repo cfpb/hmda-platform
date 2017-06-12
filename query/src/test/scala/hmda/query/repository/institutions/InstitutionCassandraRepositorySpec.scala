b1c63b59daf845e3834cd36de3871a3aa743c861
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-06-12 10:55:14 -0400

    Update test

--- query/src/test/scala/hmda/query/repository/institutions/InstitutionCassandraRepositorySpec.scala
+++ query/src/test/scala/hmda/query/repository/institutions/InstitutionCassandraRepositorySpec.scala
@@ -1,28 +1,23 @@
 package hmda.query.repository.institutions
 
-import akka.Done
 import akka.actor.{ ActorSystem, Scheduler }
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
 import com.datastax.driver.core.{ Cluster, Session }
 import hmda.model.institution.{ Agency, InstitutionGenerators }
-import hmda.persistence.model.AsyncActorSpec
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper
-import org.scalatest.{ AsyncWordSpec, BeforeAndAfterAll, MustMatchers, WordSpec }
+import org.scalatest.{ BeforeAndAfterAll, MustMatchers, WordSpec }
 import hmda.query.repository.institutions.InstitutionConverter._
 
-import scala.concurrent.duration._
-import scala.concurrent.{ Await, ExecutionContext, Future }
+import scala.concurrent.ExecutionContext
 
 class InstitutionCassandraRepositorySpec extends WordSpec with MustMatchers with BeforeAndAfterAll with InstitutionCassandraRepository {
 
-  var cluster: Cluster = _
-  var session: Session = _
+  EmbeddedCassandraServerHelper.startEmbeddedCassandra(20000L)
+  val cluster: Cluster = EmbeddedCassandraServerHelper.getCluster
+  override val session: Session = cluster.connect()
 
   override def beforeAll(): Unit = {
-    EmbeddedCassandraServerHelper.startEmbeddedCassandra(20000L)
-    cluster = EmbeddedCassandraServerHelper.getCluster()
-    session = cluster.connect()
     createKeyspace()
   }
 
