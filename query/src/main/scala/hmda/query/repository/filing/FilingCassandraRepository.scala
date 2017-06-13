e944de640bbdfbe07e4b0e09dce839ebeef78b19
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-06-13 11:16:35 -0400

    Add prepared statement

--- query/src/main/scala/hmda/query/repository/filing/FilingCassandraRepository.scala
+++ query/src/main/scala/hmda/query/repository/filing/FilingCassandraRepository.scala
@@ -1,15 +1,15 @@
 package hmda.query.repository.filing
 
-import akka.{Done, NotUsed}
+import akka.{ Done, NotUsed }
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.alpakka.cassandra.scaladsl.{CassandraSink, CassandraSource}
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.alpakka.cassandra.scaladsl.{ CassandraSink, CassandraSource }
+import akka.stream.scaladsl.{ Sink, Source }
 import com.datastax.driver.core._
 import hmda.model.fi.lar.LoanApplicationRegister
 import hmda.query.repository.CassandraRepository
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 trait FilingCassandraRepository extends CassandraRepository[LoanApplicationRegister] {
 
@@ -18,18 +18,57 @@ trait FilingCassandraRepository extends CassandraRepository[LoanApplicationRegis
   implicit val ec: ExecutionContext
 
   def preparedStatement(implicit session: Session): PreparedStatement = {
-    session.prepare("")
+    session.prepare(s"INSERT INTO $keyspace.lars" +
+      s"(id," +
+      s"respondent_id," +
+      s"agency_code," +
+      s"loan_id," +
+      s"application_type," +
+      s"loan_type," +
+      s"property_type," +
+      s"purpose," +
+      s"occupancy," +
+      s"amount," +
+      s"preapprovals," +
+      s"action_taken_type," +
+      s"action_taken_date," +
+      s"msa," +
+      s"state," +
+      s"county," +
+      s"tract," +
+      s"ethnicity," +
+      s"co_ethnicity," +
+      s"race1," +
+      s"race2," +
+      s"race3," +
+      s"race4," +
+      s"race5," +
+      s"co_race1," +
+      s"co_race2," +
+      s"co_race3," +
+      s"co_race4," +
+      s"co_race5" +
+      s"sex," +
+      s"co_sex," +
+      s"income," +
+      s"purchaser_type," +
+      s"denial1," +
+      s"denial2," +
+      s"denial3," +
+      s"rate_spread," +
+      s"hoepa_status," +
+      s"lien_status) " +
+      s" VALUES " +
+      s"(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,)")
   }
 
   val statementBinder = (lar: LoanApplicationRegister, statement: PreparedStatement) =>
-    statement.bind(
-
-    )
+    statement.bind()
 
   override def createTable(): ResultSet = {
     val query =
       s"""
-         |CREATE TABLE IF NOT EXISTS $keyspace.lar(
+         |CREATE TABLE IF NOT EXISTS $keyspace.lars(
          |
          |)
        """.stripMargin
@@ -40,7 +79,7 @@ trait FilingCassandraRepository extends CassandraRepository[LoanApplicationRegis
   override def dropTable(): ResultSet = {
     val query =
       s"""
-         |DROP TABLE IF EXISTS $keyspace.lar;
+         |DROP TABLE IF EXISTS $keyspace.lars;
        """.stripMargin
 
     session.execute(query)
@@ -52,9 +91,8 @@ trait FilingCassandraRepository extends CassandraRepository[LoanApplicationRegis
   }
 
   override def readData(fetchSize: Int): Future[Seq[Row]] = {
-    val statement = new SimpleStatement(s"SELECT * FROM $keyspace.lar").setFetchSize(fetchSize)
+    val statement = new SimpleStatement(s"SELECT * FROM $keyspace.lars").setFetchSize(fetchSize)
     CassandraSource(statement).runWith(Sink.seq).mapTo[Seq[Row]]
   }
-
 
 }
