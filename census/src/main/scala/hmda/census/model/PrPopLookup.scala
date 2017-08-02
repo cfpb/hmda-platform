7c0d18ae06b084568d4b6e926fbaa4b7c579dc87
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-08-02 15:16:21 -0400

    Remove unused variables

--- census/src/main/scala/hmda/census/model/PrPopLookup.scala
+++ census/src/main/scala/hmda/census/model/PrPopLookup.scala
@@ -9,23 +9,10 @@ object PrPopLookup extends CbsaResourceUtils {
     val lines = csvLines("/PRM-EST00INT-AGESEX-5YR.csv")
 
     lines.drop(1).map { values =>
-      val sumlev = values(0)
       val countyFips = leftPad(3, values(1))
-      val municipioName = values(2)
       val sex = values(3)
       val ageGrp = values(4)
       val popBase2000 = values(5).toInt
-      val popEst2000 = values(6).toInt
-      val popEst2001 = values(7).toInt
-      val popEst2002 = values(8).toInt
-      val popEst2003 = values(9).toInt
-      val popEst2004 = values(10).toInt
-      val popEst2005 = values(11).toInt
-      val popEst2006 = values(12).toInt
-      val popEst2007 = values(13).toInt
-      val popEst2008 = values(14).toInt
-      val popEst2009 = values(15).toInt
-      val popEst2010 = values(16).toInt
 
       PrPopulation(
         "72" + countyFips,
