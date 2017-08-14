1713e47c777c9dec4139e349e39eb2af2d35d583
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-08-14 15:53:46 -0400

    Add query and validation roles

--- cluster/src/main/scala/hmda/cluster/HmdaPlatform.scala
+++ cluster/src/main/scala/hmda/cluster/HmdaPlatform.scala
@@ -3,11 +3,17 @@ package hmda.cluster
 import org.slf4j.LoggerFactory
 import hmda.cluster.HmdaConfig._
 import akka.actor._
+import akka.pattern.ask
 import akka.cluster.Cluster
 import akka.cluster.http.management.ClusterHttpManagement
+import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
+import hmda.persistence.model.HmdaSupervisorActor.FindActorByName
 import hmda.publication.HmdaPublication
 import hmda.query.HmdaQuerySupervisor
+import hmda.query.view.institutions.InstitutionView
+import hmda.validation.ValidationStats
+import scala.concurrent.duration._
 
 object HmdaPlatform extends App {
 
@@ -17,6 +23,9 @@ object HmdaPlatform extends App {
   val system = ActorSystem(configuration.getString("clustering.name"), config)
   val cluster = Cluster(system)
 
+  val actorTimeout = config.getInt("hmda.actor.timeout")
+  implicit val timeout = Timeout(actorTimeout.seconds)
+
   //Start API
   if (cluster.selfRoles.contains("api")) {
     ClusterHttpManagement(cluster).start()
@@ -24,12 +33,19 @@ object HmdaPlatform extends App {
 
   //Start Query
   if (cluster.selfRoles.contains("query")) {
-    system.actorOf(Props[HmdaQuerySupervisor], "query-supervisor")
+    val querySupervisor = system.actorOf(Props[HmdaQuerySupervisor], "query-supervisor")
+    val institutionViewF = (querySupervisor ? FindActorByName(InstitutionView.name))
+      .mapTo[ActorRef]
   }
 
   //Start Publication
   if (cluster.selfRoles.contains("publication")) {
     system.actorOf(Props[HmdaPublication], "publication")
+  }
+
+  //Start Validation
+  if (cluster.selfRoles.contains("validation")) {
+    system.actorOf(ValidationStats.props(), "validation-stats")
   }
 
 }
