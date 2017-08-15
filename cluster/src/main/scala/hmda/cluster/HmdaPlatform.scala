663c65b1245018faeda650513988cf5a98f5393e
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-08-15 11:20:59 -0400

    Add persistence to cluster roles

--- cluster/src/main/scala/hmda/cluster/HmdaPlatform.scala
+++ cluster/src/main/scala/hmda/cluster/HmdaPlatform.scala
@@ -9,7 +9,10 @@ import akka.cluster.http.management.ClusterHttpManagement
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import hmda.api.{ HmdaAdminApi, HmdaFilingApi, HmdaPublicApi }
+import hmda.persistence.HmdaSupervisor
+import hmda.persistence.institutions.InstitutionPersistence
 import hmda.persistence.model.HmdaSupervisorActor.FindActorByName
+import hmda.persistence.processing.SingleLarValidation
 import hmda.publication.HmdaPublication
 import hmda.query.{ HmdaProjectionQuery, HmdaQuerySupervisor }
 import hmda.query.view.institutions.InstitutionView
@@ -31,9 +34,21 @@ object HmdaPlatform extends App {
   //Start API
   if (cluster.selfRoles.contains("api")) {
     ClusterHttpManagement(cluster).start()
-    system.actorOf(HmdaFilingApi.props(), "hmda-filing-api")
-    system.actorOf(HmdaAdminApi.props(), "hmda-admin-api")
-    system.actorOf(HmdaPublicApi.props(), "hmda-public-api")
+    system.actorOf(HmdaFilingApi.props().withDispatcher("api-dispatcher"), "hmda-filing-api")
+    system.actorOf(HmdaAdminApi.props().withDispatcher("api-dispatcher"), "hmda-admin-api")
+    system.actorOf(HmdaPublicApi.props().withDispatcher("api-dispatcher"), "hmda-public-api")
+  }
+
+  //Start Persistence
+  if (cluster.selfRoles.contains("persistence")) {
+    val supervisor = system.actorOf(HmdaSupervisor.props().withDispatcher("persistence-dispatcher"), "supervisor")
+    implicit val ec = system.dispatchers.lookup("persistence-dispatcher")
+    (supervisor ? FindActorByName(SingleLarValidation.name))
+      .mapTo[ActorRef]
+      .map(a => log.info(s"Started single lar validator at ${a.path}"))
+    (supervisor ? FindActorByName(InstitutionPersistence.name))
+      .mapTo[ActorRef]
+      .map(a => log.info(s"Started institutions at ${a.path}"))
   }
 
   //Start Query
@@ -42,8 +57,10 @@ object HmdaPlatform extends App {
       Props[HmdaQuerySupervisor].withDispatcher("query-dispatcher"),
       "query-supervisor"
     )
-    val institutionViewF = (querySupervisor ? FindActorByName(InstitutionView.name))
+    implicit val ec = system.dispatchers.lookup("query-dispatcher")
+    (querySupervisor ? FindActorByName(InstitutionView.name))
       .mapTo[ActorRef]
+      .map(a => log.info(s"Started institutions view at ${a.path}"))
     HmdaProjectionQuery.startUp(system)
   }
 
@@ -56,5 +73,20 @@ object HmdaPlatform extends App {
   if (cluster.selfRoles.contains("validation")) {
     system.actorOf(ValidationStats.props().withDispatcher("validation-dispatcher"), "validation-stats")
   }
+
+  //  private def cleanup(): Unit = {
+  //    // Delete persistence journal
+  //    val file = new File("target/journal")
+  //    if (file.isDirectory) {
+  //      log.info("CLEANING JOURNAL")
+  //      file.listFiles.foreach(f => f.delete())
+  //    }
+  //
+  //    val larRepository = new LarRepository(config)
+  //    val institutionRepository = new InstitutionRepository(config)
+  //
+  //    larRepository.dropSchema()
+  //    institutionRepository.dropSchema()
+  //  }
 
 }
