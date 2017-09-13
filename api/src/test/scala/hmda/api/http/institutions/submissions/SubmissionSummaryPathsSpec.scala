1881244484e5c819092de204c0f1526d7cc08cfd
Betsy Lorton <elorton@gmail.com>
2017-09-13 10:16:36 -0400

    Remove unused import

--- api/src/test/scala/hmda/api/http/institutions/submissions/SubmissionSummaryPathsSpec.scala
+++ api/src/test/scala/hmda/api/http/institutions/submissions/SubmissionSummaryPathsSpec.scala
@@ -4,7 +4,6 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import hmda.api.http.InstitutionHttpApiSpec
 import hmda.api.model.institutions.submissions.{ ContactSummary, FileSummary, RespondentSummary, SubmissionSummary }
-import hmda.model.fi.Submission
 import hmda.api.model.ErrorResponse
 import org.scalatest.BeforeAndAfterAll
 
