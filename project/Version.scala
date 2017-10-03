b3d1bdfe50d5133db7f7bf6ee7de7c7b1cb3f393
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-10-03 11:14:11 -0400

    Upgrade alpakka to version 0.13

--- project/Version.scala
+++ project/Version.scala
@@ -20,6 +20,6 @@ object Version {
   val constructrVersion = "0.17.0"
   val constructrZookeeperVersion = "0.3.3"
   val cassandraUnit = "3.1.3.2"
-  val alpakka = "0.9"
+  val alpakka = "0.13"
   val cassandraDriver = "3.2.0"
 }
