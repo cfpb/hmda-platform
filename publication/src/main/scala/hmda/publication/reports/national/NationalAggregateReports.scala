42efe2f3367e4499a2bb973b3ffe42861b7573ee
Nick Grippin <ngrippin@discovertechnologies.com>
2017-10-10 13:21:39 -0400

    add engine and config

---
+++ publication/src/main/scala/hmda/publication/reports/national/NationalAggregateReports.scala
@@ -0,0 +1,29 @@
+package hmda.publication.reports.national
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+import com.typesafe.config.ConfigFactory
+import hmda.query.repository.filing.FilingCassandraRepository
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import spray.json._
+
+class NationalAggregateReports (val sys: ActorSystem, val mat: ActorMaterializer) extends FilingCassandraRepository {
+  override implicit def system: ActorSystem = sys
+  override implicit def materializer: ActorMaterializer = mat
+  val config = ConfigFactory.load()
+  val duration = config.getInt("hmda.actor-lookup-timeout")
+  implicit val timeout = Timeout(duration.seconds)
+
+  val larSource = readData(1000)
+
+  def generateReports(fipsCode: Int, respId: String): Future[Unit] = {
+
+    val n52F = N52.generate(larSource)
+    n52F.map { n52 =>
+      println(n52.toJson.prettyPrint)
+    }
+  }
+}
