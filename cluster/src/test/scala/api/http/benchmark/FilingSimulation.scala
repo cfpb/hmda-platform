82cabccfc3765a5b7f7798dceb63dffd181d74f4
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-10-11 11:21:31 -0400

    Add institution by id and create submission

--- cluster/src/test/scala/api/http/benchmark/FilingSimulation.scala
+++ cluster/src/test/scala/api/http/benchmark/FilingSimulation.scala
@@ -16,8 +16,6 @@ class FilingSimulation extends Simulation {
   val rampUpTime = config.getInt("hmda.benchmark.rampUpTime")
   val feeder = csv(config.getString("hmda.benchmark.feederFile"))
 
-  val institutionIds = (1 to nrOfUsers).toList
-
   val httpProtocol = http
     .baseURL(s"http://$host:$port")
     .acceptHeader("text/html,application/xhtml+xml,application/json;q=0.9,*/*;q=0.8")
@@ -26,34 +24,39 @@ class FilingSimulation extends Simulation {
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
     .header("cfpb-hmda-username", "user")
-    //.header("cfpb-hmda-institutions", institutionIds.mkString(","))
     .disableCaching
 
   object Institutions {
 
-    val hmdaFiling =
+    val hmdaFilingApi =
       feed(feeder)
-        .exec(http("Search")
+        .exec(http("Search Institutions")
           .get("/institutions")
           .header("cfpb-hmda-institutions", "${institutionId}")
           .check(status is 200))
+        .pause(1)
+        .exec(http("Institution by id")
+          .get("/institutions/${institutionId}")
+          .header("cfpb-hmda-institutions", "${institutionId}")
+          .check(status is 200)
+          .check(jsonPath("$.institution.id") is "${institutionId}"))
         .pause(1)
         .exec(http("List Filings")
           .get("/institutions/${institutionId}/filings/2017")
           .header("cfpb-hmda-institutions", "${institutionId}")
           .check(status is 200))
-
-    def filings(institutionId: String) = {
-      exec(http("List Filings")
-        .get(s"/institutions/$institutionId/filings/2017")
-        .header("cfpb-hmda-institutions", institutionId)
-        .check(status is 200))
-    }
+        .pause(2)
+        .exec(http("Create Submission")
+          .post("/institutions/${institutionId}/filings/2017/submissions")
+          .header("cfpb-hmda-institutions", "${institutionId}")
+          .check(status is 201)
+          .check(jsonPath("$.id.sequenceNumber").saveAs("submissionId")))
+        .pause(1)
 
   }
 
   val user = scenario("HMDA User")
-    .exec(Institutions.hmdaFiling)
+    .exec(Institutions.hmdaFilingApi)
 
   setUp(
     user.inject(rampUsers(nrOfUsers) over (rampUpTime seconds)).protocols(httpProtocol)
