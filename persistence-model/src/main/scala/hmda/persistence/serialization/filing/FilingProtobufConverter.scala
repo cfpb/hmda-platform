1fdcafa080c94b66728c6612da50741d695a58ff
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-11-02 10:52:23 -0400

    Add protobuf serialization for filing commands

--- persistence-model/src/main/scala/hmda/persistence/serialization/filing/FilingProtobufConverter.scala
+++ persistence-model/src/main/scala/hmda/persistence/serialization/filing/FilingProtobufConverter.scala
@@ -1,10 +1,50 @@
 package hmda.persistence.serialization.filing
 
 import hmda.model.fi._
+import hmda.persistence.messages.commands.filing.FilingCommands.{ CreateFiling, GetFilingByPeriod, UpdateFilingStatus }
 import hmda.persistence.messages.events.institutions.FilingEvents.{ FilingCreated, FilingStatusUpdated }
+import hmda.persistence.model.serialization.FilingCommands.{ CreateFilingMessage, GetFilingByPeriodMessage, UpdateFilingStatusMessage }
 import hmda.persistence.model.serialization.FilingEvents._
 
 object FilingProtobufConverter {
+
+  def createFilingToProtobuf(obj: CreateFiling): CreateFilingMessage = {
+    CreateFilingMessage(
+      filing = Some(filingToProtobuf(obj.filing))
+    )
+  }
+
+  def createFilingFromProtobuf(msg: CreateFilingMessage): CreateFiling = {
+    CreateFiling(
+      filing = filingFromProtobuf(msg.filing.getOrElse(FilingMessage()))
+    )
+  }
+
+  def updateFilingStatusToProtobuf(obj: UpdateFilingStatus): UpdateFilingStatusMessage = {
+    UpdateFilingStatusMessage(
+      period = obj.period,
+      status = Some(filingStatusToProtobuf(obj.status))
+    )
+  }
+
+  def updateFilingStatusFromProtobuf(msg: UpdateFilingStatusMessage): UpdateFilingStatus = {
+    UpdateFilingStatus(
+      period = msg.period,
+      status = filingStatusFromProtobuf(msg.status.getOrElse(FilingStatusMessage()))
+    )
+  }
+
+  def getFilingByPeriodToProtobuf(obj: GetFilingByPeriod): GetFilingByPeriodMessage = {
+    GetFilingByPeriodMessage(
+      period = obj.period
+    )
+  }
+
+  def getFilingByPeriodFromProtobuf(msg: GetFilingByPeriodMessage): GetFilingByPeriod = {
+    GetFilingByPeriod(
+      period = msg.period
+    )
+  }
 
   def filingCreatedToProtobuf(obj: FilingCreated): FilingCreatedMessage = {
     FilingCreatedMessage(
