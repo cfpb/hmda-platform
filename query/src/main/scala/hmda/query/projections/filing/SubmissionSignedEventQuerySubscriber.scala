c51f5fd3fc1109c799b8e41528165775fa5e5f38
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-11-08 09:43:25 -0500

    Rename Cassandra repository logger to avoid compile conflicts

--- query/src/main/scala/hmda/query/projections/filing/SubmissionSignedEventQuerySubscriber.scala
+++ query/src/main/scala/hmda/query/projections/filing/SubmissionSignedEventQuerySubscriber.scala
@@ -1,18 +1,24 @@
 package hmda.query.projections.filing
 
-import akka.actor.Props
+import akka.actor.{ ActorSystem, Props }
 import akka.cluster.pubsub.DistributedPubSub
 import akka.cluster.pubsub.DistributedPubSubMediator.{ Subscribe, SubscribeAck }
+import akka.stream.ActorMaterializer
+import hmda.model.fi.lar.LoanApplicationRegister
+import hmda.persistence.messages.events.processing.CommonHmdaValidatorEvents.LarValidated
 import hmda.persistence.messages.events.processing.PubSubEvents.SubmissionSigned
 import hmda.persistence.model.HmdaActor
 import hmda.persistence.processing.PubSubTopics
+import hmda.persistence.processing.HmdaQuery._
+import akka.stream.alpakka.cassandra.scaladsl.CassandraSink
+import hmda.query.repository.filing.FilingCassandraRepository
 
 object SubmissionSignedEventQuerySubscriber {
   val name = "SubmissionSignedEventQuerySubscriber"
   def props(): Props = Props(new SubmissionSignedEventQuerySubscriber())
 }
 
-class SubmissionSignedEventQuerySubscriber() extends HmdaActor {
+class SubmissionSignedEventQuerySubscriber() extends HmdaActor with FilingCassandraRepository {
 
   val mediator = DistributedPubSub(context.system).mediator
 
@@ -21,14 +27,27 @@ class SubmissionSignedEventQuerySubscriber() extends HmdaActor {
   def receive: Receive = {
 
     case s: String =>
-      log.info("Got {}", s)
+      repositoryLog.info("Got {}", s)
 
     case SubscribeAck(Subscribe(PubSubTopics.submissionSigned, None, `self`)) =>
-      log.info(s"Subscribed to ${PubSubTopics.submissionSigned}")
+      repositoryLog.info(s"Subscribed to ${PubSubTopics.submissionSigned}")
 
     case SubmissionSigned(submissionId) =>
-      log.info(s"Received submission signed event with submission id: ${submissionId.toString}")
+      repositoryLog.info(s"Received submission signed event with submission id: ${submissionId.toString}")
+      val persistenceId = s"HmdaFileValidator-$submissionId"
+      val larSource = events(persistenceId).map {
+        case LarValidated(lar, _) => lar
+      }
+
+      val sink = CassandraSink[LoanApplicationRegister](parallelism = 2, preparedStatement, statementBinder)
+      larSource
+        .map { lar => repositoryLog.info(lar.toCSV); lar }
+        .to(sink).run()
 
     case _ => //do nothing
   }
+
+  override implicit def system: ActorSystem = context.system
+
+  override implicit def materializer: ActorMaterializer = ActorMaterializer()
 }
