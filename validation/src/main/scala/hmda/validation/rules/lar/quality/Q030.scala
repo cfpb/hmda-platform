c06e567cdfba0fbcd198ef92e96067cb51e65786
Nick Grippin <ngrippin@discovertechnologies.com>
2017-11-14 14:03:33 -0500

    Q030 fix

--- validation/src/main/scala/hmda/validation/rules/lar/quality/Q030.scala
+++ validation/src/main/scala/hmda/validation/rules/lar/quality/Q030.scala
@@ -1,48 +1,20 @@
 package hmda.validation.rules.lar.quality
 
-import hmda.model.fi.lar.{ Geography, LoanApplicationRegister }
-import hmda.model.institution.Institution
-import hmda.validation.context.ValidationContext
+import hmda.model.fi.lar.LoanApplicationRegister
 import hmda.validation.dsl.PredicateCommon._
-import hmda.validation.dsl.PredicateGeo._
 import hmda.validation.dsl.PredicateSyntax._
-import hmda.validation.dsl.{ Failure, Result, Success }
-import hmda.validation.rules.{ EditCheck, IfInstitutionPresentIn }
+import hmda.validation.dsl.Result
+import hmda.validation.rules.EditCheck
 
-class Q030 private (institution: Institution) extends EditCheck[LoanApplicationRegister] {
+object Q030 extends EditCheck[LoanApplicationRegister] {
   override def name: String = "Q030"
 
   override def apply(lar: LoanApplicationRegister): Result = {
     when(lar.actionTakenType is containedIn(1 to 6)) {
-      val geo: Geography = lar.geography
-      (geo, institution.cra) match {
-        case (Geography("NA", "NA", "NA", "NA"), false) => Success()
-        case (Geography(_, "NA", _, _), _) => Failure()
-        case (Geography(_, _, "NA", _), _) => Failure()
-        case _ =>
-          when(institution.cra is false) {
-            when(geo is smallCounty) {
-              geo.tract is "NA"
-            }
-          } and when(geo.tract is "NA") {
-            geo is smallCounty // and therefore also a valid state/county combination; no need to check again.
-          } and when(geo.msa not "NA") {
-            (geo is validStateCountyMsaCombination) and
-              when(geo.tract not "NA") {
-                geo is validCompleteCombination
-              }
-          } and when(geo.msa is "NA") {
-            when(geo.tract not "NA") {
-              geo is validStateCountyTractCombination
-            }
-          }
-      }
+      (lar.geography.msa not "NA") and
+        (lar.geography.tract not "NA") and
+        (lar.geography.county not "NA") and
+        (lar.geography.state not "NA")
     }
-  }
-}
-
-object Q030 {
-  def inContext(context: ValidationContext): EditCheck[LoanApplicationRegister] = {
-    IfInstitutionPresentIn(context) { new Q030(_) }
   }
 }
