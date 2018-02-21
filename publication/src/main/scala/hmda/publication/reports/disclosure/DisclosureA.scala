2e59cbb9c8f8eee486c2e9f4e27755ec8bacf02f
Nick Grippin <ngrippin@discovertechnologies.com>
2018-02-21 11:35:56 -0500

    reports done

--- publication/src/main/scala/hmda/publication/reports/disclosure/DisclosureA.scala
+++ publication/src/main/scala/hmda/publication/reports/disclosure/DisclosureA.scala
@@ -23,15 +23,16 @@ import scala.concurrent.Future
 
 object A1 extends DisclosureA {
   val reportId = "A1"
-  def filters(lar: LoanApplicationRegister): Boolean = {
+  def filters(lar: LoanApplicationRegister, msa: Int): Boolean = {
     (1 to 4).contains(lar.loan.loanType) &&
-      lar.loan.propertyType == 1
+      lar.loan.propertyType == 1 &&
+      lar.geography.msa.toInt == msa
   }
 }
 
 object A1W extends DisclosureA {
   val reportId = "A1W"
-  def filters(lar: LoanApplicationRegister): Boolean = {
+  def filters(lar: LoanApplicationRegister, msa: Int): Boolean = {
     (1 to 4).contains(lar.loan.loanType) &&
       lar.loan.propertyType == 1
   }
@@ -39,7 +40,7 @@ object A1W extends DisclosureA {
 
 trait DisclosureA extends DisclosureReport {
   val reportId: String
-  def filters(lar: LoanApplicationRegister): Boolean
+  def filters(lar: LoanApplicationRegister, msa: Int): Boolean
 
   val dispositions = List(ApplicationReceived, LoansOriginated, ApprovedButNotAccepted,
     ApplicationsDenied, ApplicationsWithdrawn, ClosedForIncompleteness)
@@ -53,13 +54,8 @@ trait DisclosureA extends DisclosureReport {
     val metaData = ReportsMetaDataLookup.values(reportId)
 
     val lars = larSource
-      .filter(lar => lar.geography.msa.toInt == fipsCode)
-      .filter(filters)
+      .filter(lar => filters(lar, fipsCode))
 
-    val incomeIntervals = larsByIncomeInterval(
-      lars.filter(lar => lar.applicant.income != "NA"),
-      calculateMedianIncomeIntervals(fipsCode)
-    )
     val msa = msaReport(fipsCode.toString).toJsonFormat
     val reportDate = formattedCurrentDate
     val yearF = calculateYear(larSource)
@@ -67,45 +63,14 @@ trait DisclosureA extends DisclosureReport {
     for {
       year <- yearF
 
-      e1 <- dispositionsOutput(filterEthnicity(lars, HispanicOrLatino))
-      e1g <- dispositionsByGender(filterEthnicity(lars, HispanicOrLatino))
-      e2 <- dispositionsOutput(filterEthnicity(lars, NotHispanicOrLatino))
-      e2g <- dispositionsByGender(filterEthnicity(lars, NotHispanicOrLatino))
-      e3 <- dispositionsOutput(filterEthnicity(lars, JointEthnicity))
-      e3g <- dispositionsByGender(filterEthnicity(lars, JointEthnicity))
-      e4 <- dispositionsOutput(filterEthnicity(lars, NotAvailable))
-      e4g <- dispositionsByGender(filterEthnicity(lars, NotAvailable))
-
-      r1 <- dispositionsOutput(filterRace(lars, AmericanIndianOrAlaskaNative))
-      r1g <- dispositionsByGender(filterRace(lars, AmericanIndianOrAlaskaNative))
-      r2 <- dispositionsOutput(filterRace(lars, Asian))
-      r2g <- dispositionsByGender(filterRace(lars, Asian))
-      r3 <- dispositionsOutput(filterRace(lars, BlackOrAfricanAmerican))
-      r3g <- dispositionsByGender(filterRace(lars, BlackOrAfricanAmerican))
-      r4 <- dispositionsOutput(filterRace(lars, HawaiianOrPacific))
-      r4g <- dispositionsByGender(filterRace(lars, HawaiianOrPacific))
-      r5 <- dispositionsOutput(filterRace(lars, White))
-      r5g <- dispositionsByGender(filterRace(lars, White))
-      r6 <- dispositionsOutput(filterRace(lars, TwoOrMoreMinority))
-      r6g <- dispositionsByGender(filterRace(lars, TwoOrMoreMinority))
-      r7 <- dispositionsOutput(filterRace(lars, JointRace))
-      r7g <- dispositionsByGender(filterRace(lars, JointRace))
-      r8 <- dispositionsOutput(filterRace(lars, NotProvided))
-      r8g <- dispositionsByGender(filterRace(lars, NotProvided))
-
-      m1 <- dispositionsOutput(filterMinorityStatus(lars, WhiteNonHispanic))
-      m1g <- dispositionsByGender(filterMinorityStatus(lars, WhiteNonHispanic))
-      m2 <- dispositionsOutput(filterMinorityStatus(lars, OtherIncludingHispanic))
-      m2g <- dispositionsByGender(filterMinorityStatus(lars, OtherIncludingHispanic))
-
-      i1 <- dispositionsOutput(incomeIntervals(LessThan50PercentOfMSAMedian))
-      i2 <- dispositionsOutput(incomeIntervals(Between50And79PercentOfMSAMedian))
-      i3 <- dispositionsOutput(incomeIntervals(Between80And99PercentOfMSAMedian))
-      i4 <- dispositionsOutput(incomeIntervals(Between100And119PercentOfMSAMedian))
-      i5 <- dispositionsOutput(incomeIntervals(GreaterThan120PercentOfMSAMedian))
-      i6 <- dispositionsOutput(lars.filter(lar => lar.applicant.income == "NA"))
-
-      total <- dispositionsOutput(lars)
+      received <- loanTypes(lars.filter(lar => (1 to 5).contains(lar.actionTakenType)))
+      originiated <- loanTypes(lars.filter(lar => lar.actionTakenType == 1))
+      appNotAcc <- loanTypes(lars.filter(lar => lar.actionTakenType == 2))
+      denied <- loanTypes(lars.filter(lar => lar.actionTakenType == 3))
+      withdrawn <- loanTypes(lars.filter(lar => lar.actionTakenType == 4))
+      closed <- loanTypes(lars.filter(lar => lar.actionTakenType == 5))
+      preapproval <- loanTypes(lars.filter(lar => lar.actionTakenType == 1 && lar.preapprovals ==1))
+      sold <- loanTypes(lars.filter(lar => (1 to 9).contains(lar.purchaserType)))
     } yield {
       val report = s"""
          |{
@@ -116,154 +81,108 @@ trait DisclosureA extends DisclosureReport {
          |    "description": "${metaData.description}",
          |    "year": "$year",
          |    "reportDate": "$reportDate",
-         |    "msa": $msa,
-         |    "ethnicities": [
+         |    "msa": $msa,,
+         |    "dispositions": [
          |        {
-         |            "ethnicity": "Hispanic or Latino",
-         |            "dispositions": $e1,
-         |            "genders": $e1g
+         |            "disposition": "Applications Received",
+         |            "loantypes": $received
          |        },
          |        {
-         |            "ethnicity": "Not Hispanic or Latino",
-         |            "dispositions": $e2,
-         |            "genders": $e2g
+         |            "disposition": "Loans Originated",
+         |            "loantypes": $originiated
          |        },
          |        {
-         |            "ethnicity": "Joint (Hispanic or Latino/Not Hispanic or Latino)",
-         |            "dispositions": $e3,
-         |            "genders": $e3g
+         |            "disposition": "Apps. Approved But Not Accepted",
+         |            "loantypes": $appNotAcc
          |        },
          |        {
-         |            "ethnicity": "Ethnicity Not Available",
-         |            "dispositions": $e4,
-         |            "genders": $e4g
+         |            "disposition": "Applications Denied",
+         |            "loantypes": $denied
+         |        },
+         |        {
+         |            "disposition": "Applications Withdrawn",
+         |            "loantypes": $withdrawn
+         |        },
+         |        {
+         |            "disposition": "Files Closed For Incompleteness",
+         |            "loantypes": $closed
+         |        },
+         |        {
+         |            "disposition": "Preapprovals Resulting in Originations",
+         |            "loantypes": $preapproval
+         |        },
+         |        {
+         |            "disposition": "Loans Sold",
+         |            "loantypes": $sold
          |        }
-         |    ],
-         |    "races": [
-         |        {
-         |            "race": "American Indian/Alaska Native",
-         |            "dispositions": $r1,
-         |            "genders": $r1g
-         |        },
-         |        {
-         |            "race": "Asian",
-         |            "dispositions": $r2,
-         |            "genders": $r2g
-         |        },
-         |        {
-         |            "race": "Black or African American",
-         |            "dispositions": $r3,
-         |            "genders": $r3g
-         |        },
-         |        {
-         |            "race": "Native Hawaiian or Other Pacific Islander",
-         |            "dispositions": $r4,
-         |            "genders": $r4g
-         |        },
-         |        {
-         |            "race": "White",
-         |            "dispositions": $r5,
-         |            "genders": $r5g
-         |        },
-         |        {
-         |            "race": "2 or more minority races",
-         |            "dispositions": $r6,
-         |            "genders": $r6g
-         |        },
-         |        {
-         |            "race": "Joint (White/Minority Race)",
-         |            "dispositions": $r7,
-         |            "genders": $r7g
-         |        },
-         |        {
-         |            "race": "Race Not Available",
-         |            "dispositions": $r8,
-         |            "genders": $r8g
-         |        }
-         |    ],
-         |    "minorityStatuses": [
-         |        {
-         |            "minorityStatus": "White Non-Hispanic",
-         |            "dispositions": $m1,
-         |            "genders": $m1g
-         |        },
-         |        {
-         |            "minorityStatus": "Others, Including Hispanic",
-         |            "dispositions": $m2,
-         |            "genders": $m2g
-         |        }
-         |    ],
-         |    "incomes": [
-         |        {
-         |            "income": "Less than 50% of MSA/MD median",
-         |            "dispositions": $i1
-         |        },
-         |        {
-         |            "income": "50-79% of MSA/MD median",
-         |            "dispositions": $i2
-         |        },
-         |        {
-         |            "income": "80-99% of MSA/MD median",
-         |            "dispositions": $i3
-         |        },
-         |        {
-         |            "income": "100-119% of MSA/MD median",
-         |            "dispositions": $i4
-         |        },
-         |        {
-         |            "income": "120% or more of MSA/MD median",
-         |            "dispositions": $i5
-         |        },
-         |        {
-         |            "income": "Income Not Available",
-         |            "dispositions": $i6
-         |        }
-         |    ],
-         |    "total": $total
+         |    ]
          |}
-         |
        """.stripMargin
 
       DisclosureReportPayload(metaData.reportTable, fipsCode.toString, report)
     }
   }
 
-  private def dispositionsOutput[ec: EC, mat: MAT, as: AS](larSource: Source[LoanApplicationRegister, NotUsed]): Future[String] = {
-    val calculatedDispositions: Future[List[ValueDisposition]] = Future.sequence(
-      dispositions.map(_.calculateValueDisposition(larSource))
-    )
-
-    calculatedDispositions.map { list =>
-      list.map(disp => disp.toJsonFormat).mkString("[", ",", "]")
+  private def loanTypes[ec: EC, mat: MAT, as: AS](larSource: Source[LoanApplicationRegister, NotUsed]): Future[String] = {
+    for {
+      conv <- purposesOutput(larSource.filter(lar => lar.loan.loanType == 1))
+      fha <- purposesOutput(larSource.filter(lar => lar.loan.loanType == 2))
+      va <- purposesOutput(larSource.filter(lar => lar.loan.loanType == 3))
+      fsa <- purposesOutput(larSource.filter(lar => lar.loan.loanType == 4))
+    } yield {
+      s"""
+         |[
+         |  {
+         |    "loantype": "Conventional",
+         |    "purposes": $conv
+         |  },
+         |  {
+         |    "loantype": "FHA",
+         |    "purposes": $fha
+         |  },
+         |  {
+         |    "loantype": "VA",
+         |    "purposes": $va
+         |  },
+         |  {
+         |    "loantype": "FSA/RHS",
+         |    "purposes": $fsa
+         |  }
+         |]
+     """.stripMargin
     }
   }
 
-  private def dispositionsByGender[ec: EC, mat: MAT, as: AS](larSource: Source[LoanApplicationRegister, NotUsed]): Future[String] = {
+  private def purposesOutput[ec: EC, mat: MAT, as: AS](larSource: Source[LoanApplicationRegister, NotUsed]): Future[String] = {
     for {
-      male <- dispositionsOutput(filterGender(larSource, Male))
-      female <- dispositionsOutput(filterGender(larSource, Female))
-      joint <- dispositionsOutput(filterGender(larSource, JointGender))
+      homePurchaseFirst <- count(larSource.filter(lar => lar.lienStatus == 1 && lar.loan.purpose == 1))
+      homePurchaseJunior <- count(larSource.filter(lar => lar.lienStatus == 2 && lar.loan.purpose == 1))
+      refinanceFirst <- count(larSource.filter(lar => lar.lienStatus == 1 && lar.loan.purpose == 3))
+      refinanceJunior <- count(larSource.filter(lar => lar.lienStatus == 2 && lar.loan.purpose == 3))
+      homeImprovementFirst <- count(larSource.filter(lar => lar.lienStatus == 1 && lar.loan.purpose == 2))
+      homeImprovementJunior <- count(larSource.filter(lar => lar.lienStatus == 2 && lar.loan.purpose == 2))
+      homeImprovementNo <- count(larSource.filter(lar => lar.lienStatus == 3 && lar.loan.purpose == 2))
     } yield {
-
       s"""
-         |
          |[
-         | {
-         |     "gender": "Male",
-         |     "dispositions": $male
-         | },
-         | {
-         |     "gender": "Female",
-         |     "dispositions": $female
-         | },
-         | {
-         |     "gender": "Joint (Male/Female)",
-         |     "dispositions": $joint
-         | }
+         |  {
+         |    "purpose": "Home Purchase",
+         |    "firstliencount": $homePurchaseFirst,
+         |    "juniorliencount": $homePurchaseJunior
+         |  },
+         |  {
+         |    "purpose": "Refinance",
+         |    "firstliencount": $refinanceFirst,
+         |    "juniorliencount": $refinanceJunior
+         |  },
+         |  {
+         |    "purpose": "Home Improvement",
+         |    "firstliencount": $homeImprovementFirst,
+         |    "juniorliencount": $homeImprovementJunior,
+         |    "noliencount": $homeImprovementNo
+         |  }
          |]
-         |
      """.stripMargin
-
     }
   }
 
