feb14f75d542742f1a86335fb3a2cae057e08b9e
Nick Grippin <ngrippin@discovertechnologies.com>
2018-03-05 13:30:47 -0500

    fix merge

--- publication/src/main/scala/hmda/publication/reports/disclosure/D1.scala
+++ publication/src/main/scala/hmda/publication/reports/disclosure/D1.scala
@@ -20,7 +20,8 @@ object D1 extends DisclosureReport with DispositionProtocol {
   def generate[ec: EC, mat: MAT, as: AS](
     larSource: Source[LoanApplicationRegister, NotUsed],
     fipsCode: Int,
-    institution: Institution
+    institution: Institution,
+    msaList: List[Int]
   ): Future[DisclosureReportPayload] = {
     val metaData = ReportsMetaDataLookup.values(reportId)
 
