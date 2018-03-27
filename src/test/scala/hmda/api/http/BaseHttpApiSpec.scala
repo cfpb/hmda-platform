2108fde296c7463fa47d7c2df1df8aa7a4fe4f4b
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-03-27 09:48:46 -0400

    Complete test for root path

--- src/test/scala/hmda/api/http/BaseHttpApiSpec.scala
+++ src/test/scala/hmda/api/http/BaseHttpApiSpec.scala
@@ -2,8 +2,13 @@ package hmda.api.http
 
 import akka.event.{LoggingAdapter, NoLogging}
 import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.headers.{HttpEncodings, `Accept-Encoding`}
+import akka.http.scaladsl.model.headers.HttpEncodings._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import hmda.api.http.model.common.HmdaServiceStatus
 import org.scalatest.{MustMatchers, WordSpec}
+import io.circe.generic.auto._
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 
 import scala.concurrent.ExecutionContext
 
@@ -19,6 +24,15 @@ class BaseHttpApiSpec
     "return OK for GET requests to the root path" in {
       Get() ~> routes("hmda-public-api") ~> check {
         response.status mustBe StatusCodes.OK
+        val serviceStatus = responseAs[HmdaServiceStatus]
+        serviceStatus.service mustBe "hmda-public-api"
+        serviceStatus.status mustBe "OK"
+      }
+    }
+
+    "use requested encoding for root path" in {
+      Get().addHeader(`Accept-Encoding`(gzip)) ~> routes("hmda-public-api") ~> check {
+        response.encoding mustBe HttpEncodings.gzip
       }
     }
   }
