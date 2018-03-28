346cfb89a2c5ba07ef726a5053f9b46acd91b453
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-03-28 14:09:42 -0400

    Add more tests to LAR Parser HTTP API

--- src/test/scala/hmda/api/http/public/LarValidationHttpApiSpec.scala
+++ src/test/scala/hmda/api/http/public/LarValidationHttpApiSpec.scala
@@ -28,13 +28,33 @@ class LarValidationHttpApiSpec
   val larCsv = lar.toCSV
 
   "LAR HTTP Service" must {
-    "parse a valid pipe delimited TS and return JSON representation" in {
+    "parse a valid pipe delimited LAR and return JSON representation" in {
       val larValidateRequest = LarValidateRequest(larCsv)
       Post("/lar/parse", larValidateRequest) ~> larRoutes ~> check {
         response.status mustBe StatusCodes.OK
         responseAs[LoanApplicationRegister] mustBe lar
       }
     }
+
+    "fail to parse an invalid pipe delimited LAR and return a list of errors" in {
+      val csv = larGen.sample.get.toCSV
+      val values = csv.split('|').map(_.trim)
+      val badValues = values.head.replace("2", "A") ++ values.tail
+      val badCsv = badValues.mkString("|")
+      Post("/lar/parse", LarValidateRequest(badCsv)) ~> larRoutes ~> check {
+        status mustBe StatusCodes.BadRequest
+        responseAs[List[String]] mustBe List("id is not numeric")
+      }
+    }
+
+    "fail to parse a valid pipe delimited LAR with too many fields and return an error" in {
+      Post("/lar/parse", LarValidateRequest(larCsv + "|too|many|fields")) ~> larRoutes ~> check {
+        status mustBe StatusCodes.BadRequest
+        responseAs[List[String]] mustBe List(
+          "An incorrect number of data fields were reported: 113 data fields were found, when 110 data fields were expected.")
+      }
+    }
+
   }
 
 }
