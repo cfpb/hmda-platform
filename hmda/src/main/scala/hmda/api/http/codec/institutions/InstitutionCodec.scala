71d5b8b4a1f3c2df4c1a7bf17bb558c10fc1d5ed
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-05-22 16:10:46 -0400

    Add institution codec

--- hmda/src/main/scala/hmda/api/http/codec/institutions/InstitutionCodec.scala
+++ hmda/src/main/scala/hmda/api/http/codec/institutions/InstitutionCodec.scala
@@ -1,6 +1,6 @@
 package hmda.api.http.codec.institutions
 
-import hmda.model.institution.{Agency, Institution, InstitutionType, Respondent}
+import hmda.model.institution._
 import io.circe.syntax._
 import io.circe.{Decoder, Encoder, HCursor, Json}
 import RespondentCodec._
@@ -32,10 +32,51 @@ object InstitutionCodec {
       )
     }
 
-
   implicit val institutionDecoder: Decoder[Institution] =
     new Decoder[Institution] {
-      override def apply(c: HCursor): Result[Institution] = ???
+      override def apply(c: HCursor): Result[Institution] =
+        for {
+          activityYear <- c.downField("activityYear").as[Int]
+          maybeLEI <- c.downField("LEI").as[String]
+          agency <- c.downField("agency").as[Int]
+          institutionType <- c.downField("institutionType").as[Int]
+          maybeInstitutionId2017 <- c.downField("institutionId2017").as[String]
+          maybeTaxId <- c.downField("taxId").as[String]
+          maybeRssdId <- c.downField("rssd").as[String]
+          maybeEmailDomain <- c.downField("emailDomain").as[String]
+          respondent <- c.downField("respondent").as[Respondent]
+          parent <- c.downField("parent").as[Parent]
+          assets <- c.downField("assets").as[Int]
+          otherLenderCode <- c.downField("otherLenderCode").as[Int]
+          topHolder <- c.downField("topHolder").as[TopHolder]
+          hmdaFiler <- c.downField("hmdaFiler").as[Boolean]
+        } yield {
+          val lei = if (maybeLEI == "") None else Some(maybeLEI)
+          val institutionId2017 =
+            if (maybeInstitutionId2017 == "") None
+            else Some(maybeInstitutionId2017)
+          val taxId = if (maybeTaxId == "") None else Some(maybeTaxId)
+          val rssd = if (maybeRssdId == "") None else Some(maybeRssdId)
+          val emailDomain =
+            if (maybeEmailDomain == "") None else Some(maybeEmailDomain)
+
+          Institution(
+            activityYear,
+            lei,
+            Some(Agency.valueOf(agency)),
+            Some(InstitutionType.valueOf(institutionType)),
+            institutionId2017,
+            taxId,
+            rssd,
+            emailDomain,
+            respondent,
+            parent,
+            Some(assets),
+            Some(otherLenderCode),
+            topHolder,
+            hmdaFiler
+          )
+        }
     }
 
 }
