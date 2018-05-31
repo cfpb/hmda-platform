9d0c109a8c467ab474f6ff47245d01fd2f713696
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-05-31 16:07:21 -0400

    Add institutions HTTP service tests

--- hmda/src/test/scala/hmda/api/http/admin/InstitutionAdminHttpApiSpec.scala
+++ hmda/src/test/scala/hmda/api/http/admin/InstitutionAdminHttpApiSpec.scala
@@ -1,17 +1,30 @@
 package hmda.api.http.admin
 
-import akka.actor.typed.ActorSystem
-import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+import akka.actor.typed.{ActorSystem, Props}
+import akka.cluster.sharding.typed.scaladsl.{ClusterSharding, EntityTypeKey}
 import akka.event.{LoggingAdapter, NoLogging}
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import hmda.persistence.AkkaCassandraPersistenceSpec
 import akka.actor.typed.scaladsl.adapter._
+import akka.cluster.sharding.typed.ClusterShardingSettings
+import akka.cluster.typed.{Cluster, Join}
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
 import hmda.model.institution.Institution
 import org.scalatest.MustMatchers
 import hmda.model.institution.InstitutionGenerators._
+import hmda.api.http.codec.institution.InstitutionCodec._
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+import hmda.persistence.institution.InstitutionPersistence
+import hmda.persistence.institution.InstitutionPersistence.{
+  InstitutionCommand,
+  InstitutionStop
+}
+import akka.testkit._
+import com.typesafe.config.ConfigFactory
+import hmda.api.http.model.admin.InstitutionDeletedResponse
+import io.circe.generic.auto._
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
@@ -24,6 +37,8 @@ class InstitutionAdminHttpApiSpec
 
   val duration = 10.seconds
 
+  implicit val routeTimeout = RouteTestTimeout(duration.dilated)
+
   override implicit val typedSystem: ActorSystem[_] = system.toTyped
   override implicit val materializer: ActorMaterializer = ActorMaterializer()
   override val log: LoggingAdapter = NoLogging
@@ -31,7 +46,20 @@ class InstitutionAdminHttpApiSpec
   override implicit val timeout: Timeout = Timeout(duration)
   override val sharding: ClusterSharding = ClusterSharding(typedSystem)
 
-  override def beforeAll(): Unit = super.beforeAll()
+  override def beforeAll(): Unit = {
+    val config = ConfigFactory.load()
+    val shardNumber = config.getInt("hmda.institutions.shardNumber")
+    super.beforeAll()
+    Cluster(typedSystem).manager ! Join(Cluster(typedSystem).selfMember.address)
+    sharding.spawn(
+      behavior = entityId => InstitutionPersistence.behavior(entityId),
+      Props.empty,
+      EntityTypeKey[InstitutionCommand](InstitutionPersistence.name),
+      ClusterShardingSettings(typedSystem),
+      maxNumberOfShards = shardNumber,
+      handOffStopMessage = InstitutionStop
+    )
+  }
 
   override def afterAll(): Unit = super.afterAll()
 
@@ -39,12 +67,46 @@ class InstitutionAdminHttpApiSpec
   val sampleInstitution =
     institutionGen.sample.getOrElse(Institution.empty).copy(LEI = Some(lei))
 
+  val modified =
+    sampleInstitution.copy(emailDomain = Some("email@bank.com"))
+
   "Institutions HTTP Service" must {
     "return OPTIONS" in {
       Options("/institutions") ~> institutionAdminRoutes ~> check {
         status mustBe StatusCodes.OK
       }
     }
+
+    "Create an institution" in {
+      Post("/institutions", sampleInstitution) ~> institutionAdminRoutes ~> check {
+        status mustBe StatusCodes.Created
+        responseAs[Institution] mustBe sampleInstitution
+      }
+    }
+
+    "Get an institution" in {
+      Get(s"/institutions/${sampleInstitution.LEI.getOrElse("")}") ~> institutionAdminRoutes ~> check {
+        status mustBe StatusCodes.OK
+        responseAs[Institution] mustBe sampleInstitution
+      }
+    }
+
+    "Modify an institution" in {
+
+      Put("/institutions", modified) ~> institutionAdminRoutes ~> check {
+        status mustBe StatusCodes.Accepted
+        responseAs[Institution] mustBe modified
+      }
+    }
+
+    "Delete an institution" in {
+      Delete("/institutions", modified) ~> institutionAdminRoutes ~> check {
+        status mustBe StatusCodes.Accepted
+        responseAs[InstitutionDeletedResponse] mustBe InstitutionDeletedResponse(
+          lei)
+      }
+    }
+
   }
 
 }
