41f819c7f6ac46069c8793d3e01673b91b02e325
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-06-08 16:44:49 -0400

    Start refactor

--- hmda/src/main/scala/hmda/api/http/public/HmdaFileValidationHttpApi.scala
+++ hmda/src/main/scala/hmda/api/http/public/HmdaFileValidationHttpApi.scala
@@ -1,15 +1,24 @@
 package hmda.api.http.public
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.event.LoggingAdapter
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
 import akka.http.scaladsl.model.MediaTypes.`text/csv`
 import akka.http.scaladsl.model.{HttpCharsets, HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Route
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, ClosedShape}
 import akka.util.{ByteString, Timeout}
 import akka.http.scaladsl.server.Directives._
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{
+  Broadcast,
+  Concat,
+  Flow,
+  GraphDSL,
+  RunnableGraph,
+  Sink,
+  Source
+}
 import hmda.api.http.model.public.{Validated, ValidatedResponse}
 import hmda.parser.filing.lar.LarCsvParser
 import hmda.parser.filing.ts.TsCsvParser
@@ -17,7 +26,7 @@ import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import hmda.api.http.directives.HmdaTimeDirectives
 import io.circe.generic.auto._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 import hmda.util.streams.FlowUtils._
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
@@ -36,7 +45,8 @@ trait HmdaFileValidationHttpApi extends HmdaTimeDirectives {
       fileUpload("file") {
         case (_, byteSource) =>
           val processF =
-            processLarFile(byteSource).runWith(Sink.seq)
+            //processLarFile(byteSource).runWith(Sink.seq)
+            processLarFile(byteSource).runWith(Seq.seq)
           onComplete(processF) {
             case Success(parsed) =>
               complete(ToResponseMarshallable(ValidatedResponse(parsed)))
@@ -87,9 +97,26 @@ trait HmdaFileValidationHttpApi extends HmdaTimeDirectives {
     }
   }
 
-  private def processLarFile(byteSource: Source[ByteString, Any]) = {
+  private def processLarFile2(byteSource: Source[ByteString, Any]) = {
 
-    val tsSource = byteSource
+    RunnableGraph.fromGraph(GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      val bcast = builder.add(Broadcast[ByteString](2))
+      val concat = builder.add(Concat[Validated](2))
+      val out = Sink.seq[Validated]
+
+      byteSource ~> bcast ~> processTsSource2 ~> concat ~> out
+      bcast ~> processLarSource2 ~> concat ~> out
+
+      ClosedShape
+
+    })
+
+  }
+
+  private def processTsSource2: Flow[ByteString, Validated, NotUsed] = {
+    Flow[ByteString]
       .via(framing("\n"))
       .map(_.utf8String)
       .map(_.trim)
@@ -105,8 +132,10 @@ trait HmdaFileValidationHttpApi extends HmdaTimeDirectives {
           Validated(i, errors.map(e => e.errorMessage).mkString(","))
       }
       .filter(x => x.errors != "OK")
+  }
 
-    val larSource = byteSource
+  private def processLarSource2: Flow[ByteString, Validated, NotUsed] = {
+    Flow[ByteString]
       .via(framing("\n"))
       .map(_.utf8String)
       .drop(1)
@@ -122,6 +151,50 @@ trait HmdaFileValidationHttpApi extends HmdaTimeDirectives {
           Validated(i, errors.map(e => e.errorMessage).mkString(","))
       }
       .filter(x => x.errors != "OK")
+  }
+
+  private def processTsSource(byteSource: Source[ByteString, Any]) = {
+    byteSource
+      .via(framing("\n"))
+      .map(_.utf8String)
+      .map(_.trim)
+      .take(1)
+      .zip(Source.fromIterator(() => Iterator.from(1)))
+      .map {
+        case (ts, index) =>
+          (index, TsCsvParser(ts))
+      }
+      .map {
+        case (i, Right(_)) => Validated(i, "OK")
+        case (i, Left(errors)) =>
+          Validated(i, errors.map(e => e.errorMessage).mkString(","))
+      }
+      .filter(x => x.errors != "OK")
+  }
+
+  private def processLarSource(byteSource: Source[ByteString, Any]) = {
+    byteSource
+      .via(framing("\n"))
+      .map(_.utf8String)
+      .drop(1)
+      .map(_.trim)
+      .zip(Source.fromIterator(() => Iterator.from(2)))
+      .map {
+        case (lar, index) =>
+          (index, LarCsvParser(lar))
+      }
+      .map {
+        case (i, Right(_)) => Validated(i, "OK")
+        case (i, Left(errors)) =>
+          Validated(i, errors.map(e => e.errorMessage).mkString(","))
+      }
+      .filter(x => x.errors != "OK")
+  }
+
+  private def processLarFile(byteSource: Source[ByteString, Any]) = {
+
+    val tsSource = processTsSource(byteSource)
+    val larSource = processLarSource(byteSource)
 
     tsSource ++ larSource
 
