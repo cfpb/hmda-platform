838655d244c921983a3a7939a95f12d01f4fbb7e
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-06-10 13:06:33 -0400

    Add V600

---
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/V600.scala
@@ -0,0 +1,15 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+
+object V600 extends EditCheck[LoanApplicationRegister] {
+  override def name: String = "V600"
+
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    lar.larIdentifier.LEI.length is equalTo(20)
+  }
+}
