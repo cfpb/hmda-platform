fc007286b970ae5b21a36f753293f8fd62584fc9
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-06-10 13:25:16 -0400

    Add V601

---
+++ hmda/src/main/scala/hmda/validation/rules/ts/validity/V601.scala
@@ -0,0 +1,19 @@
+package hmda.validation.rules.ts.validity
+
+import hmda.model.filing.ts.TransmittalSheet
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+
+object V601 extends EditCheck[TransmittalSheet] {
+  override def name: String = "V601"
+
+  override def apply(ts: TransmittalSheet): ValidationResult = {
+    (ts.institutionName not empty) and
+      (ts.contact.name not empty) and
+      (ts.contact.email not empty) and
+      (ts.contact.address.street not empty) and
+      (ts.contact.address.city not empty)
+  }
+}
