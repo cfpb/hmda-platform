6a6d326dd005b16efa8d9ae3a6d6fb314442844e
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-06-29 02:24:58 -0400

    Add modify institution serializer

--- hmda/src/main/scala/hmda/persistence/serialization/institution/InstitutionCommandsProtobufConverter.scala
+++ hmda/src/main/scala/hmda/persistence/serialization/institution/InstitutionCommandsProtobufConverter.scala
@@ -1,7 +1,14 @@
 package hmda.persistence.serialization.institution
 
-import hmda.persistence.institution.InstitutionPersistence.CreateInstitution
-import hmda.persistence.serialization.institution.commands.CreateInstitutionMessage
+import hmda.persistence.institution.InstitutionPersistence.{
+  CreateInstitution,
+  InstitutionEvent,
+  ModifyInstitution
+}
+import hmda.persistence.serialization.institution.commands.{
+  CreateInstitutionMessage,
+  ModifyInstitutionMessage
+}
 import InstitutionProtobufConverter._
 import akka.actor.typed.ActorRefResolver
 
@@ -25,4 +32,26 @@ object InstitutionCommandsProtobufConverter {
     val actorRef = resolver.resolveActorRef(msg.replyTo)
     CreateInstitution(institution, actorRef)
   }
+
+  def modifyInstitutionToProtobuf(
+      cmd: ModifyInstitution,
+      resolver: ActorRefResolver
+  ): ModifyInstitutionMessage = {
+    ModifyInstitutionMessage(
+      institution = Some(institutionToProtobuf(cmd.i)),
+      replyTo = resolver.toSerializationFormat(cmd.replyTo)
+    )
+  }
+
+  def modifyInstitutionFromProtobuf(
+      bytes: Array[Byte],
+      resolver: ActorRefResolver
+  ): ModifyInstitution = {
+    val msg = ModifyInstitutionMessage.parseFrom(bytes)
+    val institution = institutionFromProtobuf(
+      msg.institution.getOrElse(InstitutionMessage()))
+    val actorRef = resolver.resolveActorRef[InstitutionEvent](msg.replyTo)
+    ModifyInstitution(institution, actorRef)
+  }
+
 }
--- hmda/src/main/scala/hmda/persistence/serialization/institution/InstitutionCommandsSerializer.scala
+++ hmda/src/main/scala/hmda/persistence/serialization/institution/InstitutionCommandsSerializer.scala
@@ -5,7 +5,10 @@ import java.io.NotSerializableException
 import akka.actor.ExtendedActorSystem
 import akka.actor.typed.ActorRefResolver
 import akka.serialization.SerializerWithStringManifest
-import hmda.persistence.institution.InstitutionPersistence.CreateInstitution
+import hmda.persistence.institution.InstitutionPersistence.{
+  CreateInstitution,
+  ModifyInstitution
+}
 import akka.actor.typed.scaladsl.adapter._
 import InstitutionCommandsProtobufConverter._
 
@@ -17,12 +20,15 @@ class InstitutionCommandsSerializer(system: ExtendedActorSystem)
   override def identifier: Int = 101
 
   final val CreateInstitutionManifest = classOf[CreateInstitution].getName
+  final val ModifyInstitutionManifest = classOf[ModifyInstitution].getName
 
   override def manifest(o: AnyRef): String = o.getClass.getName
 
   override def toBinary(o: AnyRef): Array[Byte] = o match {
     case cmd: CreateInstitution =>
       createInstitutionToProtobuf(cmd, resolver).toByteArray
+    case cmd: ModifyInstitution =>
+      modifyInstitutionToProtobuf(cmd, resolver).toByteArray
     case _ ⇒
       throw new IllegalArgumentException(
         s"Cannot serialize object of type [${o.getClass.getName}]")
@@ -32,6 +38,8 @@ class InstitutionCommandsSerializer(system: ExtendedActorSystem)
     manifest match {
       case CreateInstitutionManifest =>
         createInstitutionFromProtobuf(bytes, resolver)
+      case ModifyInstitutionManifest =>
+        modifyInstitutionFromProtobuf(bytes, resolver)
       case _ =>
         throw new NotSerializableException(
           s"Unimplemented deserialization of message with manifest [$manifest] in [${getClass.getName}]")
