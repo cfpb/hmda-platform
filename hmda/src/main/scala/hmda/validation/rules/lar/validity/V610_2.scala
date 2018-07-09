0032d21e4b84e9d2e7070dd824bbf236c6bc8f0f
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-07-09 15:24:16 -0400

    Add V610-2

---
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/V610_2.scala
@@ -0,0 +1,20 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums.PurchasedLoan
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+
+object V610_2 extends EditCheck[LoanApplicationRegister] {
+  override def name: String = "V610-2"
+
+  override def parent: String = "V610"
+
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    when(lar.action.actionTakenType is equalTo(PurchasedLoan)) {
+      lar.loan.applicationDate is equalTo("NA")
+    }
+  }
+}
