d3950b1b5f041c872cea98ecbfba66a40f362921
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-08-09 16:22:54 -0400

    Start adding streaming

--- institutions-api/src/main/scala/hmda/institution/projection/InstitutionDBProjector.scala
+++ institutions-api/src/main/scala/hmda/institution/projection/InstitutionDBProjector.scala
@@ -1,24 +1,35 @@
 package hmda.institution.projection
 
-import akka.actor.ActorSystem
-import akka.actor.typed.{ActorRef, Behavior}
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.Scheduler
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.AskPattern._
 import hmda.query.HmdaQuery._
 import akka.actor.typed.scaladsl.adapter._
-import akka.persistence.query.{Offset, Sequence}
+import akka.persistence.query.Sequence
 import akka.persistence.typed.scaladsl.{Effect, PersistentBehaviors}
 import akka.persistence.typed.scaladsl.PersistentBehaviors.CommandHandler
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
+import akka.util.Timeout
+import com.typesafe.config.ConfigFactory
 import hmda.messages.projection.CommonProjectionMessages._
+
+import scala.concurrent.duration._
 
 object InstitutionDBProjector {
 
   final val name = "InstitutionDBProjector"
 
+  case class StartStreaming()
+
   case class InstitutionDBProjectorState(offset: Long = 0L) {
     def isEmpty: Boolean = offset == 0L
   }
+
+  val config = ConfigFactory.load()
+  val duration = config.getInt("hmda.institution.timeout")
+
+  implicit val timeout = Timeout(duration.seconds)
 
   //TODO: finish implementation of resumable projection
 //  val streamMessages: Behavior[InstitutionDBProjectorCommand] =
@@ -51,7 +62,19 @@ object InstitutionDBProjector {
                                      ProjectionEvent,
                                      InstitutionDBProjectorState] = {
     (ctx, state, cmd) =>
+      implicit val scheduler: Scheduler = ctx.system.scheduler
       cmd match {
+
+        case StartStreaming() =>
+          implicit val system = ctx.system.toUntyped
+          implicit val materializer = ActorMaterializer()
+          ctx.log.info("Streaming messages to from {}", name)
+          readJournal(system)
+              .eventsByTag("institution", Sequence(state.offset))
+              //.map(env => ctx.self ? (ref => SaveOffset(env.sequenceNr, ref)))
+              .runWith(Sink.ignore)
+          Effect.none
+
         case SaveOffset(seqNr, replyTo) =>
           Effect.persist(OffsetSaved(seqNr)).andThen {
             ctx.log.debug("Offset saved: {}", seqNr)
