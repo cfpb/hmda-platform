daeced39462b8de23f23fc66c96e424027a79942
Nick Grippin <ngrippin@discovertechnologies.com>
2018-08-09 08:40:31 -0400

    v616

---
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/V616.scala
@@ -0,0 +1,18 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+
+object V616 extends EditCheck[LoanApplicationRegister] {
+  override def name: String = "V616"
+
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    lar.loan.occupancy is oneOf(PrincipalResidence,
+                                SecondResidence,
+                                InvestmentProperty)
+  }
+}
