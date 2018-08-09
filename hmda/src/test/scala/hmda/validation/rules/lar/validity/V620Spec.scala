d4332163a28a066bf41813101745136438e34cae
Nick Grippin <ngrippin@discovertechnologies.com>
2018-08-09 13:26:33 -0400

    v620

---
+++ hmda/src/test/scala/hmda/validation/rules/lar/validity/V620Spec.scala
@@ -0,0 +1,19 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums.InvalidLoanTypeCode
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class V620Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = V620
+
+  property("Street address must be valid") {
+    forAll(larGen) { lar =>
+      lar.mustPass
+      val badGeo = lar.geography.copy(street = "")
+      lar.copy(geography = badGeo).mustFail
+    }
+  }
+}
