7785971a0b2e3cb2537b6232f46325aa64d3effc
Nick Grippin <ngrippin@discovertechnologies.com>
2018-08-15 12:06:25 -0400

    V637

---
+++ hmda/src/test/scala/hmda/validation/rules/lar/validity/V637Spec.scala
@@ -0,0 +1,35 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class V637Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = V637
+
+  property("If race is not applicable, race observed must be not applicable") {
+    forAll(larGen) { lar =>
+      val applicableLar = lar.copy(
+        applicant = lar.applicant.copy(
+          race = lar.applicant.race.copy(race1 = RaceNotApplicable)))
+
+      val unapplicableLar = lar.copy(
+        applicant = lar.applicant.copy(
+          race = lar.applicant.race.copy(race1 = InvalidRaceCode)))
+      unapplicableLar.mustPass
+
+      val raceNA = applicableLar.applicant.race
+        .copy(raceObserved = RaceObservedNotApplicable)
+      val raceInvalid = applicableLar.applicant.race
+        .copy(raceObserved = InvalidRaceObservedCode)
+      lar
+        .copy(applicant = applicableLar.applicant.copy(race = raceNA))
+        .mustPass
+      lar
+        .copy(applicant = applicableLar.applicant.copy(race = raceInvalid))
+        .mustFail
+    }
+  }
+}
