4123538ffff9ba8a40d1b330fcac6e10a0a4be7f
Nick Grippin <ngrippin@discovertechnologies.com>
2018-08-15 11:04:45 -0400

    V633

---
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/V633.scala
@@ -0,0 +1,20 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+
+object V633 extends EditCheck[LoanApplicationRegister] {
+  override def name: String = "V633"
+
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    when(
+      lar.coApplicant.ethnicity.ethnicity1 is equalTo(EthnicityNotApplicable)) {
+      lar.coApplicant.ethnicity.ethnicityObserved is equalTo(
+        EthnicityObservedNotApplicable)
+    }
+  }
+}
