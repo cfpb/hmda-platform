ee3f840f3f01130e61c364ecc4b2589d03f9bdfd
Nick Grippin <ngrippin@discovertechnologies.com>
2018-08-22 12:29:54 -0400

    checkpoint

---
+++ hmda/src/test/scala/hmda/validation/rules/lar/validity/V628_2Spec.scala
@@ -0,0 +1,37 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class V628_2Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = V628_2
+
+  property(
+    "If ethnicity is not applicable, ethnicity observed must not be applicable") {
+    forAll(larGen) { lar =>
+      val applicableLar = lar.copy(
+        applicant = lar.applicant.copy(ethnicity =
+          lar.applicant.ethnicity.copy(ethnicity1 = EthnicityNotApplicable)))
+
+      val unapplicableLar = lar.copy(
+        applicant = lar.applicant.copy(ethnicity =
+          lar.applicant.ethnicity.copy(ethnicity1 = InvalidEthnicityCode)))
+      unapplicableLar.mustPass
+
+      val ethnicityNA = applicableLar.applicant.ethnicity
+        .copy(ethnicityObserved = EthnicityObservedNotApplicable)
+      val ethnicityVis = applicableLar.applicant.ethnicity
+        .copy(ethnicityObserved = VisualOrSurnameEthnicity)
+      lar
+        .copy(applicant = applicableLar.applicant.copy(ethnicity = ethnicityNA))
+        .mustPass
+      lar
+        .copy(
+          applicant = applicableLar.applicant.copy(ethnicity = ethnicityVis))
+        .mustFail
+    }
+  }
+}
