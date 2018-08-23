4cd8e39aa185e69ef56a89185c1008d5b3018c14
Nick Grippin <ngrippin@discovertechnologies.com>
2018-08-23 16:20:28 -0400

    V646

---
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/V646_1.scala
@@ -0,0 +1,25 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+
+object V646_1 extends EditCheck[LoanApplicationRegister] {
+  override def name: String = "V646-1"
+
+  override def parent: String = "V646"
+
+  val validValues = List(Male,
+                         Female,
+                         SexInformationNotProvided,
+                         SexNotApplicable,
+                         SexNoCoApplicant,
+                         MaleAndFemale)
+
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    lar.coApplicant.sex.sexEnum is containedIn(validValues)
+  }
+}
