32002795146d7682aec201a6588011ed181d48cf
Nick Grippin <ngrippin@discovertechnologies.com>
2018-08-23 16:10:45 -0400

    V642

---
+++ hmda/src/test/scala/hmda/validation/rules/lar/validity/V642_1Spec.scala
@@ -0,0 +1,21 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class V642_1Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = V642_1
+
+  property("Applicant sex value must be valid") {
+    forAll(larGen) { lar =>
+      lar.mustPass
+      val invalidLar = lar.copy(
+        applicant = lar.applicant.copy(
+          sex = lar.applicant.sex.copy(sexEnum = InvalidSexCode)))
+      invalidLar.mustFail
+    }
+  }
+}
