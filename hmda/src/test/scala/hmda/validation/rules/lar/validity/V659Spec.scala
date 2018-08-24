3d7e7ba80bba7722b34b08288c84b99ae607420e
Nick Grippin <ngrippin@discovertechnologies.com>
2018-08-24 13:00:01 -0400

    V659

---
+++ hmda/src/test/scala/hmda/validation/rules/lar/validity/V659Spec.scala
@@ -0,0 +1,19 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.enums.InvalidLientStatusCode
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class V659Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = V659
+
+  property("Lein status must be valid") {
+    forAll(larGen) { lar =>
+      lar.mustPass
+      val invalidLar = lar.copy(lienStatus = InvalidLientStatusCode)
+      invalidLar.mustFail
+    }
+  }
+}
