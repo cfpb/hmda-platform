60fabc25ae73250c5a5938cbc7b105148c501978
Nick Grippin <ngrippin@discovertechnologies.com>
2018-08-24 12:16:55 -0400

    V651

---
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/V651_1.scala
@@ -0,0 +1,18 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+
+object V651_1 extends EditCheck[LoanApplicationRegister] {
+  override def name: String = "V651-1"
+
+  override def parent: String = "V651"
+
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    lar.applicant.age is greaterThan(0)
+  }
+}
