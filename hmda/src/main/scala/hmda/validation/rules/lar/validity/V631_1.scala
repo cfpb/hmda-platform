918374b19c7bd2f800e167f8e0d2166f3e8263fd
Nick Grippin <ngrippin@discovertechnologies.com>
2018-08-28 10:00:39 -0400

    V631

---
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/V631_1.scala
@@ -0,0 +1,32 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+
+object V631_1 extends EditCheck[LoanApplicationRegister] {
+  override def name: String = "V631-1"
+
+  override def parent: String = "V631"
+
+  val validEthnicities = List(
+    HispanicOrLatino,
+    Mexican,
+    PuertoRican,
+    Cuban,
+    OtherHispanicOrLatino,
+    NotHispanicOrLatino,
+    InformationNotProvided,
+    EthnicityNotApplicable,
+    EthnicityNoCoApplicant
+  )
+
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    when(lar.coApplicant.ethnicity.otherHispanicOrLatino is empty) {
+      lar.coApplicant.ethnicity.ethnicity1 is containedIn(validEthnicities)
+    }
+  }
+}
