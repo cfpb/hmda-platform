0869c2a9f32bbf0b2f2cd4641509d3c99fbccd5f
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-08-29 09:52:41 -0400

    Update institution references

--- hmda/src/main/scala/hmda/parser/institution/InstitutionCsvParser.scala
+++ hmda/src/main/scala/hmda/parser/institution/InstitutionCsvParser.scala
@@ -8,50 +8,45 @@ object InstitutionCsvParser {
     val values = s.split('|').map(_.trim).toList
     val acticityYear = values.head.toInt
     val lei = values(1)
-    val agencyCode = values(2)
-    val institutionTypeCode = values(3)
+    val agencyCode = values(2).toInt
+    val institutionTypeCode = values(3).toInt
     val instId2017 = values(4)
     val taxId = values(5)
-    val rssd = values(6)
+    val rssd = values(6).toInt
     val emailDomains = values(7)
     val respondentName = values(8)
     val respondentState = values(9)
     val respondentCity = values(10)
-    val parentIdRssd = values(11)
+    val parentIdRssd = values(11).toInt
     val parentName = values(12)
-    val assets = values(13)
-    val otherLenderCode = values(14)
-    val topHolderIdRssd = values(15)
+    val assets = values(13).toInt
+    val otherLenderCode = values(14).toInt
+    val topHolderIdRssd = values(15).toInt
     val topHolderName = values(16)
     val hmdaFiler = values(17)
 
     Institution(
       acticityYear,
-      if (lei == "") None else Some(lei),
-      if (agencyCode == "") Some(UndeterminedAgency)
-      else Some(Agency.valueOf(agencyCode.toInt)),
-      if (institutionTypeCode == "") Some(UndeterminedInstitutionType)
-      else Some(InstitutionType.valueOf(institutionTypeCode.toInt)),
+      lei,
+      Agency.valueOf(agencyCode.toInt),
+      InstitutionType.valueOf(institutionTypeCode),
       if (instId2017 == "") None else Some(instId2017),
       if (taxId == "") None else Some(taxId),
-      if (rssd == "") None else Some(rssd),
-      emailDomains = emailDomains.split(','),
+      rssd.toInt,
+      emailDomains.split(','),
       Respondent(
         if (respondentName == "") None else Some(respondentName),
         if (respondentState == "") None else Some(respondentState),
         if (respondentCity == "") None else Some(respondentCity)
       ),
       Parent(
-        if (parentIdRssd == "" || parentIdRssd == "0") None
-        else Some(parentIdRssd.toInt),
+        parentIdRssd,
         if (parentName == "") None else Some(parentName)
       ),
-      if (assets == "" || assets == "0") None else Some(assets.toInt),
-      if (otherLenderCode == "" || otherLenderCode == "0") None
-      else Some(otherLenderCode.toInt),
+      assets,
+      otherLenderCode,
       TopHolder(
-        if (topHolderIdRssd == "" || topHolderIdRssd == "0") None
-        else Some(topHolderIdRssd.toInt),
+        topHolderIdRssd,
         if (topHolderName == "") None else Some(topHolderName)
       ),
       if (hmdaFiler == "") false else hmdaFiler.toBoolean
