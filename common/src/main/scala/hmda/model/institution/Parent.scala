2afb61da5b3125a35179803ab73f2fc2e477cfab
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-08-29 09:39:58 -0400

    Refactor institutions data model

--- common/src/main/scala/hmda/model/institution/Parent.scala
+++ common/src/main/scala/hmda/model/institution/Parent.scala
@@ -1,17 +1,17 @@
 package hmda.model.institution
 
 object Parent {
-  def empty: Parent = Parent(None, None)
+  def empty: Parent = Parent(-1, None)
 }
 
 case class Parent(
-    idRssd: Option[Int],
+    idRssd: Int,
     name: Option[String]
 ) {
   def isEmpty: Boolean = {
     this match {
-      case Parent(None, None) => true
-      case _                  => false
+      case Parent(-1, None) => true
+      case _                => false
     }
   }
 }
