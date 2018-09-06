ef2809bbe46af43f02a72a1732d2f43ae14af5b6
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-06 15:24:31 -0400

    V641

---
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/V641.scala
@@ -0,0 +1,21 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+
+object V641 extends EditCheck[LoanApplicationRegister] {
+  override def name: String = "V641"
+
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    when(lar.coApplicant.race.race1 is equalTo(RaceNoCoApplicant)) {
+      lar.coApplicant.race.raceObserved is equalTo(RaceObservedNoCoApplicant)
+    } and when(
+      lar.coApplicant.race.raceObserved is equalTo(RaceObservedNoCoApplicant)) {
+      lar.coApplicant.race.race1 is equalTo(RaceNoCoApplicant)
+    }
+  }
+}
