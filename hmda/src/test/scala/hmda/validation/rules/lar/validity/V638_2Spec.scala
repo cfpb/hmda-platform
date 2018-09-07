1a5ac998e47b631eab1ab76f7e4c82bd42c870a7
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-07 15:14:44 -0400

    V638

---
+++ hmda/src/test/scala/hmda/validation/rules/lar/validity/V638_2Spec.scala
@@ -0,0 +1,35 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class V638_2Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = V638_2
+
+  property("Races 2-5 must contain valid values") {
+    forAll(larGen) { lar =>
+      val validRace =
+        lar.coApplicant.race.copy(race2 = EmptyRaceValue,
+                                  race3 = EmptyRaceValue,
+                                  race4 = EmptyRaceValue,
+                                  race5 = EmptyRaceValue)
+
+      val invalidRace =
+        lar.coApplicant.race.copy(race2 = EmptyRaceValue,
+                                  race3 = RaceNotApplicable,
+                                  race4 = InvalidRaceCode,
+                                  race5 = EmptyRaceValue)
+
+      val validLar =
+        lar.copy(coApplicant = lar.coApplicant.copy(race = validRace))
+      validLar.mustPass
+
+      val invalidLar =
+        lar.copy(coApplicant = lar.coApplicant.copy(race = invalidRace))
+      invalidLar.mustFail
+    }
+  }
+}
---
+++ hmda/src/test/scala/hmda/validation/rules/lar/validity/V638_4Spec.scala
@@ -0,0 +1,42 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class V638_4Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = V638_4
+
+  property("If race 1 is not available, race 2-5 must be blank") {
+    forAll(larGen) { lar =>
+      val applicableApplicant = lar.coApplicant.copy(
+        race = lar.coApplicant.race.copy(race1 = RaceNotApplicable))
+
+      val unapplicableLar = lar.copy(
+        coApplicant = lar.coApplicant.copy(
+          race = lar.coApplicant.race.copy(race1 = InvalidRaceCode)))
+      unapplicableLar.mustPass
+
+      val raceBlank =
+        applicableApplicant.race.copy(race2 = EmptyRaceValue,
+                                      race3 = EmptyRaceValue,
+                                      race4 = EmptyRaceValue,
+                                      race5 = EmptyRaceValue)
+
+      val raceNotBlank =
+        applicableApplicant.race.copy(race2 = EmptyRaceValue,
+                                      race3 = InvalidRaceCode,
+                                      race4 = EmptyRaceValue,
+                                      race5 = InvalidRaceCode)
+
+      lar
+        .copy(coApplicant = applicableApplicant.copy(race = raceBlank))
+        .mustPass
+      lar
+        .copy(coApplicant = applicableApplicant.copy(race = raceNotBlank))
+        .mustFail
+    }
+  }
+}
