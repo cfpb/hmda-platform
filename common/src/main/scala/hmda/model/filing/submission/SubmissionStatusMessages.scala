782aa6ecf6a0742f9dc0ca91367c3be95b3b4672
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-12 08:24:59 -0400

    Q610

--- common/src/main/scala/hmda/model/filing/submission/SubmissionStatusMessages.scala
+++ common/src/main/scala/hmda/model/filing/submission/SubmissionStatusMessages.scala
@@ -8,7 +8,8 @@ object SubmissionStatusMessages {
   val parsedMsg = "Your data is formatted correctly."
   val parsedWithErrorsMsg = "Your data has formatting errors."
   val validatingMsg = "Your data is being analyzed."
-  val syntacticalValidityErrorMsg = "Your data has syntactical and/or validity edits that need to be reviewed."
+  val syntacticalValidityErrorMsg =
+    "Your data has syntactical and/or validity edits that need to be reviewed."
   val qualityErrorMsg = "Your data has quality edits that need to be reviewed."
   val validatedWithErrorsMsg = "Your data has edits that need to be reviewed."
   val macroErrorMsg = "Your data has macro edits that need to be reviewed."
@@ -16,18 +17,30 @@ object SubmissionStatusMessages {
   val signedMsg = "Your submission has been accepted."
   val failedMsg = "An error occurred while submitting the data."
 
-  val createdDescription = "The filing period is open and available to accept HMDA data. Make sure your data is in a pipe-delimited text file."
-  val uploadingDescription = "Your file is currently being uploaded to the HMDA Platform."
+  val createdDescription =
+    "The filing period is open and available to accept HMDA data. Make sure your data is in a pipe-delimited text file."
+  val uploadingDescription =
+    "Your file is currently being uploaded to the HMDA Platform."
   val uploadedDescription = "Your data is ready to be analyzed."
-  val parsingDescription = "Your file is being analyzed to ensure that it meets formatting requirements specified in the HMDA Filing Instructions Guide."
-  val parsedWithErrorsDescription = "Review these errors and update your file. Then, upload the corrected file."
-  val parsedDescription = "Your file meets the formatting requirements specified in the HMDA Filing Instructions Guide. Your data will now be analyzed for any edits."
-  val validatingDescription = "Your data has been uploaded and is being checked for any edits."
-  val syntactivalValidityErrorDescription = "Your file has been uploaded, but the filing process may not proceed until the file is corrected and re-uploaded."
-  val qualityErrorDescription = "Your file has been uploaded, but the filing process may not proceed until edits are verified or the file is corrected and re-uploaded."
-  val macroErrorDescription = "Your file has been uploaded, but the filing process may not proceed until edits are verified or the file is corrected and re-uploaded."
-  val validatedWithErrorsDescription = "Your file has been uploaded, but the filing process may not proceed until edits are verified or the file is corrected and re-uploaded."
-  val validatedDescription = "Your financial institution has certified that the data is correct, but it has not been submitted yet."
-  val signedDescription = "This completes your HMDA filing process for this year. If you need to upload a new HMDA file, the previously completed filing will not be overridden until all edits have been cleared and verified, and the new file has been submitted."
+  val parsingDescription =
+    "Your file is being analyzed to ensure that it meets formatting requirements specified in the HMDA Filing Instructions Guide."
+  val parsedWithErrorsDescription =
+    "Review these errors and update your file. Then, upload the corrected file."
+  val parsedDescription =
+    "Your file meets the formatting requirements specified in the HMDA Filing Instructions Guide. Your data will now be analyzed for any edits."
+  val validatingDescription =
+    "Your data has been uploaded and is being checked for any edits."
+  val syntactivalValidityErrorDescription =
+    "Your file has been uploaded, but the filing process may not proceed until the file is corrected and re-uploaded."
+  val qualityErrorDescription =
+    "Your file has been uploaded, but the filing process may not proceed until edits are verified or the file is corrected and re-uploaded."
+  val macroErrorDescription =
+    "Your file has been uploaded, but the filing process may not proceed until edits are verified or the file is corrected and re-uploaded."
+  val validatedWithErrorsDescription =
+    "Your file has been uploaded, but the filing process may not proceed until edits are verified or the file is corrected and re-uploaded."
+  val validatedDescription =
+    "Your financial institution has certified that the data is correct, but it has not been submitted yet."
+  val signedDescription =
+    "This completes your HMDA filing process for this year. If you need to upload a new HMDA file, the previously completed filing will not be overridden until all edits have been cleared and verified, and the new file has been submitted."
   val failedDescription = "Please re-upload your file."
 }
