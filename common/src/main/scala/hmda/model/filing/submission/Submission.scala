99f5a73bc5c62969fb2938181a79d7f3bd53449c
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-09-12 11:25:30 -0400

    Add submission protobuf converter

--- common/src/main/scala/hmda/model/filing/submission/Submission.scala
+++ common/src/main/scala/hmda/model/filing/submission/Submission.scala
@@ -89,13 +89,8 @@ case object Signed extends SubmissionStatus {
   override def description: String = signedDescription
 }
 
-case class Failed(message: String) extends SubmissionStatus {
+case object Failed extends SubmissionStatus {
   override def code: Int = -1
+  override def message: String = failedMsg
   override def description: String = failedDescription
 }
-
-
-
-
-
-
