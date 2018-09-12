f484c3c6aa4171aa264bc11816d78edf634227f7
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-12 14:42:18 -0400

    add config

--- hmda/src/test/scala/hmda/validation/rules/lar/quality/Q624Spec.scala
+++ hmda/src/test/scala/hmda/validation/rules/lar/quality/Q624Spec.scala
@@ -1,5 +1,6 @@
 package hmda.validation.rules.lar.quality
 
+import com.typesafe.config.ConfigFactory
 import hmda.model.filing.lar.LarGenerators._
 import hmda.model.filing.lar.LoanApplicationRegister
 import hmda.model.filing.lar.enums.FHAInsured
@@ -10,6 +11,9 @@ class Q624Spec extends LarEditCheckSpec {
   override def check: EditCheck[LoanApplicationRegister] = Q624
 
   property("Loan amount must be valid for single unit FHA loans") {
+    val config = ConfigFactory.load()
+    val amount = config.getInt("edits.Q624.amount")
+
     forAll(larGen) { lar =>
       whenever(lar.loan.loanType != FHAInsured || lar.property.totalUnits != 1) {
         lar.mustPass
@@ -17,9 +21,9 @@ class Q624Spec extends LarEditCheckSpec {
 
       val appLar = lar.copy(loan = lar.loan.copy(loanType = FHAInsured),
                             property = lar.property.copy(totalUnits = 1))
-      appLar.copy(loan = appLar.loan.copy(amount = 600000)).mustPass
-      appLar.copy(loan = appLar.loan.copy(amount = 637000)).mustPass
-      appLar.copy(loan = appLar.loan.copy(amount = 637001)).mustFail
+      appLar.copy(loan = appLar.loan.copy(amount = amount - 1)).mustPass
+      appLar.copy(loan = appLar.loan.copy(amount = amount)).mustPass
+      appLar.copy(loan = appLar.loan.copy(amount = amount + 1)).mustFail
     }
   }
 }
