21a182f5966702c55b4187d719fe215d05e54907
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-12 10:57:36 -0400

    Q618

---
+++ hmda/src/test/scala/hmda/validation/rules/lar/quality/Q618Spec.scala
@@ -0,0 +1,37 @@
+package hmda.validation.rules.lar.quality
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums.{
+  InvalidManufacturedHomeSecuredPropertyCode,
+  ManufacturedHome,
+  ManufacturedHomeSecuredNotApplicable
+}
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class Q618Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = Q618
+
+  property(
+    "If construction method is manufactured, property type should be valid") {
+    forAll(larGen) { lar =>
+      whenever(lar.loan.constructionMethod != ManufacturedHome) {
+        lar.mustPass
+      }
+
+      val appLar =
+        lar.copy(loan = lar.loan.copy(constructionMethod = ManufacturedHome))
+      appLar
+        .copy(
+          property = appLar.property.copy(manufacturedHomeSecuredProperty =
+            ManufacturedHomeSecuredNotApplicable))
+        .mustPass
+      appLar
+        .copy(
+          property = appLar.property.copy(manufacturedHomeSecuredProperty =
+            InvalidManufacturedHomeSecuredPropertyCode))
+        .mustFail
+    }
+  }
+}
