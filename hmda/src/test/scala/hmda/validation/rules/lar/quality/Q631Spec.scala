646e96440e1d05b732e789e14afdcb1b134a7f10
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-13 09:05:20 -0400

    Q631

---
+++ hmda/src/test/scala/hmda/validation/rules/lar/quality/Q631Spec.scala
@@ -0,0 +1,32 @@
+package hmda.validation.rules.lar.quality
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class Q631Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = Q631
+
+  property(
+    "If loan type is not conventional, total units should be less than or equal to 4") {
+    forAll(larGen) { lar =>
+      whenever(lar.loan.loanType == Conventional) {
+        lar.mustPass
+      }
+
+      val appLar =
+        lar.copy(loan = lar.loan.copy(loanType = FHAInsured))
+      appLar
+        .copy(property = appLar.property.copy(totalUnits = 5))
+        .mustFail
+      appLar
+        .copy(property = appLar.property.copy(totalUnits = 4))
+        .mustPass
+      appLar
+        .copy(property = appLar.property.copy(totalUnits = 3))
+        .mustPass
+    }
+  }
+}
