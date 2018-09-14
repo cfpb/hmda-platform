67e3680284780650517719f73571f6609485a9df
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-14 13:00:01 -0400

    add config

--- hmda/src/main/scala/hmda/validation/rules/lar/quality/Q609.scala
+++ hmda/src/main/scala/hmda/validation/rules/lar/quality/Q609.scala
@@ -1,5 +1,6 @@
 package hmda.validation.rules.lar.quality
 
+import com.typesafe.config.ConfigFactory
 import hmda.model.filing.lar.LoanApplicationRegister
 import hmda.model.filing.lar.enums.{FannieMae, FarmerMac, FreddieMac, GinnieMae}
 import hmda.validation.dsl.PredicateCommon._
@@ -13,13 +14,16 @@ object Q609 extends EditCheck[LoanApplicationRegister] {
   override def name: String = "Q609"
 
   override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    val config = ConfigFactory.load()
+    val rs = config.getDouble("edits.Q609.rateSpread")
+
     when(
       lar.purchaserType is oneOf(FannieMae, GinnieMae, FreddieMac, FarmerMac)) {
       val rsAsDouble = Try(lar.loan.rateSpread.toDouble).getOrElse(9999.0)
 
       lar.loan.rateSpread is equalTo("Exempt") or
         (lar.loan.rateSpread is equalTo("NA")) or
-        (rsAsDouble is lessThanOrEqual(10.0))
+        (rsAsDouble is lessThanOrEqual(rs))
     }
   }
 }
