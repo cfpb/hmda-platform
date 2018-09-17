e9713cf9833976384a47c75d33f589ec73c2edb5
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-17 11:41:37 -0400

    V689

---
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/V689_3.scala
@@ -0,0 +1,26 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums.{
+  ManufacturedHomeSecuredExempt,
+  ManufacturedHomeSecuredNotApplicable,
+  SiteBuilt
+}
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+
+object V689_3 extends EditCheck[LoanApplicationRegister] {
+  override def name: String = "V689-3"
+
+  override def parent: String = "V689"
+
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    when(lar.loan.constructionMethod is equalTo(SiteBuilt)) {
+      lar.property.manufacturedHomeSecuredProperty is
+        oneOf(ManufacturedHomeSecuredExempt,
+              ManufacturedHomeSecuredNotApplicable)
+    }
+  }
+}
