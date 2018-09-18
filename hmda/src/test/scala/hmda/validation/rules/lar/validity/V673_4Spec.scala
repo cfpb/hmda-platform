3b277178c049c9e85111544889ceb0a4b434a788
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-18 10:05:15 -0400

    V673

---
+++ hmda/src/test/scala/hmda/validation/rules/lar/validity/V673_4Spec.scala
@@ -0,0 +1,37 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums.PrimarilyBusinessOrCommercialPurpose
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class V673_4Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = V673_4
+
+  property("If loan is for business, points and fees must be NA") {
+    forAll(larGen) { lar =>
+      whenever(
+        lar.businessOrCommercialPurpose != PrimarilyBusinessOrCommercialPurpose) {
+        lar.mustPass
+      }
+
+      val appLar = lar.copy(
+        businessOrCommercialPurpose = PrimarilyBusinessOrCommercialPurpose)
+      appLar
+        .copy(
+          loanDisclosure =
+            appLar.loanDisclosure.copy(totalPointsAndFees = "-10.0"))
+        .mustFail
+      appLar
+        .copy(
+          loanDisclosure =
+            appLar.loanDisclosure.copy(totalPointsAndFees = "10.0"))
+        .mustFail
+      appLar
+        .copy(loanDisclosure =
+          appLar.loanDisclosure.copy(totalPointsAndFees = "NA"))
+        .mustPass
+    }
+  }
+}
