3e5f362d83dedc04943f0ac04a5fa3e46ffd9f3a
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-20 12:57:29 -0400

    V694

---
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/V694_3.scala
@@ -0,0 +1,22 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.enums._
+import hmda.validation.dsl.PredicateCommon._
+import hmda.validation.dsl.PredicateSyntax._
+import hmda.validation.dsl.ValidationResult
+import hmda.validation.rules.EditCheck
+
+object V694_3 extends EditCheck[LoanApplicationRegister] {
+  override def name: String = "V694-3"
+
+  override def parent: String = "V694"
+
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+    when(lar.action.actionTakenType is equalTo(LoanOriginated)) {
+      lar.payableToInstitution is oneOf(PayableToInstitutionExempt,
+                                        InititallyPayableToInstitution,
+                                        NotInitiallyPayableToInstitution)
+    }
+  }
+}
