41e2ce21caf40a7f1a3e2b138b884574ae65e7d2
Nick Grippin <ngrippin@discovertechnologies.com>
2018-09-20 15:59:08 -0400

    V714

---
+++ hmda/src/test/scala/hmda/validation/rules/lar/validity/V714Spec.scala
@@ -0,0 +1,26 @@
+package hmda.validation.rules.lar.validity
+
+import hmda.model.filing.lar.LarGenerators._
+import hmda.model.filing.lar.enums._
+import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.validation.rules.EditCheck
+import hmda.validation.rules.lar.LarEditCheckSpec
+
+class V714Spec extends LarEditCheckSpec {
+  override def check: EditCheck[LoanApplicationRegister] = V714
+
+  property(
+    "If Application Channel exemption election is taken, all fields must be exempt") {
+    forAll(larGen) { lar =>
+      lar.mustPass
+
+      lar.copy(applicationSubmission = ApplicationSubmissionExempt).mustFail
+      lar.copy(payableToInstitution = PayableToInstitutionExempt).mustFail
+
+      lar
+        .copy(applicationSubmission = ApplicationSubmissionExempt,
+              payableToInstitution = PayableToInstitutionExempt)
+        .mustPass
+    }
+  }
+}
