fb684d0b33b3a62fbdc37fc3a31a07041637d787
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2018-09-21 15:35:28 -0400

    add institutions-api to seed

--- jenkins/seed.groovy
+++ jenkins/seed.groovy
@@ -1,6 +1,7 @@
 projects = [
     [name: "hmda-platform", repo: "hmda-platform", jenkinsfilePath: "hmda/Jenkinsfile", ],
     [name: "check-digit", repo: "hmda-platform", jenkinsfilePath: "check-digit/Jenkinsfile", ],
+    [name: "institutions-api", repo: "hmda-platform", jenkinsfilePath: "institutions-api/Jenkinsfile", ],
     [name: "hmda-help", repo: "hmda-help", jenkinsfilePath: "Jenkinsfile", ]
 ]
 
