e803c257e4110bda6a13c6d26115e64aaff295f5
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-09-28 07:26:07 -0400

    Add replyTo actor ref to filing commands

--- common/src/main/scala/hmda/messages/filing/FilingCommands.scala
+++ common/src/main/scala/hmda/messages/filing/FilingCommands.scala
@@ -1,12 +1,12 @@
 package hmda.messages.filing
 
+import akka.actor.typed.ActorRef
 import hmda.messages.CommonMessages.Command
 import hmda.model.filing.{Filing, FilingStatus}
 
 object FilingCommands {
   sealed trait FilingCommand extends Command
-  case class CreateFiling(filing: Filing) extends FilingCommand
-  case class UpdateFilingStatus(period: String, status: FilingStatus) extends FilingCommand
-  case class GetFilingByPeriod(period: String) extends FilingCommand
-
+  case class CreateFiling(filing: Filing, replyTo: ActorRef[Option[Filing]]) extends FilingCommand
+  case class UpdateFilingStatus(period: String, status: FilingStatus, replyTo: ActorRef[Filing]) extends FilingCommand
+  case class GetFilingByPeriod(period: String, replyTo: ActorRef[Option[Filing]]) extends FilingCommand
 }
