22dece736f7b69e99a3d9c07c3374a9edb24748f
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-09-28 18:16:18 -0400

    Add basic api structure

--- hmda/src/main/scala/hmda/api/http/filing/FilingHttpApi.scala
+++ hmda/src/main/scala/hmda/api/http/filing/FilingHttpApi.scala
@@ -3,14 +3,23 @@ package hmda.api.http.filing
 import akka.actor.ActorSystem
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.event.LoggingAdapter
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.StatusCodes
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import hmda.api.http.directives.HmdaTimeDirectives
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+import hmda.messages.filing.FilingCommands.GetFilingDetails
+import hmda.model.filing.FilingDetails
+import hmda.persistence.filing.FilingPersistence
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+import hmda.api.http.model.ErrorResponse
+import io.circe.generic.auto._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 trait FilingHttpApi extends HmdaTimeDirectives {
 
@@ -24,7 +33,25 @@ trait FilingHttpApi extends HmdaTimeDirectives {
   //institutions/<institutionId>/filings/<period>
   val filingReadPath =
     path("institutions" / Segment / "filings" / Segment) { (lei, period) =>
-      complete("OK")
+      val filingPersistence =
+        sharding.entityRefFor(FilingPersistence.typeKey,
+                              s"${FilingPersistence.name}-$lei-$period")
+
+      timedGet { uri =>
+        val fDetails: Future[FilingDetails] = filingPersistence ? (ref =>
+          GetFilingDetails(ref))
+
+        onComplete(fDetails) {
+          case Success(filingDetails) =>
+            complete(ToResponseMarshallable(filingDetails))
+          case Failure(error) =>
+            val errorResponse =
+              ErrorResponse(500, error.getLocalizedMessage, uri.path)
+            complete(
+              ToResponseMarshallable(
+                StatusCodes.InternalServerError -> errorResponse))
+        }
+      }
     }
 
   def filingRoutes: Route = {
