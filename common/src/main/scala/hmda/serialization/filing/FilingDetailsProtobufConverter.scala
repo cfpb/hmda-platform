35a11b61d46830e0075a89b14240f45a2885f9ac
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-10-04 10:12:44 -0400

    Add filing details protobuf converter

---
+++ common/src/main/scala/hmda/serialization/filing/FilingDetailsProtobufConverter.scala
@@ -0,0 +1,30 @@
+package hmda.serialization.filing
+
+import hmda.model.filing.FilingDetails
+import hmda.persistence.serialization.filing.filingdetails.FilingDetailsMessage
+import FilingProtobufConverter._
+import hmda.persistence.serialization.filing.FilingMessage
+import hmda.serialization.submission.SubmissionProtobufConverter._
+
+object FilingDetailsProtobufConverter {
+
+  def filingDetailsToProtobuf(
+      filingDetails: FilingDetails): FilingDetailsMessage = {
+    FilingDetailsMessage(
+      if (filingDetails.filing.isEmpty) None
+      else Some(filingToProtobuf(filingDetails.filing)),
+      filingDetails.submissions.map(s => submissionToProtobuf(s))
+    )
+  }
+
+  def filingDetailsFromProtobuf(
+      filingDetailsMessage: FilingDetailsMessage): FilingDetails = {
+    FilingDetails(
+      filing = filingFromProtobuf(
+        filingDetailsMessage.filing.getOrElse(FilingMessage())),
+      submissions = filingDetailsMessage.submissions
+        .map(s => submissionFromProtobuf(s))
+        .toList
+    )
+  }
+}
