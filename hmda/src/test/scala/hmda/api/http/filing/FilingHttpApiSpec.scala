1544d65320fc2c0160a5f5b940c1b6df9f766785
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-10-04 09:05:41 -0400

    Test empty filing details

--- hmda/src/test/scala/hmda/api/http/filing/FilingHttpApiSpec.scala
+++ hmda/src/test/scala/hmda/api/http/filing/FilingHttpApiSpec.scala
@@ -1,5 +1,6 @@
 package hmda.api.http.filing
 
+import akka.actor.testkit.typed.scaladsl.TestProbe
 import akka.actor.typed.ActorSystem
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.event.{LoggingAdapter, NoLogging}
@@ -10,11 +11,20 @@ import org.scalatest.MustMatchers
 import akka.actor.typed.scaladsl.adapter._
 import akka.cluster.typed.{Cluster, Join}
 import akka.http.scaladsl.model.StatusCodes
-import akka.testkit._
 import hmda.persistence.filing.FilingPersistence
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import hmda.api.http.codec.filing.FilingCodec._
+import hmda.messages.institution.InstitutionCommands.CreateInstitution
+import hmda.messages.institution.InstitutionEvents.{
+  InstitutionCreated,
+  InstitutionEvent
+}
+import hmda.model.institution.Institution
 import hmda.persistence.institution.InstitutionPersistence
 import io.circe.generic.auto._
+import hmda.model.institution.InstitutionGenerators._
+import akka.testkit._
+import hmda.model.filing.FilingDetails
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
@@ -35,19 +45,41 @@ class FilingHttpApiSpec
   override implicit val timeout: Timeout = Timeout(duration)
   override val sharding: ClusterSharding = ClusterSharding(typedSystem)
 
+  val sampleInstitution = institutionGen
+    .suchThat(_.LEI != "")
+    .sample
+    .getOrElse(Institution.empty.copy(LEI = "AAA"))
+
+  val institutionProbe = TestProbe[InstitutionEvent]("institution-probe")
+
   override def beforeAll(): Unit = {
     super.beforeAll()
     Cluster(typedSystem).manager ! Join(Cluster(typedSystem).selfMember.address)
     InstitutionPersistence.startShardRegion(sharding)
     FilingPersistence.startShardRegion(sharding)
+    val institutionPersistence =
+      sharding.entityRefFor(
+        InstitutionPersistence.typeKey,
+        s"${InstitutionPersistence.name}-${sampleInstitution.LEI}")
+    institutionPersistence ! CreateInstitution(sampleInstitution,
+                                               institutionProbe.ref)
+    institutionProbe.expectMessage(InstitutionCreated(sampleInstitution))
   }
 
   override def afterAll(): Unit = super.afterAll()
 
   "Filings" must {
-    "return error when institution does not exist" in {
+    "return Bad Request when institution does not exist" in {
       Get("/institutions/xxxx/filings/2018") ~> filingRoutes ~> check {
         status mustBe StatusCodes.BadRequest
+      }
+    }
+    "return empty Filing Details when institution exists but filing has not been created" in {
+      val url = s"/institutions/${sampleInstitution.LEI}/filings/2018"
+      println(url)
+      Get(url) ~> filingRoutes ~> check {
+        status mustBe StatusCodes.OK
+        responseAs[FilingDetails] mustBe FilingDetails()
       }
     }
   }
