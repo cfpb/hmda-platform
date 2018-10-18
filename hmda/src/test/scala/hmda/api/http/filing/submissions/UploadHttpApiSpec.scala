9ae1d381d06f5fb24b871f40433179dfd96cf22f
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-10-18 15:58:50 -0400

    Add file upload test

---
+++ hmda/src/test/scala/hmda/api/http/filing/submissions/UploadHttpApiSpec.scala
@@ -0,0 +1,158 @@
+package hmda.api.http.filing.submissions
+
+import akka.actor.testkit.typed.scaladsl.TestProbe
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+import akka.event.{LoggingAdapter, NoLogging}
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.util.Timeout
+import com.typesafe.config.{Config, ConfigFactory}
+import org.scalatest.MustMatchers
+import akka.actor.typed.scaladsl.adapter._
+import akka.cluster.typed.{Cluster, Join}
+import akka.http.scaladsl.model.StatusCodes
+import akka.stream.scaladsl.Source
+import hmda.api.http.filing.FileUploadUtils
+import hmda.messages.filing.FilingCommands.CreateFiling
+import hmda.messages.filing.FilingEvents.FilingEvent
+import hmda.messages.institution.InstitutionCommands.CreateInstitution
+import hmda.messages.institution.InstitutionEvents.{
+  InstitutionCreated,
+  InstitutionEvent
+}
+import hmda.messages.submission.SubmissionCommands.CreateSubmission
+import hmda.messages.submission.SubmissionEvents.{
+  SubmissionCreated,
+  SubmissionEvent
+}
+import hmda.model.filing.Filing
+import hmda.model.filing.FilingGenerator.filingGen
+import hmda.model.filing.submission.{Created, Submission, SubmissionId}
+import hmda.model.filing.ts.TransmittalSheet
+import hmda.model.institution.Institution
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+import io.circe.generic.auto._
+import hmda.api.http.codec.filing.submission.SubmissionStatusCodec._
+import hmda.model.institution.InstitutionGenerators.institutionGen
+import hmda.model.submission.SubmissionGenerator.submissionGen
+import hmda.persistence.AkkaCassandraPersistenceSpec
+import hmda.persistence.filing.FilingPersistence
+import hmda.persistence.institution.InstitutionPersistence
+import hmda.persistence.submission.SubmissionPersistence
+import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
+import hmda.model.filing.ts.TsGenerators._
+import hmda.model.filing.lar.LarGenerators._
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+class UploadHttpApiSpec
+    extends AkkaCassandraPersistenceSpec
+    with MustMatchers
+    with UploadHttpApi
+    with ScalatestRouteTest
+    with FileUploadUtils {
+
+  implicit val typedSystem = system.toTyped
+  override val ec: ExecutionContext = system.dispatcher
+  override val log: LoggingAdapter = NoLogging
+  override val sharding: ClusterSharding = ClusterSharding(typedSystem)
+  override implicit val timeout: Timeout = Timeout(10.seconds)
+  override val config: Config = ConfigFactory.load()
+
+  val period = "2018"
+
+  val institutionProbe = TestProbe[InstitutionEvent]("institution-probe")
+  val filingProbe = TestProbe[FilingEvent]("filing-probe")
+  val submissionProbe = TestProbe[SubmissionEvent]("submission-probe")
+  val maybeSubmissionProbe =
+    TestProbe[Option[Submission]]("submission-probe")
+
+  val sampleInstitution = institutionGen
+    .suchThat(_.LEI != "")
+    .sample
+    .getOrElse(Institution.empty.copy(LEI = "AAA"))
+
+  val sampleFiling = filingGen.sample
+    .getOrElse(Filing())
+    .copy(lei = sampleInstitution.LEI)
+    .copy(period = period)
+
+  val submissionId = SubmissionId(sampleInstitution.LEI, period, 1)
+
+  val sampleSubmission = submissionGen
+    .suchThat(s => !s.id.isEmpty)
+    .suchThat(s => s.id.lei != "" && s.id.lei != "AA")
+    .suchThat(s => s.status == Created)
+    .sample
+    .getOrElse(Submission(submissionId))
+    .copy(id = submissionId)
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    Cluster(typedSystem).manager ! Join(Cluster(typedSystem).selfMember.address)
+    implicit val embeddedKafkaConfig: EmbeddedKafkaConfig = EmbeddedKafkaConfig(
+      9092,
+      2182,
+      Map("offsets.topic.replication.factor" -> "1",
+          "zookeeper.connection.timeout.ms" -> "20000"))
+    EmbeddedKafka.start()
+
+    InstitutionPersistence.startShardRegion(sharding)
+    FilingPersistence.startShardRegion(sharding)
+    SubmissionPersistence.startShardRegion(sharding)
+    val institutionPersistence =
+      sharding.entityRefFor(
+        InstitutionPersistence.typeKey,
+        s"${InstitutionPersistence.name}-${sampleInstitution.LEI}")
+    institutionPersistence ! CreateInstitution(sampleInstitution,
+                                               institutionProbe.ref)
+    institutionProbe.expectMessage(InstitutionCreated(sampleInstitution))
+
+    val filingPersistence =
+      sharding.entityRefFor(
+        FilingPersistence.typeKey,
+        s"${FilingPersistence.name}-${sampleInstitution.LEI}-$period"
+      )
+    filingPersistence ! CreateFiling(sampleFiling, filingProbe.ref)
+
+    val submissionPersistence =
+      sharding.entityRefFor(
+        SubmissionPersistence.typeKey,
+        s"${SubmissionPersistence.name}-${sampleSubmission.id.toString}")
+
+    submissionPersistence ! CreateSubmission(sampleSubmission.id,
+                                             submissionProbe.ref)
+    submissionProbe.expectMessageType[SubmissionCreated]
+  }
+
+  override def afterAll(): Unit = {
+    EmbeddedKafka.stop()
+    super.afterAll()
+  }
+
+  val url =
+    s"/institutions/${sampleInstitution.LEI}/filings/$period/submissions/1"
+
+  val ts = tsGen.sample.getOrElse(TransmittalSheet())
+  val tsCsv = ts.toCSV + "\n"
+  val tsSource = Source.fromIterator(() => List(tsCsv).iterator)
+
+  val larList = larNGen(10).suchThat(_.nonEmpty).sample.getOrElse(Nil)
+  val larCsv = larList.map(lar => lar.toCSV + "\n")
+  val larSource = Source.fromIterator(() => larCsv.iterator)
+
+  val hmdaFileCsv = List(tsCsv) ++ larCsv
+  val hmdaFile = multiPartFile(hmdaFileCsv.mkString(""), "sample.txt")
+
+  "Upload API" must {
+    "upload HMDA File" in {
+      Post(url, hmdaFile) ~> uploadRoutes ~> check {
+        status mustBe StatusCodes.Accepted
+        val submission = responseAs[Submission]
+        submission.start must be < System.currentTimeMillis()
+        submission.id mustBe submissionId
+      }
+    }
+  }
+
+}
