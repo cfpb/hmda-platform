94d5f54a5164a4186dc1a9012deda502843159b9
Nick Grippin <ngrippin@discovertechnologies.com>
2018-10-19 16:08:32 -0400

    Fix typo

--- hmda/src/test/scala/hmda/validation/rules/lar/validity/V659Spec.scala
+++ hmda/src/test/scala/hmda/validation/rules/lar/validity/V659Spec.scala
@@ -1,7 +1,7 @@
 package hmda.validation.rules.lar.validity
 
 import hmda.model.filing.lar.LarGenerators._
-import hmda.model.filing.lar.enums.InvalidLientStatusCode
+import hmda.model.filing.lar.enums.InvalidLienStatusCode
 import hmda.model.filing.lar.LoanApplicationRegister
 import hmda.validation.rules.EditCheck
 import hmda.validation.rules.lar.LarEditCheckSpec
@@ -12,7 +12,7 @@ class V659Spec extends LarEditCheckSpec {
   property("Lein status must be valid") {
     forAll(larGen) { lar =>
       lar.mustPass
-      val invalidLar = lar.copy(lienStatus = InvalidLientStatusCode)
+      val invalidLar = lar.copy(lienStatus = InvalidLienStatusCode)
       invalidLar.mustFail
     }
   }
