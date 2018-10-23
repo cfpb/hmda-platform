38bda130769f9aa85405dd528afe5436c13fd8eb
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-10-23 10:18:57 -0400

    Update processing submission event response

--- hmda/src/main/scala/hmda/persistence/submission/SubmissionManager.scala
+++ hmda/src/main/scala/hmda/persistence/submission/SubmissionManager.scala
@@ -6,8 +6,9 @@ import akka.cluster.sharding.typed.ShardingEnvelope
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import hmda.actor.HmdaTypedActor
 import hmda.messages.submission.SubmissionCommands.ModifySubmission
-import hmda.messages.submission.SubmissionEvents.SubmissionEvent
+import hmda.messages.submission.SubmissionEvents.{SubmissionEvent, SubmissionModified}
 import hmda.messages.submission.SubmissionManagerCommands._
+import hmda.model.filing.submission.Uploaded
 
 object SubmissionManager extends HmdaTypedActor[SubmissionManagerCommand] {
 
@@ -37,11 +38,17 @@ object SubmissionManager extends HmdaTypedActor[SubmissionManagerCommand] {
           Behaviors.same
 
         case WrappedSubmissionEventResponse(submissionEvent) =>
-          log.info(s"$submissionEvent")
-          Behaviors.same
-
-        case _ =>
-          Behaviors.unhandled
+         submissionEvent match {
+           case SubmissionModified(submission) => submission.status match {
+             case Uploaded =>
+              log.info(s"Data uploaded for ${submission.id.toString}")
+             case _ =>
+           }
+             Behaviors.same
+           case _ =>
+             log.info(s"$submissionEvent")
+             Behaviors.same
+         }
       }
 
     }
