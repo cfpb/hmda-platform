a48413a7a9a7db960e98cc76624b09c074b3be69
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-10-24 15:30:27 -0400

    Add parser flow

--- hmda/src/main/scala/hmda/persistence/submission/HmdaParserError.scala
+++ hmda/src/main/scala/hmda/persistence/submission/HmdaParserError.scala
@@ -1,22 +1,24 @@
 package hmda.persistence.submission
 
+import akka.NotUsed
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorContext, ActorRef, Behavior}
 import akka.cluster.sharding.typed.ShardingEnvelope
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+import akka.kafka.scaladsl.Consumer
+import akka.kafka.{ConsumerSettings, Subscriptions}
 import akka.persistence.typed.scaladsl.{Effect, PersistentBehaviors}
 import akka.persistence.typed.scaladsl.PersistentBehaviors.CommandHandler
-import hmda.messages.submission.SubmissionProcessingCommands.{
-  HmdaParserStop,
-  StartParsing,
-  SubmissionProcessingCommand
-}
-import hmda.messages.submission.SubmissionProcessingEvents
-import hmda.messages.submission.SubmissionProcessingEvents.{
-  HmdaRowParsed,
-  SubmissionProcessingEvent
-}
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import hmda.messages.pubsub.KafkaTopics.uploadTopic
+import hmda.messages.submission.SubmissionProcessingCommands.{HmdaParserStop, StartParsing, SubmissionProcessingCommand}
+import hmda.messages.submission.SubmissionProcessingEvents.{HmdaRowParsed, SubmissionProcessingEvent}
+import hmda.model.filing.submission.SubmissionId
+import hmda.parser.filing.ParserFlow.parseHmdaFile
 import hmda.persistence.HmdaTypedPersistentActor
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.kafka.common.serialization.StringDeserializer
 
 object HmdaParserError
     extends HmdaTypedPersistentActor[SubmissionProcessingCommand,
@@ -48,6 +50,15 @@ object HmdaParserError
     cmd match {
       case StartParsing(submissionId) =>
         log.info(s"Start parsing for ${submissionId.toString}")
+        uploadConsumer(ctx, submissionId)
+          .map(_.value())
+          .map(ByteString(_))
+          .via(parseHmdaFile)
+          .filter(x => x.isLeft)
+          .collect {
+            case Left(errors) => errors
+          }
+
         Effect.none
       case _ =>
         Effect.none
@@ -64,6 +75,18 @@ object HmdaParserError
   def startShardRegion(sharding: ClusterSharding)
     : ActorRef[ShardingEnvelope[SubmissionProcessingCommand]] = {
     super.startShardRegion(sharding, HmdaParserStop)
+  }
+
+  def uploadConsumer(ctx: ActorContext[_], submissionId: SubmissionId): Source[ConsumerRecord[String, String], Consumer.Control] = {
+    val kafkaConfig = ctx.asScala.system.settings.config.getConfig("akka.kafka.consumer")
+    val consumerSettings =
+      ConsumerSettings(kafkaConfig, new StringDeserializer, new StringDeserializer)
+        .withBootstrapServers("localhost:9092")
+        .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+
+    Consumer.plainSource(consumerSettings, Subscriptions.topics(uploadTopic))
+        .filter(_.key() == submissionId.toString)
+
   }
 
 }
