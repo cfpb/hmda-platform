9a9cbfef11bf31d78b0e9716bb7a37723df43059
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-11-30 07:36:03 -0500

    Add raw data persistence

---
+++ hmda/src/main/scala/hmda/persistence/submission/HmdaRawData.scala
@@ -0,0 +1,85 @@
+package hmda.persistence.submission
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorContext, ActorRef, Behavior}
+import akka.cluster.sharding.typed.ShardingEnvelope
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.scaladsl.{Effect, PersistentBehavior}
+import akka.persistence.typed.scaladsl.PersistentBehavior.CommandHandler
+import hmda.messages.CommonMessages.{Command, Event}
+import hmda.model.filing.submission.SubmissionId
+import hmda.persistence.HmdaTypedPersistentActor
+
+sealed trait HmdaRawDataCommand extends Command
+
+case class AddLine(submissionId: SubmissionId,
+                   timestamp: Long,
+                   data: String,
+                   replyTo: Option[ActorRef[HmdaRawDataEvent]])
+    extends HmdaRawDataCommand
+case class CompleteUpload(submissionId: SubmissionId) extends HmdaRawDataCommand
+
+sealed trait HmdaRawDataEvent extends Event
+case class LineAdded(timestamp: Long, data: String) extends HmdaRawDataEvent
+
+case class HmdaRawDataState(size: Int = 0) {
+  def update(event: HmdaRawDataEvent): HmdaRawDataState = event match {
+    case LineAdded(_, _) =>
+      HmdaRawDataState(size + 1)
+    case _ => this
+  }
+}
+
+object HmdaRawData
+    extends HmdaTypedPersistentActor[HmdaRawDataCommand,
+                                     HmdaRawDataEvent,
+                                     HmdaRawDataState] {
+
+  override val name: String = "HmdaRawData"
+
+  override def behavior(entityId: String): Behavior[HmdaRawDataCommand] =
+    Behaviors.setup { ctx =>
+      PersistentBehavior[HmdaRawDataCommand,
+                         HmdaRawDataEvent,
+                         HmdaRawDataState](
+        persistenceId = PersistenceId(entityId),
+        emptyState = HmdaRawDataState(),
+        commandHandler = commandHandler(ctx),
+        eventHandler = eventHandler
+      ).snapshotEvery(1000)
+    }
+
+  override def commandHandler(ctx: ActorContext[HmdaRawDataCommand])
+    : CommandHandler[HmdaRawDataCommand, HmdaRawDataEvent, HmdaRawDataState] = {
+    (_, cmd) =>
+      val log = ctx.asScala.log
+      cmd match {
+        case AddLine(_, timestamp, data, maybeReplyTo) =>
+          val evt = LineAdded(timestamp, data)
+          Effect.persist(evt).thenRun { _ =>
+            log.debug(s"Persisted: $data")
+            maybeReplyTo match {
+              case Some(replyTo) =>
+                replyTo ! evt
+              case None => //Do Nothing
+            }
+          }
+
+        case CompleteUpload(submissionId) =>
+          log.debug(s"Upload completed for $submissionId")
+          Effect.none
+      }
+  }
+
+  override def eventHandler
+    : (HmdaRawDataState, HmdaRawDataEvent) => HmdaRawDataState = {
+    case (state, evt @ LineAdded(_, _)) => state.update(evt)
+  }
+
+  def startShardRegion(sharding: ClusterSharding)
+    : ActorRef[ShardingEnvelope[HmdaRawDataCommand]] = {
+    super.startShardRegion(sharding)
+  }
+
+}
