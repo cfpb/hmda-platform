32486aac9d3b68a5ac3e0e8e3a3a713d832c2936
picimaci <Fanni.Medvey@wanari.com>
2018-12-01 03:00:05 +0100

    Add test for EditDetailsEventsSerializer

---
+++ common/src/test/scala/hmda/model/edits/EditDetailsGenerator.scala
@@ -0,0 +1,27 @@
+package hmda.model.edits
+
+import org.scalacheck.Gen
+
+object EditDetailsGenerator {
+
+  implicit def fieldDetailsGen: Gen[FieldDetails] = {
+    for {
+      name <- Gen.alphaStr
+      value <- Gen.choose(Integer.MIN_VALUE, Integer.MAX_VALUE)
+    } yield FieldDetails(name, value)
+  }
+
+  implicit def editDetailsRowGen: Gen[EditDetailsRow] = {
+    for {
+      id <- Gen.alphaStr
+      fields <- Gen.listOf(fieldDetailsGen)
+    } yield EditDetailsRow(id, fields)
+  }
+
+  implicit def editDetailsGen: Gen[EditDetails] = {
+    for {
+      edit <- Gen.alphaStr
+      rows <- Gen.listOf(editDetailsRowGen)
+    } yield EditDetails(edit, rows)
+  }
+}
