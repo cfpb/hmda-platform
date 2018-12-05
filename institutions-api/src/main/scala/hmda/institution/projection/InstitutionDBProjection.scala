11301bf8c8971bcd4c7a6a6630eb63d210c9b4b6
Nick Grippin <ngrippin@discovertechnologies.com>
2018-12-05 11:15:36 -0500

    execute inserts in serial

--- institutions-api/src/main/scala/hmda/institution/projection/InstitutionDBProjection.scala
+++ institutions-api/src/main/scala/hmda/institution/projection/InstitutionDBProjection.scala
@@ -4,16 +4,13 @@ import akka.persistence.query.EventEnvelope
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import hmda.institution.api.http.InstitutionConverter
-import hmda.institution.query.InstitutionComponent
-import hmda.messages.institution.InstitutionEvents.{
-  InstitutionCreated,
-  InstitutionDeleted,
-  InstitutionModified
-}
+import hmda.institution.query.{InstitutionComponent, InstitutionEmailEntity}
+import hmda.messages.institution.InstitutionEvents.{InstitutionCreated, InstitutionDeleted, InstitutionModified}
+import hmda.model.institution.Institution
 import hmda.projection.ResumableProjection
 import hmda.query.DbConfiguration._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 
 object InstitutionDBProjection
@@ -37,20 +34,35 @@ object InstitutionDBProjection
     val event = envelope.event
     event match {
       case InstitutionCreated(i) =>
-        institutionRepository.insertOrUpdate(InstitutionConverter.convert(i))
-        val emails = InstitutionConverter.emailsFromInstitution(i)
-        emails.foreach(email => updateEmails(email))
+        updateTables(i)
 
       case InstitutionModified(i) =>
-        institutionRepository.insertOrUpdate(InstitutionConverter.convert(i))
-        val emails = InstitutionConverter.emailsFromInstitution(i)
-        emails.foreach(email => updateEmails(email))
+        updateTables(i)
 
       case InstitutionDeleted(lei) =>
         institutionRepository.deleteById(lei)
         deleteEmails(lei)
     }
     envelope
+  }
+
+  private def updateTables(inst: Institution): Future[List[Int]] = {
+    val insertResult = institutionRepository.insertOrUpdate(InstitutionConverter.convert(inst))
+    val emails = InstitutionConverter.emailsFromInstitution(inst).toList
+    for {
+      institutionRow <- insertResult
+      emailsRows <- updateEmailsInSerial(emails)
+    } yield emailsRows :+ institutionRow
+  }
+
+  private def updateEmailsInSerial(emails: List[InstitutionEmailEntity]): Future[List[Int]] = {
+    emails.foldLeft(Future(List.empty[Int])) {
+      (previousInserts, nextEmail) =>
+        for {
+          completedInserts <- previousInserts
+          insertResult <- updateEmails(nextEmail)
+        } yield completedInserts :+ insertResult
+    }
   }
 
 }
