ca7c40d2164154132196cc378a8092700401c354
Patrick Asata <patrick.asata@cfpb.gov>
2018-12-06 05:57:40 -0500

    resolving issue where submissions with no issues could not be signed

--- hmda/src/main/scala/hmda/persistence/submission/HmdaValidationError.scala
+++ hmda/src/main/scala/hmda/persistence/submission/HmdaValidationError.scala
@@ -213,7 +213,7 @@ object HmdaValidationError
         if (state.statusCode == Verified.code) {
           val timestamp = Instant.now().toEpochMilli
           val signed = SubmissionSigned(submissionId, timestamp, Signed)
-          if (state.qualityVerified && state.macroVerified) {
+          if ((state.qualityVerified && state.macroVerified) || state.statusCode == Verified.code) {
             Effect.persist(signed).thenRun { _ =>
               log.info(
                 s"Submission $submissionId signed at ${Instant.ofEpochMilli(timestamp)}")
