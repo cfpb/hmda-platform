61e3d09a71048f1a2bc381eadb17689d6d3c1805
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-12-07 10:59:40 -0500

    Refactor total count test

--- hmda/src/test/scala/hmda/validation/filing/MacroValidationFlowSpec.scala
+++ hmda/src/test/scala/hmda/validation/filing/MacroValidationFlowSpec.scala
@@ -1,7 +1,7 @@
 package hmda.validation.filing
 
 import akka.stream.scaladsl.Source
-import org.scalatest.{AsyncWordSpec, MustMatchers}
+import org.scalatest.{AsyncWordSpec, BeforeAndAfterAll, MustMatchers}
 import hmda.util.SourceUtils._
 import akka.NotUsed
 import akka.actor.ActorSystem
@@ -9,19 +9,30 @@ import akka.stream.ActorMaterializer
 import hmda.model.filing.lar.LoanApplicationRegister
 import hmda.parser.filing.lar.LarCsvParser
 
-class MacroValidationFlowSpec extends AsyncWordSpec with MustMatchers {
+class MacroValidationFlowSpec
+    extends AsyncWordSpec
+    with MustMatchers
+    with BeforeAndAfterAll {
 
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
   implicit val ec = system.dispatcher
 
+  override def afterAll(): Unit = {
+    system.terminate()
+  }
+
   val fileSource = scala.io.Source.fromURL(
     getClass.getResource("/clean_file_1000_rows_Bank0_syntax_validity.txt"))
 
-  val lars = fileSource.getLines().drop(1)
-  val source: Source[LoanApplicationRegister, NotUsed] = Source
-    .fromIterator(() => lars)
+  val lars = fileSource
+    .getLines()
+    .drop(1)
     .map(x => LarCsvParser(x).getOrElse(LoanApplicationRegister()))
+    .toList
+
+  val source: Source[LoanApplicationRegister, NotUsed] = Source
+    .fromIterator(() => lars.toIterator)
 
   "Macro Validation" must {
     "count total number of LARs" in {
