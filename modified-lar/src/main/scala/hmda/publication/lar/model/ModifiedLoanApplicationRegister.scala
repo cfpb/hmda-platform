698e090c9cce1b221a9400d40752482589e637fb
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-12-12 18:22:28 -0500

    Add modified LAR structure

--- modified-lar/src/main/scala/hmda/publication/lar/model/ModifiedLoanApplicationRegister.scala
+++ modified-lar/src/main/scala/hmda/publication/lar/model/ModifiedLoanApplicationRegister.scala
@@ -1,61 +1,116 @@
 package hmda.publication.lar.model
 
-import hmda.model.filing.lar.enums._
-import hmda.model.filing.lar._
 import hmda.model.filing.PipeDelimited
 
 case class ModifiedLoanApplicationRegister(
-    larIdentifier: LarIdentifier = LarIdentifier(),
-    loan: Loan = Loan(),
-    action: LarAction = LarAction(),
-    geography: Geography = Geography(),
-    applicant: Applicant = Applicant(),
-    coApplicant: Applicant = Applicant(),
-    income: String = "",
-    purchaserType: PurchaserEnum = InvalidPurchaserCode,
-    hoepaStatus: HOEPAStatusEnum = InvalidHoepaStatusCode,
-    lienStatus: LienStatusEnum = InvalidLienStatusCode,
-    denial: Denial = Denial(),
-    loanDisclosure: LoanDisclosure = LoanDisclosure(),
-    nonAmortizingFeatures: NonAmortizingFeatures = NonAmortizingFeatures(),
-    property: Property = Property(),
-    applicationSubmission: ApplicationSubmissionEnum =
-      InvalidApplicationSubmissionCode,
-    payableToInstitution: PayableToInstitutionEnum =
-      InvalidPayableToInstitutionCode,
-    AUS: AutomatedUnderwritingSystem = AutomatedUnderwritingSystem(),
-    ausResult: AutomatedUnderwritingSystemResult =
-      AutomatedUnderwritingSystemResult(),
-    reverseMortgage: MortgageTypeEnum = InvalidMortgageTypeCode,
-    lineOfCredit: LineOfCreditEnum = InvalidLineOfCreditCode,
-    businessOrCommercialPurpose: BusinessOrCommercialBusinessEnum =
-      InvalidBusinessOrCommercialBusinessCode
+    id: Int,
+    lei: String,
+    loanType: Int,
+    loanPurpose: Int,
+    preapproval: Int,
+    constructionMethod: Int,
+    occupancy: Int,
+    loanAmount: Int,
+    actionTakenType: Int,
+    state: String,
+    county: String,
+    tract: String,
+    ethnicity1: String,
+    ethnicity2: String,
+    ethnicity3: String,
+    ethnicity4: String,
+    ethnicity5: String,
+    ethnicityVisualObservation: Int,
+    coEthnicity1: String,
+    coEthnicity2: String,
+    coEthnicity3: String,
+    coEthnicity4: String,
+    coEthnicity5: String,
+    coEthnicityVisualObservation: Int,
+    race1: String,
+    race2: String,
+    race3: String,
+    race4: String,
+    race5: String,
+    coRace1: String,
+    coRace2: String,
+    coRace3: String,
+    coRace4: String,
+    coRace5: String,
+    raceVisualObservation: Int,
+    coRaceVisualObservation: Int,
+    sex: Int,
+    coSex: Int,
+    sexVisualObservation: Int,
+    coSexVisualObservation: Int,
+    age: String,
+    ageGreaterThanOrEqual62: String,
+    coAge: String,
+    coAgeGreaterThanOrEqual62: String,
+    income: String,
+    purchaserType: Int,
+    rateSpread: String,
+    hoepaStatus: Int,
+    lienStatus: Int,
+    applicantCredisScoreModel: Int,
+    coApplicantCreditScoreModel: Int,
+    denial1: Int,
+    denial2: String,
+    denial3: String,
+    denial4: String,
+    totalLoanCosts: String,
+    totalPointsAndFees: String,
+    originationCharges: String,
+    discountPoints: String,
+    lenderCredits: String,
+    interestRate: String,
+    prepaymentPenalty: String,
+    debtToIncomeRatio: String,
+    loanToValueRatio: String,
+    loanTerm: String,
+    introductoryRatePeriod: String,
+    balloonPayment: Int,
+    interestOnlyPayment: Int,
+    negativeAmortization: Int,
+    otherNonAmortizingFeatures: Int,
+    propertyValue: String,
+    homeSecuredPropertyType: Int,
+    homeLandPropertyType: Int,
+    totalUnits: String,
+    multifamilyAffordableUnits: String,
+    applicationSubmission: Int,
+    initiallyPayableToInstitution: Int,
+    AUS1: Int,
+    AUS2: Int,
+    AUS3: Int,
+    AUS4: Int,
+    AUS5: Int,
+    reverseMortgage: Int,
+    openEndLineOfCredit: Int,
+    businessOrCommercialPurpose: Int
 ) extends PipeDelimited {
 
   override def toCSV: String = {
 
-    s"${larIdentifier.id}|${larIdentifier.LEI}|${loan.ULI}|${loan.applicationDate}|${loan.loanType.code}|${loan.loanPurpose.code}|${action.preapproval.code}|" +
-      s"${loan.constructionMethod.code}|${loan.occupancy.code}|${loan.amount}|${action.actionTakenType.code}|${action.actionTakenDate}|" +
-      s"${geography.toCSV}|" +
-      s"${applicant.ethnicity.ethnicity1.code}|${applicant.ethnicity.ethnicity2.code}|${applicant.ethnicity.ethnicity3.code}|" +
-      s"${applicant.ethnicity.ethnicity4.code}|${applicant.ethnicity.ethnicity5.code}|${applicant.ethnicity.otherHispanicOrLatino}|" +
-      s"${coApplicant.ethnicity.ethnicity1.code}|${coApplicant.ethnicity.ethnicity2.code}|${coApplicant.ethnicity.ethnicity3.code}|" +
-      s"${coApplicant.ethnicity.ethnicity4.code}|${coApplicant.ethnicity.ethnicity5.code}|${coApplicant.ethnicity.otherHispanicOrLatino}|" +
-      s"${applicant.ethnicity.ethnicityObserved.code}|${coApplicant.ethnicity.ethnicityObserved.code}|${applicant.race.race1.code}|" +
-      s"${applicant.race.race2.code}|${applicant.race.race3.code}|${applicant.race.race4.code}|${applicant.race.race5.code}|${applicant.race.otherNativeRace}|" +
-      s"${applicant.race.otherAsianRace}|${applicant.race.otherPacificIslanderRace}|${coApplicant.race.race1.code}|${coApplicant.race.race2.code}|" +
-      s"${coApplicant.race.race3.code}|${coApplicant.race.race4.code}|${coApplicant.race.race5.code}|${coApplicant.race.otherNativeRace}|${coApplicant.race.otherAsianRace}|" +
-      s"${coApplicant.race.otherPacificIslanderRace}|${applicant.race.raceObserved.code}|${coApplicant.race.raceObserved.code}|" +
-      s"${applicant.sex.sexEnum.code}|${coApplicant.sex.sexEnum.code}|${applicant.sex.sexObservedEnum.code}|${coApplicant.sex.sexObservedEnum.code}|" +
-      s"${applicant.age}|${coApplicant.age}|$income|${purchaserType.code}|${loan.rateSpread}|${hoepaStatus.code}|${lienStatus.code}|${applicant.creditScore}|${coApplicant.creditScore}|" +
-      s"${applicant.creditScoreType.code}|${applicant.otherCreditScoreModel}|${coApplicant.creditScoreType.code}|${coApplicant.otherCreditScoreModel}|" +
-      s"${denial.denialReason1.code}|${denial.denialReason2.code}|${denial.denialReason3.code}|${denial.denialReason4.code}|${denial.otherDenialReason}|${loanDisclosure.totalLoanCosts}|" +
-      s"${loanDisclosure.totalPointsAndFees}|${loanDisclosure.originationCharges}|${loanDisclosure.discountPoints}|${loanDisclosure.lenderCredits}|${loan.interestRate}|" +
-      s"${loan.prepaymentPenaltyTerm}|${loan.debtToIncomeRatio}|${loan.combinedLoanToValueRatio}|${loan.loanTerm}|${loan.introductoryRatePeriod}|${nonAmortizingFeatures.balloonPayment.code}|" +
-      s"${nonAmortizingFeatures.interestOnlyPayments.code}|${nonAmortizingFeatures.negativeAmortization.code}|${nonAmortizingFeatures.otherNonAmortizingFeatures.code}|" +
-      s"${property.propertyValue}|${property.manufacturedHomeSecuredProperty.code}|${property.manufacturedHomeLandPropertyInterest.code}|${property.totalUnits}|${property.multiFamilyAffordableUnits}|${applicationSubmission.code}|" +
-      s"${payableToInstitution.code}|${larIdentifier.NMLSRIdentifier}|${AUS.toCSV}|${ausResult.toCSV}|${reverseMortgage.code}|${lineOfCredit.code}|${businessOrCommercialPurpose.code}"
-
+    s"$id|$lei|$loanType|$loanPurpose|$preapproval|$constructionMethod|$occupancy|$loanAmount|" +
+      s"$actionTakenType|$state|$county|$tract|$ethnicity1|" +
+      s"$ethnicity2|" +
+      s"$ethnicity3|$ethnicity4|$ethnicity5|" +
+      s"$coEthnicity1|$coEthnicity2|$coEthnicity3|" +
+      s"$coEthnicity4|$coEthnicity5|$ethnicityVisualObservation|" +
+      s"$coEthnicityVisualObservation|$race1|$race2|" +
+      s"$race3|$race4|$race5|" +
+      s"$coRace1|$coRace2|$coRace3|$coRace4|$coRace5|" +
+      s"$raceVisualObservation|$coRaceVisualObservation|$sex|$coSex|" +
+      s"$age|$ageGreaterThanOrEqual62|$coAge|$coAgeGreaterThanOrEqual62|$income|" +
+      s"$purchaserType|$rateSpread|$hoepaStatus|" +
+      s"$lienStatus|$applicantCredisScoreModel|$coApplicantCreditScoreModel|$denial1|" +
+      s"$denial2|$denial3|$denial4|$totalLoanCosts|$totalPointsAndFees|$originationCharges|$discountPoints|$lenderCredits|$interestRate|" +
+      s"$prepaymentPenalty|$debtToIncomeRatio|$loanToValueRatio|$loanTerm|" +
+      s"$introductoryRatePeriod|$balloonPayment|$interestOnlyPayment|$negativeAmortization|$otherNonAmortizingFeatures|$propertyValue|" +
+      s"$homeSecuredPropertyType|$homeLandPropertyType|$totalUnits|$multifamilyAffordableUnits|$applicationSubmission|" +
+      s"$initiallyPayableToInstitution|$AUS1|$AUS2|$AUS3|$AUS4|$AUS5|" +
+      s"$reverseMortgage|$openEndLineOfCredit|$businessOrCommercialPurpose"
   }
 
 }
