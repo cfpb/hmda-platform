5db064c62dff4d41ad06b0a89450c2ce5de4c719
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-12-15 01:01:19 -0500

    Add configuration files

---
+++ hmda-analytics/src/test/resources/hmda.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE hmda
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.utf8'
+       LC_CTYPE = 'en_US.utf8'
+       CONNECTION LIMIT = -1;
+
