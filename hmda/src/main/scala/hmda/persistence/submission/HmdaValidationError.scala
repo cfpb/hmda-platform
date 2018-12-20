b8c086c8b36665895e9a4fd771bc72d32b93b687
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-12-20 12:30:59 -0500

    Close GRPC connections after they are used

--- hmda/src/main/scala/hmda/persistence/submission/HmdaValidationError.scala
+++ hmda/src/main/scala/hmda/persistence/submission/HmdaValidationError.scala
@@ -23,19 +23,33 @@ import akka.cluster.sharding.typed.ShardingEnvelope
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.stream.ActorMaterializer
 import akka.stream.typed.scaladsl.ActorFlow
-import hmda.messages.institution.InstitutionCommands.{GetInstitution, ModifyInstitution}
+import hmda.messages.institution.InstitutionCommands.{
+  GetInstitution,
+  ModifyInstitution
+}
 import hmda.model.filing.ts.{TransmittalLar, TransmittalSheet}
 import hmda.model.institution.Institution
 import hmda.persistence.institution.InstitutionPersistence
 import hmda.validation.context.ValidationContext
 import hmda.parser.filing.ParserFlow._
 import hmda.validation.filing.ValidationFlow._
-import HmdaProcessingUtils.{readRawData, updateSubmissionReceipt, updateSubmissionStatus}
+import HmdaProcessingUtils.{
+  readRawData,
+  updateSubmissionReceipt,
+  updateSubmissionStatus
+}
 import EditDetailsConverter._
 import akka.{Done, NotUsed}
 import akka.cluster.sharding.typed.scaladsl.EntityRef
-import hmda.messages.institution.InstitutionEvents.{InstitutionEvent, InstitutionKafkaEvent, InstitutionModified}
-import hmda.messages.submission.EditDetailsCommands.{EditDetailsPersistenceCommand, PersistEditDetails}
+import hmda.messages.institution.InstitutionEvents.{
+  InstitutionEvent,
+  InstitutionKafkaEvent,
+  InstitutionModified
+}
+import hmda.messages.submission.EditDetailsCommands.{
+  EditDetailsPersistenceCommand,
+  PersistEditDetails
+}
 import hmda.messages.submission.EditDetailsEvents.EditDetailsPersistenceEvent
 import hmda.publication.KafkaUtils._
 import hmda.messages.pubsub.HmdaTopics._
@@ -570,12 +584,11 @@ object HmdaValidationError
       sharding: ClusterSharding): Unit = {
 
     val institutionPersistence =
-      sharding.entityRefFor(
-        InstitutionPersistence.typeKey,
-        s"${InstitutionPersistence.name}-$institutionID")
+      sharding.entityRefFor(InstitutionPersistence.typeKey,
+                            s"${InstitutionPersistence.name}-$institutionID")
 
     val fInstitution: Future[Option[Institution]] = institutionPersistence ? (
-      ref => GetInstitution(ref))
+        ref => GetInstitution(ref))
 
     for {
       maybeInst <- fInstitution
