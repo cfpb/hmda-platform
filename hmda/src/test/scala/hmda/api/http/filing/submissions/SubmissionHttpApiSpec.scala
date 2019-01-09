f4d2df0be15e9db81327d2ebadf0319b12659263
bsharma <bsharma@healthstarinfo.com>
2019-01-08 22:05:21 -0500

    add specs

--- hmda/src/test/scala/hmda/api/http/filing/submissions/SubmissionHttpApiSpec.scala
+++ hmda/src/test/scala/hmda/api/http/filing/submissions/SubmissionHttpApiSpec.scala
@@ -36,108 +36,108 @@ import org.keycloak.adapters.KeycloakDeploymentBuilder
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
-class SubmissionHttpApiSpec {
-//    extends AkkaCassandraPersistenceSpec
-//    with MustMatchers
-//    with SubmissionHttpApi
-//    with ScalatestRouteTest {
-//
-//  val duration = 10.seconds
-//
-//  implicit val routeTimeout = RouteTestTimeout(duration.dilated)
-//
-//  override implicit val typedSystem: ActorSystem[_] = system.toTyped
-//  override val log: LoggingAdapter = NoLogging
-//  val ec: ExecutionContext = system.dispatcher
-//  override implicit val timeout: Timeout = Timeout(duration)
-//  override val sharding: ClusterSharding = ClusterSharding(typedSystem)
-//
-//  val oAuth2Authorization = OAuth2Authorization(
-//    log,
-//    new KeycloakTokenVerifier(
-//      KeycloakDeploymentBuilder.build(
-//        getClass.getResourceAsStream("/keycloak.json")
-//      )
-//    )
-//  )
-//
-//  val period = "2018"
-//
-//  val sampleInstitution = institutionGen
-//    .suchThat(_.LEI != "")
-//    .sample
-//    .getOrElse(Institution.empty.copy(LEI = "AAA"))
-//
-//  val sampleFiling = filingGen.sample
-//    .getOrElse(Filing())
-//    .copy(lei = sampleInstitution.LEI)
-//    .copy(period = period)
-//
-//  val institutionProbe = TestProbe[InstitutionEvent]("institution-probe")
-//  val filingProbe = TestProbe[FilingEvent]("filing-probe")
-//
-//  override def beforeAll(): Unit = {
-//    super.beforeAll()
-//    Cluster(typedSystem).manager ! Join(Cluster(typedSystem).selfMember.address)
-//    InstitutionPersistence.startShardRegion(sharding)
-//    FilingPersistence.startShardRegion(sharding)
-//    SubmissionPersistence.startShardRegion(sharding)
-//    val institutionPersistence =
-//      sharding.entityRefFor(
-//        InstitutionPersistence.typeKey,
-//        s"${InstitutionPersistence.name}-${sampleInstitution.LEI}")
-//    institutionPersistence ! CreateInstitution(sampleInstitution,
-//                                               institutionProbe.ref)
-//    institutionProbe.expectMessage(InstitutionCreated(sampleInstitution))
-//
-//    val filingPersistence =
-//      sharding.entityRefFor(
-//        FilingPersistence.typeKey,
-//        s"${FilingPersistence.name}-${sampleInstitution.LEI}-$period"
-//      )
-//    filingPersistence ! CreateFiling(sampleFiling, filingProbe.ref)
-//  }
-//
-//  override def afterAll(): Unit = super.afterAll()
-//
-//  val url =
-//    s"/institutions/${sampleInstitution.LEI}/filings/$period/submissions"
-//  val noInstitutionUrl = s"/institutions/xxx/filings/$period/submissions"
-//  val noFilingUrl =
-//    s"/institutions/${sampleInstitution.LEI}/filings/2017/submissions"
-//
-//  "Submissions HTTP API" must {
-//    "create new submission" in {
-//      Post(url) ~> submissionRoutes(oAuth2Authorization) ~> check {
-//        status mustBe StatusCodes.Created
-//        responseAs[Submission].id mustBe SubmissionId(sampleInstitution.LEI,
-//                                                      period,
-//                                                      1)
-//      }
-//    }
-//    "fail to create a new submission if institutions does not exist" in {
-//      Post(noInstitutionUrl) ~> submissionRoutes(oAuth2Authorization) ~> check {
-//        status mustBe StatusCodes.BadRequest
-//      }
-//    }
-//    "fail to create a new submission if filing does not exist" in {
-//      Post(noFilingUrl) ~> submissionRoutes(oAuth2Authorization) ~> check {
-//        status mustBe StatusCodes.BadRequest
-//      }
-//    }
-//    "return latest submission" in {
-//      Get(s"$url/latest") ~> submissionRoutes(oAuth2Authorization) ~> check {
-//        status mustBe StatusCodes.OK
-//        responseAs[Submission].id.sequenceNumber mustBe 1
-//      }
-//      Post(url) ~> submissionRoutes(oAuth2Authorization) ~> check {
-//        status mustBe StatusCodes.Created
-//      }
-//      Get(s"$url/latest") ~> submissionRoutes(oAuth2Authorization) ~> check {
-//        status mustBe StatusCodes.OK
-//        responseAs[Submission].id.sequenceNumber mustBe 2
-//      }
-//    }
-//  }
+class SubmissionHttpApiSpec 
+    extends AkkaCassandraPersistenceSpec
+    with MustMatchers
+    with SubmissionHttpApi
+    with ScalatestRouteTest {
+
+  val duration = 10.seconds
+
+  implicit val routeTimeout = RouteTestTimeout(duration.dilated)
+
+  override implicit val typedSystem: ActorSystem[_] = system.toTyped
+  override val log: LoggingAdapter = NoLogging
+  val ec: ExecutionContext = system.dispatcher
+  override implicit val timeout: Timeout = Timeout(duration)
+  override val sharding: ClusterSharding = ClusterSharding(typedSystem)
+
+  val oAuth2Authorization = OAuth2Authorization(
+    log,
+    new KeycloakTokenVerifier(
+      KeycloakDeploymentBuilder.build(
+        getClass.getResourceAsStream("/keycloak.json")
+      )
+    )
+  )
+
+  val period = "2018"
+
+  val sampleInstitution = institutionGen
+    .suchThat(_.LEI != "")
+    .sample
+    .getOrElse(Institution.empty.copy(LEI = "AAA"))
+
+  val sampleFiling = filingGen.sample
+    .getOrElse(Filing())
+    .copy(lei = sampleInstitution.LEI)
+    .copy(period = period)
+
+  val institutionProbe = TestProbe[InstitutionEvent]("institution-probe")
+  val filingProbe = TestProbe[FilingEvent]("filing-probe")
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    Cluster(typedSystem).manager ! Join(Cluster(typedSystem).selfMember.address)
+    InstitutionPersistence.startShardRegion(sharding)
+    FilingPersistence.startShardRegion(sharding)
+    SubmissionPersistence.startShardRegion(sharding)
+    val institutionPersistence =
+      sharding.entityRefFor(
+        InstitutionPersistence.typeKey,
+        s"${InstitutionPersistence.name}-${sampleInstitution.LEI}")
+    institutionPersistence ! CreateInstitution(sampleInstitution,
+                                               institutionProbe.ref)
+    institutionProbe.expectMessage(InstitutionCreated(sampleInstitution))
+
+    val filingPersistence =
+      sharding.entityRefFor(
+        FilingPersistence.typeKey,
+        s"${FilingPersistence.name}-${sampleInstitution.LEI}-$period"
+      )
+    filingPersistence ! CreateFiling(sampleFiling, filingProbe.ref)
+  }
+
+  override def afterAll(): Unit = super.afterAll()
+
+  val url =
+    s"/institutions/${sampleInstitution.LEI}/filings/$period/submissions"
+  val noInstitutionUrl = s"/institutions/xxx/filings/$period/submissions"
+  val noFilingUrl =
+    s"/institutions/${sampleInstitution.LEI}/filings/2017/submissions"
+
+  "Submissions HTTP API" must {
+    "create new submission" in {
+      Post(url) ~> submissionRoutes(oAuth2Authorization) ~> check {
+        status mustBe StatusCodes.Created
+        responseAs[Submission].id mustBe SubmissionId(sampleInstitution.LEI,
+                                                      period,
+                                                      1)
+      }
+    }
+    "fail to create a new submission if institutions does not exist" in {
+      Post(noInstitutionUrl) ~> submissionRoutes(oAuth2Authorization) ~> check {
+        status mustBe StatusCodes.BadRequest
+      }
+    }
+    "fail to create a new submission if filing does not exist" in {
+      Post(noFilingUrl) ~> submissionRoutes(oAuth2Authorization) ~> check {
+        status mustBe StatusCodes.BadRequest
+      }
+    }
+    "return latest submission" in {
+      Get(s"$url/latest") ~> submissionRoutes(oAuth2Authorization) ~> check {
+        status mustBe StatusCodes.OK
+        responseAs[Submission].id.sequenceNumber mustBe 1
+      }
+      Post(url) ~> submissionRoutes(oAuth2Authorization) ~> check {
+        status mustBe StatusCodes.Created
+      }
+      Get(s"$url/latest") ~> submissionRoutes(oAuth2Authorization) ~> check {
+        status mustBe StatusCodes.OK
+        responseAs[Submission].id.sequenceNumber mustBe 2
+      }
+    }
+  }
 
 }
--- hmda/src/test/scala/hmda/persistence/submission/HmdaValidationErrorSpec.scala
+++ hmda/src/test/scala/hmda/persistence/submission/HmdaValidationErrorSpec.scala
@@ -13,117 +13,117 @@ import hmda.model.validation._
 import hmda.persistence.AkkaCassandraPersistenceSpec
 import hmda.persistence.institution.InstitutionPersistence
 
-class HmdaValidationErrorSpec {
-//  extends AkkaCassandraPersistenceSpec {
-//   override implicit val system = actor.ActorSystem()
-//  override implicit val typedSystem = system.toTyped
-//
-//  val sharding = ClusterSharding(typedSystem)
-//  SubmissionManager.startShardRegion(sharding)
-//  SubmissionPersistence.startShardRegion(sharding)
-//  HmdaValidationError.startShardRegion(sharding)
-//  EditDetailsPersistence.startShardRegion(sharding)
-//  InstitutionPersistence.startShardRegion(sharding)
-//
-//  val submissionId = SubmissionId("12345", "2018", 1)
-//
-//  val errorsProbe = TestProbe[HmdaRowValidatedError]("processing-event")
-//  val stateProbe = TestProbe[HmdaValidationErrorState]("state-probe")
-//  val eventsProbe = TestProbe[SubmissionProcessingEvent]("events-probe")
-//  val signedProbe = TestProbe[SubmissionSignedEvent]("sign-event")
-//
-//  "Validation Errors" must {
-//    Cluster(typedSystem).manager ! Join(Cluster(typedSystem).selfMember.address)
-//    val hmdaValidationError = sharding.entityRefFor(
-//      HmdaValidationError.typeKey,
-//      s"${HmdaValidationError.name}-${submissionId.toString}")
-//    "be persisted and read back" in {
-//      val tsError: ValidationError =
-//        SyntacticalValidationError("12345XXX", "S300", TsValidationError)
-//      val larErrors: Seq[ValidationError] = Seq(
-//        SyntacticalValidationError("12345XXX", "S300", LarValidationError),
-//        SyntacticalValidationError("12345XXX", "S301", LarValidationError),
-//        ValidityValidationError("", "V600", LarValidationError),
-//        QualityValidationError("12345XXX", "Q601")
-//      )
-//      hmdaValidationError ! PersistHmdaRowValidatedError(submissionId,
-//                                                         1,
-//                                                         List(tsError),
-//                                                         Some(errorsProbe.ref))
-//      errorsProbe.expectMessage(HmdaRowValidatedError(1, List(tsError)))
-//      val larErrorsWithIndex = Iterator.from(2).zip(larErrors.toIterator)
-//      larErrorsWithIndex.foreach { errorWithIndex =>
-//        val index = errorWithIndex._1
-//        val error = errorWithIndex._2
-//        hmdaValidationError ! PersistHmdaRowValidatedError(
-//          submissionId,
-//          index,
-//          List(error),
-//          Some(errorsProbe.ref))
-//        errorsProbe.expectMessage(HmdaRowValidatedError(index, List(error)))
-//      }
-//      hmdaValidationError ! GetHmdaValidationErrorState(submissionId,
-//                                                        stateProbe.ref)
-//
-//      val syntacticalEditSummary =
-//        Set(
-//          EditSummary(
-//            "S300",
-//            Syntactical,
-//            TsValidationError
-//          ),
-//          EditSummary(
-//            "S300",
-//            Syntactical,
-//            LarValidationError
-//          ),
-//          EditSummary(
-//            "S301",
-//            Syntactical,
-//            LarValidationError
-//          )
-//        )
-//      val validityEditSummary =
-//        Set(
-//          EditSummary(
-//            "V600",
-//            Validity,
-//            LarValidationError
-//          )
-//        )
-//      val qualityEditSummary =
-//        Set(
-//          EditSummary(
-//            "Q601",
-//            Quality,
-//            LarValidationError
-//          )
-//        )
-//
-//      stateProbe.expectMessage(
-//        HmdaValidationErrorState(
-//          1,
-//          syntacticalEditSummary,
-//          validityEditSummary,
-//          qualityEditSummary,
-//          qualityVerified = false
-//        ))
-//    }
-//
-//    "finish validation, verify and sign" in {
-//      hmdaValidationError ! VerifyQuality(submissionId, true, eventsProbe.ref)
-//      eventsProbe.expectMessage(NotReadyToBeVerified(submissionId))
-//      hmdaValidationError ! CompleteQuality(submissionId)
-//
-//      hmdaValidationError ! SignSubmission(submissionId, signedProbe.ref)
-//      signedProbe.expectMessage(SubmissionNotReadyToBeSigned(submissionId))
-//
-//      hmdaValidationError ! VerifyQuality(submissionId, true, eventsProbe.ref)
-//      eventsProbe.expectMessage(QualityVerified(submissionId, true, Verified))
-//
-//      hmdaValidationError ! SignSubmission(submissionId, signedProbe.ref)
-//      signedProbe.expectMessageType[SubmissionSigned]
-//    }
-//  }
+class HmdaValidationErrorSpec
+  extends AkkaCassandraPersistenceSpec {
+   override implicit val system = actor.ActorSystem()
+  override implicit val typedSystem = system.toTyped
+
+  val sharding = ClusterSharding(typedSystem)
+  SubmissionManager.startShardRegion(sharding)
+  SubmissionPersistence.startShardRegion(sharding)
+  HmdaValidationError.startShardRegion(sharding)
+  EditDetailsPersistence.startShardRegion(sharding)
+  InstitutionPersistence.startShardRegion(sharding)
+
+  val submissionId = SubmissionId("12345", "2018", 1)
+
+  val errorsProbe = TestProbe[HmdaRowValidatedError]("processing-event")
+  val stateProbe = TestProbe[HmdaValidationErrorState]("state-probe")
+  val eventsProbe = TestProbe[SubmissionProcessingEvent]("events-probe")
+  val signedProbe = TestProbe[SubmissionSignedEvent]("sign-event")
+
+  "Validation Errors" must {
+    Cluster(typedSystem).manager ! Join(Cluster(typedSystem).selfMember.address)
+    val hmdaValidationError = sharding.entityRefFor(
+      HmdaValidationError.typeKey,
+      s"${HmdaValidationError.name}-${submissionId.toString}")
+    "be persisted and read back" in {
+      val tsError: ValidationError =
+        SyntacticalValidationError("12345XXX", "S300", TsValidationError)
+      val larErrors: Seq[ValidationError] = Seq(
+        SyntacticalValidationError("12345XXX", "S300", LarValidationError),
+        SyntacticalValidationError("12345XXX", "S301", LarValidationError),
+        ValidityValidationError("", "V600", LarValidationError),
+        QualityValidationError("12345XXX", "Q601")
+      )
+      hmdaValidationError ! PersistHmdaRowValidatedError(submissionId,
+                                                         1,
+                                                         List(tsError),
+                                                         Some(errorsProbe.ref))
+      errorsProbe.expectMessage(HmdaRowValidatedError(1, List(tsError)))
+      val larErrorsWithIndex = Iterator.from(2).zip(larErrors.toIterator)
+      larErrorsWithIndex.foreach { errorWithIndex =>
+        val index = errorWithIndex._1
+        val error = errorWithIndex._2
+        hmdaValidationError ! PersistHmdaRowValidatedError(
+          submissionId,
+          index,
+          List(error),
+          Some(errorsProbe.ref))
+        errorsProbe.expectMessage(HmdaRowValidatedError(index, List(error)))
+      }
+      hmdaValidationError ! GetHmdaValidationErrorState(submissionId,
+                                                        stateProbe.ref)
+
+      val syntacticalEditSummary =
+        Set(
+          EditSummary(
+            "S300",
+            Syntactical,
+            TsValidationError
+          ),
+          EditSummary(
+            "S300",
+            Syntactical,
+            LarValidationError
+          ),
+          EditSummary(
+            "S301",
+            Syntactical,
+            LarValidationError
+          )
+        )
+      val validityEditSummary =
+        Set(
+          EditSummary(
+            "V600",
+            Validity,
+            LarValidationError
+          )
+        )
+      val qualityEditSummary =
+        Set(
+          EditSummary(
+            "Q601",
+            Quality,
+            LarValidationError
+          )
+        )
+
+      stateProbe.expectMessage(
+        HmdaValidationErrorState(
+          1,
+          syntacticalEditSummary,
+          validityEditSummary,
+          qualityEditSummary,
+          qualityVerified = false
+        ))
+    }
+
+    "finish validation, verify and sign" in {
+      hmdaValidationError ! VerifyQuality(submissionId, true, eventsProbe.ref)
+      eventsProbe.expectMessage(NotReadyToBeVerified(submissionId))
+      hmdaValidationError ! CompleteQuality(submissionId)
+
+      hmdaValidationError ! SignSubmission(submissionId, signedProbe.ref)
+      signedProbe.expectMessage(SubmissionNotReadyToBeSigned(submissionId))
+
+      hmdaValidationError ! VerifyQuality(submissionId, true, eventsProbe.ref)
+      eventsProbe.expectMessage(QualityVerified(submissionId, true, Verified))
+
+      hmdaValidationError ! SignSubmission(submissionId, signedProbe.ref)
+      signedProbe.expectMessageType[SubmissionSigned]
+    }
+  }
 
 }
