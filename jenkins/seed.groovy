56c171128611c73255c561b08d24fcf83a97266b
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2019-01-24 10:53:19 -0500

    remove parameter test

--- jenkins/seed.groovy
+++ jenkins/seed.groovy
@@ -1,10 +1,23 @@
 projects = [
-    [name: "hmda-help", repo: "hmda-help", jenkinsfilePath: "Jenkinsfile"]
+    [name: "auth", repo: "hmda-platform", jenkinsfilePath: "auth/Jenkinsfile"],
+    [name: "census-api", repo: "hmda-platform", jenkinsfilePath: "census-api/Jenkinsfile"],
+    [name: "check-digit", repo: "hmda-platform", jenkinsfilePath: "check-digit/Jenkinsfile"],
+    [name: "hmda-analytics", repo: "hmda-platform", jenkinsfilePath: "hmda-analytics/Jenkinsfile"],
+    [name: "hmda-help", repo: "hmda-help", jenkinsfilePath: "Jenkinsfile"],
+    [name: "hmda-homepage", repo: "hmda-homepage", jenkinsfilePath: "Jenkinsfile"],
+    [name: "hmda-platform", repo: "hmda-platform", jenkinsfilePath: "hmda/Jenkinsfile"],
+    [name: "hmda-platform-tools", repo: "hmda-platform-tools", jenkinsfilePath: "Jenkinsfile"],
+    [name: "hmda-pub-ui", repo: "hmda-pub-ui", jenkinsfilePath: "Jenkinsfile"],
+    [name: "hmda-regulator", repo: "hmda-platform", jenkinsfilePath: "hmda-regulator/Jenkinsfile"],
+    [name: "keycloak", repo: "hmda-platform", jenkinsfilePath: "kubernetes/keycloak/Jenkinsfile"],
+    [name: "institutions-api", repo: "hmda-platform", jenkinsfilePath: "institutions-api/Jenkinsfile"],
+    [name: "irs-publisher", repo: "hmda-platform", jenkinsfilePath: "irs-publisher/Jenkinsfile"],
+    [name: "modified-lar", repo: "hmda-platform", jenkinsfilePath: "modified-lar/Jenkinsfile"],
+    [name: "theme-provider", repo: "hmda-platform", jenkinsfilePath: "kubernetes/keycloak/theme-provider/Jenkinsfile"]
 ]
 
 projects.each { project ->
     multibranchPipelineJob(project.name) {
-        properties([[$class: 'ParametersDefinitionProperty', parameterDefinitions: [[$class: 'StringParameterDefinition', name: 'myparam', defaultValue: 'default value']]]])
         branchSources {
             github {
                 repoOwner('cfpb')
