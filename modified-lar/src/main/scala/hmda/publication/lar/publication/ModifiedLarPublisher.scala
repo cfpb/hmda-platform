d388cf70ecec23f97b2f3ab1ed54133b70c6dc84
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2019-04-02 15:14:37 -0400

    do not process institutions when they are in bank filter list

--- modified-lar/src/main/scala/hmda/publication/lar/publication/ModifiedLarPublisher.scala
+++ modified-lar/src/main/scala/hmda/publication/lar/publication/ModifiedLarPublisher.scala
@@ -16,7 +16,10 @@ import com.amazonaws.regions.AwsRegionProvider
 import com.typesafe.config.ConfigFactory
 import hmda.model.census.Census
 import hmda.model.filing.submission.SubmissionId
-import hmda.model.modifiedlar.{EnrichedModifiedLoanApplicationRegister, ModifiedLoanApplicationRegister}
+import hmda.model.modifiedlar.{
+  EnrichedModifiedLoanApplicationRegister,
+  ModifiedLoanApplicationRegister
+}
 import hmda.publication.lar.parser.ModifiedLarCsvParser
 import hmda.query.HmdaQuery._
 import hmda.query.repository.ModifiedLarRepository
@@ -27,7 +30,7 @@ import scala.util.{Failure, Success}
 sealed trait ModifiedLarCommand
 case class PersistToS3AndPostgres(submissionId: SubmissionId,
                                   respondTo: ActorRef[PersistModifiedLarResult])
-  extends ModifiedLarCommand
+    extends ModifiedLarCommand
 sealed trait UploadStatus
 case object UploadSucceeded extends UploadStatus
 case class UploadFailed(exception: Throwable) extends UploadStatus
@@ -58,8 +61,8 @@ object ModifiedLarPublisher {
   }
 
   def behavior(
-                indexTractMap: Map[String, Census],
-                modifiedLarRepo: ModifiedLarRepository): Behavior[ModifiedLarCommand] =
+      indexTractMap: Map[String, Census],
+      modifiedLarRepo: ModifiedLarRepository): Behavior[ModifiedLarCommand] =
     Behaviors.setup { ctx =>
       val log = ctx.log
       val decider: Supervision.Decider = { e: Throwable =>
@@ -88,77 +91,83 @@ object ModifiedLarPublisher {
       Behaviors.receiveMessage {
 
         case PersistToS3AndPostgres(submissionId, respondTo) =>
-          log.info(s"Publishing Modified LAR for $submissionId")
+          if (!bankFilterList.contains(submissionId.lei)) {
+            log.info(s"Publishing Modified LAR for $submissionId")
 
-          val fileName = s"${submissionId.lei}.txt"
+            val fileName = s"${submissionId.lei}.txt"
 
-          val s3Sink = s3Client.multipartUpload(
-            bucket,
-            s"$environment/modified-lar/$year/$fileName")
+            val s3Sink = s3Client.multipartUpload(
+              bucket,
+              s"$environment/modified-lar/$year/$fileName")
 
-          def removeLei: Future[Int] =
-            modifiedLarRepo.deleteByLei(submissionId.lei, filingYear)
+            def removeLei: Future[Int] =
+              modifiedLarRepo.deleteByLei(submissionId.lei, filingYear)
 
-          val mlarSource: Source[ModifiedLoanApplicationRegister, NotUsed] =
-            readRawData(submissionId)
-              .map(l => l.data)
-              .drop(1)
-              .map(s => ModifiedLarCsvParser(s))
+            val mlarSource: Source[ModifiedLoanApplicationRegister, NotUsed] =
+              readRawData(submissionId)
+                .map(l => l.data)
+                .drop(1)
+                .map(s => ModifiedLarCsvParser(s))
 
-          val s3Out: Sink[ModifiedLoanApplicationRegister,
-            Future[MultipartUploadResult]] =
-            Flow[ModifiedLoanApplicationRegister]
-              .map(mlar => mlar.toCSV + "\n")
-              .map(ByteString(_))
-              .toMat(s3Sink)(Keep.right)
+            val s3Out: Sink[ModifiedLoanApplicationRegister,
+                            Future[MultipartUploadResult]] =
+              Flow[ModifiedLoanApplicationRegister]
+                .map(mlar => mlar.toCSV + "\n")
+                .map(ByteString(_))
+                .toMat(s3Sink)(Keep.right)
 
-          def postgresOut(parallelism: Int)
-          : Sink[ModifiedLoanApplicationRegister, Future[Done]] =
-            Flow[ModifiedLoanApplicationRegister]
-              .map(
-                mlar =>
-                  EnrichedModifiedLoanApplicationRegister(
-                    mlar,
-                    indexTractMap.getOrElse(mlar.tract, Census())
+            def postgresOut(parallelism: Int)
+              : Sink[ModifiedLoanApplicationRegister, Future[Done]] =
+              Flow[ModifiedLoanApplicationRegister]
+                .map(
+                  mlar =>
+                    EnrichedModifiedLoanApplicationRegister(
+                      mlar,
+                      indexTractMap.getOrElse(mlar.tract, Census())
                   )
-              )
-              .mapAsync(parallelism)(enriched =>
-                modifiedLarRepo
-                  .insert(enriched, submissionId.toString, filingYear))
-              .toMat(Sink.ignore)(Keep.right)
+                )
+                .mapAsync(parallelism)(enriched =>
+                  modifiedLarRepo
+                    .insert(enriched, submissionId.toString, filingYear))
+                .toMat(Sink.ignore)(Keep.right)
 
-          val graph = mlarSource
-            .alsoToMat(postgresOut(2))(Keep.right) // TODO: provide a way to make this configurable
-            .toMat(s3Out)(Keep.both)
-            .mapMaterializedValue {
-              // We listen on the completion of both materialized values but we only keep the S3 as the result
-              // since that is a meaningful value
-              case (futPostgresRes, futS3Res) =>
-                for {
-                  _ <- futPostgresRes
-                  s3Res <- futS3Res
-                } yield s3Res
+            val graph = mlarSource
+              .alsoToMat(postgresOut(2))(Keep.right) // TODO: provide a way to make this configurable
+              .toMat(s3Out)(Keep.both)
+              .mapMaterializedValue {
+                // We listen on the completion of both materialized values but we only keep the S3 as the result
+                // since that is a meaningful value
+                case (futPostgresRes, futS3Res) =>
+                  for {
+                    _ <- futPostgresRes
+                    s3Res <- futS3Res
+                  } yield s3Res
+              }
+
+            val finalResult: Future[Unit] = for {
+              _ <- removeLei
+              _ <- graph.run()
+            } yield ()
+
+            finalResult.onComplete {
+              case Success(_) =>
+                log.info("Successfully completed persisting for {}",
+                         submissionId)
+                respondTo ! PersistModifiedLarResult(submissionId,
+                                                     UploadSucceeded)
+
+              case Failure(exception) =>
+                log.error(
+                  s"Failed to delete and persist records for $submissionId {}",
+                  exception)
+                respondTo ! PersistModifiedLarResult(submissionId,
+                                                     UploadFailed(exception))
+                // bubble this up to the supervisor
+                throw exception
             }
-
-          val finalResult: Future[Unit] = for {
-            _ <- removeLei
-            _ <- graph.run()
-          } yield ()
-
-          finalResult.onComplete {
-            case Success(_) =>
-              log.info("Successfully completed persisting for {}", submissionId)
-              respondTo ! PersistModifiedLarResult(submissionId,
-                UploadSucceeded)
-
-            case Failure(exception) =>
-              log.error(
-                s"Failed to delete and persist records for $submissionId {}",
-                exception)
-              respondTo ! PersistModifiedLarResult(submissionId,
-                UploadFailed(exception))
-              // bubble this up to the supervisor
-              throw exception
+          } else {
+            log.info("Instituion filtered from modified lar: {}", submissionId)
+            respondTo ! PersistModifiedLarResult(submissionId, UploadSucceeded)
           }
 
           Behaviors.same
