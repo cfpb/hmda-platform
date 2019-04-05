82f26d674ddf46a1a46db31d9d37ced5149c8672
bsharma <bsharma@healthstarinfo.com>
2019-04-05 11:27:47 -0400

    Refactor disclosure reports

--- hmda-spark-reporting/src/main/scala/com/hmda/reports/processing/DisclosureProcessing.scala
+++ hmda-spark-reporting/src/main/scala/com/hmda/reports/processing/DisclosureProcessing.scala
@@ -67,157 +67,6 @@ object DisclosureProcessing {
       ec: ExecutionContext): Future[Unit] = {
     import spark.implicits._
 
-    def prepare(df: DataFrame): DataFrame =
-      df.filter(col("msa_md") =!= lit(0))
-        .filter(upper(col("tract")) =!= lit("NA"))
-        .filter(upper(col("filing_year")) === lit(year))
-
-    def includeZeroAndNonZero(dispInput: DataFrame,
-                              title: String,
-                              dispositionName: String,
-                              allUniqueMsaMdTract: DataFrame): DataFrame = {
-      val leftAnti = allUniqueMsaMdTract.join(
-        dispInput,
-        dispInput.col("tract") === allUniqueMsaMdTract
-          .col("tract") and dispInput.col("msa_md") === allUniqueMsaMdTract.col(
-          "msa_md"),
-        "left_anti")
-      leftAnti
-        .withColumn("loan_amount", lit(0.0))
-        .withColumn("count", lit(0))
-        .union(dispInput)
-        .withColumn("dispositionName", lit(dispositionName))
-        .withColumn("title", lit(title))
-    }
-
-    def dispositionA(input: DataFrame,
-                     title: String,
-                     actionsTaken: List[Int],
-                     allUniqueMsaMdTract: DataFrame): Dataset[Data] = {
-      val dispA = prepare(input)
-        .filter(col("action_taken_type").isin(actionsTaken: _*))
-        .filter(col("total_units") isin ("1", "2", "3", "4"))
-        .filter(col("loan_purpose") === 1)
-        .filter(col("loan_type") isin (2, 3, 4))
-        .groupBy(col("tract"), col("msa_md"), col("msa_md_name"), col("state"))
-        .agg(sum("loan_amount") as "loan_amount", count("*") as "count")
-      includeZeroAndNonZero(dispA,
-                            title,
-                            "FHA, FSA/RHS & VA (A)",
-                            allUniqueMsaMdTract)
-        .as[Data]
-    }
-
-    def dispositionB(input: DataFrame,
-                     title: String,
-                     actionsTaken: List[Int],
-                     allUniqueMsaMdTract: DataFrame): Dataset[Data] = {
-      val dispB = prepare(input)
-        .filter(col("action_taken_type").isin(actionsTaken: _*))
-        .filter(col("total_units") isin ("1", "2", "3", "4"))
-        .filter(col("loan_purpose") === lit(1))
-        .filter(col("loan_type") === lit(1))
-        .groupBy(col("tract"), col("msa_md"), col("msa_md_name"), col("state"))
-        .agg(sum("loan_amount") as "loan_amount", count("*") as "count")
-      includeZeroAndNonZero(dispB,
-                            title,
-                            "Conventional (B)",
-                            allUniqueMsaMdTract)
-        .as[Data]
-    }
-
-    def dispositionC(input: DataFrame,
-                     title: String,
-                     actionsTaken: List[Int],
-                     allUniqueMsaMdTract: DataFrame): Dataset[Data] = {
-      val dispC = prepare(input)
-        .filter(col("action_taken_type").isin(actionsTaken: _*))
-        .filter(col("total_units") isin ("1", "2", "3", "4"))
-        .filter(col("loan_purpose") isin (31, 32))
-        .groupBy(col("tract"), col("msa_md"), col("msa_md_name"), col("state"))
-        .agg(sum("loan_amount") as "loan_amount", count("*") as "count")
-      includeZeroAndNonZero(dispC,
-                            title,
-                            "Refinancings (C)",
-                            allUniqueMsaMdTract)
-        .as[Data]
-    }
-
-    def dispositionD(input: DataFrame,
-                     title: String,
-                     actionsTaken: List[Int],
-                     allUniqueMsaMdTract: DataFrame): Dataset[Data] = {
-      val dispD = prepare(input)
-        .filter(col("action_taken_type").isin(actionsTaken: _*))
-        .filter(col("total_units") isin ("1", "2", "3", "4"))
-        .filter(col("loan_purpose") === lit(2))
-        .groupBy(col("tract"), col("msa_md"), col("msa_md_name"), col("state"))
-        .agg(sum("loan_amount") as "loan_amount", count("*") as "count")
-      includeZeroAndNonZero(dispD,
-                            title,
-                            "Home Improvement Loans (D)",
-                            allUniqueMsaMdTract)
-        .as[Data]
-    }
-
-    def dispositionE(input: DataFrame,
-                     title: String,
-                     actionsTaken: List[Int],
-                     allUniqueMsaMdTract: DataFrame): Dataset[Data] = {
-      val dispE = prepare(input)
-        .filter(col("action_taken_type").isin(actionsTaken: _*))
-        .filter(col("total_units") =!= lit("1"))
-        .filter(col("total_units") =!= lit("2"))
-        .filter(col("total_units") =!= lit("3"))
-        .filter(col("total_units") =!= lit("4"))
-        .groupBy(col("tract"), col("msa_md"), col("msa_md_name"), col("state"))
-        .agg(sum("loan_amount") as "loan_amount", count("*") as "count")
-      includeZeroAndNonZero(dispE,
-                            title,
-                            "Loans on Dwellings For 5 or More Families (E)",
-                            allUniqueMsaMdTract)
-        .as[Data]
-    }
-
-    def dispositionF(input: DataFrame,
-                     title: String,
-                     actionsTaken: List[Int],
-                     allUniqueMsaMdTract: DataFrame): Dataset[Data] = {
-      val dispF = prepare(input)
-        .filter(col("action_taken_type").isin(actionsTaken: _*))
-        .filter(col("total_units") isin ("1", "2", "3", "4"))
-        .filter(col("loan_purpose") isin (1, 2, 31, 32))
-        .filter(col("loan_type") isin (1, 2, 3, 4)) //should match A - D
-        .filter(col("occupancy_type") isin (2, 3))
-        .groupBy(col("tract"), col("msa_md"), col("msa_md_name"), col("state"))
-        .agg(sum("loan_amount") as "loan_amount", count("*") as "count")
-      includeZeroAndNonZero(dispF,
-                            title,
-                            "Nonoccupant Loans from Columns A, B, C ,& D (F)",
-                            allUniqueMsaMdTract)
-        .as[Data]
-    }
-
-    def dispositionG(input: DataFrame,
-                     title: String,
-                     actionsTaken: List[Int],
-                     allUniqueMsaMdTract: DataFrame): Dataset[Data] = {
-      val dispG = prepare(input)
-        .filter(col("action_taken_type").isin(actionsTaken: _*))
-        .filter(col("total_units") isin ("1", "2", "3", "4"))
-        .filter(col("loan_purpose") isin (1, 2, 31, 32))
-        .filter(col("loan_type") isin (1, 2, 3, 4)) //should match A - D
-        .filter(col("construction_method") isin ("2"))
-        .groupBy(col("tract"), col("msa_md"), col("msa_md_name"), col("state"))
-        .agg(sum("loan_amount") as "loan_amount", count("*") as "count")
-      includeZeroAndNonZero(
-        dispG,
-        title,
-        "Loans On Manufactured Home Dwellings From Columns A, B, C & D (G)",
-        allUniqueMsaMdTract)
-        .as[Data]
-    }
-
     def jsonFormationTable1(msaMd: Msa,
                             input: List[Data],
                             leiDetails: Institution): OutDisclosure1 = {
@@ -318,22 +167,15 @@ object DisclosureProcessing {
       )
     }
 
-    def persistSingleFile(fileName: String,
-                          data: String,
-                          s3Bucket: String): Future[MultipartUploadResult] =
-      Source
-        .single(data)
-        .map(ByteString(_))
-        .runWith(s3Client.multipartUpload(s3Bucket, fileName))
-
     def persistJson(input: List[OutDisclosure1]): Future[Done] =
       Source(input)
         .mapAsyncUnordered(10) { input =>
           val data: String = input.asJson.noSpaces
-          persistSingleFile(
+          BaseProcessing.persistSingleFile(
             s"$bucket/reports/disclosure/$year/$lei/${input.msa.id}/1.json",
             data,
-            "cfpb-hmda-public")
+            "cfpb-hmda-public",
+            s3Client)(mat, ec)
         }
         .runWith(Sink.ignore)
 
@@ -341,10 +183,11 @@ object DisclosureProcessing {
       Source(input)
         .mapAsyncUnordered(10) { input =>
           val data: String = input.asJson.noSpaces
-          persistSingleFile(
+          BaseProcessing.persistSingleFile(
             s"$bucket/reports/disclosure/$year/$lei/${input.msa.id}/2.json",
             data,
-            "cfpb-hmda-public")
+            "cfpb-hmda-public",
+            s3Client)(mat, ec)
         }
         .runWith(Sink.ignore)
 
@@ -372,206 +215,9 @@ object DisclosureProcessing {
         .load()
         .cache()
 
-    def allUniqueMsaMdTract =
-      cachedRecordsDf
-        .select(col("tract"), col("msa_md"), col("msa_md_name"), col("state"))
-        .dropDuplicates()
-        .cache()
-
-    def outputCollectionTable1(allUniqueMsaMdTract: DataFrame,
-                               cachedRecordsDf: DataFrame): List[Data] = {
-      val actionsTakenTable1 = Map(
-        "Applications Received" -> List(1, 2, 3, 4, 5),
-        "Loans Originated" -> List(1),
-        "Applications Approved but not Accepted" -> List(2),
-        "Applications Denied by Financial Institution" -> List(3),
-        "Applications Withdrawn by Applicant" -> List(4),
-        "File Closed for Incompleteness" -> List(5)
-      )
-
-      val outputATable1 = actionsTakenTable1
-        .map {
-          case (description, eachList) =>
-            dispositionA(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputBTable1 = actionsTakenTable1
-        .map {
-          case (description, eachList) =>
-            dispositionB(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputCTable1 = actionsTakenTable1
-        .map {
-          case (description, eachList) =>
-            dispositionC(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputDTable1 = actionsTakenTable1
-        .map {
-          case (description, eachList) =>
-            dispositionD(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputETable1 = actionsTakenTable1
-        .map {
-          case (description, eachList) =>
-            dispositionE(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputFTable1 = actionsTakenTable1
-        .map {
-          case (description, eachList) =>
-            dispositionF(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputGTable1 = actionsTakenTable1
-        .map {
-          case (description, eachList) =>
-            dispositionG(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      spark.sparkContext.setJobGroup(lei, "Table 1 Disposition A")
-      val aList = outputATable1.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 1 Disposition B")
-      val bList = outputBTable1.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 1 Disposition C")
-      val cList = outputCTable1.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 1 Disposition D")
-      val dList = outputDTable1.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 1 Disposition E")
-      val eList = outputETable1.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 1 Disposition F")
-      val fList = outputFTable1.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 1 Disposition G")
-      val gList = outputGTable1.collect().toList
-
-      aList ++ bList ++ cList ++ dList ++ eList ++ fList ++ gList
-    }
-
-    def outputCollectionTable2(allUniqueMsaMdTract: DataFrame): List[Data] = {
-      val actionsTakenTable2 = Map(
-        "Purchased Loans" -> List(6)
-      )
-
-      val outputATable2 = actionsTakenTable2
-        .map {
-          case (description, eachList) =>
-            dispositionA(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputBTable2 = actionsTakenTable2
-        .map {
-          case (description, eachList) =>
-            dispositionB(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputCTable2 = actionsTakenTable2
-        .map {
-          case (description, eachList) =>
-            dispositionC(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputDTable2 = actionsTakenTable2
-        .map {
-          case (description, eachList) =>
-            dispositionD(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputETable2 = actionsTakenTable2
-        .map {
-          case (description, eachList) =>
-            dispositionE(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputFTable2 = actionsTakenTable2
-        .map {
-          case (description, eachList) =>
-            dispositionF(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      val outputGTable2 = actionsTakenTable2
-        .map {
-          case (description, eachList) =>
-            dispositionG(cachedRecordsDf,
-                         description,
-                         eachList,
-                         allUniqueMsaMdTract)
-        }
-        .reduce(_ union _)
-
-      spark.sparkContext.setJobGroup(lei, "Table 2 Disposition A")
-      val aList = outputATable2.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 2 Disposition B")
-      val bList = outputBTable2.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 2 Disposition C")
-      val cList = outputCTable2.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 2 Disposition D")
-      val dList = outputDTable2.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 2 Disposition E")
-      val eList = outputETable2.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 2 Disposition F")
-      val fList = outputFTable2.collect().toList
-      spark.sparkContext.setJobGroup(lei, "Table 2 Disposition G")
-      val gList = outputGTable2.collect().toList
-
-      aList ++ bList ++ cList ++ dList ++ eList ++ fList ++ gList
-    }
-
     def disclosuresTable1: List[OutDisclosure1] =
-      outputCollectionTable1(allUniqueMsaMdTract, cachedRecordsDf)
+      BaseProcessing
+        .outputCollectionTable1(cachedRecordsDf, spark)
         .groupBy(d => d.msa_md)
         .map {
           case (key, values) =>
@@ -585,7 +231,8 @@ object DisclosureProcessing {
         .toList
 
     def disclosuresTable2: List[OutDisclosure2] =
-      outputCollectionTable2(allUniqueMsaMdTract)
+      BaseProcessing
+        .outputCollectionTable2(cachedRecordsDf, spark)
         .groupBy(d => d.msa_md)
         .map {
           case (key, values) =>
