65765640fe46769f0ab8dff692df12535c8e7fa5
Patrick Asata <patrick.asata@cfpb.gov>
2019-04-15 03:08:32 -0400

    cleaning up logic for minority counter

--- modified-lar/src/main/scala/hmda/publication/lar/parser/RaceCategorization.scala
+++ modified-lar/src/main/scala/hmda/publication/lar/parser/RaceCategorization.scala
@@ -1,6 +1,6 @@
 package hmda.publication.lar
 
-import hmda.model.filing.lar.LoanApplicationRegister
+import hmda.model.filing.lar.{LoanApplicationRegister, Race}
 import hmda.model.filing.lar.enums._
 
 
@@ -13,173 +13,190 @@ object RaceCategorization {
     val race = lar.applicant.race
     val coRace = lar.coApplicant.race
 
-    val raceFields = Array(race.race1,
-      race.race2,
-      race.race3,
-      race.race4,
-      race.race5)
-    val coRaceFields = Array(coRace.race1,
-      coRace.race2,
-      coRace.race3,
-      coRace.race4,
-      coRace.race5)
+    val raceFields = Array(race.race1, race.race2, race.race3, race.race4, race.race5)
+    val coRaceFields = Array(coRace.race1, coRace.race2, coRace.race3, coRace.race4, coRace.race5)
 
-    val asianEnums= Array(Asian,
-      AsianIndian,
-      Chinese,
-      Filipino,
-      Japanese,
-      Korean,
-      Vietnamese,
-      OtherAsian)
+    val asianEnums= Array(Asian, AsianIndian, Chinese, Filipino, Japanese, Korean, Vietnamese, OtherAsian)
+    val hawaiianIslanderEnums= Array(NativeHawaiianOrOtherPacificIslander, NativeHawaiian, GuamanianOrChamorro, Samoan, OtherPacificIslander)
 
+    val isRaceTwoToFiveEmpty =  checkRaceTwoToFiveEmpty(race)
+    val isRaceThreeToFiveEmpty =  isRaceThreeToFiveEmpty(race)
+    val isCoRaceTwoToFiveEmpty =  checkRaceTwoToFiveEmpty(coRace)
+    val isCoWhite = isAnyApplicanttWhite(coRace)
 
-
-    val hawaiianIslanderEnums= Array(NativeHawaiianOrOtherPacificIslander,
-      NativeHawaiian,
-      GuamanianOrChamorro,
-      Samoan,
-      OtherPacificIslander)
-
-    val isRaceEmpty =  race.race2 ==EmptyRaceValue &&
-      race.race3 ==EmptyRaceValue &&
-      race.race4 ==EmptyRaceValue &&
-      race.race5 ==EmptyRaceValue
-
-    val isCoRaceEmpty =  coRace.race2 ==EmptyRaceValue &&
-      coRace.race3 ==EmptyRaceValue &&
-      coRace.race4 ==EmptyRaceValue &&
-      coRace.race5 ==EmptyRaceValue
-
-
-      val isWhite = race.race1==White &&
-        isRaceEmpty &&(
-        coRace.race1==White ||
-          coRace.race1==RaceInformationNotProvided ||
-          coRace.race1==RaceNotApplicable ||
-        coRace.race1 ==RaceNoCoApplicant
-      )&& isCoRaceEmpty
-
-    val isCoWhite = coRace.race1 == White ||
-      coRace.race2 ==White ||
-      coRace.race3 ==White ||
-      coRace.race4 ==White ||
-      coRace.race5 ==White
-
-
-      if (asianEnums.contains(race.race1)){
-        Asian.description
-      }
-      else if (hawaiianIslanderEnums.contains((race.race1))){
-        NativeHawaiianOrOtherPacificIslander.description
-      }
-
-      else if (race.race1==EmptyRaceValue){
+     if (race.race1==EmptyRaceValue){
         "Free Form Text Only"
       }
 
       else if (race.race1 == RaceInformationNotProvided ||
         race.race1 == RaceNotApplicable){
-        "Race not available"}
+        "Race not available"
+      }
 
-        //American Indian
+      // American Indian or Alaska Native
       else if (race.race1 == AmericanIndianOrAlaskaNative &&
-      !isCoWhite){
-        AmericanIndianOrAlaskaNative.description}
+       isRaceTwoToFiveEmpty &&
+       !isCoWhite){
+       AmericanIndianOrAlaskaNative.description
+      }
 
-
-        //Asian
-      else if (asianEnums.contains(race.race1)&&
-        isRaceEmpty && !isCoWhite){
+      // Asian
+      else if (asianEnums.contains(race.race1) &&
+       isRaceTwoToFiveEmpty &&
+       !isCoWhite){
         Asian.description
       }
-        //Afrcan American
+
+      // Black or African American
       else if (race.race1 == BlackOrAfricanAmerican &&
-        isRaceEmpty && !isCoWhite){
+       isRaceTwoToFiveEmpty &&
+       !isCoWhite){
         BlackOrAfricanAmerican.description
       }
 
+      // Native Hawaiian or Pacific Islander
       else if (hawaiianIslanderEnums.contains(race.race1) &&
-        isRaceEmpty && !isCoWhite){
+       isRaceTwoToFiveEmpty &&
+       !isCoWhite){
         NativeHawaiianOrOtherPacificIslander.description
       }
-      else if (race.race1 == AmericanIndianOrAlaskaNative&&
-               race.race2==White&&
-      race.race3==EmptyRaceValue &&
-      race.race4 == EmptyRaceValue &&
-      race.race5 == EmptyRaceValue &&
-      !isCoWhite){
+
+      // American Indian or Alaska Native Alt
+      else if (race.race1 == AmericanIndianOrAlaskaNative &&
+               race.race2 == White &&
+       isRaceThreeToFiveEmpty &&
+       !isCoWhite){
         AmericanIndianOrAlaskaNative.description
       }
-      else if (asianEnums.contains(race.race1)&&race.race2==White&&
-        race.race3==EmptyRaceValue &&
-        race.race4 == EmptyRaceValue &&
-        race.race5 == EmptyRaceValue &&
-        !isCoWhite
+
+      // Asian Alt
+      else if (asianEnums.contains(race.race1) &&
+       race.race2 == White &&
+       isRaceThreeToFiveEmpty &&
+       !isCoWhite
       ){
         Asian.description
       }
 
-      else if (race.race1 == BlackOrAfricanAmerican&&race.race2==White&&
-        race.race3==EmptyRaceValue &&
-        race.race4 == EmptyRaceValue &&
-        race.race5 == EmptyRaceValue &&
-        !isCoWhite
+      // Black or African American Alt
+      else if (race.race1 == BlackOrAfricanAmerican &&
+       race.race2==White &&
+       isRaceThreeToFiveEmpty &&
+       !isCoWhite
       ){
         BlackOrAfricanAmerican.description
       }
 
-      else if (race.race1 == NativeHawaiianOrOtherPacificIslander &&race.race2==White&&
-        race.race3==EmptyRaceValue &&
-        race.race4 == EmptyRaceValue &&
-        race.race5 == EmptyRaceValue &&
-        !isCoWhite
+      // Native Hawaiian or Pacific Islander Alt
+      else if (race.race1 == NativeHawaiianOrOtherPacificIslander &&
+       race.race2==White &&
+       isRaceThreeToFiveEmpty &&
+       !isCoWhite
       ){
         NativeHawaiianOrOtherPacificIslander.description
       }
 
-      else if (raceFields.exists(asianEnums.contains)  && !isCoWhite){
+      // Asian Alt Two
+      else if (raceFields.exists(asianEnums.contains) &&
+       raceFields.contains(White) &&
+       !raceFields.exists(hawaiianIslanderEnums.contains)&&
+       !raceFields.contains(BlackOrAfricanAmerican) &&
+       !raceFields.contains(AmericanIndianOrAlaskaNative) &&
+       !isCoWhite){
         Asian.description
       }
 
-      else if (raceFields.exists(hawaiianIslanderEnums.contains)  && !isCoWhite){
-        NativeHawaiianOrOtherPacificIslander.description
-      }
+     // Native Hawaiian or Pacific Islander Alt Two
+     else if (raceFields.exists(hawaiianIslanderEnums.contains) &&
+       raceFields.contains(White) &&
+       !raceFields.exists(asianEnums.contains) &&
+       !raceFields.contains(BlackOrAfricanAmerican) &&
+       !raceFields.contains(AmericanIndianOrAlaskaNative) &&
+       !isCoWhite){
+       NativeHawaiianOrOtherPacificIslander.description
+     }
 
-      else if (raceFields.exists(hawaiianIslanderEnums.contains) && raceFields.exists(asianEnums.contains) && !isCoWhite){
+     else if (moreThanOneMinority(raceFields, asianEnums, hawaiianIslanderEnums) &&
+       !isCoWhite){
         "2 or more minority races"
       }
 
-      else if (race.race1 ==White &&
-        isRaceEmpty &&
-        coRace.race1 == White ||
-        coRace.race1 == RaceInformationNotProvided ||
-        coRace.race1 == RaceNotApplicable ||
-        coRace.race1 == RaceNoCoApplicant &&(
-        coRace.race2 == EmptyRaceValue &&
-          coRace.race3 == EmptyRaceValue &&
-          coRace.race4 == EmptyRaceValue &&
-          coRace.race5 == EmptyRaceValue)){
-        White.description}
-      else if ( (raceFields.exists(hawaiianIslanderEnums.contains)||
-                 raceFields.exists(asianEnums.contains)||
-         race.race1==AmericanIndianOrAlaskaNative||
-        race.race1== BlackOrAfricanAmerican) &&
+     else if (isFirstApplicantWhite(race, coRace, isRaceTwoToFiveEmpty, isCoRaceTwoToFiveEmpty)){
+        White.description
+      }
+
+     else if ( moreThanOneMinority(raceFields, asianEnums, hawaiianIslanderEnums) &&
         isCoWhite
           ){
         "Joint"
       }
-      else if ( (coRaceFields.exists(hawaiianIslanderEnums.contains)||
-        coRaceFields.exists(asianEnums.contains) ||
-        coRaceFields== AmericanIndianOrAlaskaNative||
-        race.race1== BlackOrAfricanAmerican) &&
-        isWhite
+
+     else if (moreThanOneMinority(coRaceFields, asianEnums, hawaiianIslanderEnums) &&
+        isAnyApplicanttWhite(race)
       ){
         "Joint"
       }
-      else
+
+     else
         "Race not available"
   }
 
+  private def moreThanOneMinority(raceFields: Array[RaceEnum],
+                                  asianEnums: Array[RaceEnum with Product with Serializable],
+                                  hawaiianIslanderEnums: Array[RaceEnum with Product with Serializable]):Boolean = {
+    var counter = 0
+    if (raceFields.exists(hawaiianIslanderEnums.contains)){
+      counter +=1
+    }
 
+    if (raceFields.exists(asianEnums.contains)){
+      counter +=1
+    }
+
+    if (raceFields.contains(BlackOrAfricanAmerican)){
+      counter +=1
+    }
+
+    if (raceFields.contains(AmericanIndianOrAlaskaNative)){
+      counter +=1
+    }
+
+    if (counter >1) {
+      return true
+    }
+
+    false
+  }
+
+  private def isAnyApplicanttWhite(race: Race): Boolean = {
+    (
+      race.race1 == White ||
+        race.race2 == White ||
+        race.race3 == White ||
+        race.race5 == White)
+  }
+
+  private def isFirstApplicantWhite(race: Race, coRace: Race, isRaceTwoToFiveEmpty: Boolean, isCoRaceTwoToFiveEmpty: Boolean): Boolean = {
+    race.race1 == White &&
+      isRaceTwoToFiveEmpty &&
+      isCoRaceTwoToFiveEmpty && (
+      coRace.race1 == White ||
+        coRace.race1 == RaceInformationNotProvided ||
+        coRace.race1 == RaceNotApplicable ||
+        coRace.race1 == RaceNoCoApplicant
+      )
+  }
+
+  private def isRaceThreeToFiveEmpty(race: Race): Boolean = {
+    race.race3 == EmptyRaceValue &&
+      race.race4 == EmptyRaceValue &&
+      race.race5 == EmptyRaceValue
+  }
+
+  private def checkRaceTwoToFiveEmpty(race: Race): Boolean = {
+      race.race2 == EmptyRaceValue &&
+        race.race3 == EmptyRaceValue &&
+        race.race4 == EmptyRaceValue &&
+        race.race5 == EmptyRaceValue
+  }
 }
