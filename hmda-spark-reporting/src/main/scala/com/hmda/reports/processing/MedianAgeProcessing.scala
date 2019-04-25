36533a067a9a4969cb404ea30ec836330cae09ff
bsharma <bsharma@healthstarinfo.com>
2019-04-25 07:23:27 -0400

    save

--- hmda-spark-reporting/src/main/scala/com/hmda/reports/processing/MedianAgeProcessing.scala
+++ hmda-spark-reporting/src/main/scala/com/hmda/reports/processing/MedianAgeProcessing.scala
@@ -243,7 +243,7 @@ object MedianAgeProcessing {
                              spark: SparkSession): List[DataMedAge] = {
     import spark.implicits._
     val actionsTakenTable1 = Map(
-      "Applications Received" -> List(1, 2, 3, 4, 5),
+      "Applications Received" -> List(1,2,3,4,5),
       "Loans Originated" -> List(1),
       "Applications Approved but not Accepted" -> List(2),
       "Applications Denied by Financial Institution" -> List(3),
