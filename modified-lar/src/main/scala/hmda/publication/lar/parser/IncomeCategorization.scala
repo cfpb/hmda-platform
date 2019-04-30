c90863371aa531c4e6b9baf7c5b4d191beed0e90
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2019-04-30 11:05:28 -0400

    add case for na in income categorization

--- modified-lar/src/main/scala/hmda/publication/lar/parser/IncomeCategorization.scala
+++ modified-lar/src/main/scala/hmda/publication/lar/parser/IncomeCategorization.scala
@@ -6,27 +6,32 @@ import hmda.model.census._
 object IncomeCategorization {
   def assignIncomeCategorization(lar: LoanApplicationRegister,
                                  censusRecords: List[Census]): String = {
-    val tract =
-      censusRecords
-        .filter(record => record.state + record.county == lar.geography.county)
-        .head
-    val medianIncome = tract.medianIncome.toDouble
-    val income = lar.income.toDouble
-
-    val fifty = medianIncome * .5
-    val eighty = medianIncome * .8
-    val oneTwenty = medianIncome * 1.2
-
-    if (income < fifty) {
-      "<50%"
-    } else if (income > fifty && income < eighty) {
-      "50-79%"
-    } else if (income > eighty && income < medianIncome) {
-      "80-99%"
-    } else if (income > medianIncome && income < oneTwenty) {
-      "100-119%"
+    
+    if (lar.geography.county = "NA") {
+      "NA"
     } else {
-      ">120%"
+      val tract =
+        censusRecords
+          .filter(record => record.state + record.county == lar.geography.county)
+          .head
+      val medianIncome = tract.medianIncome.toDouble
+      val income = lar.income.toDouble
+
+      val fifty = medianIncome * .5
+      val eighty = medianIncome * .8
+      val oneTwenty = medianIncome * 1.2
+
+      if (income < fifty) {
+        "<50%"
+      } else if (income > fifty && income < eighty) {
+        "50-79%"
+      } else if (income > eighty && income < medianIncome) {
+        "80-99%"
+      } else if (income > medianIncome && income < oneTwenty) {
+        "100-119%"
+      } else {
+        ">120%"
+      }
     }
 
   }
