012a7aed50b71ce89888a993853fddfdf7b6b13a
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2019-05-08 14:23:06 -0400

    add test logging to grpc service and ratelimit annotations to rate limit and reporting

--- rate-limit/src/main/scala/hmda/api/grpc/RateLimitServiceImpl.scala
+++ rate-limit/src/main/scala/hmda/api/grpc/RateLimitServiceImpl.scala
@@ -1,6 +1,7 @@
 package hmda.rateLimit.api.grpc
 
 import akka.stream.Materializer
+import scala.concurrent.Future
 import hmda.grpc.services.{
   RateLimitService,
   RateLimitRequest,
@@ -9,12 +10,16 @@ import hmda.grpc.services.{
 import hmda.grpc.services.RateLimitResponse.Code._
 import hmda.grpc.services._
 import scala.concurrent.Future
+import org.slf4j.LoggerFactory
 
 class RateLimitServiceImpl(materializer: Materializer)
     extends RateLimitService {
 
+  val log = LoggerFactory.getLogger("hmda")
+
   override def shouldRateLimit(
       in: RateLimitRequest): Future[RateLimitResponse] = {
+    log.info("endpoint hit")
     Future.successful(RateLimitResponse(OVER_LIMIT))
   }
 
