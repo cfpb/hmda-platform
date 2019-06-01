b920b16215041a8edf7354e7a8180b2a10c6008a
bsharma <bsharma@healthstarinfo.com>
2019-06-01 14:30:33 -0400

    fetch editDescriptions based on year

--- common/src/main/scala/hmda/model/filing/EditDescriptionLookup.scala
+++ common/src/main/scala/hmda/model/filing/EditDescriptionLookup.scala
@@ -3,6 +3,8 @@ package hmda.model.filing
 import com.typesafe.config.ConfigFactory
 import hmda.model.ResourceUtils._
 
+import scala.collection.immutable
+
 object EditDescriptionLookup {
 
   case class EditDescription(editName: String,
@@ -10,11 +12,21 @@ object EditDescriptionLookup {
                              affectedFields: List[String])
 
   val config = ConfigFactory.load()
-  val editDescriptionFileName =
-    config.getString("hmda.filing.edits.descriptions.filename")
+  val editDescriptionFileName2018 = config.getString("hmda.filing.2018.edits.descriptions.filename")
+  val editDescriptionFileName2019 = config.getString("hmda.filing.2019.edits.descriptions.filename")
 
-  val editDescriptionList = {
-    val lines = fileLines(s"/$editDescriptionFileName")
+  def fileForYear(period: String): hmda.Iterable[String] = {
+    period match {
+      case "2018" => fileLines(s"/$editDescriptionFileName2018")
+      case "2019" => fileLines(s"/$editDescriptionFileName2019")
+      case _ =>
+        val currentYear = config.getString("hmda.filing.current")
+        fileLines(config.getString(s"hmda.filing.$currentYear.edits.descriptions.filename"))
+    }
+  }
+
+  def editDescriptionList(period: String): immutable.Iterable[EditDescription] = {
+    val lines = fileForYear(period)
     lines
       .drop(1)
       .map { s =>
@@ -24,19 +36,19 @@ object EditDescriptionLookup {
         val affectedDataFields = values(2).split(";").map(_.trim)
         EditDescription(editName, editDetails, affectedDataFields.toList)
       }
-
   }
 
-  val editDescriptionMap: Map[String, EditDescription] =
-    editDescriptionList.map(e => (e.editName, e)).toMap
+  def editDescriptionMap(period: String): Map[String, EditDescription] =
+    editDescriptionList(period).map(e => (e.editName, e)).toMap
 
-  def lookupDescription(editName: String): String =
-    editDescriptionMap
+  def lookupDescription(editName: String, period: String): String =
+    editDescriptionMap(period)
       .getOrElse(editName, EditDescription("", "", List()))
       .description
 
-  def lookupFields(editName: String): List[String] =
-    editDescriptionMap
+  //TODO (pass in the year from ValidationFlow)
+  def lookupFields(editName: String, period: String = "2018"): List[String] =
+    editDescriptionMap(period)
       .getOrElse(editName, EditDescription("", "", List()))
       .affectedFields
 
