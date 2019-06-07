5ac24b1448a82d1e6e0dc1c74eadc13498b9989a
Patrick Asata <patrick.asata@cfpb.gov>
2019-06-07 17:08:42 -0400

    updating result set

--- hmda/src/main/scala/hmda/validation/rules/lar/quality/Q632_2019.scala
+++ hmda/src/main/scala/hmda/validation/rules/lar/quality/Q632_2019.scala
@@ -10,7 +10,7 @@ import hmda.validation.rules.EditCheck
 object Q632_2019 extends EditCheck[LoanApplicationRegister] {
   override def name: String = "Q632_2019"
 
-  val results = List(Accept, Refer)
+  val results = List(ApproveEligible,ApproveIneligible,ReferEligilbe,ReferIneligible,Accept, Refer,OtherAutomatedUnderwritingResult)
 
   override def apply(lar: LoanApplicationRegister): ValidationResult = {
 
