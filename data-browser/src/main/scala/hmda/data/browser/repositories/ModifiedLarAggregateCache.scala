3e5143a052f6829585bc4689865f35e9c4ad66dc
bsharma <bsharma@healthstarinfo.com>
2019-06-14 09:20:53 -0400

    fix caching in redis

--- data-browser/src/main/scala/hmda/data/browser/repositories/ModifiedLarAggregateCache.scala
+++ data-browser/src/main/scala/hmda/data/browser/repositories/ModifiedLarAggregateCache.scala
@@ -1,81 +1,13 @@
 package hmda.data.browser.repositories
 
+import hmda.data.browser.models.QueryField
 import monix.eval.Task
 
 trait ModifiedLarAggregateCache {
-  def find(msaMd: Int,
-           field1Name: String,
-           field1: String,
-           field2Name: String,
-           field2: String): Task[Option[Statistic]]
+  def find(queryFields: List[QueryField]): Task[Option[Statistic]]
 
-  def find(state: String,
-           field1Name: String,
-           field1: String,
-           field2Name: String,
-           field2: String): Task[Option[Statistic]]
+  def update(queryFields: List[QueryField],
+             statistic: Statistic): Task[Statistic]
 
-  def find(field1Name: String,
-           field1: String,
-           field2Name: String,
-           field2: String): Task[Option[Statistic]]
-
-  def find(msaMd: Int,
-           state: String,
-           field1Name: String,
-           field1: String,
-           field2Name: String,
-           field2: String): Task[Option[Statistic]]
-
-  def update(msaMd: Int,
-             field1Name: String,
-             field1: String,
-             field2Name: String,
-             field2: String,
-             stat: Statistic): Task[Statistic]
-
-  def update(state: String,
-             field1Name: String,
-             field1: String,
-             field2Name: String,
-             field2: String,
-             stat: Statistic): Task[Statistic]
-
-  def update(field1Name: String,
-             field1: String,
-             field2Name: String,
-             field2: String,
-             stat: Statistic): Task[Statistic]
-
-  def update(msaMd: Int,
-             state: String,
-             field1Name: String,
-             field1: String,
-             field2Name: String,
-             field2: String,
-             stat: Statistic): Task[Statistic]
-
-  def invalidate(msaMd: Int,
-                 field1Name: String,
-                 field1: String,
-                 field2Name: String,
-                 field2: String): Task[Unit]
-
-  def invalidate(state: String,
-                 field1Name: String,
-                 field1: String,
-                 field2Name: String,
-                 field2: String): Task[Unit]
-
-  def invalidate(field1Name: String,
-                 field1: String,
-                 field2Name: String,
-                 field2: String): Task[Unit]
-
-  def invalidate(msaMd: Int,
-                 state: String,
-                 field1Name: String,
-                 field1: String,
-                 field2Name: String,
-                 field2: String): Task[Unit]
+  def invalidate(queryField: List[QueryField]): Task[Unit]
 }
--- data-browser/src/main/scala/hmda/data/browser/services/ModifiedLarBrowserService.scala
+++ data-browser/src/main/scala/hmda/data/browser/services/ModifiedLarBrowserService.scala
@@ -8,186 +8,11 @@ import hmda.data.browser.repositories.{
 }
 import monix.eval.Task
 
-class ModifiedLarBrowserService(repo: ModifiedLarRepository)
-    extends BrowserService {
-
-  //  /**
-  //    * This is responsible for performing the following logic for all provided combinations of races and actions taken:
-  //    *   1. find if the entry is in the cache
-  //    *   2a. if the entry is in the cache, proceed to 3
-  //    *   2b. if the entry is not in the cache then obtain it from the database and update the cache
-  //    *   3. tag entry with race and action taken information
-  //    *   4. serve all entries
-  //    *
-  //    * This function relies on callers of the function to use currying to cover all use cases:
-  //    * - queries by MSAMD
-  //    * - queries by State
-  //    * - nationwide queries
-  //    *
-  //    * @param findInDatabase is a function that retrieves data from the database
-  //    * @param findInCache is a function that retrieves data from the cache (the data may not be in the cache)
-  //    * @param updateCache is a function that updates the cache with the data from the database
-  //    * @return
-  //    */
-  //  private def fetchAgg(
-  //      field1: BrowserField,
-  //      field2: BrowserField,
-  //      findInDatabase: (String, String, String, String) => Task[Statistic],
-  //      findInCache: (String, String, String, String) => Task[Option[Statistic]],
-  //      updateCache: (String,
-  //                    String,
-  //                    String,
-  //                    String,
-  //                    Statistic) => Task[Statistic]): Task[Seq[Aggregation]] = {
-  //    val taskList = for {
-  //      field1Entry <- field1.value
-  //      field2Entry <- field2.value
-  //    } yield {
-  //      // this is only executed if the initial cache query comes back empty
-  //      val findInDbThenUpdateCache = for {
-  //        stat <- findInDatabase(field1.dbName,
-  //                               field1Entry,
-  //                               field2.dbName,
-  //                               field2Entry)
-  //        _ <- updateCache(field1.redisName,
-  //                         field1Entry,
-  //                         field2.redisName,
-  //                         field2Entry,
-  //                         stat)
-  //      } yield stat
-  //
-  //      for {
-  //        optC <- findInCache(field1.redisName,
-  //                            field1Entry,
-  //                            field2.redisName,
-  //                            field2Entry)
-  //        stat <- optC.fold(ifEmpty = findInDbThenUpdateCache)(cachedStat =>
-  //          Task(cachedStat))
-  //      } yield
-  //        Aggregation(
-  //          count = stat.count,
-  //          sum = stat.sum,
-  //          BrowserField(field1.name,
-  //                       Seq(field1Entry),
-  //                       field1.dbName,
-  //                       field1.redisName),
-  //          BrowserField(field2.name,
-  //                       Seq(field2Entry),
-  //                       field2.dbName,
-  //                       field2.redisName)
-  //        )
-  //    }
-  //
-  //    Task.gatherUnordered(taskList)
-  //  }
-
-  //  override def fetchAggregate(msaMd: MsaMd,
-  //                              field1: BrowserField,
-  //                              field2: BrowserField): Task[Seq[Aggregation]] = {
-  //    def findDb(oneName: String,
-  //               one: String,
-  //               twoName: String,
-  //               two: String): Task[Statistic] =
-  //      repo.findAndAggregate(msaMd.msaMd, oneName, one, twoName, two)
-  //
-  //    def findCache(oneName: String,
-  //                  one: String,
-  //                  twoName: String,
-  //                  two: String): Task[Option[Statistic]] =
-  //      cache.find(msaMd.msaMd, oneName, one, twoName, two)
-  //
-  //    def updateCache(oneName: String,
-  //                    one: String,
-  //                    twoName: String,
-  //                    two: String,
-  //                    s: Statistic): Task[Statistic] =
-  //      cache.update(msaMd.msaMd, oneName, one, twoName, two, s)
-  //
-  //    fetchAgg(field1, field2, findDb, findCache, updateCache)
-  //  }
-  //
-  //  override def fetchAggregate(state: State,
-  //                              field1: BrowserField,
-  //                              field2: BrowserField): Task[Seq[Aggregation]] = {
-  //    def findDb(oneName: String,
-  //               one: String,
-  //               twoName: String,
-  //               two: String): Task[Statistic] =
-  //      repo.findAndAggregate(state.entryName, oneName, one, twoName, two)
-  //
-  //    def findCache(oneName: String,
-  //                  one: String,
-  //                  twoName: String,
-  //                  two: String): Task[Option[Statistic]] =
-  //      cache.find(state.entryName, oneName, one, twoName, two)
-  //
-  //    def updateCache(oneName: String,
-  //                    one: String,
-  //                    twoName: String,
-  //                    two: String,
-  //                    s: Statistic): Task[Statistic] =
-  //      cache.update(state.entryName, oneName, one, twoName, two, s)
-  //
-  //    fetchAgg(field1, field2, findDb, findCache, updateCache)
-  //  }
-  //
-  //  override def fetchAggregate(field1: BrowserField,
-  //                              field2: BrowserField): Task[Seq[Aggregation]] = {
-  //    def findInDb(oneName: String,
-  //                 one: String,
-  //                 twoName: String,
-  //                 two: String): Task[Statistic] =
-  //      repo.findAndAggregate(oneName, one, twoName, two)
-  //
-  //    def findInCache(oneName: String,
-  //                    one: String,
-  //                    twoName: String,
-  //                    two: String): Task[Option[Statistic]] =
-  //      cache.find(oneName, one, twoName, two)
-  //
-  //    def updateCache(oneName: String,
-  //                    one: String,
-  //                    twoName: String,
-  //                    two: String,
-  //                    s: Statistic): Task[Statistic] =
-  //      cache.update(oneName, one, twoName, two, s)
-  //
-  //    fetchAgg(field1, field2, findInDb, findInCache, updateCache)
-  //  }
-  //
-  //  override def fetchAggregate(msaMd: MsaMd,
-  //                              state: State,
-  //                              field1: BrowserField,
-  //                              field2: BrowserField): Task[Seq[Aggregation]] = {
-  //    def findDb(oneName: String,
-  //               one: String,
-  //               twoName: String,
-  //               two: String): Task[Statistic] =
-  //      repo.findAndAggregate(msaMd.msaMd,
-  //                            state.entryName,
-  //                            oneName,
-  //                            one,
-  //                            twoName,
-  //                            two)
-  //
-  //    def findCache(oneName: String,
-  //                  one: String,
-  //                  twoName: String,
-  //                  two: String): Task[Option[Statistic]] =
-  //      cache.find(msaMd.msaMd, state.entryName, oneName, one, twoName, two)
-  //
-  //    def updateCache(oneName: String,
-  //                    one: String,
-  //                    twoName: String,
-  //                    two: String,
-  //                    s: Statistic): Task[Statistic] =
-  //      cache.update(msaMd.msaMd, state.entryName, oneName, one, twoName, two, s)
-  //
-  //    fetchAgg(field1, field2, findDb, findCache, updateCache)
-  //  }
-
+class ModifiedLarBrowserService(repo: ModifiedLarRepository,
+                                cache: ModifiedLarAggregateCache)
+  extends BrowserService {
   override def fetchData(
-      browserFields: List[QueryField]): Source[ModifiedLarEntity, NotUsed] =
+                          browserFields: List[QueryField]): Source[ModifiedLarEntity, NotUsed] =
     repo.find(browserFields)
 
   private def generateCombinations[T](x: List[List[T]]): List[List[T]] = {
@@ -209,7 +34,7 @@ class ModifiedLarBrowserService(repo: ModifiedLarRepository)
   }
 
   override def fetchAggregate(
-      fields: List[QueryField]): Task[Seq[Aggregation]] = {
+                               fields: List[QueryField]): Task[Seq[Aggregation]] = {
     val optState: Option[QueryField] =
       fields.filter(_.values.nonEmpty).find(_.name == "state")
     val optMsaMd: Option[QueryField] =
@@ -221,12 +46,20 @@ class ModifiedLarBrowserService(repo: ModifiedLarRepository)
 
     Task.gatherUnordered {
       queryFieldCombinations.map { eachCombination =>
-        println(eachCombination)
-        val fieldInfos =
-          eachCombination.map(field =>
-            FieldInfo(field.name, field.values.mkString(",")))
-        repo
-          .findAndAggregate(eachCombination)
+        val fieldInfos = eachCombination.map(field =>
+          FieldInfo(field.name, field.values.mkString(",")))
+
+        cache
+          .find(eachCombination)
+          .flatMap {
+            case None =>
+              repo
+                .findAndAggregate(eachCombination)
+                .flatMap(stat => cache.update(eachCombination, stat))
+
+            case Some(stat) =>
+              Task.now(stat)
+          }
           .map(statistic =>
             Aggregation(statistic.count, statistic.sum, fieldInfos))
       }
