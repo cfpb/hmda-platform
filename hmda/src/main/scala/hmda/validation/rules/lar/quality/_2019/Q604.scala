e8a6fb6936f549782c75db919bdb4b57517ee63f
bsharma <bsharma@healthstarinfo.com>
2019-06-30 00:22:41 -0400

    update Q604 for 2019

--- hmda/src/main/scala/hmda/validation/rules/lar/quality/_2019/Q604.scala
+++ hmda/src/main/scala/hmda/validation/rules/lar/quality/_2019/Q604.scala
@@ -1,32 +1,27 @@
-package hmda.validation.rules.lar.quality.common
+package hmda.validation.rules.lar.quality._2019
 
-import com.typesafe.config.ConfigFactory
 import hmda.census.records.CensusRecords
+import hmda.model.census.{Census, State}
 import hmda.model.filing.lar.LoanApplicationRegister
-import hmda.validation.dsl.{
-  ValidationFailure,
-  ValidationResult,
-  ValidationSuccess
-}
+import hmda.validation.dsl.{ValidationFailure, ValidationResult, ValidationSuccess}
 import hmda.validation.rules.EditCheck
 
 object Q604 extends EditCheck[LoanApplicationRegister] {
 
   override def name: String = "Q604"
 
-  val config = ConfigFactory.load()
-
-  val host = config.getString("hmda.census.http.host")
-  val port = config.getInt("hmda.census.http.port")
-
   override def apply(lar: LoanApplicationRegister): ValidationResult = {
 
     val county = lar.geography.county
     val state = lar.geography.state
-
     if (county.toLowerCase != "na" && state.toLowerCase != "na") {
       if (CensusRecords.indexedCounty.contains(county)) {
-        ValidationSuccess
+        val countyState = Census.states.getOrElse(state, State("", ""))
+        if (county.take(2) == countyState.code) {
+          ValidationSuccess
+        } else {
+          ValidationFailure
+        }
       } else {
         ValidationFailure
       }
