6f7b82e359fa8c58bb8eca8d144794d8dfa0b3b8
Patrick Asata <patrick.asata@cfpb.gov>
2019-08-13 12:08:19 -0400

    uncommenting actor systems

--- hmda-regulator/src/main/scala/hmda/regulator/HmdaRegulatorApp.scala
+++ hmda-regulator/src/main/scala/hmda/regulator/HmdaRegulatorApp.scala
@@ -46,35 +46,31 @@ object HmdaRegulatorApp extends App {
   log.info("LAR Public 2018: " + larPublicTimer2018)
   log.info("TS Public 2018: " + tsPublicTimer2018)
 
-//  val panelActorSystem =
-//    ActorSystem(
-//      "panelTask",
-//      ConfigFactory
-//        .parseString(panelTimer2018)
-//        .withValue(panelTimer2019(0),
-//                   ConfigValueFactory.fromAnyRef(panelTimer2019(1)))
-//        .withFallback(config)
-//    )
-//  panelActorSystem.actorOf(Props[PanelScheduler], "PanelScheduler")
-//
-//  val larActorSystem =
-//    ActorSystem("larTask",
-//                ConfigFactory
-//                  .parseString(larTimer2018)
-//                  .withValue(larTimer2019(0),
-//                             ConfigValueFactory
-//                               .fromAnyRef(larTimer2019(1)))
-//                  .withFallback(config))
-//  larActorSystem.actorOf(Props[LarScheduler], "LarScheduler")
-//
-//  val tsActorSystem = ActorSystem("tsTask",
-//                                  ConfigFactory
-//                                    .parseString(tsTimer2018)
-//                                    .withValue(tsTimer2019(0),
-//                                               ConfigValueFactory
-//                                                 .fromAnyRef(tsTimer2019(1)))
-//                                    .withFallback(config))
-//  tsActorSystem.actorOf(Props[TsScheduler], "TsScheduler")
+  val panelActorSystem =
+    ActorSystem("panelTask", ConfigFactory
+        .parseString(panelTimer2018)
+        .withValue(panelTimer2019(0),ConfigValueFactory.fromAnyRef(panelTimer2019(1)))
+        .withFallback(config)
+    )
+  panelActorSystem.actorOf(Props[PanelScheduler], "PanelScheduler")
+
+  val larActorSystem =
+    ActorSystem("larTask",
+                ConfigFactory
+                  .parseString(larTimer2018)
+                  .withValue(larTimer2019(0), ConfigValueFactory.fromAnyRef(larTimer2019(1)))
+                  .withFallback(config))
+  larActorSystem.actorOf(Props[LarScheduler], "LarScheduler")
+
+  val tsActorSystem =
+    ActorSystem("tsTask",
+      ConfigFactory
+        .parseString(tsTimer2018)
+        .withValue(tsTimer2019(0),
+          ConfigValueFactory
+            .fromAnyRef(tsTimer2019(1)))
+        .withFallback(config))
+  tsActorSystem.actorOf(Props[TsScheduler], "TsScheduler")
 
   //Public Data Actor Systems
 
@@ -82,6 +78,7 @@ object HmdaRegulatorApp extends App {
     ActorSystem(
       "panelPublicTask",
       ConfigFactory.parseString(panelPublicTimer2018).withFallback(config))
+
   panelPublicActorSystem.actorOf(Props[PanelPublicScheduler],
                                  "PanelPublicScheduler")
 
@@ -89,12 +86,14 @@ object HmdaRegulatorApp extends App {
     ActorSystem(
       "larPublicTask",
       ConfigFactory.parseString(larPublicTimer2018).withFallback(config))
+
   larPublicActorSystem.actorOf(Props[LarPublicScheduler], "LarPublicScheduler")
 
   val tsPublicActorSystem =
     ActorSystem(
       "tsPublicTask",
       ConfigFactory.parseString(tsPublicTimer2018).withFallback(config))
+
   tsPublicActorSystem.actorOf(Props[TsPublicScheduler], "TsPublicScheduler")
 
 }
