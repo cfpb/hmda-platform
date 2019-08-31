6ba2b57dba6e60a785dd20298adbf7240676e646
bsharma <bsharma@healthstarinfo.com>
2019-08-30 22:00:27 -0400

    open connection once

--- hmda-reporting/src/main/scala/hmda/reporting/api/http/ReportingHttpApi.scala
+++ hmda-reporting/src/main/scala/hmda/reporting/api/http/ReportingHttpApi.scala
@@ -7,21 +7,14 @@ import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
-import ch.megard.akka.http.cors.scaladsl.CorsDirectives.{
-  cors,
-  corsRejectionHandler
-}
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives.{cors, corsRejectionHandler}
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
-import hmda.model.institution.{
-  HmdaFiler,
-  HmdaFilerResponse,
-  MsaMd,
-  MsaMdResponse
-}
+import hmda.model.institution.{HmdaFiler, HmdaFilerResponse, MsaMd, MsaMdResponse}
 import hmda.query.repository.ModifiedLarRepository
 import hmda.reporting.repository.InstitutionComponent
 import hmda.util.http.FilingResponseUtils.entityNotPresentResponse
 import io.circe.generic.auto._
+import org.slf4j.LoggerFactory
 import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
 
@@ -38,11 +31,13 @@ trait ReportingHttpApi extends InstitutionComponent {
   val bankFilter = config.getConfig("filter")
   val bankFilterList =
     bankFilter.getString("bank-filter-list").toUpperCase.split(",")
-
+  val databaseConfig = DatabaseConfig.forConfig[JdbcProfile]("db")
+  val repo = new ModifiedLarRepository(databaseConfig)
   val institutionRepository: InstitutionRepository
-
+  val log = LoggerFactory.getLogger("hmda")
   val filerListRoute: Route = {
     path("filers" / IntNumber) { filingYear =>
+      log.info("Fileres ")
       get {
 
         val futFilerSet =
@@ -66,8 +61,7 @@ trait ReportingHttpApi extends InstitutionComponent {
       }
     } ~ path("filers" / IntNumber / Segment / "msaMds") { (year, lei) =>
       extractUri { uri =>
-        val databaseConfig = DatabaseConfig.forConfig[JdbcProfile]("db")
-        val repo = new ModifiedLarRepository(databaseConfig)
+        log.info("msaMds endpoint with lei: " + lei)
         val resultset = for {
           msaMdsResult <- repo.msaMds(lei, year)
           institutionResult <- institutionRepository.findByLei(lei)
@@ -85,6 +79,7 @@ trait ReportingHttpApi extends InstitutionComponent {
           case Success(leiMsaMds) =>
             complete(leiMsaMds)
           case Failure(error) =>
+            log.error("Errored in the /msaMds endpoint with: " + error)
             entityNotPresentResponse("institution", lei, uri)
         }
       }
