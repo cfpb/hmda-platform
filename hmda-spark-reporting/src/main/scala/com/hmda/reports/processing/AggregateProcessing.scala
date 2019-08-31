d925a81ec7b481e338c8737fd3677dfab4d89431
bsharma <bsharma@healthstarinfo.com>
2019-08-31 01:50:02 -0400

    sample

--- hmda-spark-reporting/src/main/scala/com/hmda/reports/processing/AggregateProcessing.scala
+++ hmda-spark-reporting/src/main/scala/com/hmda/reports/processing/AggregateProcessing.scala
@@ -547,22 +547,22 @@ object AggregateProcessing {
         .runWith(Sink.ignore)
 
     val result = for {
-      _ <- persistJson(aggregateTable1)
-      _ <- persistJson2(aggregateTable2)
-      _ <- persistJson9(aggregateTable9)
+//      _ <- persistJson(aggregateTable1)
+//      _ <- persistJson2(aggregateTable2)
+//      _ <- persistJson9(aggregateTable9)
       _ <- persistJsonI(aggregateTableI.toList)
-      _ <- persistJsonRaceSex(
-        jsonFormationRaceThenGender(
-          RaceGenderProcessing.outputCollectionTable3and4(cachedRecordsDf,
-                                                          spark)))
-      _ <- persistJsonEthnicitySex(
-        jsonTransformationReportByEthnicityThenGender(
-          RaceGenderProcessing.outputCollectionTable3and4(cachedRecordsDf,
-                                                          spark)))
-      _ <- persistIncomeRaceEthnicity(
-        IncomeRaceEthnicityProcessing.jsonFormationApplicantIncome(
-          IncomeRaceEthnicityProcessing
-            .outputCollectionTableIncome(cachedRecordsDf, spark)))
+//      _ <- persistJsonRaceSex(
+//        jsonFormationRaceThenGender(
+//          RaceGenderProcessing.outputCollectionTable3and4(cachedRecordsDf,
+//                                                          spark)))
+//      _ <- persistJsonEthnicitySex(
+//        jsonTransformationReportByEthnicityThenGender(
+//          RaceGenderProcessing.outputCollectionTable3and4(cachedRecordsDf,
+//                                                          spark)))
+//      _ <- persistIncomeRaceEthnicity(
+//        IncomeRaceEthnicityProcessing.jsonFormationApplicantIncome(
+//          IncomeRaceEthnicityProcessing
+//            .outputCollectionTableIncome(cachedRecordsDf, spark)))
     } yield ()
 
     result.onComplete {
