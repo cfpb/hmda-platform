c0e8de48182dbd115ebce9fcf898de8b2341aa8b
bsharma <bsharma@healthstarinfo.com>
2019-08-31 08:03:07 -0400

    debugging

--- hmda-spark-reporting/src/main/scala/com/hmda/reports/processing/AggregateProcessing.scala
+++ hmda-spark-reporting/src/main/scala/com/hmda/reports/processing/AggregateProcessing.scala
@@ -76,7 +76,7 @@ object AggregateProcessing {
         .option("upperBound", 99999)
         .option(
           "dbtable",
-          s"(select * from modifiedlar2018_snapshot where filing_year = $year and state <> 'NA' and county <> 'NA' and lei not in ('BANK1LEIFORTEST12345','BANK3LEIFORTEST12345','BANK4LEIFORTEST12345','999999LE3ZOZXUS7W648','28133080042813308004','B90YWS6AFX2LGWOXJ1LD')) as mlar"
+          s"(select * from modifiedlar2018_snapshot where filing_year = $year and msa_md = 31084 and state <> 'NA' and county <> 'NA' and lei not in ('BANK1LEIFORTEST12345','BANK3LEIFORTEST12345','BANK4LEIFORTEST12345','999999LE3ZOZXUS7W648','28133080042813308004','B90YWS6AFX2LGWOXJ1LD')) as mlar"
         )
         .load()
         .withColumnRenamed("race_categorization", "race")
