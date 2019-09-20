30e83c337ef36497468fae231e53160f8adebf49
Bhaarat Sharma <bhaarat.sharma@cfpb.gov>
2019-09-19 21:24:17 -0400

    change to dev default email

--- hmda/src/main/scala/hmda/api/http/model/filing/submissions/SignedResponse.scala
+++ hmda/src/main/scala/hmda/api/http/model/filing/submissions/SignedResponse.scala
@@ -2,7 +2,7 @@ package hmda.api.http.model.filing.submissions
 
 import hmda.model.filing.submission.SubmissionStatus
 
-case class SignedResponse(email: String = "test@test.com",
+case class SignedResponse(email: String = "dev@email.com",
                           timestamp: Long,
                           receipt: String,
                           status: SubmissionStatus)
