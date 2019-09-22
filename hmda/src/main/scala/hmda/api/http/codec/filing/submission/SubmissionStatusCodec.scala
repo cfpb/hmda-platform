d141f93bbe6075e780594c90182a8d03e458ec57
bsharma <bsharma@healthstarinfo.com>
2019-09-22 06:26:19 -0400

    modify json encoder to include extra information

--- hmda/src/main/scala/hmda/api/http/codec/filing/submission/SubmissionStatusCodec.scala
+++ hmda/src/main/scala/hmda/api/http/codec/filing/submission/SubmissionStatusCodec.scala
@@ -1,27 +1,20 @@
 package hmda.api.http.codec.filing.submission
 
 import hmda.model.filing.submission.SubmissionStatus
-import io.circe.Decoder.Result
 import io.circe.{Decoder, Encoder, HCursor, Json}
 
 object SubmissionStatusCodec {
 
   implicit val submissionStatusEncoder: Encoder[SubmissionStatus] =
-    new Encoder[SubmissionStatus] {
-      override def apply(a: SubmissionStatus): Json = Json.obj(
+    (a: SubmissionStatus) =>
+      Json.obj(
         ("code", Json.fromInt(a.code)),
         ("message", Json.fromString(a.message)),
-        ("description", Json.fromString(a.description))
+        ("description", Json.fromString(a.description)),
+        ("qualityVerified", Json.fromBoolean(a.qualityVerified)),
+        ("macroVerified", Json.fromBoolean(a.macroVerified))
       )
-    }
 
   implicit val submissionStatusDecoder: Decoder[SubmissionStatus] =
-    new Decoder[SubmissionStatus] {
-      override def apply(c: HCursor): Result[SubmissionStatus] =
-        for {
-          code <- c.downField("code").as[Int]
-        } yield {
-          SubmissionStatus.valueOf(code)
-        }
-    }
+    (c: HCursor) => c.downField("code").as[Int].map(SubmissionStatus.valueOf)
 }
