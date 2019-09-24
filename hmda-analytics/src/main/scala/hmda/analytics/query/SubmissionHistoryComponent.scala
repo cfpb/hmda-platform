3f2e11d6dcc7a7204b4183761191e289deeadab3
bsharma <bsharma@healthstarinfo.com>
2019-09-24 10:50:18 -0400

    Support 2018 tables for hmda-analytics in beta namespace

--- hmda-analytics/src/main/scala/hmda/analytics/query/SubmissionHistoryComponent.scala
+++ hmda-analytics/src/main/scala/hmda/analytics/query/SubmissionHistoryComponent.scala
@@ -14,7 +14,7 @@ trait SubmissionHistoryComponent {
   class SubmissionHistoryRepository(config: DatabaseConfig[JdbcProfile], tableName: String) {
     def fetchYearTable(year: Int): String = {
       year match {
-        case 2018 => "submission_history"
+        case 2018 => tableName
         case 2019 => tableName
         case _    => tableName
       }
