8f73c45f85c91a85a9dbeab76ca7329f0789fcc1
bsharma <bsharma@healthstarinfo.com>
2019-09-24 23:58:41 -0400

    account for createSchema in inst-postgres-credentials secret.

--- institutions-api/src/main/scala/hmda/institution/api/http/InstitutionQueryHttpApi.scala
+++ institutions-api/src/main/scala/hmda/institution/api/http/InstitutionQueryHttpApi.scala
@@ -33,10 +33,10 @@ trait InstitutionQueryHttpApi
   implicit val timeout: Timeout
   val log: LoggingAdapter
 
-  implicit val institutionRepository2018 = new InstitutionRepository2018(
-    dbConfig)
-  implicit val institutionRepository2019 = new InstitutionRepository2019(
-    dbConfig)
+  implicit val institutionRepository2018 =
+    new InstitutionRepository2018(dbConfig, "")
+  implicit val institutionRepository2019 =
+    new InstitutionRepository2019(dbConfig, "")
   implicit val institutionEmailsRepository = new InstitutionEmailsRepository(
     dbConfig)
 
--- institutions-api/src/main/scala/hmda/institution/query/InstitutionComponent2019.scala
+++ institutions-api/src/main/scala/hmda/institution/query/InstitutionComponent2019.scala
@@ -9,8 +9,8 @@ trait InstitutionComponent2019 {
 
   import dbConfig.profile.api._
 
-  class InstitutionsTable2019(tag: Tag)
-      extends Table[InstitutionEntity](tag, "institutions2019") {
+  class InstitutionsTable2019(tag: Tag, tableName: String)
+    extends Table[InstitutionEntity](tag, tableName) {
     def lei = column[String]("lei", O.PrimaryKey)
     def activityYear = column[Int]("activity_year")
     def agency = column[Int]("agency")
@@ -49,51 +49,53 @@ trait InstitutionComponent2019 {
        hmdaFiler) <> (InstitutionEntity.tupled, InstitutionEntity.unapply)
   }
 
-  class InstitutionsTable2019Beta(tag: Tag)
-    extends Table[InstitutionEntity](tag, "hmda_beta_user.institutions2019") {
-    def lei = column[String]("lei", O.PrimaryKey)
-    def activityYear = column[Int]("activity_year")
-    def agency = column[Int]("agency")
-    def institutionType = column[Int]("institution_type")
-    def id2017 = column[String]("id2017")
-    def taxId = column[String]("tax_id")
-    def rssd = column[Int]("rssd")
-    def respondentName = column[String]("respondent_name")
-    def respondentState = column[String]("respondent_state")
-    def respondentCity = column[String]("respondent_city")
-    def parentIdRssd = column[Int]("parent_id_rssd")
-    def parentName = column[String]("parent_name")
-    def assets = column[Int]("assets")
-    def otherLenderCode = column[Int]("other_lender_code")
-    def topHolderIdRssd = column[Int]("topholder_id_rssd")
-    def topHolderName = column[String]("topholder_name")
-    def hmdaFiler = column[Boolean]("hmda_filer")
+  //  class InstitutionsTable2019Beta(tag: Tag)
+  //      extends Table[InstitutionEntity](tag, "hmda_beta_user.institutions2019") {
+  //    def lei = column[String]("lei", O.PrimaryKey)
+  //    def activityYear = column[Int]("activity_year")
+  //    def agency = column[Int]("agency")
+  //    def institutionType = column[Int]("institution_type")
+  //    def id2017 = column[String]("id2017")
+  //    def taxId = column[String]("tax_id")
+  //    def rssd = column[Int]("rssd")
+  //    def respondentName = column[String]("respondent_name")
+  //    def respondentState = column[String]("respondent_state")
+  //    def respondentCity = column[String]("respondent_city")
+  //    def parentIdRssd = column[Int]("parent_id_rssd")
+  //    def parentName = column[String]("parent_name")
+  //    def assets = column[Int]("assets")
+  //    def otherLenderCode = column[Int]("other_lender_code")
+  //    def topHolderIdRssd = column[Int]("topholder_id_rssd")
+  //    def topHolderName = column[String]("topholder_name")
+  //    def hmdaFiler = column[Boolean]("hmda_filer")
+  //
+  //    def * =
+  //      (lei,
+  //       activityYear,
+  //       agency,
+  //       institutionType,
+  //       id2017,
+  //       taxId,
+  //       rssd,
+  //       respondentName,
+  //       respondentState,
+  //       respondentCity,
+  //       parentIdRssd,
+  //       parentName,
+  //       assets,
+  //       otherLenderCode,
+  //       topHolderIdRssd,
+  //       topHolderName,
+  //       hmdaFiler) <> (InstitutionEntity.tupled, InstitutionEntity.unapply)
+  //  }
+  val institutionsTable2019 = TableQuery[InstitutionsTable2019]((tag: Tag) =>
+    new InstitutionsTable2019(tag, "institutions2019"))
 
-    def * =
-      (lei,
-        activityYear,
-        agency,
-        institutionType,
-        id2017,
-        taxId,
-        rssd,
-        respondentName,
-        respondentState,
-        respondentCity,
-        parentIdRssd,
-        parentName,
-        assets,
-        otherLenderCode,
-        topHolderIdRssd,
-        topHolderName,
-        hmdaFiler) <> (InstitutionEntity.tupled, InstitutionEntity.unapply)
-  }
-
-  val institutionsTable2019 = TableQuery[InstitutionsTable2019]
-  val institutionsTable2019Beta = TableQuery[InstitutionsTable2019Beta]
-
-  class InstitutionRepository2019(val config: DatabaseConfig[JdbcProfile])
+  class InstitutionRepository2019(val config: DatabaseConfig[JdbcProfile],
+                                  tableName: String)
       extends TableRepository[InstitutionsTable2019, String] {
+    val institutionsTable2019 = TableQuery[InstitutionsTable2019]((tag: Tag) =>
+      new InstitutionsTable2019(tag, tableName))
     val table = institutionsTable2019
     def getId(table: InstitutionsTable2019) = table.lei
     def deleteById(lei: String) = db.run(filterById(lei).delete)
@@ -102,10 +104,14 @@ trait InstitutionComponent2019 {
     def dropSchema() = db.run(table.schema.drop)
   }
 
-  class InstitutionRepository2019Beta(val config: DatabaseConfig[JdbcProfile])
-    extends TableRepository[InstitutionsTable2019Beta, String] {
+  class InstitutionRepository2019Beta(val config: DatabaseConfig[JdbcProfile],
+                                      tableName: String)
+    extends TableRepository[InstitutionsTable2019, String] {
+    val institutionsTable2019Beta = TableQuery[InstitutionsTable2019](
+      (tag: Tag) => new InstitutionsTable2019(tag, tableName))
     val table = institutionsTable2019Beta
-    def getId(table: InstitutionsTable2019Beta) = table.lei
+
+    def getId(table: InstitutionsTable2019) = table.lei
     def deleteById(lei: String) = db.run(filterById(lei).delete)
 
     def createSchema() = db.run(table.schema.create)
