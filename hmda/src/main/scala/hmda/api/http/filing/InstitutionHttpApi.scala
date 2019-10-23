e49665199bc13d6fcb163a5d3d6da4ca6a4d9500
bsharma <bsharma@healthstarinfo.com>
2019-10-22 23:41:16 -0400

    implement institution http api endpoints

--- hmda/src/main/scala/hmda/api/http/filing/InstitutionHttpApi.scala
+++ hmda/src/main/scala/hmda/api/http/filing/InstitutionHttpApi.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.event.LoggingAdapter
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{ StatusCodes, Uri }
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
@@ -14,14 +14,14 @@ import hmda.api.http.directives.HmdaTimeDirectives
 import hmda.util.http.FilingResponseUtils._
 import hmda.messages.institution.InstitutionCommands.GetInstitutionDetails
 import hmda.model.institution.InstitutionDetail
-import hmda.persistence.institution.InstitutionPersistence
+import hmda.api.http.PathMatchers._
 
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success }
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import hmda.api.http.model.ErrorResponse
 import hmda.auth.OAuth2Authorization
-import hmda.utils.YearUtils._
+import hmda.persistence.institution.InstitutionPersistence.selectInstitution
 
 trait InstitutionHttpApi extends HmdaTimeDirectives {
 
@@ -32,55 +32,39 @@ trait InstitutionHttpApi extends HmdaTimeDirectives {
   implicit val timeout: Timeout
   val sharding: ClusterSharding
 
-  //institutions/<lei>
+  // GET /institutions/<lei>/year/<y>
+  // GET /institutions/<lei>/year/<y>/quarter/<q>
   def institutionReadPath(oAuth2Authorization: OAuth2Authorization): Route =
-    oAuth2Authorization.authorizeToken { _ =>
-      path("institutions" / Segment / "year" / Segment) { (lei, period) =>
-        val institutionPersistence = {
-          if (period == "2018") {
-            sharding.entityRefFor(InstitutionPersistence.typeKey,
-              s"${InstitutionPersistence.name}-$lei")
-          } else {
-            sharding.entityRefFor(
-              InstitutionPersistence.typeKey,
-              s"${InstitutionPersistence.name}-$lei-$period")
-          }
-        }
-
-        val iDetails
-        : Future[Option[InstitutionDetail]] = institutionPersistence ? (ref =>
-          GetInstitutionDetails(ref))
-
-        val filingDetailsF = for {
-          i <- iDetails
-        } yield i
-
+    pathPrefix("institutions" / Segment / "year" / Year) { (lei, year) =>
+      oAuth2Authorization.authorizeTokenWithLei(lei) { _ =>
         timedGet { uri =>
-          if (!isValidYear(period.toInt)) {
-            complete(
-              ErrorResponse(500, s"Invalid Year Provided: $period", uri.path))
-          } else {
-            onComplete(filingDetailsF) {
-              case Success(Some(institutionDetails)) =>
-                complete(ToResponseMarshallable(institutionDetails))
-              case Success(None) =>
-                val errorResponse =
-                  ErrorResponse(404,
-                    s"Institution: $lei does not exist",
-                    uri.path)
-                complete(
-                  ToResponseMarshallable(StatusCodes.NotFound -> errorResponse)
-                )
-              case Failure(error) =>
-                failedResponse(StatusCodes.InternalServerError, uri, error)
-            }
-
+          pathEndOrSingleSlash {
+            obtainFilingDetails(lei, year, None, uri)
+          } ~ path("quarter" / Quarter) { quarter =>
+            obtainFilingDetails(lei, year, Option(quarter), uri)
           }
         }
       }
     }
 
-  def institutionRoutes(oAuth2Authorization: OAuth2Authorization): Route = {
+  def obtainFilingDetails(lei: String, year: Int, quarter: Option[String], uri: Uri): Route = {
+    val institutionPersistence                      = selectInstitution(sharding, lei, year, quarter)
+    val iDetails: Future[Option[InstitutionDetail]] = institutionPersistence ? (ref => GetInstitutionDetails(ref))
+    onComplete(iDetails) {
+      case Success(Some(institutionDetails)) =>
+        complete(ToResponseMarshallable(institutionDetails))
+      case Success(None) =>
+        val errorResponse =
+          ErrorResponse(404, s"Institution: $lei does not exist", uri.path)
+        complete(
+          ToResponseMarshallable(StatusCodes.NotFound -> errorResponse)
+        )
+      case Failure(error) =>
+        failedResponse(StatusCodes.InternalServerError, uri, error)
+    }
+  }
+
+  def institutionRoutes(oAuth2Authorization: OAuth2Authorization): Route =
     handleRejections(corsRejectionHandler) {
       cors() {
         encodeResponse {
@@ -88,5 +72,4 @@ trait InstitutionHttpApi extends HmdaTimeDirectives {
         }
       }
     }
-  }
 }
