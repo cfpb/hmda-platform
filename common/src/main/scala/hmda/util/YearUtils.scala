30fc431c0bd6c68a82d8e3699a57719410f443df
bsharma <bsharma@healthstarinfo.com>
2019-10-23 09:16:15 -0400

    parse period

--- common/src/main/scala/hmda/util/YearUtils.scala
+++ common/src/main/scala/hmda/util/YearUtils.scala
@@ -1,6 +1,7 @@
 package hmda.utils
 
 import com.typesafe.config.ConfigFactory
+import cats.implicits._
 
 object YearUtils {
 
@@ -22,4 +23,13 @@ object YearUtils {
 
   def period(year: Int, quarter: Option[String]): String =
     quarter.fold(ifEmpty = s"$year")(quarter => s"$year-$quarter")
+
+  case class Period(year: Int, quarter: Option[String])
+
+  def parsePeriod(period: String): Either[Exception, Period] = {
+    val raw = period.split("-")
+    if (raw.length == 1) Period(raw(0).toInt, None).asRight
+    else if (raw.length == 2) Period(raw(0).toInt, Option(raw(1))).asRight
+    else new Exception(s"Failed to parse invalid period: $period").asLeft
+  }
 }
