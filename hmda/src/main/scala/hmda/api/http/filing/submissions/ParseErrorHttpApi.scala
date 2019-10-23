0bfe5a1377299d45967d97e5d2c32b1f9917457a
bsharma <bsharma@healthstarinfo.com>
2019-10-22 23:55:36 -0400

    implement parser error http api

--- hmda/src/main/scala/hmda/api/http/filing/submissions/ParseErrorHttpApi.scala
+++ hmda/src/main/scala/hmda/api/http/filing/submissions/ParseErrorHttpApi.scala
@@ -3,7 +3,7 @@ package hmda.api.http.filing.submissions
 import akka.actor.ActorSystem
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.event.LoggingAdapter
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{ StatusCodes, Uri }
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
@@ -15,16 +15,18 @@ import hmda.api.http.model.filing.submissions.ParsingErrorSummary
 import hmda.auth.OAuth2Authorization
 import hmda.messages.submission.SubmissionCommands.GetSubmission
 import hmda.messages.submission.SubmissionProcessingCommands.GetParsingErrors
-import hmda.model.filing.submission.{Submission, SubmissionId}
+import hmda.model.filing.submission.{ Submission, SubmissionId }
 import hmda.model.processing.state.HmdaParserErrorState
-import hmda.persistence.submission.{HmdaParserError, SubmissionPersistence}
+import hmda.persistence.submission.{ HmdaParserError, SubmissionPersistence }
 import hmda.util.http.FilingResponseUtils._
 import hmda.model.filing.ParserValidValuesLookup._
 import hmda.api.http.model.filing.submissions._
 import hmda.messages.submission.SubmissionProcessingEvents.HmdaRowParsedError
+import hmda.api.http.PathMatchers._
+import hmda.utils.YearUtils
 
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success }
 
 trait ParseErrorHttpApi extends HmdaTimeDirectives {
 
@@ -35,72 +37,62 @@ trait ParseErrorHttpApi extends HmdaTimeDirectives {
   implicit val timeout: Timeout
   val sharding: ClusterSharding
 
-  //institutions/<lei>/filings/<period>/submissions/<submissionId>/parseErrors
-  def parseErrorPath(oAuth2Authorization: OAuth2Authorization): Route =
-    path(
-      "institutions" / Segment / "filings" / Segment / "submissions" / IntNumber / "parseErrors") {
-      (lei, period, seqNr) =>
-        oAuth2Authorization.authorizeTokenWithLei(lei) { _ =>
-          timedGet { uri =>
-            parameters('page.as[Int] ? 1) { page =>
-              val submissionId = SubmissionId(lei, period, seqNr)
-
-              val submissionPersistence =
-                sharding.entityRefFor(
-                  SubmissionPersistence.typeKey,
-                  s"${SubmissionPersistence.name}-${submissionId.toString}")
-
-              val fSubmission
-              : Future[Option[Submission]] = submissionPersistence ? (ref =>
-                GetSubmission(ref))
-
-              val fCheckSubmission = for {
-                s <- fSubmission.mapTo[Option[Submission]]
-              } yield s
-
-              onComplete(fCheckSubmission) {
-                case Success(check) =>
-                  check match {
-                    case Some(submission) =>
-                      val hmdaParserError =
-                        sharding.entityRefFor(
-                          HmdaParserError.typeKey,
-                          s"${HmdaParserError.name}-${submissionId.toString}")
-                      val fErrors
-                        : Future[HmdaParserErrorState] = hmdaParserError ? (
-                          ref => GetParsingErrors(page, ref))
-                      onComplete(fErrors) {
-                        case Success(state) =>
-                          val parsingErrorSummary = ParsingErrorSummary(
-                            state.transmittalSheetErrors.map(
-                              parserErrorSummaryConvertor(_)),
-                            state.larErrors.map(parserErrorSummaryConvertor(_)),
-                            uri.path.toString,
-                            page,
-                            state.totalErrors,
-                            submission.status
-                          )
-                          complete(parsingErrorSummary)
-                        case Failure(error) =>
-                          failedResponse(StatusCodes.InternalServerError,
-                            uri,
-                            error)
-                      }
-
-                    case None =>
-                      entityNotPresentResponse("submission",
-                        submissionId.toString,
-                        uri)
-                  }
-                case Failure(error) =>
-                  failedResponse(StatusCodes.InternalServerError, uri, error)
-              }
-            }
+  // GET institutions/<lei>/filings/<year>/submissions/<submissionId>/parseErrors
+  // GET institutions/<lei>/filings/<year>/quarter/<q>/submissions/<submissionId>/parseErrors
+  def parseErrorPath(oauth2Authorization: OAuth2Authorization): Route = timedGet { uri =>
+    parameters('page.as[Int] ? 1) { page =>
+      pathPrefix("institutions" / Segment / "filings" / Year) { (lei, year) =>
+        oauth2Authorization.authorizeTokenWithLei(lei) { _ =>
+          path("submissions" / IntNumber / "parseErrors") { seqNr =>
+            checkSubmission(lei, year, None, seqNr, page, uri)
+          } ~ path("quarter" / Quarter / "submissions" / IntNumber / "parseErrors") { (quarter, seqNr) =>
+            checkSubmission(lei, year, Option(quarter), seqNr, page, uri)
           }
         }
+      }
     }
+  }
 
-  def parserErrorRoute(oAuth2Authorization: OAuth2Authorization): Route = {
+  private def checkSubmission(lei: String, year: Int, quarter: Option[String], seqNr: Int, page: Int, uri: Uri) = {
+    val period                                  = YearUtils.period(year, quarter)
+    val submissionId                            = SubmissionId(lei, period, seqNr)
+    val submissionPersistence                   = SubmissionPersistence.selectSubmissionPersistence(sharding, submissionId)
+    val fSubmission: Future[Option[Submission]] = submissionPersistence ? GetSubmission
+    val fCheckSubmission = for {
+      s <- fSubmission.mapTo[Option[Submission]]
+    } yield s
+
+    onComplete(fCheckSubmission) {
+      case Success(check) =>
+        check match {
+          case Some(submission) =>
+            val hmdaParserError =
+              sharding.entityRefFor(HmdaParserError.typeKey, s"${HmdaParserError.name}-${submissionId.toString}")
+            val fErrors: Future[HmdaParserErrorState] = hmdaParserError ? (ref => GetParsingErrors(page, ref))
+            onComplete(fErrors) {
+              case Success(state) =>
+                val parsingErrorSummary = ParsingErrorSummary(
+                  state.transmittalSheetErrors.map(parserErrorSummaryConvertor(_)),
+                  state.larErrors.map(parserErrorSummaryConvertor(_)),
+                  uri.path.toString,
+                  page,
+                  state.totalErrors,
+                  submission.status
+                )
+                complete(parsingErrorSummary)
+              case Failure(error) =>
+                failedResponse(StatusCodes.InternalServerError, uri, error)
+            }
+
+          case None =>
+            entityNotPresentResponse("submission", submissionId.toString, uri)
+        }
+      case Failure(error) =>
+        failedResponse(StatusCodes.InternalServerError, uri, error)
+    }
+  }
+
+  def parserErrorRoute(oAuth2Authorization: OAuth2Authorization): Route =
     handleRejections(corsRejectionHandler) {
       cors() {
         encodeResponse {
@@ -108,10 +100,8 @@ trait ParseErrorHttpApi extends HmdaTimeDirectives {
         }
       }
     }
-  }
 
-  def parserErrorSummaryConvertor(
-      hmdaRowParsedError: HmdaRowParsedError): HmdaRowParsedErrorSummary = {
+  private def parserErrorSummaryConvertor(hmdaRowParsedError: HmdaRowParsedError): HmdaRowParsedErrorSummary =
     HmdaRowParsedErrorSummary(
       hmdaRowParsedError.rowNumber,
       hmdaRowParsedError.estimatedULI,
@@ -121,8 +111,8 @@ trait ParseErrorHttpApi extends HmdaTimeDirectives {
             errorMessage.fieldName,
             errorMessage.inputValue,
             lookupParserValidValues(errorMessage.fieldName)
-        ))
+          )
+      )
     )
-  }
 
 }
