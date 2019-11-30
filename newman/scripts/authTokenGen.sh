41d7a6822b589ca1056a692412011d900b52b7af
PatrickHSI <pasata@healthstarinfo.com>
2019-11-30 00:46:34 -0500

    updates to scripts

--- newman/scripts/authTokenGen.sh
+++ newman/scripts/authTokenGen.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
- BEARER=$(curl -X POST $3 -d 'client_id=hmda2-api&grant_type=password&username='"$1"'&password='"$2"''| awk -F'"' '$2=="access_token"{print $4}')
+ BEARER=$(curl -X POST $3 -d "$AUTH_DATA"| awk -F'"' '$2=="access_token"{print $4}')
 echo $BEARER
-
-
-#$1 user name
-#$2 password
-#$3 auth url
--- newman/scripts/errorNotify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "NEWMAN TEST! \n SOMEONE FIX IT! :tada:"}' $1
