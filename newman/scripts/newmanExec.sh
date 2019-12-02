3b5a8f1e0c1250a08451fba04193fd901bd35ed1
PatrickHSI <pasata@healthstarinfo.com>
2019-12-02 15:16:56 -0500

    updated kubernetes settings

--- newman/scripts/newmanExec.sh
+++ newman/scripts/newmanExec.sh
@@ -1,23 +1,41 @@
 #!/bin/sh
 
-authToken=$(./scripts/authTokenGen.sh $KC_UN $KC_PW  $KC_URL $KC_CLIENT_ID)
-test=$(newman run dev/hmda-filing/hmda-filing-api-test.json -d\
+authToken=$(./scripts/authTokenGen.sh $1 $2 $3 $4)
+test=$(./node_modules/.bin/newman run dev/hmda-filing/hmda-filing-api-test.json -d\
  dev/hmda-filing/hmda-filing-api-config.json \
- --env-var host_filing=$HOST_FILING \
- --env-var host_admin_api=$HOST_ADMIN \
- --env-var host_public=$HOST_PUBLIC \
+ --env-var host_filing=$5 \
+ --env-var host_admin_api=$6 \
+ --env-var host_public=$7 \
  --env-var authToken="Bearer $authToken" \
  --bail |\
   tee scripts/results.txt)
 
 #extract the failure details of the test
-test=$(sed -n '/failure/,$p' scripts/results.txt)
+testResults=$(sed -n '/failure/,$p' scripts/results.txt)
 
 #escape newman output for curl command
-data="$( jq -nc --arg str "# $HMDA_ENV Newman Error (Filing Test): $test" '{"text": $str}' )"
+data="$( jq -nc --arg str "# $8 Newman Error (Filing Test): $testResults" '{"text": $str}' )"
 
 #post message is error is found
  if [[ $data == *"failure"* ]]; then
-     curl -i -X POST -H 'Content-Type: application/json' -d "$data" $MM_HOOK
+     curl -i -X POST -H 'Content-Type: application/json' -d "$data" $9
 fi
+
+
+# Keycloak env vars
+#$1 $KC_UN
+#$2 $KC_PW
+#$3 $KC_URL
+#$4 $KC_CLIENT_ID
+
+#API en vars
+#$5 $HOST_FILING
+#$6 $HOST_ADMIN
+#$7 $HOST_PUBLIC
+
+#MM Web hook Env Vars
+#$8 $HMDA_ENV
+#$9 $MM_HOOK
+
+
 
