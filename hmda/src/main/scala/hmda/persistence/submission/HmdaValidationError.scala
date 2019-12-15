b5d5740fbd606ef04342ef2bfc293c9880de4e28
bsharma <bsharma@healthstarinfo.com>
2019-12-15 10:24:14 -0500

    add starvation detection for blocking dispatcher

--- hmda/src/main/scala/hmda/persistence/submission/HmdaValidationError.scala
+++ hmda/src/main/scala/hmda/persistence/submission/HmdaValidationError.scala
@@ -53,7 +53,7 @@ import hmda.validation.rules.lar.quality.common.Q600
 import hmda.validation.rules.lar.syntactical.S305
 import hmda.validation.rules.lar.syntactical.S304
 import hmda.validation.{ AS, EC, MAT }
-
+import com.lightbend.akka.diagnostics.{StarvationDetector, StarvationDetectorSettings}
 import scala.collection.immutable.ListMap
 import scala.concurrent.duration._
 import scala.concurrent.{ ExecutionContext, Future }
@@ -88,6 +88,10 @@ object HmdaValidationError
     implicit val ec: ExecutionContext            = system.dispatcher
     val blockingEc: ExecutionContext =
       system.dispatchers.lookup("akka.blocking-quality-dispatcher")
+
+    StarvationDetector.checkExecutionContext(blockingEc, system.log, StarvationDetectorSettings.fromConfig(
+      system.settings.config.getConfig("akka.diagnostics.starvation-detector")), () => system.whenTerminated.isCompleted)
+
     val sharding = ClusterSharding(ctx.asScala.system)
 
     cmd match {
