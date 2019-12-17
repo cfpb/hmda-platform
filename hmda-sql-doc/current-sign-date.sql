314cea6fc338e44d1796bc9d23b889160dd9cccf
PatrickHSI <pasata@healthstarinfo.com>
2019-12-17 02:18:27 -0500

    queries to get the max sign date per lei

---
+++ hmda-sql-doc/current-sign-date.sql
@@ -0,0 +1,13 @@
+-- get the most recent sign_date for every LEI by year 2019
+select max_signed.* from (SELECT lei, submission_id, sign_date ,rank() OVER (PARTITION BY lei ORDER BY sign_date DESC) as number
+	FROM hmda_user.submission_history sub
+	where sub.submission_id like CONCAT('%',sub.lei,'-2019%') and sub.sign_date>0
+	group by lei ,submission_id
+	order by lei,number asc) max_signed where max_signed.number=1;
+
+-- get the most recent signdate for every LEI by year 2019
+select max_signed.* from (SELECT lei, submission_id, sign_date ,rank() OVER (PARTITION BY lei ORDER BY sign_date DESC) as number
+    	FROM hmda_user.submission_history sub
+    	where sub.submission_id like CONCAT('%',sub.lei,'-2018%') and sub.sign_date>0
+    	group by lei ,submission_id
+    	order by lei,number asc) max_signed where max_signed.number=1;
