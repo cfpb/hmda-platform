d332b7af98a025b077f41ca02cfe47b433812837
PatrickHSI <pasata@healthstarinfo.com>
2019-12-17 02:24:24 -0500

    queries to get the max sign date per lei

--- hmda-sql-doc/current-sign-date.sql
+++ hmda-sql-doc/current-sign-date.sql
@@ -1,13 +1,12 @@
 -- get the most recent sign_date for every LEI by year 2019
-select max_signed.* from (SELECT lei, submission_id, sign_date ,rank() OVER (PARTITION BY lei ORDER BY sign_date DESC) as number
+select max_signed.lei,max_signed.submission_id,max_signed.sign_date from (SELECT lei, submission_id, sign_date ,rank() OVER (PARTITION BY lei ORDER BY sign_date DESC) as number
 	FROM hmda_user.submission_history sub
 	where sub.submission_id like CONCAT('%',sub.lei,'-2019%') and sub.sign_date>0
 	group by lei ,submission_id
 	order by lei,number asc) max_signed where max_signed.number=1;
-
--- get the most recent signdate for every LEI by year 2019
-select max_signed.* from (SELECT lei, submission_id, sign_date ,rank() OVER (PARTITION BY lei ORDER BY sign_date DESC) as number
-    	FROM hmda_user.submission_history sub
-    	where sub.submission_id like CONCAT('%',sub.lei,'-2018%') and sub.sign_date>0
-    	group by lei ,submission_id
-    	order by lei,number asc) max_signed where max_signed.number=1;
+-- get the most recent signdate for every LEI by year 2018
+select max_signed.lei,max_signed.submission_id,max_signed.sign_date from (SELECT lei, submission_id, sign_date ,rank() OVER (PARTITION BY lei ORDER BY sign_date DESC) as number
+	FROM hmda_user.submission_history sub
+	where sub.submission_id like CONCAT('%',sub.lei,'-2018%') and sub.sign_date>0
+	group by lei ,submission_id
+	order by lei,number asc) max_signed where max_signed.number=1;
