979a379a477c9240330f0e4bea3e13e997588145
Barak Stout <barakstout@gmail.com>
2019-12-18 15:05:02 -0500

    more k8s tweaks

--- hmda-dashboard/src/main/scala/hmda/dashboard/HmdaDashboard.scala
+++ hmda-dashboard/src/main/scala/hmda/dashboard/HmdaDashboard.scala
@@ -1,19 +1,9 @@
 package hmda.dashboard
 
-import akka.Done
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.util.Timeout
 import hmda.dashboard.api.HmdaDashboardApi
 import org.slf4j.LoggerFactory
-import hmda.query.DbConfiguration.dbConfig
-import hmda.query.HmdaQuery.{readRawData, readSubmission}
-import hmda.util.BankFilterUtils._
-import hmda.util.streams.FlowUtils.framing
-import hmda.utils.YearUtils.Period
 
-import scala.concurrent.Future
-import scala.concurrent.duration._
 
 object HmdaDashboard extends App {
 
