9058a8aeaa0535af4857730a3d25fe9cf432dcf8
PatrickHSI <pasata@healthstarinfo.com>
2019-12-18 22:48:04 -0500

    better conditions for the pipe removal logic

--- common/src/main/scala/hmda/util/conversion/ColumnDataFormatter.scala
+++ common/src/main/scala/hmda/util/conversion/ColumnDataFormatter.scala
@@ -7,17 +7,18 @@ import java.util.Date
 trait ColumnDataFormatter {
 
   private val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS").withZone(ZoneId.systemDefault());
-  private val bomTypes = Seq("""\ufeff""","""ï»¿""","""\efbbbf""","""\ffe""");
+  private val bomTypes = Seq("""\ufeff""", """ï»¿""", """\efbbbf""", """\ffe""");
 
-  def dateToString(option:Option[Long] ): String =
-    if(option !=null) {
+  def dateToString(option: Option[Long]): String =
+    if (option != null) {
 
-      val epochLong = new Date(option.getOrElse(0L) )
-       val entryTime = dateFormatter.format(epochLong.toInstant)
+      val epochLong = new Date(option.getOrElse(0L))
+      val entryTime = dateFormatter.format(epochLong.toInstant)
       entryTime
-    }else{
+    } else {
       "NA"
     }
+
   def extractOpt(option: Option[Any]): Any =
     option.getOrElse("")
 
@@ -27,13 +28,21 @@ trait ColumnDataFormatter {
     } else {
       value
     }
-  def removeTrailingPipe(value: String): String =
-    if (!value.isEmpty && value.endsWith("|")) {
+
+  def removeTrailingLARPipe(value: String): String = {
+  if (!value.isEmpty && value.count(_ == '|') == 110 && value.endsWith("|")) {
+    value.init
+  } else {
+    value
+  }
+}
+  def removeTrailingTSPipe(value: String): String = {
+    if (!value.isEmpty && value.count(_ == '|') == 15 && value.endsWith("|")) {
       value.init
     } else {
       value
     }
-
+  }
   def removeBOM(value: String): String =
     if (!value.isEmpty && bomTypes.exists(value.contains(_)) ){
       bomTypes.foldLeft(value)((dataLine, str) => dataLine.replaceAll(str, ""))
