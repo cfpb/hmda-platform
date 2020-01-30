e46a8155be5b5f4dd0cfabf772299b3545d5c53a
PatrickHSI <pasata@healthstarinfo.com>
2020-01-30 02:53:57 -0500

    updating package ref in Q650Sepc

--- hmda/src/test/scala/hmda/validation/rules/lar/quality/_2020/Q650Spec.scala
+++ hmda/src/test/scala/hmda/validation/rules/lar/quality/_2020/Q650Spec.scala
@@ -8,7 +8,7 @@ import hmda.validation.rules.lar.LarEditCheckSpec
 class Q650Spec extends LarEditCheckSpec {
   override def check: EditCheck[LoanApplicationRegister] = Q650
 
-  property("Interest rate should note be greater than 0 but less than 0.5") {
+  property("Interest rate should not be greater than 0 but less than 0.5") {
     forAll(larGen) { lar =>
       lar.copy(loan = lar.loan.copy(interestRate = "8.5")).mustPass
       lar.copy(loan = lar.loan.copy(interestRate = "0.2")).mustFail
