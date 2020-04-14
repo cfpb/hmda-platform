422e18bfbf555dd73d12ee49aa8786349c40c26a
PatrickHSI <pasata@healthstarinfo.com>
2020-04-14 11:09:30 -0400

    creating common refernce to LAR entity files for slick database ops

---
+++ common/src/main/scala/hmda/model/pg/lar/LarEntity2018.scala
@@ -0,0 +1,193 @@
+package hmda.model.pg.lar
+
+import hmda.util.conversion.ColumnDataFormatter
+
+case class LarPartOne2018(id: Int = 0,
+                          lei: String = "",
+                          uli: String = "",
+                          applicationDate: String = "",
+                          loanType: Int = 0,
+                          loanPurpose: Int = 0,
+                          preapproval: Int = 0,
+                          constructionMethod: Int = 0,
+                          occupancyType: Int = 0,
+                          loanAmount: Double = 0.0,
+                          actionTakenType: Int = 0,
+                          actionTakenDate: Int = 0,
+                          street: String = "",
+                          city: String = "",
+                          state: String = "",
+                          zip: String = "",
+                          county: String = "",
+                          tract: String = "") {
+  def isEmpty: Boolean = lei == ""
+
+  def toRegulatorPSV: String = {
+    s"$id|$lei|$uli|$applicationDate|$loanType|" +
+      s"$loanPurpose|$preapproval|$constructionMethod|$occupancyType|" +
+      BigDecimal.valueOf(loanAmount).bigDecimal.toPlainString +
+      s"|$actionTakenType|$actionTakenDate|$street|$city|$state|" +
+      s"$zip|$county|$tract|"
+  }
+
+}
+case class LarPartTwo2018(ethnicityApplicant1: String = "",
+                          ethnicityApplicant2: String = "",
+                          ethnicityApplicant3: String = "",
+                          ethnicityApplicant4: String = "",
+                          ethnicityApplicant5: String = "",
+                          otherHispanicApplicant: String = "",
+                          ethnicityCoApplicant1: String = "",
+                          ethnicityCoApplicant2: String = "",
+                          ethnicityCoApplicant3: String = "",
+                          ethnicityCoApplicant4: String = "",
+                          ethnicityCoApplicant5: String = "",
+                          otherHispanicCoApplicant: String = "",
+                          ethnicityObservedApplicant: Int = 0,
+                          ethnicityObservedCoApplicant: Int = 0,
+                          raceApplicant1: String = "",
+                          raceApplicant2: String = "",
+                          raceApplicant3: String = "",
+                          raceApplicant4: String = "",
+                          raceApplicant5: String = "") {
+
+  def toRegulatorPSV: String = {
+    s"$ethnicityApplicant1|$ethnicityApplicant2|$ethnicityApplicant3|" +
+      s"$ethnicityApplicant4|$ethnicityApplicant5|$otherHispanicApplicant|$ethnicityCoApplicant1|" +
+      s"$ethnicityCoApplicant2|$ethnicityCoApplicant3|$ethnicityCoApplicant4|$ethnicityCoApplicant5|" +
+      s"$otherHispanicCoApplicant|$ethnicityObservedApplicant|$ethnicityObservedCoApplicant|$raceApplicant1" +
+      s"|$raceApplicant2|$raceApplicant3|$raceApplicant4|$raceApplicant5|"
+  }
+
+}
+
+case class LarPartThree2018(otherNativeRaceApplicant: String = "",
+                            otherAsianRaceApplicant: String = "",
+                            otherPacificRaceApplicant: String = "",
+                            raceCoApplicant1: String = "",
+                            raceCoApplicant2: String = "",
+                            raceCoApplicant3: String = "",
+                            raceCoApplicant4: String = "",
+                            raceCoApplicant5: String = "",
+                            otherNativeRaceCoApplicant: String = "",
+                            otherAsianRaceCoApplicant: String = "",
+                            otherPacificRaceCoApplicant: String = "",
+                            raceObservedApplicant: Int = 0,
+                            raceObservedCoApplicant: Int = 0,
+                            sexApplicant: Int = 0,
+                            sexCoApplicant: Int = 0,
+                            observedSexApplicant: Int = 0,
+                            observedSexCoApplicant: Int = 0,
+                            ageApplicant: Int = 0,
+                            ageCoApplicant: Int = 0,
+                            income: String = "") {
+
+  def toRegulatorPSV: String = {
+    s"$otherNativeRaceApplicant|" +
+      s"$otherAsianRaceApplicant|$otherPacificRaceApplicant|$raceCoApplicant1|$raceCoApplicant2|" +
+      s"$raceCoApplicant3|$raceCoApplicant4|$raceCoApplicant5|$otherNativeRaceCoApplicant|" +
+      s"$otherAsianRaceCoApplicant|$otherPacificRaceCoApplicant|$raceObservedApplicant|" +
+      s"$raceObservedCoApplicant|$sexApplicant|$sexCoApplicant|$observedSexApplicant|$observedSexCoApplicant|" +
+      s"$ageApplicant|$ageCoApplicant|$income|"
+  }
+
+}
+
+case class LarPartFour2018(purchaserType: Int = 0,
+                           rateSpread: String = "",
+                           hoepaStatus: Int = 0,
+                           lienStatus: Int = 0,
+                           creditScoreApplicant: Int = 0,
+                           creditScoreCoApplicant: Int = 0,
+                           creditScoreTypeApplicant: Int = 0,
+                           creditScoreModelApplicant: String = "",
+                           creditScoreTypeCoApplicant: Int = 0,
+                           creditScoreModelCoApplicant: String = "",
+                           denialReason1: String = "",
+                           denialReason2: String = "",
+                           denialReason3: String = "",
+                           denialReason4: String = "",
+                           otherDenialReason: String = "",
+                           totalLoanCosts: String = "",
+                           totalPoints: String = "",
+                           originationCharges: String = "") {
+
+  def toRegulatorPSV: String = {
+    s"$purchaserType|$rateSpread|$hoepaStatus|$lienStatus|" +
+      s"$creditScoreApplicant|$creditScoreCoApplicant|$creditScoreTypeApplicant|$creditScoreModelApplicant|" +
+      s"$creditScoreTypeCoApplicant|$creditScoreModelCoApplicant|" +
+      s"$denialReason1|$denialReason2|$denialReason3|$denialReason4|" +
+      s"$otherDenialReason|$totalLoanCosts|$totalPoints|$originationCharges|"
+  }
+
+}
+
+case class LarPartFive2018(discountPoints: String = "",
+                           lenderCredits: String = "",
+                           interestRate: String = "",
+                           paymentPenalty: String = "",
+                           debtToIncome: String = "",
+                           loanValueRatio: String = "",
+                           loanTerm: String = "",
+                           rateSpreadIntro: String = "",
+                           baloonPayment: Int = 0,
+                           insertOnlyPayment: Int = 0,
+                           amortization: Int = 0,
+                           otherAmortization: Int = 0,
+                           propertyValue: String = "",
+                           homeSecurityPolicy: Int = 0,
+                           landPropertyInterest: Int = 0,
+                           totalUnits: Int = 0,
+                           mfAffordable: String = "",
+                           applicationSubmission: Int = 0)
+    extends ColumnDataFormatter {
+
+  def toRegulatorPSV: String = {
+    s"$discountPoints|$lenderCredits|$interestRate|$paymentPenalty|$debtToIncome|$loanValueRatio|$loanTerm|" +
+      s"$rateSpreadIntro|$baloonPayment|$insertOnlyPayment|$amortization|$otherAmortization|" +
+      toBigDecimalString(propertyValue) + "|" +
+      s"$homeSecurityPolicy|$landPropertyInterest|$totalUnits|$mfAffordable|$applicationSubmission|"
+  }
+
+}
+
+case class LarPartSix2018(payable: Int = 0,
+                          nmls: String = "",
+                          aus1: String = "",
+                          aus2: String = "",
+                          aus3: String = "",
+                          aus4: String = "",
+                          aus5: String = "",
+                          otheraus: String = "",
+                          aus1Result: String = "",
+                          aus2Result: String = "",
+                          aus3Result: String = "",
+                          aus4Result: String = "",
+                          aus5Result: String = "",
+                          otherAusResult: String = "",
+                          reverseMortgage: Int = 0,
+                          lineOfCredits: Int = 0,
+                          businessOrCommercial: Int = 0) {
+
+  def toRegulatorPSV: String = {
+    s"$payable|" +
+      s"$nmls|$aus1|$aus2|$aus3|$aus4|$aus5|$otheraus|$aus1Result|$aus2Result|$aus3Result|$aus4Result|$aus5Result|" +
+      s"$otherAusResult|$reverseMortgage|$lineOfCredits|$businessOrCommercial"
+  }
+}
+
+case class LarEntityImpl2018(larPartOne: LarPartOne2018,
+                             larPartTwo: LarPartTwo2018,
+                             larPartThree: LarPartThree2018,
+                             larPartFour: LarPartFour2018,
+                             larPartFive: LarPartFive2018,
+                             larPartSix: LarPartSix2018) {
+
+  def toRegulatorPSV: String =
+    (larPartOne.toRegulatorPSV +
+      larPartTwo.toRegulatorPSV +
+      larPartThree.toRegulatorPSV +
+      larPartFour.toRegulatorPSV +
+      larPartFive.toRegulatorPSV +
+      larPartSix.toRegulatorPSV).replaceAll("(\r\n)|\r|\n", "")
+}
---
+++ common/src/main/scala/hmda/model/pg/lar/ModifiedLarEntity.scala
@@ -0,0 +1,213 @@
+package hmda.model.pg.lar
+
+import hmda.util.conversion.ColumnDataFormatter
+
+case class ModifiedLarPartOne(
+    filingYear: Option[Int] = Some(0),
+    lei: String = "",
+    msaMd: Option[Int] = Some(0),
+    state: Option[String] = Some(""),
+    county: Option[String] = Some(""),
+    tract: Option[String] = Some(""),
+    conformingLoanLimit: Option[String] = Some(""),
+    loanFlag: Option[String] = Some(""),
+    loanProductType: Option[String] = Some(""),
+    dwellingCategory: Option[String] = Some(""),
+    ethnicityCategorization: Option[String] = Some(""),
+    raceCategorization: Option[String] = Some(""),
+    sexCategorization: Option[String] = Some(""),
+    actionTakenType: Option[Int] = Some(0),
+    purchaserType: Option[Int] = Some(0),
+    preapproval: Option[Int] = Some(0),
+    loanType: Option[Int] = Some(0),
+    loanPurpose: Option[Int] = Some(0),
+    lienStatus: Option[Int] = Some(0)
+) extends ColumnDataFormatter {
+  def isEmpty: Boolean = lei == ""
+
+  def toPublicPSV: String = {
+    s"${extractOpt(filingYear)}|$lei|${extractOpt(msaMd)}|${extractOpt(state)}|${extractOpt(county)}|" +
+      s"${extractOpt(tract)}|${extractOpt(conformingLoanLimit)}|${extractOpt(
+        loanProductType)}|${extractOpt(dwellingCategory)}|${extractOpt(ethnicityCategorization)}|" +
+      s"${extractOpt(raceCategorization)}|${extractOpt(sexCategorization)}|${extractOpt(
+        actionTakenType)}|${extractOpt(purchaserType)}|" +
+      s"${extractOpt(preapproval)}|${extractOpt(loanType)}|${extractOpt(
+        loanPurpose)}|${extractOpt(lienStatus)}|"
+  }
+
+}
+case class ModifiedLarPartTwo(reverseMortgage: Option[Int] = Some(0),
+                              lineOfCredits: Option[Int] = Some(0),
+                              businessOrCommercial: Option[Int] = Some(0),
+                              loanAmount: Double = 0.0,
+                              loanValueRatio: Option[String] = Some(""),
+                              interestRate: Option[String] = Some(""),
+                              rateSpread: Option[String] = Some(""),
+                              hoepaStatus: Option[Int] = Some(0),
+                              totalLoanCosts: Option[String] = Some(""),
+                              totalPoints: Option[String] = Some(""),
+                              originationCharges: Option[String] = Some(""),
+                              discountPoints: Option[String] = Some(""),
+                              lenderCredits: Option[String] = Some(""),
+                              loanTerm: Option[String] = Some(""),
+                              paymentPenalty: Option[String] = Some(""),
+                              rateSpreadIntro: Option[String] = Some(""),
+                              amortization: Option[Int] = Some(0),
+                              insertOnlyPayment: Option[Int] = Some(0))
+    extends ColumnDataFormatter {
+
+  def toPublicPSV: String = {
+    s"${extractOpt(reverseMortgage)}|${extractOpt(lineOfCredits)}|${extractOpt(businessOrCommercial)}|" +
+      BigDecimal.valueOf(loanAmount).bigDecimal.toPlainString +
+      s"|${extractOpt(loanValueRatio)}|${extractOpt(interestRate)}|${extractOpt(
+        rateSpread)}|${extractOpt(hoepaStatus)}|" +
+      s"${extractOpt(totalLoanCosts)}|${extractOpt(totalPoints)}|${extractOpt(
+        originationCharges)}|${extractOpt(discountPoints)}|" +
+      s"${extractOpt(lenderCredits)}|${extractOpt(loanTerm)}|${extractOpt(
+        paymentPenalty)}|${extractOpt(rateSpreadIntro)}" +
+      s"|${extractOpt(amortization)}|${extractOpt(insertOnlyPayment)}|"
+  }
+
+}
+
+case class ModifiedLarPartThree(baloonPayment: Option[Int] = Some(0),
+                                otherAmortization: Option[Int] = Some(0),
+                                propertyValue: String = "",
+                                constructionMethod: Option[String] = Some(""),
+                                occupancyType: Option[Int] = Some(0),
+                                homeSecurityPolicy: Option[Int] = Some(0),
+                                landPropertyInterest: Option[Int] = Some(0),
+                                totalUnits: Option[String] = Some(""),
+                                mfAffordable: Option[String] = Some(""),
+                                income: Option[String] = Some(""),
+                                debtToIncome: Option[String] = Some(""),
+                                creditScoreTypeApplicant: Option[Int] = Some(0),
+                                creditScoreTypeCoApplicant: Option[Int] = Some(
+                                  0),
+                                ethnicityApplicant1: Option[String] = Some(""),
+                                ethnicityApplicant2: Option[String] = Some(""),
+                                ethnicityApplicant3: Option[String] = Some(""),
+                                ethnicityApplicant4: Option[String] = Some(""))
+    extends ColumnDataFormatter {
+
+  def toPublicPSV: String = {
+    s"${extractOpt(baloonPayment)}|" +
+      s"${extractOpt(otherAmortization)}|" +
+      toBigDecimalString(propertyValue) + "|" +
+      s"${extractOpt(constructionMethod)}|${extractOpt(occupancyType)}|" +
+      s"${extractOpt(homeSecurityPolicy)}|${extractOpt(landPropertyInterest)}|${extractOpt(
+        totalUnits)}|${extractOpt(mfAffordable)}|" +
+      s"${extractOpt(income)}|${extractOpt(debtToIncome)}|${extractOpt(creditScoreTypeApplicant)}|" +
+      s"${extractOpt(creditScoreTypeCoApplicant)}|${extractOpt(
+        ethnicityApplicant1)}|${extractOpt(ethnicityApplicant2)}" +
+      s"|${extractOpt(ethnicityApplicant3)}|${extractOpt(ethnicityApplicant4)}|"
+  }
+
+}
+
+case class ModifiedLarPartFour(
+    ethnicityApplicant5: Option[String] = Some(""),
+    ethnicityCoApplicant1: Option[String] = Some(""),
+    ethnicityCoApplicant2: Option[String] = Some(""),
+    ethnicityCoApplicant3: Option[String] = Some(""),
+    ethnicityCoApplicant4: Option[String] = Some(""),
+    ethnicityCoApplicant5: Option[String] = Some(""),
+    ethnicityObservedApplicant: Option[Int] = Some(0),
+    ethnicityObservedCoApplicant: Option[Int] = Some(0),
+    raceApplicant1: Option[String] = Some(""),
+    raceApplicant2: Option[String] = Some(""),
+    raceApplicant3: Option[String] = Some(""),
+    raceApplicant4: Option[String] = Some(""),
+    raceApplicant5: Option[String] = Some(""),
+    raceCoApplicant1: Option[String] = Some(""),
+    raceCoApplicant2: Option[String] = Some(""),
+    raceCoApplicant3: Option[String] = Some(""),
+    raceCoApplicant4: Option[String] = Some(""))
+    extends ColumnDataFormatter {
+
+  def toPublicPSV: String = {
+    s"${extractOpt(ethnicityApplicant5)}|${extractOpt(ethnicityCoApplicant1)}|${extractOpt(
+      ethnicityCoApplicant2)}|${extractOpt(ethnicityCoApplicant3)}|" +
+      s"${extractOpt(ethnicityCoApplicant4)}|${extractOpt(ethnicityCoApplicant5)}|${extractOpt(
+        ethnicityObservedApplicant)}|${extractOpt(ethnicityObservedCoApplicant)}|" +
+      s"${extractOpt(raceApplicant1)}|${extractOpt(raceApplicant2)}|" +
+      s"${extractOpt(raceApplicant3)}|${extractOpt(raceApplicant4)}|${extractOpt(
+        raceApplicant5)}|${extractOpt(raceCoApplicant1)}|" +
+      s"${extractOpt(raceCoApplicant2)}|${extractOpt(raceCoApplicant3)}|${extractOpt(raceCoApplicant4)}|"
+  }
+
+}
+
+case class ModifiedLarPartFive(
+    raceCoApplicant5: Option[String] = Some(""),
+    raceObservedApplicant: Option[Int] = Some(0),
+    raceObservedCoApplicant: Option[Int] = Some(0),
+    sexApplicant: Option[Int] = Some(0),
+    sexCoApplicant: Option[Int] = Some(0),
+    observedSexApplicant: Option[Int] = Some(0),
+    observedSexCoApplicant: Option[Int] = Some(0),
+    ageApplicant: Option[String] = Some(""),
+    ageCoApplicant: Option[String] = Some(""),
+    applicantAgeGreaterThan62: Option[String] = Some(""),
+    coapplicantAgeGreaterThan62: Option[String] = Some(""),
+    applicationSubmission: Option[Int] = Some(0),
+    payable: Option[Int] = Some(0),
+    aus1: Option[Int] = Some(0),
+    aus2: Option[Int] = Some(0),
+    aus3: Option[Int] = Some(0),
+    aus4: Option[Int] = Some(0))
+    extends ColumnDataFormatter {
+
+  def toPublicPSV: String = {
+    s"${extractOpt(raceCoApplicant5)}|${extractOpt(raceObservedApplicant)}|${extractOpt(
+      raceObservedCoApplicant)}|${extractOpt(sexApplicant)}|${extractOpt(
+      sexCoApplicant)}|${extractOpt(observedSexApplicant)}|${extractOpt(observedSexCoApplicant)}|" +
+      s"${extractOpt(ageApplicant)}|${extractOpt(ageCoApplicant)}|${extractOpt(applicantAgeGreaterThan62)}|${extractOpt(
+        coapplicantAgeGreaterThan62)}|${extractOpt(applicationSubmission)}|" +
+      s"${extractOpt(payable)}|${extractOpt(aus1)}|${extractOpt(aus2)}|${extractOpt(
+        aus3)}|${extractOpt(aus4)}|"
+  }
+
+}
+
+case class ModifiedLarPartSix(aus5: Option[Int] = Some(0),
+                              denialReason1: Option[Int] = Some(0),
+                              denialReason2: Option[Int] = Some(0),
+                              denialReason3: Option[Int] = Some(0),
+                              denialReason4: Option[Int] = Some(0),
+                              population: Option[String] = Some(""),
+                              minorityPopulationPercent: Option[String] = Some(
+                                ""),
+                              ffiecMedFamIncome: Option[String] = Some(""),
+                              medianIncomePercentage: Option[Int] = Some(0),
+                              ownerOccupiedUnits: Option[String] = Some(""),
+                              oneToFourFamUnits: Option[String] = Some(""),
+                              medianAge: Option[Int] = Some(0))
+    extends ColumnDataFormatter {
+
+  def toPublicPSV: String = {
+    s"${extractOpt(aus5)}|" +
+      s"${extractOpt(denialReason1)}|${extractOpt(denialReason2)}|${extractOpt(
+        denialReason3)}|${extractOpt(denialReason4)}|${extractOpt(population)}|" +
+      s"${extractOpt(minorityPopulationPercent)}|${extractOpt(ffiecMedFamIncome)}|${extractOpt(medianIncomePercentage)}|" +
+      s"${extractOpt(ownerOccupiedUnits)}|${extractOpt(oneToFourFamUnits)}|${extractOpt(medianAge)}"
+  }
+
+}
+
+case class ModifiedLarEntityImpl(mlarPartOne: ModifiedLarPartOne,
+                                 mlarPartTwo: ModifiedLarPartTwo,
+                                 mlarPartThree: ModifiedLarPartThree,
+                                 mlarPartFour: ModifiedLarPartFour,
+                                 mlarPartFive: ModifiedLarPartFive,
+                                 mlarPartSix: ModifiedLarPartSix) {
+
+  def toPublicPSV: String =
+    (mlarPartOne.toPublicPSV +
+      mlarPartTwo.toPublicPSV +
+      mlarPartThree.toPublicPSV +
+      mlarPartFour.toPublicPSV +
+      mlarPartFive.toPublicPSV +
+      mlarPartSix.toPublicPSV).replaceAll("(\r\n)|\r|\n", "")
+
+}
