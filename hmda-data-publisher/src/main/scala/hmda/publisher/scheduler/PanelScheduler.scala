244213a4be7e03e34f42b2b83e8775aef7efd91d
bsharma <bsharma@healthstarinfo.com>
2020-04-27 06:53:56 -0400

    Finished porting the entire repo to 2.6

--- hmda-data-publisher/src/main/scala/hmda/publisher/scheduler/PanelScheduler.scala
+++ hmda-data-publisher/src/main/scala/hmda/publisher/scheduler/PanelScheduler.scala
@@ -3,64 +3,56 @@ package hmda.publisher.scheduler
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-import akka.stream.ActorMaterializer
+import akka.stream.Materializer
 import akka.stream.alpakka.s3.ApiVersion.ListBucketVersion2
 import akka.stream.alpakka.s3.scaladsl.S3
-import akka.stream.alpakka.s3.{MemoryBufferType, MultipartUploadResult, S3Attributes, S3Settings}
+import akka.stream.alpakka.s3.{ MemoryBufferType, MultipartUploadResult, S3Attributes, S3Settings }
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
-import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
-import com.amazonaws.regions.AwsRegionProvider
 import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension
 import com.typesafe.config.ConfigFactory
 import hmda.actor.HmdaActor
-import hmda.util.BankFilterUtils._
+import hmda.publisher.query.component.{ InstitutionEmailComponent, PublisherComponent2018, PublisherComponent2019 }
+import hmda.publisher.query.panel.{ InstitutionAltEntity, InstitutionEmailEntity, InstitutionEntity }
+import hmda.publisher.scheduler.schedules.Schedules.{ PanelScheduler2018, PanelScheduler2019 }
 import hmda.query.DbConfiguration.dbConfig
-import hmda.publisher.query.component.{InstitutionEmailComponent, PublisherComponent2018, PublisherComponent2019}
-import hmda.publisher.query.panel.{InstitutionAltEntity, InstitutionEmailEntity, InstitutionEntity}
-import hmda.publisher.scheduler.schedules.Schedules.{PanelScheduler2018, PanelScheduler2019}
+import hmda.util.BankFilterUtils._
+import software.amazon.awssdk.auth.credentials.{ AwsBasicCredentials, StaticCredentialsProvider }
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.regions.providers.AwsRegionProvider
 
-import scala.concurrent.Future
-import scala.util.{Failure, Success}
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success }
 
-class PanelScheduler
-    extends HmdaActor
-    with PublisherComponent2018
-    with PublisherComponent2019
-      with InstitutionEmailComponent {
+class PanelScheduler extends HmdaActor with PublisherComponent2018 with PublisherComponent2019 with InstitutionEmailComponent {
 
-  implicit val ec = context.system.dispatcher
-  implicit val materializer = ActorMaterializer()
-  private val fullDate = DateTimeFormatter.ofPattern("yyyy-MM-dd-")
-  def institutionRepository2018 = new InstitutionRepository2018(dbConfig)
-  def institutionRepository2019 = new InstitutionRepository2019(dbConfig)
-  def emailRepository = new InstitutionEmailsRepository2018(dbConfig)
+  implicit val ec: ExecutionContext       = context.system.dispatcher
+  implicit val materializer: Materializer = Materializer(context)
+  private val fullDate                    = DateTimeFormatter.ofPattern("yyyy-MM-dd-")
+  def institutionRepository2018           = new InstitutionRepository2018(dbConfig)
+  def institutionRepository2019           = new InstitutionRepository2019(dbConfig)
+  def emailRepository                     = new InstitutionEmailsRepository2018(dbConfig)
 
   val awsConfig =
     ConfigFactory.load("application.conf").getConfig("private-aws")
-  val accessKeyId = awsConfig.getString("private-access-key-id")
+  val accessKeyId  = awsConfig.getString("private-access-key-id")
   val secretAccess = awsConfig.getString("private-secret-access-key ")
-  val region = awsConfig.getString("private-region")
-  val bucket = awsConfig.getString("private-s3-bucket")
-  val environment = awsConfig.getString("private-environment")
-  val year = awsConfig.getString("private-year")
+  val region       = awsConfig.getString("private-region")
+  val bucket       = awsConfig.getString("private-s3-bucket")
+  val environment  = awsConfig.getString("private-environment")
+  val year         = awsConfig.getString("private-year")
 
-  val awsCredentialsProvider = new AWSStaticCredentialsProvider(
-    new BasicAWSCredentials(accessKeyId, secretAccess))
+  val awsCredentialsProvider = StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretAccess))
 
-  val awsRegionProvider = new AwsRegionProvider {
-    override def getRegion: String = region
-  }
+  val awsRegionProvider: AwsRegionProvider = () => Region.of(region)
 
-  val s3Settings =
-    S3Settings(
-      MemoryBufferType,
-      awsCredentialsProvider,
-      awsRegionProvider,
-      ListBucketVersion2
-    )
+  val s3Settings = S3Settings(context.system)
+    .withBufferType(MemoryBufferType)
+    .withCredentialsProvider(awsCredentialsProvider)
+    .withS3RegionProvider(awsRegionProvider)
+    .withListBucketApiVersion(ListBucketVersion2)
 
-  override def preStart() = {
+  override def preStart(): Unit = {
     QuartzSchedulerExtension(context.system)
       .schedule("PanelScheduler2018", self, PanelScheduler2018)
     QuartzSchedulerExtension(context.system)
@@ -68,7 +60,7 @@ class PanelScheduler
 
   }
 
-  override def postStop() = {
+  override def postStop(): Unit = {
     QuartzSchedulerExtension(context.system).cancelJob("PanelScheduler2018")
     QuartzSchedulerExtension(context.system).cancelJob("PanelScheduler2019")
 
@@ -77,133 +69,121 @@ class PanelScheduler
   override def receive: Receive = {
     case PanelScheduler2018 =>
       println("Panel test 2018")
-      panelSync2018
+      panelSync2018()
 
     case PanelScheduler2019 =>
       println("Panel test 2019")
-      panelSync2019
+      panelSync2019()
 
   }
 
-  private def panelSync2018() = {
-    log.info("starting job for panelSync2018 ")
+  private def panelSync2018(): Unit = {
 
     val allResults: Future[Seq[InstitutionEntity]] =
       institutionRepository2018.findActiveFilers(getFilterList())
-    val now = LocalDateTime.now().minusDays(1)
+    val now           = LocalDateTime.now().minusDays(1)
     val formattedDate = fullDate.format(now)
-    val fileName = s"$formattedDate" + "2018_panel.txt"
+    val fileName      = s"$formattedDate" + "2018_panel.txt"
     val s3Sink =
       S3.multipartUpload(bucket, s"$environment/panel/$fileName")
         .withAttributes(S3Attributes.settings(s3Settings))
     val results: Future[MultipartUploadResult] = Source
-      .fromFuture(allResults)
-      .map(seek => seek.toList)
-      .mapConcat(identity)
+      .future(allResults)
+      .mapConcat(seek => seek.toList)
       .mapAsync(1)(institution => appendEmailDomains2018(institution))
       .map(institution => institution.toPSV + "\n")
       .map(s => ByteString(s))
       .runWith(s3Sink)
 
     results onComplete {
-      case Success(result) => {
-        log.info(
-          "Pushed to S3: " + s"$bucket/$environment/panel/$fileName" + ".")
-      }
+      case Success(result) =>
+        log.info("Pushed to S3: " + s"$bucket/$environment/panel/$fileName" + ".")
       case Failure(t) =>
-        println(
-          "An error has occurred getting Panel Data 2018: " + t.getMessage)
+        println("An error has occurred getting Panel Data 2018: " + t.getMessage)
     }
   }
 
-  private def panelSync2019() = {
-    log.info("starting job for panelSync2019 ")
+  private def panelSync2019(): Unit = {
 
     val allResults: Future[Seq[InstitutionEntity]] =
       institutionRepository2019.findActiveFilers(getFilterList())
-    val now = LocalDateTime.now().minusDays(1)
+    val now           = LocalDateTime.now().minusDays(1)
     val formattedDate = fullDate.format(now)
-    val fileName = s"$formattedDate" + "2019_panel.txt"
+    val fileName      = s"$formattedDate" + "2019_panel.txt"
     val s3Sink =
       S3.multipartUpload(bucket, s"$environment/panel/$fileName")
         .withAttributes(S3Attributes.settings(s3Settings))
     val results: Future[MultipartUploadResult] = Source
-      .fromFuture(allResults)
-      .map(seek => seek.toList)
-      .mapConcat(identity)
+      .future(allResults)
+      .mapConcat(seek => seek.toList)
       .mapAsync(1)(institution => appendEmailDomains(institution))
       .map(institution => institution.toPSV + "\n")
       .map(s => ByteString(s))
       .runWith(s3Sink)
 
     results onComplete {
-      case Success(result) => {
-        log.info(
-          "Pushed to S3: " + s"$bucket/$environment/panel/$fileName" + ".")
-      }
+      case Success(result) =>
+        log.info("Pushed to S3: " + s"$bucket/$environment/panel/$fileName" + ".")
+
       case Failure(t) =>
-        println(
-          "An error has occurred getting Panel Data 2019: " + t.getMessage)
+        println("An error has occurred getting Panel Data 2019: " + t.getMessage)
     }
   }
 
-  def appendEmailDomains2018(
-      institution: InstitutionEntity): Future[InstitutionAltEntity] = {
+  def appendEmailDomains2018(institution: InstitutionEntity): Future[InstitutionAltEntity] = {
 
     val emails: Future[Seq[InstitutionEmailEntity]] =
       emailRepository.findByLei(institution.lei)
 
-    emails.map(
-      emailList =>
-        InstitutionAltEntity(
-          lei = institution.lei,
-          activityYear = institution.activityYear,
-          agency = institution.agency,
-          institutionType = institution.institutionType,
-          id2017 = institution.id2017,
-          taxId = institution.taxId,
-          rssd = institution.rssd,
-          respondentName = institution.respondentName,
-          respondentState = institution.respondentState,
-          respondentCity = institution.respondentCity,
-          parentIdRssd = institution.parentIdRssd,
-          parentName = institution.parentName,
-          assets = institution.assets,
-          otherLenderCode = institution.otherLenderCode,
-          topHolderIdRssd = institution.topHolderIdRssd,
-          topHolderName = institution.topHolderName,
-          hmdaFiler = institution.hmdaFiler,
-          emailDomains = emailList.map(email => email.emailDomain).mkString(",")
-      ))
+    emails.map(emailList =>
+      InstitutionAltEntity(
+        lei = institution.lei,
+        activityYear = institution.activityYear,
+        agency = institution.agency,
+        institutionType = institution.institutionType,
+        id2017 = institution.id2017,
+        taxId = institution.taxId,
+        rssd = institution.rssd,
+        respondentName = institution.respondentName,
+        respondentState = institution.respondentState,
+        respondentCity = institution.respondentCity,
+        parentIdRssd = institution.parentIdRssd,
+        parentName = institution.parentName,
+        assets = institution.assets,
+        otherLenderCode = institution.otherLenderCode,
+        topHolderIdRssd = institution.topHolderIdRssd,
+        topHolderName = institution.topHolderName,
+        hmdaFiler = institution.hmdaFiler,
+        emailDomains = emailList.map(email => email.emailDomain).mkString(",")
+      )
+    )
   }
 
-  def appendEmailDomains(
-      institution: InstitutionEntity): Future[InstitutionAltEntity] = {
-
+  def appendEmailDomains(institution: InstitutionEntity): Future[InstitutionAltEntity] = {
     val emails: Future[Seq[InstitutionEmailEntity]] =
       emailRepository.findByLei(institution.lei)
 
-    emails.map(
-      emailList =>
-        InstitutionAltEntity(
-          lei = institution.lei,
-          activityYear = institution.activityYear,
-          agency = institution.agency,
-          institutionType = institution.institutionType,
-          id2017 = institution.id2017,
-          taxId = institution.taxId,
-          rssd = institution.rssd,
-          respondentName = institution.respondentName,
-          respondentState = institution.respondentState,
-          respondentCity = institution.respondentCity,
-          parentIdRssd = institution.parentIdRssd,
-          parentName = institution.parentName,
-          assets = institution.assets,
-          otherLenderCode = institution.otherLenderCode,
-          topHolderIdRssd = institution.topHolderIdRssd,
-          topHolderName = institution.topHolderName,
-          hmdaFiler = institution.hmdaFiler,
-          emailDomains = emailList.map(email => email.emailDomain).mkString(",")
-      ))
+    emails.map(emailList =>
+      InstitutionAltEntity(
+        lei = institution.lei,
+        activityYear = institution.activityYear,
+        agency = institution.agency,
+        institutionType = institution.institutionType,
+        id2017 = institution.id2017,
+        taxId = institution.taxId,
+        rssd = institution.rssd,
+        respondentName = institution.respondentName,
+        respondentState = institution.respondentState,
+        respondentCity = institution.respondentCity,
+        parentIdRssd = institution.parentIdRssd,
+        parentName = institution.parentName,
+        assets = institution.assets,
+        otherLenderCode = institution.otherLenderCode,
+        topHolderIdRssd = institution.topHolderIdRssd,
+        topHolderName = institution.topHolderName,
+        hmdaFiler = institution.hmdaFiler,
+        emailDomains = emailList.map(email => email.emailDomain).mkString(",")
+      )
+    )
   }
 }
--- institutions-api/src/main/scala/hmda/institution/HmdaInstitutionApi.scala
+++ institutions-api/src/main/scala/hmda/institution/HmdaInstitutionApi.scala
@@ -2,28 +2,22 @@ package hmda.institution
 
 import akka.Done
 import akka.actor.ActorSystem
+import akka.actor.typed.scaladsl.adapter._
+import akka.kafka.scaladsl.Consumer.DrainingControl
+import akka.kafka.scaladsl.{ Committer, Consumer }
+import akka.kafka.{ CommitterSettings, ConsumerSettings, Subscriptions }
+import akka.stream.Materializer
+import akka.stream.scaladsl.{ Keep, Sink, Source }
 import com.typesafe.config.ConfigFactory
 import hmda.institution.api.http.HmdaInstitutionQueryApi
-import org.slf4j.LoggerFactory
-import akka.actor.typed.scaladsl.adapter._
-import akka.kafka.scaladsl.Consumer
-import akka.kafka.scaladsl.Consumer.DrainingControl
-import akka.kafka.{ConsumerSettings, Subscriptions}
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Keep, Sink, Source}
-import hmda.institution.projection.{InstitutionDBProjection, ProjectEvent}
-import hmda.messages.institution.InstitutionEvents.{
-  InstitutionCreated,
-  InstitutionDeleted,
-  InstitutionKafkaEvent,
-  InstitutionModified
-}
+import hmda.institution.projection.{ InstitutionDBProjection, ProjectEvent }
+import hmda.messages.institution.InstitutionEvents.{ InstitutionCreated, InstitutionDeleted, InstitutionKafkaEvent, InstitutionModified }
+import hmda.messages.pubsub.{ HmdaGroups, HmdaTopics }
 import hmda.publication.KafkaUtils.kafkaHosts
-import hmda.messages.pubsub.HmdaTopics
-import hmda.messages.pubsub.HmdaGroups
 import hmda.serialization.kafka.InstitutionKafkaEventsDeserializer
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.Future
 
@@ -31,21 +25,20 @@ object HmdaInstitutionApi extends App {
 
   val log = LoggerFactory.getLogger("hmda")
 
-  log.info(
-    """
-      | _____          _   _ _         _   _                    ___  ______ _____
-      ||_   _|        | | (_) |       | | (_)                  / _ \ | ___ \_   _|
-      |  | | _ __  ___| |_ _| |_ _   _| |_ _  ___  _ __  ___  / /_\ \| |_/ / | |
-      |  | || '_ \/ __| __| | __| | | | __| |/ _ \| '_ \/ __| |  _  ||  __/  | |
-      | _| || | | \__ \ |_| | |_| |_| | |_| | (_) | | | \__ \ | | | || |    _| |_
-      | \___/_| |_|___/\__|_|\__|\__,_|\__|_|\___/|_| |_|___/ \_| |_/\_|    \___/
+  log.info("""
+             | _____          _   _ _         _   _                    ___  ______ _____
+             ||_   _|        | | (_) |       | | (_)                  / _ \ | ___ \_   _|
+             |  | | _ __  ___| |_ _| |_ _   _| |_ _  ___  _ __  ___  / /_\ \| |_/ / | |
+             |  | || '_ \/ __| __| | __| | | | __| |/ _ \| '_ \/ __| |  _  ||  __/  | |
+             | _| || | | \__ \ |_| | |_| |_| | |_| | (_) | | | \__ \ | | | || |    _| |_
+             | \___/_| |_|___/\__|_|\__|\__,_|\__|_|\___/|_| |_|___/ \_| |_/\_|    \___/
     """.stripMargin)
 
   val config = ConfigFactory.load()
 
-  implicit val system = ActorSystem("hmda-institutions")
-  implicit val materializer = ActorMaterializer()
-  implicit val ec = system.dispatcher
+  implicit val system       = ActorSystem("hmda-institutions")
+  implicit val materializer = Materializer(system)
+  implicit val ec           = system.dispatcher
 
   val host = config.getString("hmda.institution.http.host")
   val port = config.getString("hmda.institution.http.port")
@@ -55,31 +48,25 @@ object HmdaInstitutionApi extends App {
   val jdbcUrl = config.getString("db.db.url")
   log.info(s"Connection URL is \n\n$jdbcUrl\n")
 
-  system.actorOf(HmdaInstitutionQueryApi.props(), "hmda-institutions-api")
+  system.spawn[Nothing](HmdaInstitutionQueryApi(), HmdaInstitutionQueryApi.name)
 
   val consumerSettings: ConsumerSettings[String, InstitutionKafkaEvent] =
-    ConsumerSettings(kafkaConfig,
-                     new StringDeserializer,
-                     new InstitutionKafkaEventsDeserializer)
+    ConsumerSettings(kafkaConfig, new StringDeserializer, new InstitutionKafkaEventsDeserializer)
       .withBootstrapServers(kafkaHosts)
       .withGroupId(HmdaGroups.institutionsGroup)
       .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
-  Consumer
-    .committableSource(consumerSettings,
-                       Subscriptions.topics(HmdaTopics.institutionTopic))
-    .map { msg =>
-      processData(msg.record.value()).map(_ => msg.committableOffset)
-    }
-    .map(offsetF => offsetF.map(offset => offset.commitScaladsl()))
-    .toMat(Sink.seq)(Keep.both)
+  val control: DrainingControl[Done] = Consumer
+    .committableSource(consumerSettings, Subscriptions.topics(HmdaTopics.institutionTopic))
+    .mapAsync(1)(msg => processData(msg.record.value()).map(_ => msg.committableOffset))
+    .toMat(Committer.sink(CommitterSettings(system).withParallelism(1)))(Keep.both)
     .mapMaterializedValue(DrainingControl.apply)
     .run()
 
   val institutionDBProjector =
     system.spawn(InstitutionDBProjection.behavior, InstitutionDBProjection.name)
 
-  def processData(evt: InstitutionKafkaEvent): Future[Done] = {
+  def processData(evt: InstitutionKafkaEvent): Future[Done] =
     Source
       .single(evt)
       .map { evt =>
@@ -96,6 +83,5 @@ object HmdaInstitutionApi extends App {
       }
       .toMat(Sink.ignore)(Keep.right)
       .run()
-  }
 
 }
