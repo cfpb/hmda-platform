cb72180a774fead03d3152b9ec001c2e6f94472c
Barak Stout <barakstout@gmail.com>
2020-07-06 14:02:39 -0400

    WIP Newman Test

--- newman/README.md
+++ newman/README.md
@@ -15,7 +15,7 @@
 
 `kubectl config use-context <dev-context>`
 
-```
+```sh
 helm install  --namespace=newman  \
 --set env.KC_UN=$KC_UN \
 --set env.KC_PW=$KC_PW \
@@ -34,7 +34,7 @@ helm install  --namespace=newman  \
 
 ### Newman as a docker container
 * Run docker container
-```
+```sh
 docker run -it -e KC_UN=$KC_UN -e KC_PW=$KC_PW -e KC_URL=$KC_URL -e KC_CLIENT_ID=$KC_CLIENT_ID \
 -e HOST_FILING=$HOST_FILING -e HOST_ADMIN=$HOST_ADMIN -e HOST_PUBLIC=$HOST_PUBLIC -e HMDA_ENV=$HMDA_ENV \
 -e MM_HOOK=$MM_HOOK hmda/newman-automation:latest /bin/sh
@@ -77,7 +77,13 @@ curl --location --request PUT $HOST_ADMIN'/institutions' \
 }'
 ```
 * Run newmantest
-```
+```sh
  ./scripts/newmanFiling.sh $KC_UN $KC_PW $KC_URL $KC_CLIENT_ID $HOST_FILING $HOST_ADMIN \
  $HOST_PUBLIC $HMDA_ENV $MM_HOOK $NEWMAN_NOTIFY
+```
+
+### Running via Docker Compose
+You can use `docker-compose` to run a single Newman filing test. If the `HOST_FILING` url is set to `http://localhost:8080`, the script will bypess getting an authentication token.
+```sh
+docker-compose up
 ```
--- newman/scripts/run_one_newman.sh
+++ newman/scripts/run_one_newman.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+#
+# Run ./run_one_newman.sh 
 
-authToken=$(./scripts/authTokenGen.sh $KC_UN $KC_PW $KC_URL $KC_CLIENT_ID) && \
+echo env
+echo $HOST_FILING
+
+# if [ $HOST_FILING = 'hmda-platform:8080' ]; then 
+# 	echo 'localhost config detected'
+# else 
+# 	echo 'running against server'
+# 	authToken=$(./scripts/authTokenGen.sh $KC_UN $KC_PW $KC_URL $KC_CLIENT_ID)
+# fi
+
+CONFIG_PATH="tests/hmda-filing/configs/$NEWMAN_YEAR/test_no_m_no_q/hmda-filing-api-config.json"
+
+# curl --location --request PUT $HOST_ADMIN'/institutions' --header 'Content-Type: application/json' --header "Authorization: Bearer $authToken" --data-raw '{ "activityYear": 2019, "lei": "NEWMANLEITEST1234678", "agency": 9, "institutionType": -1, "institutionId2017": "", "taxId": "84-7652134", "rssd": -1, "emailDomains": [ "newmantest.com" ], "respondent": { "name": "Newman Bank", "state": "", "city": "" }, "parent": { "idRssd": -1, "name": "" }, "assets": 5, "otherLenderCode": -1, "topHolder": { "idRssd": -1, "name": "" }, "hmdaFiler": true, "quarterlyFiler": true, "quarterlyFilerHasFiledQ1": false, "quarterlyFilerHasFiledQ2": false, "quarterlyFilerHasFiledQ3": false }'
+curl --location --request PUT $HOST_ADMIN'/institutions' --header 'Content-Type: application/json' --data-raw '{ "activityYear": 2019, "lei": "NEWMANLEITEST1234678", "agency": 9, "institutionType": -1, "institutionId2017": "", "taxId": "84-7652134", "rssd": -1, "emailDomains": [ "newmantest.com" ], "respondent": { "name": "Newman Bank", "state": "", "city": "" }, "parent": { "idRssd": -1, "name": "" }, "assets": 5, "otherLenderCode": -1, "topHolder": { "idRssd": -1, "name": "" }, "hmdaFiler": true, "quarterlyFiler": true, "quarterlyFilerHasFiledQ1": false, "quarterlyFilerHasFiledQ2": false, "quarterlyFilerHasFiledQ3": false }'
+
 newman run tests/hmda-filing/tests/test_no_m_no_q/hmda-filing-api-test.json -d \
-tests/hmda-filing/configs/$YEAR/test_no_m_no_q/hmda-filing-api-config.json \
+$CONFIG_PATH \
 --env-var host_filing=$HOST_FILING \
 --env-var host_admin_api=$HOST_ADMIN \
---env-var host_public=$HOST_PUBLIC \
---env-var authToken="Bearer $authToken" \
---bail
+--env-var host_public=$HOST_PUBLIC  
+
+echo "done"
