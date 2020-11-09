ee96507e7e6ce41ad4d58216013c83c377d8cf7d
Barak Stout <barakstout@gmail.com>
2020-11-09 13:50:19 -0500

    updates to larString and emLarString functions to match

--- common/src/main/scala/hmda/util/conversion/LarStringFormatter.scala
+++ common/src/main/scala/hmda/util/conversion/LarStringFormatter.scala
@@ -11,7 +11,6 @@ object LarStringFormatter {
   def larString(lar: LoanApplicationRegister): String =
       lar.larIdentifier.LEI +
       lar.loan.ULI +
-//    lar.loan.applicationDate +
       lar.loan.loanType.code +
       lar.loan.loanPurpose.code +
       lar.action.preapproval.code +
@@ -19,24 +18,16 @@ object LarStringFormatter {
       lar.loan.occupancy.code +
       lar.action.actionTakenType.code +
       lar.action.actionTakenDate +
-//      lar.geography.street +
-//      lar.geography.city +
-//      lar.geography.state +
-//      lar.geography.zipCode +
-//      lar.geography.county +
-//      lar.geography.tract +
       convertEmptyField(lar.applicant.ethnicity.ethnicity1.code) +
       convertEmptyField(lar.applicant.ethnicity.ethnicity2.code) +
       convertEmptyField(lar.applicant.ethnicity.ethnicity3.code) +
       convertEmptyField(lar.applicant.ethnicity.ethnicity4.code) +
       convertEmptyField(lar.applicant.ethnicity.ethnicity5.code) +
-//      lar.applicant.ethnicity.otherHispanicOrLatino +
       convertEmptyField(lar.coApplicant.ethnicity.ethnicity1.code) +
       convertEmptyField(lar.coApplicant.ethnicity.ethnicity2.code) +
       convertEmptyField(lar.coApplicant.ethnicity.ethnicity3.code) +
       convertEmptyField(lar.coApplicant.ethnicity.ethnicity4.code) +
       convertEmptyField(lar.coApplicant.ethnicity.ethnicity5.code) +
-//      lar.coApplicant.ethnicity.otherHispanicOrLatino +
       lar.applicant.ethnicity.ethnicityObserved.code +
       lar.coApplicant.ethnicity.ethnicityObserved.code +
       convertEmptyField(lar.applicant.race.race1.code) +
@@ -44,41 +35,26 @@ object LarStringFormatter {
       convertEmptyField(lar.applicant.race.race3.code) +
       convertEmptyField(lar.applicant.race.race4.code) +
       convertEmptyField(lar.applicant.race.race5.code) +
-//      lar.applicant.race.otherNativeRace +
-//      lar.applicant.race.otherAsianRace +
-//      lar.applicant.race.otherPacificIslanderRace +
       convertEmptyField(lar.coApplicant.race.race1.code) +
       convertEmptyField(lar.coApplicant.race.race2.code) +
       convertEmptyField(lar.coApplicant.race.race3.code) +
       convertEmptyField(lar.coApplicant.race.race4.code) +
       convertEmptyField(lar.coApplicant.race.race5.code) +
-//      lar.coApplicant.race.otherNativeRace +
-//      lar.coApplicant.race.otherAsianRace +
-//      lar.coApplicant.race.otherPacificIslanderRace +
       lar.applicant.race.raceObserved.code +
       lar.coApplicant.race.raceObserved.code +
       lar.applicant.sex.sexEnum.code +
       lar.coApplicant.sex.sexEnum.code +
       lar.applicant.sex.sexObservedEnum.code +
       lar.coApplicant.sex.sexObservedEnum.code +
-//      lar.applicant.age +
-//      lar.coApplicant.age +
       lar.income +
       lar.purchaserType.code +
       lar.loan.rateSpread +
       lar.hoepaStatus.code +
       lar.lienStatus.code +
-//      lar.applicant.creditScore +
-//      lar.coApplicant.creditScore +
-//      lar.applicant.creditScoreType.code +
-//      lar.applicant.otherCreditScoreModel +
-//      lar.coApplicant.creditScoreType.code +
-//      lar.coApplicant.otherCreditScoreModel //+
       convertEmptyField(lar.denial.denialReason1.code) +
       convertEmptyField(lar.denial.denialReason2.code) +
       convertEmptyField(lar.denial.denialReason3.code) +
       convertEmptyField(lar.denial.denialReason4.code) +
-//      lar.denial.otherDenialReason +
       lar.loanDisclosure.totalLoanCosts +
       lar.loanDisclosure.totalPointsAndFees +
       lar.loanDisclosure.originationCharges +
@@ -86,7 +62,6 @@ object LarStringFormatter {
       lar.loanDisclosure.lenderCredits +
       lar.loan.interestRate +
       lar.loan.prepaymentPenaltyTerm +
-      lar.loan.debtToIncomeRatio +
       lar.loan.combinedLoanToValueRatio +
       lar.loan.loanTerm +
       lar.loan.introductoryRatePeriod +
@@ -94,27 +69,9 @@ object LarStringFormatter {
       lar.nonAmortizingFeatures.interestOnlyPayments.code +
       lar.nonAmortizingFeatures.negativeAmortization.code +
       lar.nonAmortizingFeatures.otherNonAmortizingFeatures.code +
-////      lar.property.propertyValue // +
-////      lar.property.manufacturedHomeSecuredProperty.code +
-////      lar.property.manufacturedHomeLandPropertyInterest.code +
-      lar.property.totalUnits +
-      lar.property.multiFamilyAffordableUnits +
       lar.applicationSubmission.code +
       lar.payableToInstitution.code +
-////      lar.larIdentifier.NMLSRIdentifier +
-////      convertEmptyField(lar.AUS.aus1.code) +
-////      convertEmptyField(lar.AUS.aus2.code) +
-////      convertEmptyField(lar.AUS.aus3.code) +
-////      convertEmptyField(lar.AUS.aus4.code) +
-////      convertEmptyField(lar.AUS.aus5.code) +
-////      lar.AUS.otherAUS +
-////      lar.ausResult.ausResult1.code +
-////      convertEmptyField(lar.ausResult.ausResult2.code) +
-////      convertEmptyField(lar.ausResult.ausResult3.code) +
-////      convertEmptyField(lar.ausResult.ausResult4.code) +
-////      convertEmptyField(lar.ausResult.ausResult5.code) +
-////      lar.ausResult.otherAusResult +
-      lar.reverseMortgage.code +
+       lar.reverseMortgage.code +
       lar.lineOfCredit.code +
       lar.businessOrCommercialPurpose.code
 
@@ -161,32 +118,29 @@ object LarStringFormatter {
     emlar.mlar.rateSpread +
     emlar.mlar.hoepaStatus +
     emlar.mlar.lienStatus +
-      emlar.mlar.denial1 +
-      emlar.mlar.denial2 +
-      emlar.mlar.denial3 +
-      emlar.mlar.denial4 +
-      emlar.mlar.totalLoanCosts +
-      emlar.mlar.totalPointsAndFees +
-      emlar.mlar.originationCharges +
-      emlar.mlar.discountPoints +
-      emlar.mlar.lenderCredits +
-      emlar.mlar.interestRate +
-      emlar.mlar.prepaymentPenalty +
-  emlar.mlar.debtToIncomeRatio +
-      emlar.mlar.loanToValueRatio +
-      emlar.mlar.loanTerm +
-      emlar.mlar.introductoryRatePeriod +
-      emlar.mlar.balloonPayment +
-      emlar.mlar.interestOnlyPayment +
-      emlar.mlar.negativeAmortization +
-      emlar.mlar.otherNonAmortizingFeatures +
-  emlar.mlar.totalUnits +
-      emlar.mlar.multifamilyAffordableUnits +
-      emlar.mlar.applicationSubmission +
-      emlar.mlar.initiallyPayableToInstitution +
-  emlar.mlar.reverseMortgage +
-      emlar.mlar.openEndLineOfCredit +
-      emlar.mlar.businessOrCommercialPurpose +
+    emlar.mlar.denial1 +
+    emlar.mlar.denial2 +
+    emlar.mlar.denial3 +
+    emlar.mlar.denial4 +
+    emlar.mlar.totalLoanCosts +
+    emlar.mlar.totalPointsAndFees +
+    emlar.mlar.originationCharges +
+    emlar.mlar.discountPoints +
+    emlar.mlar.lenderCredits +
+    emlar.mlar.interestRate +
+    emlar.mlar.prepaymentPenalty +
+    emlar.mlar.loanToValueRatio +
+    emlar.mlar.loanTerm +
+    emlar.mlar.introductoryRatePeriod +
+    emlar.mlar.balloonPayment +
+    emlar.mlar.interestOnlyPayment +
+    emlar.mlar.negativeAmortization +
+    emlar.mlar.otherNonAmortizingFeatures +
+    emlar.mlar.applicationSubmission +
+    emlar.mlar.initiallyPayableToInstitution +
+    emlar.mlar.reverseMortgage +
+    emlar.mlar.openEndLineOfCredit +
+    emlar.mlar.businessOrCommercialPurpose
 
 
 }
