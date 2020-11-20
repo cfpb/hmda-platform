99352cbd397ff64d5aeaaf5c86929fa33436a975
Barak Stout <barakstout@gmail.com>
2020-11-20 10:09:21 -0500

    WIP Update quartely query for dashboard

--- hmda-dashboard/src/main/scala/hmda/dashboard/models/QuarterDetails.scala
+++ hmda-dashboard/src/main/scala/hmda/dashboard/models/QuarterDetails.scala
@@ -4,15 +4,17 @@ import io.circe.Codec
 import slick.jdbc.GetResult
 
 case class QuarterDetails(
-                      q1:  Int,
-                      q2:  Int,
-                      q3:  Int,
-                      q4:  Int
+                   institution_name: String,
+                   lei: String,
+                    q1:  Int,
+                    q2:  Int,
+                    q3:  Int,
+                    q4:  Int
                     )
 
 object QuarterDetails {
-  implicit val getResults: GetResult[QuarterDetails] = GetResult(r => QuarterDetails(r.<<,r.<<,r.<<,r.<<))
+  implicit val getResults: GetResult[QuarterDetails] = GetResult(r => QuarterDetails(r.<<,r.<<,r.<<,r.<<,r.<<,r.<<))
 
   implicit val codec: Codec[QuarterDetails] =
-    Codec.forProduct4("q1", "q2", "q3", "q4")(QuarterDetails.apply)(f => (f.q1,f.q2,f.q3,f.q4))
+    Codec.forProduct6("Institution Name","LEI","q1", "q2", "q3", "q4")(QuarterDetails.apply)(f => (f.institution_name,f.lei,f.q1,f.q2,f.q3,f.q4))
 }
