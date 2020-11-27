751ff1dd55cc58309b26b874f86be0f4b36b72d8
bsharma <bsharma@healthstarinfo.com>
2020-11-27 10:37:04 -0500

    Fix key value pairs being accepted

--- common/src/main/scala/hmda/messages/HmdaMessageFilter.scala
+++ common/src/main/scala/hmda/messages/HmdaMessageFilter.scala
@@ -8,28 +8,31 @@ import scala.util.Try
 
 object HmdaMessageFilter extends StrictLogging {
 
-  case class StandardMsgKey(lei: String, year: Int, quarter: Option[String], sequenceNumber: Option[String])
+  case class StandardMsg(lei: String, year: Int, quarter: Option[String], sequenceNumber: Option[String])
 
-  def parse(key: String): Option[StandardMsgKey] = {
+  def parse(key: String, value: String): Option[StandardMsg] = {
     Try {
+      val leiRegex = "(?<lei>[A-Z0-9]+)"
+      val keyRegex = s"^${leiRegex}$$".r
       // lei1:lei2-year-q1-seq_num
-      val regex = "^(?<lei1>[A-Z0-9]+):(?<lei2>[A-Z0-9]+)-(?<year>[0-9]{4})(-(?<quarter>[qQ][1-3]))?(-(?<seqNum>[0-9]+))?$".r
+      val msgRegex = s"^${leiRegex}-(?<year>[0-9]{4})(-(?<quarter>[qQ][1-3]))?(-(?<seqNum>[0-9]+))?$$".r
       for {
-        onlyMatch <- regex.findFirstMatchIn(key)
-        lei1 = onlyMatch.group("lei1")
-        lei2 = onlyMatch.group("lei2")
-        year = onlyMatch.group("year").toInt
-        quarterOpt = Option(onlyMatch.group("quarter"))
-        seqNum = Option(onlyMatch.group("seqNum"))
+        keyMatch <- keyRegex.findFirstMatchIn(key)
+        msgMatch <- msgRegex.findFirstMatchIn(value)
+        lei1 = keyMatch.group("lei")
+        lei2 = msgMatch.group("lei")
+        year = msgMatch.group("year").toInt
+        quarterOpt = Option(msgMatch.group("quarter"))
+        seqNum = Option(msgMatch.group("seqNum"))
         _ <- if (lei1 == lei2) Some(()) else None
-      } yield StandardMsgKey(lei1, year, quarterOpt, seqNum)
+      } yield StandardMsg(lei1, year, quarterOpt, seqNum)
     }.toOption.flatten // regex api is not the safest one and we don't want it to throw accidentally
   }
 
   type Processor = CommittableMessage[String, String] => Future[CommittableOffset]
 
   def processOnlyValidKeys[V](f: Processor)(implicit ec: ExecutionContext): Processor = msg => {
-    parse(msg.record.key()) match {
+    parse(msg.record.key(), msg.record.value()) match {
       case Some(_) => f(msg)
       case None =>
         logger.warn(s"Kafka message has invalid key and will be committed without being processed. Msg: ${msg}")
