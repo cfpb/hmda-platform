b8700eb86cea8d49f4604a083bf9a57666448dcb
bsharma <bsharma@healthstarinfo.com>
2020-12-17 11:10:58 -0500

    PR for Q650 for 2021

--- hmda/src/main/scala/hmda/validation/rules/lar/quality/_2021/Q650_2.scala
+++ hmda/src/main/scala/hmda/validation/rules/lar/quality/_2021/Q650_2.scala
@@ -1,9 +1,9 @@
 package hmda.validation.rules.lar.quality._2021
 
 import hmda.model.filing.lar.LoanApplicationRegister
-import hmda.validation.dsl.PredicateCommon.{greaterThan, greaterThanOrEqual, when}
+import hmda.validation.dsl.PredicateCommon.{greaterThan, lessThanOrEqual, when}
 import hmda.validation.dsl.PredicateSyntax.PredicateOps
-import hmda.validation.dsl.{ValidationFailure, ValidationResult}
+import hmda.validation.dsl.ValidationResult
 import hmda.validation.rules.EditCheck
 
 import scala.util.Try
@@ -15,8 +15,9 @@ object Q650_2 extends EditCheck[LoanApplicationRegister] {
 
   override def apply(lar: LoanApplicationRegister): ValidationResult ={
     val interest = Try(lar.loan.interestRate.toDouble).getOrElse(-1.0)
+
     when(interest is greaterThan(0.0)) {
-      interest is greaterThanOrEqual(20.0)
+      interest is lessThanOrEqual(20.0)
     }
   }
 }
