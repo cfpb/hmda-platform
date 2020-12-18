8c4edb31b5d504a4a31daa40a9aaeb7d42fb700f
PatrickHSI <pasata@healthstarinfo.com>
2020-12-18 13:43:08 -0500

    testing changes based on feedback

--- hmda/src/main/scala/hmda/validation/engine/2021/LarEngine2021.scala
+++ hmda/src/main/scala/hmda/validation/engine/2021/LarEngine2021.scala
@@ -1,15 +1,14 @@
 package hmda.validation.engine
 
+import hmda.census.records.CensusRecords
 import hmda.model.filing.lar.LoanApplicationRegister
 import hmda.validation.context.ValidationContext
 import hmda.validation.rules.lar.quality._2019._
-import hmda.validation.rules.lar.quality.{common, _2020 => quality2020, _2021 => quality2021}
-import hmda.validation.rules.lar.quality.common._
-import hmda.validation.rules.lar.syntactical.{S300, S301}
-import hmda.validation.rules.lar.validity._
-import hmda.validation.rules.lar.validity._2020
-import hmda.census.records.CensusRecords
 import hmda.validation.rules.lar.quality._2021.{Q650_1, Q650_2}
+import hmda.validation.rules.lar.quality.common._
+import hmda.validation.rules.lar.quality.{_2020 => quality2020, _2021 => quality2021}
+import hmda.validation.rules.lar.syntactical.{S300, S301}
+import hmda.validation.rules.lar.validity.{_2020, _}
 import hmda.validation.rules.lar.validity._2020.V716
 
 private[engine] object LarEngine2021 extends ValidationEngine[LoanApplicationRegister] {
@@ -287,7 +286,6 @@ private[engine] object LarEngine2021 extends ValidationEngine[LoanApplicationReg
     quality2020.Q649_2,
     quality2020.Q651,
     quality2020.Q652,
-    common.Q650,
     quality2020.Q653_1,
     quality2020.Q653_2,
     quality2020.Q654
