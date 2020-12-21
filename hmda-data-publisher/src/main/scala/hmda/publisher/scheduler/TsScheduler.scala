6fdc967ce3c14e0d8b93c018e85a98351ae75b36
PatrickHSI <pasata@healthstarinfo.com>
2020-12-21 18:43:47 -0500

    updates to the data publisher

--- hmda-data-publisher/src/main/scala/hmda/publisher/scheduler/TsScheduler.scala
+++ hmda-data-publisher/src/main/scala/hmda/publisher/scheduler/TsScheduler.scala
@@ -148,7 +148,7 @@ class TsScheduler
             .withAttributes(S3Attributes.settings(s3Settings))
 
         val results: Future[MultipartUploadResult] =
-          uploadFileToS3(s3Sink, tsRepository2019.getAllSheets(getFilterList()))
+          uploadFileToS3(s3Sink, tsRepository2020.getAllSheets(getFilterList()))
 
         results onComplete {
           case Success(result) =>
