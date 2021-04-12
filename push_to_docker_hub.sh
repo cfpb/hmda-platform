fd63e39507c593d7c4dc5a46b9c9cf8d768f69d3
bsharma <bsharma@healthstarinfo.com>
2021-04-11 22:57:20 -0400

    try

--- push_to_docker_hub.sh
+++ push_to_docker_hub.sh
@@ -4,12 +4,14 @@ set -e
 
 
 if [[ "$TRAVIS_BRANCH" == "push-to-dockerhub-from-travis" ]]; then
+    echo "Pushing master branch to latest tag on Docker Hub"
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD ;
     sbt -batch hmda-platform/docker:publishLocal
     docker tag hmda/hmda-platform:latest hmda/hmda-platform:latest
     docker push hmda/hmda-platform:latest
 fi
 if [ ! -z "${TRAVIS_TAG}" ]; then
+    echo "Pushing ${TRAVIS_TAG} tag on Docker Hub"
     docker tag hmda/hmda-platform:latest hmda/hmda-platform:${TRAVIS_TAG}
     docker push hmda/hmda-platform:${TRAVIS_TAG}
 fi
