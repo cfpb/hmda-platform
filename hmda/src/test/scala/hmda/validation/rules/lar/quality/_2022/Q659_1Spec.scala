63a0e604048bd0884ee38d4decb989adaba58689
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2021-11-18 11:41:57 -0500

    added some passing criteria

--- hmda/src/test/scala/hmda/validation/rules/lar/quality/_2022/Q659_1Spec.scala
+++ hmda/src/test/scala/hmda/validation/rules/lar/quality/_2022/Q659_1Spec.scala
@@ -13,13 +13,19 @@ class Q659_1Spec extends LarEditCheckSpec {
     forAll(larGen) { lar =>
       invalidExemptCodes.foreach(invalidExemptCode => {
         lar.copy(applicant = lar.applicant.copy(age = invalidExemptCode.toInt)).mustFail
+        lar.copy(applicant = lar.applicant.copy(age = invalidExemptCode.toInt / 2)).mustPass
         lar.copy(coApplicant = lar.coApplicant.copy(creditScore = invalidExemptCode.toInt)).mustFail
+        lar.copy(coApplicant = lar.coApplicant.copy(creditScore = invalidExemptCode.toInt / 2)).mustPass
         lar.copy(geography = lar.geography.copy(street = invalidExemptCode)).mustFail
+        lar.copy(geography = lar.geography.copy(street = s"$invalidExemptCode ave.")).mustPass
         lar.copy(income = invalidExemptCode).mustFail
+        lar.copy(income = s"$$$invalidExemptCode").mustPass
         lar.copy(loan = lar.loan.copy(rateSpread = invalidExemptCode)).mustFail
         lar.copy(loan = lar.loan.copy(rateSpread = s"$invalidExemptCode.0")).mustFail
+        lar.copy(loan = lar.loan.copy(rateSpread = s"123$invalidExemptCode.0")).mustPass
         lar.copy(loanDisclosure = lar.loanDisclosure.copy(totalLoanCosts = invalidExemptCode)).mustFail
         lar.copy(loanDisclosure = lar.loanDisclosure.copy(totalLoanCosts = s"$invalidExemptCode.0")).mustFail
+        lar.copy(loanDisclosure = lar.loanDisclosure.copy(totalLoanCosts = s"123$invalidExemptCode.0")).mustPass
         lar.copy(property = lar.property.copy(multiFamilyAffordableUnits = invalidExemptCode)).mustFail
         lar.copy(larIdentifier = lar.larIdentifier.copy(NMLSRIdentifier = invalidExemptCode)).mustFail
       })
