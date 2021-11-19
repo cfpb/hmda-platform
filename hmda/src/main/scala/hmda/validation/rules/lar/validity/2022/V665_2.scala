dc6bd918a3d3770ca7579250a735a546ce1d25d7
PatrickHSI <pasata@healthstarinfo.com>
2021-11-19 12:37:34 -0500

    2022 Quarterly edit V665_1 and V665_2

--- hmda/src/main/scala/hmda/validation/rules/lar/validity/2022/V665_2.scala
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/2022/V665_2.scala
@@ -1,5 +1,6 @@
 package hmda.validation.rules.lar.validity._2022
 
+import com.google.protobuf.any.Any.defaultInstance.is
 import hmda.model.filing.lar.LoanApplicationRegister
 import hmda.model.filing.lar.enums._
 import hmda.validation.dsl.PredicateCommon._
@@ -12,6 +13,22 @@ object V665_2 extends EditCheck[LoanApplicationRegister] {
 
   override def parent: String = "V665"
 
-  override def apply(lar: LoanApplicationRegister): ValidationResult =
-    lar.coApplicant.creditScoreType not equalTo(new InvalidCreditScoreCode)
+  override def apply(lar: LoanApplicationRegister): ValidationResult = {
+
+    val scoringModels = List(
+      EquifaxBeacon5,
+      ExperianFairIsaac,
+      FICORiskScoreClassic04,
+      FICORiskScoreClassic98,
+      VantageScore2,
+      VantageScore3,
+      OneOrMoreCreditScoreModels,
+      OtherCreditScoreModel,
+      CreditScoreNotApplicable,
+      CreditScoreNoCoApplicant,
+      FICOScore9,
+      CreditScoreExempt
+    )
+    lar.coApplicant.creditScoreType is containedIn(scoringModels)
+  }
 }
