8c0e908f65730b495867b7f085e7c14a22db1acd
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2022-06-24 14:26:48 -0400

    remove unnecessary splice in favor of binding

--- hmda-quarterly-data-service/src/main/scala/hmda/quarterly/data/api/dao/repo/QuarterlyGraphRepo.scala
+++ hmda-quarterly-data-service/src/main/scala/hmda/quarterly/data/api/dao/repo/QuarterlyGraphRepo.scala
@@ -4,7 +4,6 @@ import hmda.model.filing.lar.enums.{ Conventional, LoanTypeEnum }
 import hmda.quarterly.data.api.dao._
 import monix.eval.Task
 import slick.jdbc.{ PositionedParameters, SQLActionBuilder }
-import slick.sql.SqlStreamingAction
 import QuarterlyGraphMvConfig._
 
 object QuarterlyGraphRepo {
@@ -15,60 +14,59 @@ object QuarterlyGraphRepo {
   private val ordering = sql" order by quarter"
 
   def fetchApplicationsVolumeByType(loanType: LoanTypeEnum, heloc: Boolean, conforming: Boolean): Task[Seq[DataPoint]] = {
-    val stmts = getAppVolStmts(APP_VOL_MV, APP_VOL_PERIODS, loanType, heloc, conforming) ++ Seq(ordering)
-    runQuery(combineStatements(stmts:_*).as[DataPoint])
+    val stmts = getAppVolStmts(APP_VOL_MV, APP_VOL_PERIODS, loanType, heloc, conforming)
+    runStatements(stmts)
   }
 
   def fetchLoansVolumeByType(loanType: LoanTypeEnum, heloc: Boolean, conforming: Boolean): Task[Seq[DataPoint]] = {
-    val stmts = getAppVolStmts(APP_VOL_MV, APP_VOL_PERIODS, loanType, heloc, conforming, "and action_taken_type = 1") ++ Seq(ordering)
-    runQuery(combineStatements(stmts:_*).as[DataPoint])
+    val stmts = getAppVolStmts(APP_VOL_MV, APP_VOL_PERIODS, loanType, heloc, conforming, loanOriginated = true)
+    runStatements(stmts)
   }
 
-  private def getAppVolStmts(mv: String, periods: Seq[String], loanType: LoanTypeEnum, heloc: Boolean, conforming: Boolean, additionalCriteria: String = ""): Seq[SQLActionBuilder] = {
+  private def getAppVolStmts(mv: String, periods: Seq[String], loanType: LoanTypeEnum, heloc: Boolean, conforming: Boolean,
+                             loanOriginated: Boolean = false): Seq[SQLActionBuilder] =
     unionStatements(periods.map(period => {
       sql"""
          select last_updated, quarter, sum(agg) as value from #${s"${mv}_$period"}
          where #${if (heloc) "line_of_credits = 1" else s"loan_type = ${loanType.code} and line_of_credits != 1"}
            #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
-           #$additionalCriteria
+           #${if (loanOriginated) "and action_taken_type = 1" else ""}
          group by last_updated, quarter
          """
-    }))
-  }
+    })) ++ Seq(ordering)
 
   def fetchMedianCreditScoreByType(loanType: LoanTypeEnum, heloc: Boolean, conforming: Boolean): Task[Seq[DataPoint]] = {
     val stmts = unionStatements(CRED_SCORE_BY_LOAN_PERIODS.map(period => {
-        sql"""
-           select last_updated, quarter, median_credit_score as value from #${s"${CRED_SCORE_BY_LOAN_MV}_$period"}
-           where lt = #${loanType.code}
-             and loc #${if (heloc) "= 1" else "!= 1"}
-             #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
-           """
-      })) ++ Seq(ordering)
-
+      sql"""
+         select last_updated, quarter, median_credit_score as value from #${s"${CRED_SCORE_BY_LOAN_MV}_$period"}
+         where lt = ${loanType.code}
+           and loc #${if (heloc) "= 1" else "!= 1"}
+           #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
+         """
+    })) ++ Seq(ordering)
     runStatements(stmts)
   }
 
   def fetchMedianCreditScoreByTypeByRace(loanType: LoanTypeEnum, race: String, conforming: Boolean = false): Task[Seq[DataPoint]] = {
     val stmts = unionStatements(CRED_SCORE_BY_RE_PERIODS.map(period => {
-        sql"""
-           select last_updated, quarter, median_credit_score as value from #${s"${CRED_SCORE_BY_RE_MV}_$period"}
-           where loan_type = #${loanType.code} and race_ethnicity = '#$race'
-             #${getAdditionalParams(loanType, conforming)}
-           """
-      })) ++ Seq(ordering)
+      sql"""
+         select last_updated, quarter, median_credit_score as value from #${s"${CRED_SCORE_BY_RE_MV}_$period"}
+         where loan_type = ${loanType.code} and race_ethnicity = $race
+           #${getAdditionalParams(loanType, conforming)}
+         """
+    })) ++ Seq(ordering)
     runStatements(stmts)
   }
 
   def fetchMedianCLTVByType(loanType: LoanTypeEnum, heloc: Boolean, conforming: Boolean): Task[Seq[DataPoint]] = {
     val stmts = unionStatements(CLTV_BY_LOAN_PERIODS.map(period => {
-        sql"""
-           select last_updated, quarter, median_lv as value from #${s"${CLTV_BY_LOAN_MV}_$period"}
-           where lt = #${loanType.code}
-             and loc #${if (heloc) "= 1" else "!= 1"}
-             #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
-           """
-      })) ++ Seq(ordering)
+      sql"""
+         select last_updated, quarter, median_lv as value from #${s"${CLTV_BY_LOAN_MV}_$period"}
+         where lt = ${loanType.code}
+           and loc #${if (heloc) "= 1" else "!= 1"}
+           #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
+         """
+    })) ++ Seq(ordering)
     runStatements(stmts)
   }
 
@@ -76,7 +74,7 @@ object QuarterlyGraphRepo {
     val stmts = unionStatements(CLTV_BY_RE_PERIODS.map(period => {
       sql"""
          select last_updated, quarter, median_lv as value from #${s"${CLTV_BY_RE_MV}_$period"}
-         where lt = #${loanType.code} and race_ethnicity = '#$race'
+         where lt = ${loanType.code} and race_ethnicity = $race
            and loc #${if (heloc) "= 1" else "!= 1"}
              #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
          """
@@ -88,7 +86,7 @@ object QuarterlyGraphRepo {
     val stmts = unionStatements(DTI_BY_LOAN_PERIODS.map(period => {
       sql"""
          select last_updated, quarter, median_dti as value from #${s"${DTI_BY_LOAN_MV}_$period"}
-         where lt = #${loanType.code}
+         where lt = ${loanType.code}
            and loc #${if (heloc) "= 1" else "!= 1"}
            #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
          """
@@ -100,7 +98,7 @@ object QuarterlyGraphRepo {
     val stmts = unionStatements(DTI_BY_RE_PERIODS.map(period => {
       sql"""
          select last_updated, quarter, median_dti as value from #${s"${DTI_BY_RE_MV}_$period"}
-         where lt = #${loanType.code} and race_ethnicity = '#$race'
+         where lt = ${loanType.code} and race_ethnicity = $race
            and loc #${if (heloc) "= 1" else "!= 1"}
            #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
          """
@@ -112,7 +110,7 @@ object QuarterlyGraphRepo {
     val stmts = unionStatements(DENIAL_RATES_BY_LOAN_PERIODS.map(period => {
       sql"""
          select last_updated, quarter, denial_rate as value from #${s"${DENIAL_RATES_BY_LOAN_MV}_$period"}
-         where lt = #${loanType.code}
+         where lt = ${loanType.code}
            and loc #${if (heloc) "= 1" else "!= 1"}
            #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
          """
@@ -124,7 +122,7 @@ object QuarterlyGraphRepo {
     val stmts = unionStatements(DENIAL_RATES_BY_RE_PERIODS.map(period => {
       sql"""
          select last_updated, quarter, denial_rate as value from #${s"${DENIAL_RATES_BY_RE_MV}_$period"}
-         where lt = #${loanType.code} and race_ethnicity = '#$race'
+         where lt = ${loanType.code} and race_ethnicity = $race
            and loc #${if (heloc) "= 1" else "!= 1"}
            #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
          """
@@ -136,7 +134,7 @@ object QuarterlyGraphRepo {
     val stmts = unionStatements(INTEREST_RATES_BY_LOAN_PERIODS.map(period => {
       sql"""
          select last_updated, quarter, median_interest_rate as value from #${s"${INTEREST_RATES_BY_LOAN_MV}_$period"}
-         where lt = #${loanType.code}
+         where lt = ${loanType.code}
            and loc #${if (heloc) "= 1" else "!= 1"}
            #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
          """
@@ -148,7 +146,7 @@ object QuarterlyGraphRepo {
     val stmts = unionStatements(INTEREST_RATES_BY_RE_PERIODS.map(period => {
       sql"""
          select last_updated, quarter, median_interest_rate as value from #${s"${INTEREST_RATES_BY_RE_MV}_$period"}
-         where lt = #${loanType.code} and race_ethnicity = '#$race'
+         where lt = ${loanType.code} and race_ethnicity = $race
            and loc #${if (heloc) "= 1" else "!= 1"}
           #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
          """
@@ -160,7 +158,7 @@ object QuarterlyGraphRepo {
     val stmts = unionStatements(TLC_BY_LOAN_PERIODS.map(period => {
       sql"""
          select last_updated, quarter, median_total_loan_costs as value from #${s"${TLC_BY_LOAN_MV}_$period"}
-         where lt = #${loanType.code}
+         where lt = ${loanType.code}
            and loc #${if (heloc) "= 1" else "!= 1"}
            #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
          """
@@ -172,7 +170,7 @@ object QuarterlyGraphRepo {
     val stmts = unionStatements(TLC_BY_RE_PERIODS.map(period => {
       sql"""
          select last_updated, quarter, median_total_loan_costs as value from #${s"${TLC_BY_RE_MV}_$period"}
-         where lt = #${loanType.code} and race_ethnicity = '#$race'
+         where lt = ${loanType.code} and race_ethnicity = $race
            and loc #${if (heloc) "= 1" else "!= 1"}
           #${if (heloc) "" else getAdditionalParams(loanType, conforming)}
          """
@@ -180,27 +178,22 @@ object QuarterlyGraphRepo {
     runStatements(stmts)
   }
 
-  private def combineStatements(statements: SQLActionBuilder*): SQLActionBuilder = {
+  private def combineStatements(statements: SQLActionBuilder*): SQLActionBuilder =
     SQLActionBuilder(statements.map(_.queryParts).reduce(_ ++ _),
       (position: Unit, positionedParameters: PositionedParameters) => {
         statements.foreach(_.unitPConv.apply(position, positionedParameters))
       })
-  }
 
-  private def unionStatements(statements: Seq[SQLActionBuilder]): Seq[SQLActionBuilder] = {
+  private def unionStatements(statements: Seq[SQLActionBuilder]): Seq[SQLActionBuilder] =
     (for {
       statement <- statements
       unioned <- Seq(union, statement)
     } yield unioned).tail
-  }
 
   private def runStatements(statements: Seq[SQLActionBuilder]): Task[Vector[DataPoint]] = {
     val query = combineStatements(statements:_*).as[DataPoint]
-    runQuery(query)
-  }
-
-  private def runQuery[T](query: SqlStreamingAction[Vector[T], T, Effect]): Task[Vector[T]] =
     Task.deferFuture(db.run(query)).guarantee(Task.shift)
+  }
 
   private def getAdditionalParams(loanType: LoanTypeEnum, conforming: Boolean): String =
     if (loanType == Conventional) {
