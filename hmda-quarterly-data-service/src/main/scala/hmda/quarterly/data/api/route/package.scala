0cf6ce64abc9337ec7a0ac7818627995ec549e36
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2022-06-29 16:25:19 -0400

    summary verbiage configurable

--- hmda-quarterly-data-service/src/main/scala/hmda/quarterly/data/api/route/package.scala
+++ hmda-quarterly-data-service/src/main/scala/hmda/quarterly/data/api/route/package.scala
@@ -1,12 +1,10 @@
 package hmda.quarterly.data.api
 
+import com.typesafe.config.ConfigFactory
 import hmda.quarterly.data.api.dto.QuarterGraphData._
 
 package object route {
-  val graphRoutes: GraphRouteInfo = GraphRouteInfo(
-    "The following graphs present data for the 19 financial institutions" +
-      " reporting HMDA quarterly data throughout 2020 and displays data for each of those institutions" +
-      " for 2019 and 2018 as well.",
+  val graphRoutes: GraphRouteInfo = GraphRouteInfo(ConfigFactory.load().getString("graph.verbiage.summary"),
     Seq(ApplicationsVolume, LoansVolume, MedianCreditScores, MedianCreditScoresCCByRace, MedianCreditScoresFHAByRace,
       MedianCLTVByType, MedianCLTVCCByRace, MedianCLTVFHAByRace, MedianDTIByType, MedianDTICCByRace, MedianDTIFHAByRace,
       DenialRatesByType, DenialRatesCCByRace, DenialRatesFHAByRace, MedianInterestRates, MedianInterestRatesCCByRace, MedianInterestRatesFHAByRace,
