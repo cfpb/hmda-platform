ea47db595769051d8ca76e5615813bae1a57d4e6
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2022-07-26 12:45:12 -0400

    added agency to quarterly data, and combined with addition of yearly totals of all

--- institutions-api/src/main/scala/hmda/institution/query/QuarterlyInstitutionLarCounts.scala
+++ institutions-api/src/main/scala/hmda/institution/query/QuarterlyInstitutionLarCounts.scala
@@ -1,20 +1,22 @@
 package hmda.institution.query
 
+import hmda.model.institution.Agency
 import slick.jdbc.GetResult
 
+case class LarCountSummary(yearly: Seq[AnnualLarCount], quarterly: Seq[QuarterlyInstitutionLarCounts])
 case class AnnualLarCount(year: String, count: Int)
-case class QuarterlyInstitutionLarCounts(lei: String, name: String, larCounts: Seq[AnnualLarCount])
+case class QuarterlyInstitutionLarCounts(lei: String, name: String, agency: String, larCounts: Seq[AnnualLarCount])
 object QuarterlyInstitutionLarCounts {
   implicit val getResult: GetResult[QuarterlyInstitutionLarCounts] = GetResult(result => {
     val rs = result.rs
     val year = rs.getInt("activity_year")
 
     val colCount = rs.getMetaData.getColumnCount
-    val larCountIdxStart = 4
+    val larCountIdxStart = 5
 
     /**
-     * based on the InstitutionTsRepo.fetchPastLarCountsForQuarterlies, the first 3 fields don't change,
-     * fields 4 and onwards contain dynamic number of lar counts based on the request.
+     * based on the InstitutionTsRepo.fetchPastLarCountsForQuarterlies, the first 4 fields are static (check the query to see number of static fields),
+     * fields 5 and onwards contain dynamic number of lar counts based on the request.
      * */
     val larCounts = (larCountIdxStart to colCount).map(col =>
       AnnualLarCount(s"${year + larCountIdxStart - col - 1}", rs.getInt(col))
@@ -23,6 +25,7 @@ object QuarterlyInstitutionLarCounts {
     QuarterlyInstitutionLarCounts(
       rs.getString("lei"),
       rs.getString("respondent_name"),
+      Agency.valueOf(rs.getInt("agency")).fullName,
       larCounts
     )
   })
