baa7acae0805fa55138517482bbe6a47871c0ac9
PatrickHSI <pasata@healthstarinfo.com>
2023-10-12 16:33:43 -0400

    added exclusion logic so exempt , na, n/a don't trigger the Q660 edit

--- hmda/src/main/scala/hmda/validation/rules/lar/quality/_2024/Q660.scala
+++ hmda/src/main/scala/hmda/validation/rules/lar/quality/_2024/Q660.scala
@@ -14,26 +14,18 @@ object Q660 extends EditCheck[LoanApplicationRegister] {
 
   override def apply(lar: LoanApplicationRegister): ValidationResult = {
 
-    /*
-    Trigger Conditions:
-
-      1). The Street Address is more than 100 characters.
-      2) The Street Address includes the values: “;”, “&”, or “and“.
-      3) The reported Street Address includes the values: ?, !, *, %, ^, @, [, ], {, } ,<,>_, +, = or “tbd”.
-      4) The reported Street Address does not contain numbers or contains only numbers.
-     */
     val specialRegex =  ".*[~!@#$^%&*_+={}\\[\\];:<,>.?\\\\].*".r.regex
     val termRegex = ".*(?i:(\\btbd\\b)|(\\band\\b)).*".r.regex
     val numOnlyRegex="^(?=[^A-Za-z]+$).*[0-9].*$".r.regex
     val wordOnlyRegex="^[^0-9]+$".r.regex
-    val street = lar.geography.street
-
+    val street = lar.geography.street.toLowerCase()
+    val exemptPhrases = List("exempt","na","n/a")
     if (
       street.length > 100 ||
       street.matches(specialRegex) ||
       street.matches(termRegex) ||
       street.matches(numOnlyRegex) ||
-      street.matches(wordOnlyRegex)
+      (street.matches(wordOnlyRegex) && !exemptPhrases.contains(street))
     ) {
       ValidationFailure
     } else {
