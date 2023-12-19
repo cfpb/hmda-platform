7feb5ab0e5a5cfd740b3f08750249daeb58bce2f
PatrickHSI <pasata@healthstarinfo.com>
2023-12-19 09:51:32 -0500

    addint static table refs to filer count endpoint

--- institutions-api/src/main/scala/hmda/institution/query/InstitutionTsRepo.scala
+++ institutions-api/src/main/scala/hmda/institution/query/InstitutionTsRepo.scala
@@ -1,13 +1,28 @@
 package hmda.institution.query
 
+import com.typesafe.config.{Config, ConfigFactory}
 import hmda.institution.projection.InstitutionDBProjection.bankFilterList
+import hmda.model.institution.HmdaFiler
 
 import scala.concurrent.Future
 
 object InstitutionTsRepo {
+
   import hmda.query.DbConfiguration.dbConfig
   import dbConfig.db
   import dbConfig.profile.api._
+
+
+  val filerCountConfig: Config = ConfigFactory.load().getConfig("hmda.institution.filerCountTables")
+
+  val deafultInstitution = filerCountConfig.getString("institution.filerCountTables.deafultInstitution")
+  val staticTS2022 = filerCountConfig.getString("institution.filerCountTables.transmittalsheet.2022")
+  val staticTS2021 = filerCountConfig.getString("institution.filerCountTables.transmittalsheet.2021")
+  val staticTS2020 = filerCountConfig.getString("institution.filerCountTables.transmittalsheet.2020")
+  val staticTS2019 = filerCountConfig.getString("institution.filerCountTables.transmittalsheet.2019")
+  val staticTS2018 = filerCountConfig.getString("institution.filerCountTables.transmittalsheet.2018")
+
+
 
   def fetchPastLarCountsForQuarterlies(year: Int, pastCount: Int): Future[Vector[QuarterlyInstitutionLarCounts]] = {
     /**
@@ -16,17 +31,32 @@ object InstitutionTsRepo {
      * getTsSql generates the x number of lar counts for each prior year.
      * e.g. parameters (2022, 3) would have the lar counts for 2021, 2020, and 2019
      */
+
+
     val query =
       sql"""
         select lei, respondent_name, agency, activity_year, #${getTsSql(year, pastCount)}
-        from #${s"institutions$year"} inst
+        from #${deafultInstitution} inst
         where quarterly_filer = true and not lei in ('#${bankFilterList.mkString("','")}')
          """.as[QuarterlyInstitutionLarCounts]
-     db.run(query)
+    db.run(query)
   }
 
   private def getTsSql(year: Int, pastCount: Int) =
     (1 to pastCount).map(i => {
-      s"(select total_lines from ${s"transmittalsheet${year - i}"} ts where ts.lei = inst.lei order by created_at desc limit 1) as lar_prev$i"
+
+      s"(select total_lines from ${selectTransmittalSheet(year - i)} ts where ts.lei = inst.lei order by created_at desc limit 1) as lar_prev$i"
     }).mkString(",")
+
+  def selectTransmittalSheet(year: Int): String = {
+
+    year match {
+      case 2018 => staticTS2022
+      case 2019 => staticTS2021
+      case 2020 => staticTS2020
+      case 2021 => staticTS2019
+      case 2022 => staticTS2018
+      case _ => staticTS2022
+    }
+  }
 }
