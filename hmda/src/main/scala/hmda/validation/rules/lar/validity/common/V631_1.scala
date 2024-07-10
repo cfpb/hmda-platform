6d0fb1c61985679c6d5bf79e909f0ed9548c2aaa
jaredb96 <goldpineresin124@gmail.com>
2024-07-10 12:24:28 -0400

    Switched to equalTo in V631.

--- hmda/src/main/scala/hmda/validation/rules/lar/validity/common/V631_1.scala
+++ hmda/src/main/scala/hmda/validation/rules/lar/validity/common/V631_1.scala
@@ -26,9 +26,9 @@ object V631_1 extends EditCheck[LoanApplicationRegister] {
 
   override def apply(lar: LoanApplicationRegister): ValidationResult =
     if(lar.coApplicant.ethnicity.otherHispanicOrLatino.isEmpty) {
-      lar.coApplicant.ethnicity.ethnicity1 is containedIn(validEthnicities) and (lar.coApplicant.ethnicity.ethnicity1.code not equalTo(0))
+      lar.coApplicant.ethnicity.ethnicity1 is containedIn(validEthnicities) and (lar.coApplicant.ethnicity.ethnicity1 is equalTo(EmptyEthnicityValue))
     }
     else {
-      lar.coApplicant.ethnicity.ethnicity1 is containedIn(validEthnicities) or (lar.coApplicant.ethnicity.ethnicity1.code is equalTo(0))
+      lar.coApplicant.ethnicity.ethnicity1 is containedIn(validEthnicities) or (lar.coApplicant.ethnicity.ethnicity1 is equalTo(EmptyEthnicityValue))
     }
 }
