8a5872f024405545f8eefcd148f65c1a3120ce83
jaredb96 <goldpineresin124@gmail.com>
2024-07-12 13:42:58 -0400

    Change variable scope.

--- hmda-data-publisher/src/test/scala/hmda/publisher/helper/DataGeneratorSpec.scala
+++ hmda-data-publisher/src/test/scala/hmda/publisher/helper/DataGeneratorSpec.scala
@@ -6,19 +6,19 @@ import scala.util.matching.Regex
 
 class DateGeneratorSpec extends WordSpec with Matchers {
 
-  val annualPattern: Regex = raw"\d{4}-\d{2}-\d{2}-".r
-  val quarterPattern: Regex = raw"\d{4}-\d{2}-\d{2}_".r
   val generator = DateGenerator
 
 
   "date generator" should {
 
     "return a correctly formatted annual date string" in {
+      val annualPattern: Regex = raw"\d{4}-\d{2}-\d{2}-".r
       val annualMatch = annualPattern.findFirstIn(generator.currentDate)
       assert(annualMatch.contains(generator.currentDate))
     }
 
     "return a correctly formatted quarterly date string" in {
+      val quarterPattern: Regex = raw"\d{4}-\d{2}-\d{2}_".r
       val quarterMatch = quarterPattern.findFirstIn(generator.currentQuarterlyDate)
       assert(quarterMatch.contains(generator.currentQuarterlyDate))
     }
