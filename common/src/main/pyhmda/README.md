ad028b51cc907cdd19084d5a15b6390901403d97
Tom Tignor <tptignor@yahoo.com>
2024-09-04 11:28:40 -0400

    Added parse_census_file.py and doc

--- common/src/main/pyhmda/README.md
+++ common/src/main/pyhmda/README.md
@@ -39,3 +39,28 @@ This utility parses Conforming Loan Limit (CLL) files produced annually by the F
 1. Commit the output file to the hmda-platform repo and process as normal.
 
 **NOTE:** The utility strips input file header lines and performs validations and transformations based on expected line and field formats. It uses an all-or-nothing approach, and so any unexpected input data will likely trigger an abort with some useful error message. Line and field formats and their year-to-year changes are wholely owned and determined by FHFA (and not anyone at CFPB.)
+
+### parse_census_file
+
+```
+(.venv) $ parse_census_file.py -h
+usage: parse_census_file [-h] [-v] [-o OUTPUT_FILE] censusfile
+
+positional arguments:
+  censusfile
+
+options:
+  -h, --help            show this help message and exit
+  -v, --verbose
+  -o OUTPUT_FILE, --output-file OUTPUT_FILE
+(.venv) $
+```
+
+This utility parses Census Flat Files produced annually by the Federal Financial Institutions Examination Council (FFIEC) and Delineation Files produced by the US Census Bureau on a less frequent and irregular basis. Small subsets of data from both files are captured and joined. The tool should be run with the output file option to generate ffiec_census_{YYYY}.txt files suitable for HMDA ingest. The whole annual process can be performed with these steps.
+
+1. Download the zip archive for the current year Census Flat File from [here](https://www.ffiec.gov/censusapp.htm). Extract the CSV format file from the archive.
+1. Download the most recent Delineation File in Excel format from either [here](https://www.census.gov/geographies/reference-files/time-series/demo/metro-micro/delineation-files.html) or [here](https://www2.census.gov/programs-surveys/metro-micro/geographies/reference-files/). For a given year, multiple Delineation File variants are made available. The tool requires the variant for CBSAs and CSAs. This information will be noted on the download page or the file header.
+1. Open the Delineation File in Excel and save it in CSV file format.
+1. Run parse_census_file.py on the CSV files.
+1. Commit the output file to the hmda-platform repo and process as normal.
+
---
+++ common/src/main/pyhmda/parse_census_file.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import pandas as pd
+import os
+import re
+import sys
+
+from utils import *
+
+parser = argparse.ArgumentParser("parse_census_file")
+parser.add_argument("censusfile")
+parser.add_argument("delineationfile")
+parser.add_argument('-v', '--verbose', action='store_true')
+parser.add_argument("-o", "--output-file")
+args = parser.parse_args()
+
+loglevel = logging.DEBUG if args.verbose else logging.INFO
+logging.basicConfig(level=loglevel)
+
+def conv_scf(val: str) -> str:
+    val = val.strip()
+    if val not in ["T", "S", "I"]: raise ValueError(f"invalid scf: \"{val}\"")
+    return val    
+
+
+census_file_columns = {
+    0: ["CollectionYear", conv_num], 1: ["CBSACode", conv_dgstr], 2: ["FIPSStateCode", conv_dgstr],
+    3: ["FIPSCountyCode", conv_dgstr], 4: ["CensusTract", conv_dgstr],
+    13: ["FFIECMedianFamilyIncome", conv_num], 22: ["Population", conv_optnum],
+    28: ["MinorityPopulationPct", conv_optpct], 879: ["NumOwnerOccupiedUnits", conv_optnum],
+    899: ["Num1To4FamilyUnits", conv_optnum], 585: ["TractMFI", conv_optnum],
+    12: ["TractToMSAIncomePct", conv_optpct], 1057: ["MedianAge", conv_optnum],
+    6: ["SmallCounty", conv_scf]
+}
+cfkeys = census_file_columns.keys()
+cfcolnames = {k: v[0] for k, v in census_file_columns.items()}
+cfconverters = {k: v[1] for k, v in census_file_columns.items()}
+parsed_census_df = pd.read_csv(args.censusfile, sep=',', header=None, usecols=cfkeys,
+                               converters=cfconverters).rename(cfcolnames, axis=1)
+logging.info(f"Parsed {args.censusfile}")
+
+delineation_file_columns = {
+    3: ["CBSATitle", conv_str], 5: ["MDTitle", conv_optstr],
+    9: ["FIPSStateCode", conv_dgstr], 10: ["FIPSCountyCode", conv_dgstr]
+}
+dfkeys = delineation_file_columns.keys()
+dfcolnames = {k: v[0] for k, v in delineation_file_columns.items()}
+dfconverters = {k: v[1] for k, v in delineation_file_columns.items()}
+
+root, ext = os.path.splitext(args.delineationfile)
+prepared_file = f"{root}-prepared{ext}"
+data_pattern = r'\d+,\d*,\d*,"[^"]+",[\w\s]+,("[^"]+")?,("[^"]+")?,[^,]+,[\w\s]+,\d+,\d+,\w+'
+prepare_file(args.delineationfile, prepared_file, data_pattern)
+parsed_delin_df = pd.read_csv(prepared_file, sep=',', header=None, usecols=dfkeys,
+                               converters=dfconverters).rename(dfcolnames, axis=1)
+logging.info(f"Parsed {prepared_file}")
+parsed_delin_df["MSAOrMDTitle"] = parsed_delin_df.apply(lambda row:
+    row.MDTitle if pd.notna(row.MDTitle) else row.CBSATitle, axis=1)
+parsed_delin_df.drop(columns=["CBSATitle", "MDTitle"], inplace=True)
+logging.info("Calculated MSAOrMDTitles")
+
+output_file = args.output_file if args.output_file \
+    else f"{os.path.splitext(args.censusfile)[0]}-parsed.txt"
+output_df = parsed_census_df.merge(parsed_delin_df,
+    how="left", on=["FIPSStateCode", "FIPSCountyCode"])
+output_df.to_csv(output_file, sep='|', index=False)
+logging.info(f"Wrote output file {output_file}")
+os.remove(prepared_file)
