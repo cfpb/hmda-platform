7d130430990c80795dff89e38381280007936f92
PatrickGoRaft <pasata@teamraft.com>
2025-08-13 10:42:21 -0400

    reintroducing scientific notation rejection

--- common/src/main/scala/hmda/parser/filing/lar/LarParser.scala
+++ common/src/main/scala/hmda/parser/filing/lar/LarParser.scala
@@ -24,6 +24,7 @@ trait LarParser {
 
   def validateDoubleFieldReturnString(value: String, parserValidationError: ParserValidationError): LarParserValidationResult[String] =
     Try(BigDecimal(value)) match {
+      // added logic to reject scientific notation
       case Success(i) => if ("[-\\d.]+".r.matches(value))  i.bigDecimal.toPlainString.validNel else parserValidationError.invalidNel
       case Failure(_) => parserValidationError.invalidNel
     }
