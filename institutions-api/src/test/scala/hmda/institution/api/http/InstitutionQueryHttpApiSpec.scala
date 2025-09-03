3e09eecb0b790db26c30d3537c3e0a9be2ae39db
PatrickGoRaft <pasata@teamraft.com>
2025-09-02 21:56:10 -0400

    updating unit tests

--- institutions-api/src/test/scala/hmda/institution/api/http/InstitutionQueryHttpApiSpec.scala
+++ institutions-api/src/test/scala/hmda/institution/api/http/InstitutionQueryHttpApiSpec.scala
@@ -34,15 +34,15 @@ class InstitutionQueryHttpApiSpec extends WordSpec with MustMatchers with Before
   "Institution Query HTTP API" must {
     "search by LEI" in {
       Get("/institutions/XXX/year/2018") ~> institutionPublicRoutes ~> check {
-        status mustBe StatusCodes.BadRequest
+        status mustBe StatusCodes.NotFound
       }
 
       Get("/institutions/XXX/year/2019") ~> institutionPublicRoutes ~> check {
-        status mustBe StatusCodes.BadRequest
+        status mustBe StatusCodes.NotFound
       }
 
       Get("/institutions/XXX/year/2020") ~> institutionPublicRoutes ~> check {
-        status mustBe StatusCodes.BadRequest
+        status mustBe StatusCodes.NotFound
       }
 
       Get(s"/institutions/AAA/year/2018") ~> institutionPublicRoutes ~> check {
