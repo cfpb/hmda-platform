fedfca5fd4262426c4e311a90f5cde853382eb51
PatrickGoRaft <pasata@teamraft.com>
2025-10-02 22:48:36 -0400

    updates to remove trailing zeros from CLTV precision check

--- hmda/src/main/scala/hmda/validation/rules/lar/quality/common/Q617.scala
+++ hmda/src/main/scala/hmda/validation/rules/lar/quality/common/Q617.scala
@@ -35,15 +35,16 @@ object Q617 extends EditCheck[LoanApplicationRegister] {
       val ratioToPrecision =
         calculatedRatio.setScale(precision, RoundingMode.HALF_UP).underlying()
 
+   if ( lar.loan.combinedLoanToValueRatio =="25.860") {
+     println("Original CLTV: " + combinedLoanValueRatio)
 
-      println("Original CLTV: " + combinedLoanValueRatio)
-        println("Stripped CLTV: " + combinedLoanValueRatioStripped)
-        println("Calculated Precision: " + precision)
-        println("Calculated LTV: " + calculatedRatio)
-        println("LTV to Precision :" + ratioToPrecision)
-        val x = combinedLoanValueRatioStripped is greaterThanOrEqual(ratioToPrecision)
-        println("Test if CLTV is GTEQ to LTV: " + x.toString)
-
+     println("Stripped CLTV: " + combinedLoanValueRatioStripped)
+     println("Calculated Precision: " + precision)
+     println("Calculated LTV: " + calculatedRatio)
+     println("LTV to Precision :" + ratioToPrecision)
+     val x = combinedLoanValueRatioStripped is greaterThanOrEqual(ratioToPrecision)
+     println("Test if CLTV is GTEQ to LTV: " + x.toString)
+   }
 
       combinedLoanValueRatioStripped is greaterThanOrEqual(ratioToPrecision)
     }
