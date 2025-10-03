e7086efeed8cbd2581e987a36df408e31346d2a5
PatrickGoRaft <pasata@teamraft.com>
2025-10-02 23:15:31 -0400

    updates to remove trailing zeros from CLTV precision check

--- hmda/src/main/scala/hmda/validation/rules/lar/quality/common/Q617.scala
+++ hmda/src/main/scala/hmda/validation/rules/lar/quality/common/Q617.scala
@@ -5,14 +5,12 @@ import hmda.validation.dsl.PredicateCommon._
 import hmda.validation.dsl.PredicateSyntax._
 import hmda.validation.dsl.ValidationResult
 import hmda.validation.rules.EditCheck
-import org.slf4j.LoggerFactory
 
 import scala.math.BigDecimal.RoundingMode
 import scala.util.Try
 
 object Q617 extends EditCheck[LoanApplicationRegister] {
   override def name: String = "Q617"
-  val log = LoggerFactory.getLogger("q617-logger")
 
   override def apply(lar: LoanApplicationRegister): ValidationResult =
     when(
@@ -26,27 +24,14 @@ object Q617 extends EditCheck[LoanApplicationRegister] {
         Try(BigDecimal(lar.loan.combinedLoanToValueRatio))
           .getOrElse(BigDecimal(0))
 
-      val combinedLoanValueRatioStripped = combinedLoanValueRatio.underlying().stripTrailingZeros()
-
-      val precision = getPrecision(combinedLoanValueRatioStripped)
+      val precision = getPrecision(combinedLoanValueRatio)
 
       val calculatedRatio = (lar.loan.amount / propValue) * 100
 
       val ratioToPrecision =
-        calculatedRatio.setScale(precision, RoundingMode.HALF_UP).underlying()
+        calculatedRatio.setScale(precision, RoundingMode.HALF_UP)
 
-   if ( lar.loan.combinedLoanToValueRatio =="25.860") {
-     println("Original CLTV: " + combinedLoanValueRatio)
-
-     println("Stripped CLTV: " + combinedLoanValueRatioStripped)
-     println("Calculated Precision: " + precision)
-     println("Calculated LTV: " + calculatedRatio)
-     println("LTV to Precision :" + ratioToPrecision)
-     val x = combinedLoanValueRatioStripped is greaterThanOrEqual(ratioToPrecision)
-     println("Test if CLTV is GTEQ to LTV: " + x.toString)
-   }
-
-      combinedLoanValueRatioStripped is greaterThanOrEqual(ratioToPrecision)
+      combinedLoanValueRatio is greaterThanOrEqual(ratioToPrecision)
     }
 
   private def getPrecision(number: BigDecimal): Int =
