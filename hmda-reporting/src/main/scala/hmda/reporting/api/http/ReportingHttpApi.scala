6545dba2a9ad07bbf9e74ce1c806813fcf4ee320
kgudel <kenneth.gudel@cfpb.gov>
2026-07-21 14:08:38 -0400

    add snapshot route for filers and msa endpoints

--- hmda-reporting/src/main/scala/hmda/reporting/api/http/ReportingHttpApi.scala
+++ hmda-reporting/src/main/scala/hmda/reporting/api/http/ReportingHttpApi.scala
@@ -13,6 +13,7 @@ import hmda.util.http.FilingResponseUtils.entityNotPresentResponse
 import io.circe.generic.auto._
 import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
+import akka.http.scaladsl.model.Uri
 
 import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.{ Failure, Success }
@@ -30,69 +31,84 @@ private class ReportingHttpApi(config: Config)(implicit ec: ExecutionContext) ex
   private val institutionRepository     = new InstitutionRepository(databaseConfig)
 
 
-  private val filerListRoute: Route = {
-    path("filers" / IntNumber) { filingYear =>
+  private val filerReportingRoutes: Route = {
+    path("filers" / IntNumber) { year =>
       get {
-
-        val futFilerSet: Future[Set[HmdaFiler]] = {
-          institutionRepository
-            .getFilteredFilers(bankFilterList, filingYear, false)
-            .map(sheets =>
-              sheets
-                .map(instituionEntity =>
-                  HmdaFiler(
-                    instituionEntity.lei.toUpperCase,
-                    instituionEntity.respondentName,
-                    instituionEntity.activityYear.toString
-                  )
-                )
-                .toSet
-            )
-        }
-
-        onComplete(futFilerSet) {
-          case Success(filerSet) =>
-            complete(HmdaFilerResponse(filerSet))
-          case Failure(error) =>
-            complete(StatusCodes.BadRequest -> error.getLocalizedMessage)
-        }
-
+        filersListRoute(year, false)
+      }
+    } ~ path("filers" / IntNumber / "snapshot") { year =>
+      get {
+        filersListRoute(year, true)
       }
     } ~ path("filers" / IntNumber / Segment / "msaMds") { (year, lei) =>
       extractUri { uri =>
-        val resultset = for {
-          msaMdsResult      <- modifiedLarRepository.msaMdsSnapshot(lei, year)
-          institutionResult <- institutionRepository.findByLei(lei, year, false)
-        } yield {
-          val msaMds =
-            msaMdsResult.map(myEntity => MsaMd(myEntity._1, myEntity._2)).toSet
-          MsaMdResponse(
-            new HmdaFiler(
-              institutionResult.head.lei.toUpperCase,
-              institutionResult.head.respondentName,
-              institutionResult.head.activityYear.toString
-            ),
-            msaMds
-          )
-        }
-
-        onComplete(resultset) {
-          case Success(leiMsaMds) =>
-            complete(leiMsaMds)
-          case Failure(error) =>
-            entityNotPresentResponse("institution", lei, uri)
-        }
+        filerMsaMdRoute(uri, year, lei, false)
       }
-    }
+    } ~ path("filers" / IntNumber / Segment / "msaMds" / "snapshot") { (year, lei) =>
+      extractUri { uri =>
+        filerMsaMdRoute(uri, year, lei, true)
+      }
 
+    }
   }
 
   def hmdaFilerRoutes: Route =
     handleRejections(corsRejectionHandler) {
       cors() {
         encodeResponse {
-          filerListRoute
+          filerReportingRoutes
         }
       }
     }
+
+  private def filersListRoute(year: Int, snapshot: Boolean): Route = {
+    val futFilerSet: Future[Set[HmdaFiler]] = {
+      institutionRepository
+        .getFilteredFilers(bankFilterList, year, snapshot)
+        .map(sheets =>
+          sheets
+            .map(instituionEntity =>
+              HmdaFiler(
+                instituionEntity.lei.toUpperCase,
+                instituionEntity.respondentName,
+                instituionEntity.activityYear.toString
+              )
+            )
+            .toSet
+        )
+    }
+
+    onComplete(futFilerSet) {
+      case Success(filerSet) =>
+        complete(HmdaFilerResponse(filerSet))
+      case Failure(error) =>
+        complete(StatusCodes.BadRequest -> error.getLocalizedMessage)
+    }
+
+  }
+  
+  private def filerMsaMdRoute(uri: Uri, year: Int, lei: String, snapshot: Boolean): Route = {
+    val resultset = for {
+      msaMdsResult      <- modifiedLarRepository.msaMdsSnapshot(lei, year)
+      institutionResult <- institutionRepository.findByLei(lei, year, snapshot)
+    } yield {
+      val msaMds =
+        msaMdsResult.map(myEntity => MsaMd(myEntity._1, myEntity._2)).toSet
+      MsaMdResponse(
+        new HmdaFiler(
+          institutionResult.head.lei.toUpperCase,
+          institutionResult.head.respondentName,
+          institutionResult.head.activityYear.toString
+        ),
+        msaMds
+      )
+    }
+
+    onComplete(resultset) {
+      case Success(leiMsaMds) =>
+        complete(leiMsaMds)
+      case Failure(error) =>
+        entityNotPresentResponse("institution", lei, uri)
+    }
+  }
 }
